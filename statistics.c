059af993c921ef49aa5c8c1ad991e66761198920
Bogdan-Andrei Iancu <bogdan@opensips.org>
2006-12-08 21:56:42 +0000

    - flags updated on the core MI commands
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@1347 689a6050-402a-0410-94f2-e92a70836424

--- statistics.c
+++ statistics.c
@@ -50,8 +50,8 @@ static struct mi_root *mi_get_stats(struct mi_root *cmd, void *param);
 static struct mi_root *mi_reset_stats(struct mi_root *cmd, void *param);
 
 static mi_export_t mi_stat_cmds[] = {
-	{ "get_statistics",    mi_get_stats,    0,  0 },
-	{ "reset_statistics",  mi_reset_stats,  0,  0 },
+	{ "get_statistics",    mi_get_stats,    0  ,  0,  0 },
+	{ "reset_statistics",  mi_reset_stats,  0  ,  0,  0 },
 	{ 0, 0, 0, 0}
 };
 
