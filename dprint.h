0b2ed23479bac41a96a820dc8db9993baf9959e6
Bogdan-Andrei Iancu <bogdan@opensips.org>
2007-07-16 20:04:59 +0000

    - new macros for logging that automatically log the module name and   log level
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@2460 689a6050-402a-0410-94f2-e92a70836424

--- dprint.h
+++ dprint.h
@@ -36,8 +36,25 @@
 #define L_INFO   3
 #define L_DBG    4
 
+#define LOG_ALERT_LABEL  "ALERT:"
+#define LOG_CRIT_LABEL   "CRITICAL:"
+#define LOG_ERR_LABEL    "ERROR:"
+#define LOG_WARN_LABEL   "WARNING:"
+#define LOG_NOTICE_LABEL "NOTICE:"
+#define LOG_INFO_LABEL   "INFO:"
+#define LOG_DBG_LABEL    "DBG:"
+
 #define DPRINT_LEV   L_ERR
 
+#ifndef MOD_NAME
+	#define MOD_NAME "core"
+#endif
+
+#ifndef NO_DEBUG
+	#undef NO_LOG
+#endif
+
+#define LOG_PREFIX  MOD_NAME ":"
 
 /* vars:*/
 
@@ -63,15 +80,26 @@ int str2facility(char *s);
 #endif
 
 
-#ifndef NO_DEBUG
-	#undef NO_LOG
-#endif
 
 #ifdef NO_LOG
 	#ifdef __SUNPRO_C
 		#define LOG(lev, ...)
+		#define LM_ALERT(lev, ...)
+		#define LM_CRIT(lev, ...)
+		#define LM_ERR(lev, ...)
+		#define LM_WARN(lev, ...)
+		#define LM_NOTICE(lev, ...)
+		#define LM_INFO(lev, ...)
+		#define LM_DBG(lev, ...)
 	#else
 		#define LOG(lev, fmt, args...)
+		#define LM_ALERT(lev, fmt, args...)
+		#define LM_CRIT(lev, fmt, args...)
+		#define LM_ERR(lev, fmt, args...)
+		#define LM_WARN(lev, fmt, args...)
+		#define LM_NOTICE(lev, fmt, args...)
+		#define LM_INFO(lev, fmt, args...)
+		#define LM_DBG(lev, fmt, args...)
 	#endif
 #else
 	#ifdef __SUNPRO_C
@@ -106,11 +134,94 @@ int str2facility(char *s);
 					} \
 				} \
 			}while(0)
+
+		#define LM_ALERT( fmt, ...) \
+			do { \
+				if (is_printable(L_ALERT)){ \
+					if (log_stderr)\
+						dprint( LOG_ALERT_LABEL LOG_PREFIX fmt, __VA_ARGS__);\
+					else \
+						syslog( LOG_ALERT|log_facility, \
+							LOG_PREFIX fmt, __VA_ARGS__);\
+				} \
+			}while(0)
+
+		#define LM_CRIT( fmt, ...) \
+			do { \
+				if (is_printable(L_CRIT)){ \
+					if (log_stderr)\
+						dprint( LOG_CRIT_LABEL LOG_PREFIX fmt, __VA_ARGS__);\
+					else \
+						syslog( LOG_CRIT|log_facility, \
+							LOG_PREFIX fmt, __VA_ARGS__);\
+				} \
+			}while(0)
+
+		#define LM_ERR( fmt, ...) \
+			do { \
+				if (is_printable(L_ERR)){ \
+					if (log_stderr)\
+						dprint( LOG_ERR_LABEL LOG_PREFIX fmt, __VA_ARGS__);\
+					else \
+						syslog( LOG_ERR|log_facility, \
+							LOG_PREFIX fmt, __VA_ARGS__);\
+				} \
+			}while(0)
+
+		#define LM_WARN( fmt, ...) \
+			do { \
+				if (is_printable(L_WARN)){ \
+					if (log_stderr)\
+						dprint( LOG_WARN_LABEL LOG_PREFIX fmt, __VA_ARGS__);\
+					else \
+						syslog( LOG_WARNING|log_facility, \
+							LOG_PREFIX fmt, __VA_ARGS__);\
+				} \
+			}while(0)
+
+		#define LM_NOTICE( fmt, ...) \
+			do { \
+				if (is_printable(L_NOTICE)){ \
+					if (log_stderr)\
+						dprint( LOG_NOTICE_LABEL LOG_PREFIX fmt, __VA_ARGS__);\
+					else \
+						syslog( LOG_NOTICE|log_facility, \
+							LOG_PREFIX fmt, __VA_ARGS__);\
+				} \
+			}while(0)
+
+		#define LM_INFO( fmt, ...) \
+			do { \
+				if (is_printable(L_INFO)){ \
+					if (log_stderr)\
+						dprint( LOG_INFO_LABEL LOG_PREFIX fmt, __VA_ARGS__);\
+					else \
+						syslog( LOG_INFO|log_facility, \
+							LOG_PREFIX fmt, __VA_ARGS__);\
+				} \
+			}while(0)
+
+#ifdef NO_DEBUG
+			#define LM_DBG( fmt, ...)
+		#else
+			#define LM_DBG( fmt, ...) \
+				do { \
+					if (is_printable(L_DBG)){ \
+						if (log_stderr)\
+							dprint(LOG_DBG_LABEL LOG_PREFIX fmt, __VA_ARGS__);\
+						else \
+							syslog( LOG_DEBUG|log_facility, \
+								LOG_PREFIX fmt, __VA_ARGS__);\
+					} \
+				}while(0)
+		#endif /*NO_DEBUG*/
+
 	#else
+
 		#define LOG(lev, fmt, args...) \
 			do { \
 				if (is_printable(lev)){ \
-					if (log_stderr) dprint (fmt, ## args); \
+					if (log_stderr) dprint ( fmt, ## args); \
 					else { \
 						switch(lev){ \
 							case L_CRIT: \
@@ -138,8 +249,91 @@ int str2facility(char *s);
 					} \
 				} \
 			}while(0)
+
+		#define LM_ALERT( fmt, args...) \
+			do { \
+				if (is_printable(L_ALERT)){ \
+					if (log_stderr)\
+						dprint( LOG_ALERT_LABEL LOG_PREFIX fmt, ## args);\
+					else \
+						syslog( LOG_ALERT|log_facility, \
+							LOG_PREFIX fmt, ##args);\
+				} \
+			}while(0)
+
+		#define LM_CRIT( fmt, args...) \
+			do { \
+				if (is_printable(L_CRIT)){ \
+					if (log_stderr)\
+						dprint( LOG_CRIT_LABEL LOG_PREFIX fmt, ## args);\
+					else \
+						syslog( LOG_CRIT|log_facility, \
+							LOG_PREFIX fmt, ##args);\
+				} \
+			}while(0)
+
+		#define LM_ERR( fmt, args...) \
+			do { \
+				if (is_printable(L_ERR)){ \
+					if (log_stderr)\
+						dprint( LOG_ERR_LABEL LOG_PREFIX fmt, ## args);\
+					else \
+						syslog( LOG_ERR|log_facility, \
+							LOG_PREFIX fmt, ##args);\
+				} \
+			}while(0)
+
+		#define LM_WARN( fmt, args...) \
+			do { \
+				if (is_printable(L_WARN)){ \
+					if (log_stderr)\
+						dprint( LOG_WARN_LABEL LOG_PREFIX fmt, ## args);\
+					else \
+						syslog( LOG_WARNING|log_facility, \
+							LOG_PREFIX fmt, ##args);\
+				} \
+			}while(0)
+
+		#define LM_NOTICE( fmt, args...) \
+			do { \
+				if (is_printable(L_NOTICE)){ \
+					if (log_stderr)\
+						dprint( LOG_NOTICE_LABEL LOG_PREFIX fmt, ## args);\
+					else \
+						syslog( LOG_NOTICE|log_facility, \
+							LOG_PREFIX fmt, ##args);\
+				} \
+			}while(0)
+
+		#define LM_INFO( fmt, args...) \
+			do { \
+				if (is_printable(L_INFO)){ \
+					if (log_stderr)\
+						dprint( LOG_INFO_LABEL LOG_PREFIX fmt, ## args);\
+					else \
+						syslog( LOG_INFO|log_facility, \
+							LOG_PREFIX fmt, ##args);\
+				} \
+			}while(0)
+
+#ifdef NO_DEBUG
+			#define LM_DBG( fmt, args...)
+		#else
+			#define LM_DBG( fmt, args...) \
+				do { \
+					if (is_printable(L_DBG)){ \
+						if (log_stderr)\
+							dprint( LOG_DBG_LABEL LOG_PREFIX fmt, ## args);\
+						else \
+							syslog( LOG_DEBUG|log_facility, \
+								LOG_PREFIX fmt, ##args);\
+					} \
+				}while(0)
+		#endif /*NO_DEBUG*/
 	#endif /*SUN_PRO_C*/
 #endif
+
+
 
 
 #ifdef NO_DEBUG
