02f1296c820acfd302ef23b3457d6408e72c947e
Bogdan-Andrei Iancu <bogdan@opensips.org>
2008-07-29 10:53:32 +0000

    - project name updates
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@4507 689a6050-402a-0410-94f2-e92a70836424

--- modules/uac/auth_alg.c
+++ modules/uac/auth_alg.c
@@ -5,12 +5,12 @@
  *
  * This file is part of opensips, a free SIP server.
  *
- * UAC OpenSER-module is free software; you can redistribute it and/or
+ * UAC OpenSIPS-module is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- * UAC OpenSER-module is distributed in the hope that it will be useful,
+ * UAC OpenSIPS-module is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
