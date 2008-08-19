e56bab3dd4bd994fca4189c1c4e2be57efbab494
Bogdan-Andrei Iancu <bogdan@opensips.org>
2008-08-19 12:27:36 +0000

    Add received via param when a rport param is present in via. Reported by Alex Massover Closes bug #2059624.
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@4621 689a6050-402a-0410-94f2-e92a70836424

--- msg_translator.c
+++ msg_translator.c
@@ -1382,8 +1382,10 @@ char * build_req_buf_from_sip_req( struct sip_msg* msg,
 
 	/* check if received needs to be added:
 	 *  - if the VIA address and the received address are different 
-	 *  - if the rport was forced (rport requires received) */
-	if ( (msg->msg_flags&FL_FORCE_RPORT) || received_test(msg) ) { 
+	 *  - if the rport was forced (rport requires received)
+	 *  - if the rport was received in the VIA hdr */
+	if ( msg->via1->rport || (msg->msg_flags&FL_FORCE_RPORT) ||
+	received_test(msg) ) {
 		if ((received_buf=received_builder(msg,&received_len))==0){
 			LM_ERR("received_builder failed\n");
 			goto error01;  /* free also line_buf */
