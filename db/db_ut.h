8f91f161af42df4a091cf49a2bff7fde8f2767ff
Bogdan-Andrei Iancu <bogdan@opensips.org>
2008-08-20 16:18:23 +0000

    - fixed compiling on Solaris: _XOPEN_SOURCE needs to be undefined in order to avoid the conflicting types for swab function.   Credits go to Sergio Gutierrez. Closes patch 2049771.
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@4622 689a6050-402a-0410-94f2-e92a70836424

--- db/db_ut.h
+++ db/db_ut.h
@@ -44,10 +44,18 @@
 #define _XOPEN_SOURCE 600          /* glibc2 on linux, bsd */
 #define _XOPEN_SOURCE_EXTENDED 1   /* solaris */
 
+/**
+ * _XOPEN_SOURCE creates conflict in swab definition in Solaris
+ */
+#ifdef __OS_solaris
+	#undef _XOPEN_SOURCE
+#endif
+
 #include <time.h>
 
 #undef _XOPEN_SOURCE
 #undef _XOPEN_SOURCE_EXTENDED
+
 
 #include "db_key.h"
 #include "db.h"
