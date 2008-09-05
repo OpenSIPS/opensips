6c254f5abd4c64aa9ca7fa3fcc8e14e0698e846f
Dan Pascu <dan@ag-projects.com>
2008-09-05 11:09:01 +0000

    - Simplified the presence_xcapdiff module to automatically configure itself   based on the availability of the presence and/or pua modules in the   OpenSIPS configuration, eliminating the need to have module parameters   to manually configure presence/pua specific functionality - Made error messages more human readable - Added copyright notice at the top of the source file - Removed unused functions - Updated and reworded documentation for clarity and to reflect the above   changes
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@4719 689a6050-402a-0410-94f2-e92a70836424

--- modules/presence_xcapdiff/presence_xcapdiff.c
+++ modules/presence_xcapdiff/presence_xcapdiff.c
@@ -1,3 +1,25 @@
+/* $Id: presence_xcapdiff.c 1 2008-09-05 08:11:06Z dan_pascu $
+ *
+ * Copyright (C) 2008 AG Projects
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 #include "../presence/bind_presence.h"
 #include "../pua/pua_bind.h"
 
@@ -7,20 +29,12 @@ str s_event_name = str_init("xcap-diff");
 str s_content_type = str_init("application/xcap-diff+xml");
 
 static int mod_init(void);
-static int child_init(int);
-static void destroy(void);
 
-static cmd_export_t cmds[] =
-{
+static cmd_export_t cmds[] = {
     {0, 0, 0, 0, 0, 0}
 };
 
-int enable_presence = 1;
-int enable_pua = 1;
-
 static param_export_t params[] = {
-    { "enable_presence", INT_PARAM, &enable_presence },
-    { "enable_pua",      INT_PARAM, &enable_pua },
     {0, 0, 0}
 };
 
@@ -35,87 +49,68 @@ struct module_exports exports= {
     0,                          /* extra processes */
     mod_init,                   /* module initialization function */
     (response_function) 0,      /* response handling function */
-    destroy,                    /* destroy function */
-    child_init                  /* per-child init function */ //perhaps should be NULL?
+    0,                          /* destroy function */
+    0                           /* per-child init function */
 };
 
-int xcapdiff_process_body(publ_info_t* publ, str** fin_body, int ver, str** tuple)
+
+int
+xcapdiff_process_body(publ_info_t *publ, str **fin_body, int ver, str **tuple)
 {
-	*fin_body= publ->body;
-	return 0;
+    *fin_body = publ->body;
+    return 0;
 }
 
-static int mod_init(void)
+
+static int
+mod_init(void)
 {
+    bind_presence_t bind_presence;
+    presence_api_t pres;
+    bind_pua_t bind_pua;
+    pres_ev_t event;
+    pua_api_t pua;
+
     LM_INFO("initializing...\n");
 
-    if (enable_presence)
-    {
-        bind_presence_t bind_presence;
-        presence_api_t pres;
-
-        bind_presence= (bind_presence_t)find_export("bind_presence", 1,0);
-        if (!bind_presence) {
-            LM_ERR("find_export(\"bind_presence\") failed\n");
-            return -1;
-        }
-
+    bind_presence = (bind_presence_t)find_export("bind_presence", 1, 0);
+    if (bind_presence) {
         if (bind_presence(&pres) < 0) {
-            LM_ERR("bind_presence failed\n");
+            LM_ERR("could not bind to the presence module API\n");
             return -1;
         }
-
-        pres_ev_t event;
 
         memset(&event, 0, sizeof(pres_ev_t));
         event.name = s_event_name;
         event.content_type = s_content_type;
-        event.default_expires= 3600;
+        event.default_expires = 3600;
         event.type = PUBL_TYPE;
         event.req_auth = 0;
 
         if (pres.add_event(&event) < 0) {
-            LM_ERR("pres.add_event(\"xcap-diff\")\n");
+            LM_ERR("could not add xcap-diff event to presence\n");
             return -1;
         }
+    } else {
+        LM_NOTICE("subscribing to the xcap-diff event is not available as the presence module is not loaded\n");
     }
 
-    if (enable_pua)
-    {
-        bind_pua_t bind_pua = (bind_pua_t)find_export("bind_pua", 1,0);
-        if (!bind_pua)
-        {
-            LM_ERR("find_export(\"bind_pua\", 1,0) failed\n");
+    bind_pua = (bind_pua_t)find_export("bind_pua", 1, 0);
+    if (bind_pua) {
+        if (bind_pua(&pua) < 0) {
+            LM_ERR("could not bind to the pua module API\n");
             return -1;
         }
 
-        pua_api_t pua;
-        if (bind_pua(&pua) < 0)
-        {
-            LM_ERR("bind_pua() failed\n");
+        if(pua.add_event(XCAPDIFF_EVENT, s_event_name.s, s_content_type.s, xcapdiff_process_body) < 0) {
+            LM_ERR("could not add xcap-diff event to pua\n");
             return -1;
         }
-
-        if(pua.add_event(XCAPDIFF_EVENT, s_event_name.s, s_content_type.s, xcapdiff_process_body)< 0)
-        {
-            LM_ERR("pua.add_event(XCAPDIFF_EVENT) failed\n");
-            return -1;
-        }
+    } else {
+        LM_NOTICE("publishing of xcap-diff events is not available as the pua module is not loaded\n");
     }
 
     return 0;
 }
 
-static int child_init(int rank)
-{
-    LM_DBG("[%d] pid [%d]\n", rank, getpid());
 
-    return 0;
-}
-
-static void destroy(void)
-{
-    LM_DBG("destroying module ...\n");
-
-    return;
-}
