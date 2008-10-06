a2b5ffd4e900920035f64ab1dd7d67ee2a1a24be
Bogdan-Andrei Iancu <bogdan@opensips.org>
2008-10-06 17:18:33 +0000

    - uploaded DB definitions for the Dynamic Routing related tables
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@4766 689a6050-402a-0410-94f2-e92a70836424

---
+++ scripts/oracle/drouting-create.sql
@@ -0,0 +1,55 @@
+INSERT INTO version (table_name, table_version) values ('dr_gateways','1');
+CREATE TABLE dr_gateways (
+    gwid NUMBER(10) PRIMARY KEY,
+    type NUMBER(10) DEFAULT 0 NOT NULL,
+    address VARCHAR2(128),
+    strip NUMBER(10) DEFAULT 0 NOT NULL,
+    pri_prefix VARCHAR2(16) DEFAULT NULL,
+    description VARCHAR2(128) DEFAULT ''
+);
+
+CREATE OR REPLACE TRIGGER dr_gateways_tr
+before insert on dr_gateways FOR EACH ROW
+BEGIN
+  auto_id(:NEW.id);
+END dr_gateways_tr;
+/
+BEGIN map2users('dr_gateways'); END;
+/
+INSERT INTO version (table_name, table_version) values ('dr_rules','1');
+CREATE TABLE dr_rules (
+    ruleid NUMBER(10) PRIMARY KEY,
+    groupid VARCHAR2(255),
+    prefix VARCHAR2(64),
+    timerec VARCHAR2(255),
+    priority NUMBER(10) DEFAULT 0 NOT NULL,
+    routeid NUMBER(10) DEFAULT 0 NOT NULL,
+    timerec VARCHAR2(255),
+    description VARCHAR2(128) DEFAULT ''
+);
+
+CREATE OR REPLACE TRIGGER dr_rules_tr
+before insert on dr_rules FOR EACH ROW
+BEGIN
+  auto_id(:NEW.id);
+END dr_rules_tr;
+/
+BEGIN map2users('dr_rules'); END;
+/
+INSERT INTO version (table_name, table_version) values ('dr_groups','1');
+CREATE TABLE dr_groups (
+    id NUMBER(10) PRIMARY KEY,
+    username VARCHAR2(64),
+    domain VARCHAR2(128) DEFAULT '',
+    groupid VARCHAR2(255),
+    description VARCHAR2(128) DEFAULT ''
+);
+
+CREATE OR REPLACE TRIGGER dr_groups_tr
+before insert on dr_groups FOR EACH ROW
+BEGIN
+  auto_id(:NEW.id);
+END dr_groups_tr;
+/
+BEGIN map2users('dr_groups'); END;
+/
---
+++ scripts/postgres/drouting-create.sql
@@ -0,0 +1,31 @@
+INSERT INTO version (table_name, table_version) values ('dr_gateways','1');
+CREATE TABLE dr_gateways (
+    gwid SERIAL PRIMARY KEY NOT NULL,
+    type INTEGER DEFAULT 0 NOT NULL,
+    address VARCHAR(128) NOT NULL,
+    strip INTEGER DEFAULT 0 NOT NULL,
+    pri_prefix VARCHAR(16) DEFAULT NULL,
+    description VARCHAR(128) DEFAULT '' NOT NULL
+);
+
+INSERT INTO version (table_name, table_version) values ('dr_rules','1');
+CREATE TABLE dr_rules (
+    ruleid SERIAL PRIMARY KEY NOT NULL,
+    groupid VARCHAR(255) NOT NULL,
+    prefix VARCHAR(64) NOT NULL,
+    timerec VARCHAR(255) NOT NULL,
+    priority INTEGER DEFAULT 0 NOT NULL,
+    routeid INTEGER DEFAULT 0 NOT NULL,
+    timerec VARCHAR(255) NOT NULL,
+    description VARCHAR(128) DEFAULT '' NOT NULL
+);
+
+INSERT INTO version (table_name, table_version) values ('dr_groups','1');
+CREATE TABLE dr_groups (
+    id SERIAL PRIMARY KEY NOT NULL,
+    username VARCHAR(64) NOT NULL,
+    domain VARCHAR(128) DEFAULT '' NOT NULL,
+    groupid VARCHAR(255) NOT NULL,
+    description VARCHAR(128) DEFAULT '' NOT NULL
+);
+
