36f01c3bf9d1580a0c4a69a0a77bc1ff14a9ccd0
Dan Pascu <dan@ag-projects.com>
2008-11-20 07:13:38 +0000

    Avoid potential read overflow on non NULL terminated string
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/branches/1.4@4979 689a6050-402a-0410-94f2-e92a70836424

--- db/db.c
+++ db/db.c
@@ -154,6 +154,7 @@ int db_bind_mod(const str* mod, db_func_t* mydbf)
 	name = pkg_malloc(mod->len + 4);
 	memcpy(name, "db_", 3);
 	memcpy(name+3, mod->s, mod->len);
+	name[mod->len+3] = 0;
 
 	/* for safety we initialize mydbf with 0 (this will cause
 	 *  a segfault immediately if someone tries to call a function
