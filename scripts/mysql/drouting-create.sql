c1dce1be14d03f5b3d7a3574a5661e6421a1bfb9
Bogdan-Andrei Iancu <bogdan@opensips.org>
2009-01-20 09:19:58 +0000

    Dynamic Routing related changes in the DB schema: - dr_gateway has a new field "attrs" - this will contain properties of the GW (like SST time, timeout values, etc) to be used from the script. Each time the GW is to be used, the module will provide the properties string into an AVP.
    - increased version of dr_gateway table 
    
    - new table "dr_gw_list" - this will allow definition of lists of GWs; in the rule definition, you may point to an laredy existing list of GW or you can define your own. The idea is to simplify the provisioning as multiple rules may have the same list of GW
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@5182 689a6050-402a-0410-94f2-e92a70836424

--- scripts/mysql/drouting-create.sql
+++ scripts/mysql/drouting-create.sql
@@ -1,10 +1,11 @@
-INSERT INTO version (table_name, table_version) values ('dr_gateways','1');
+INSERT INTO version (table_name, table_version) values ('dr_gateways','2');
 CREATE TABLE dr_gateways (
     gwid INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     type INT(11) UNSIGNED DEFAULT 0 NOT NULL,
     address VARCHAR(128) NOT NULL,
     strip INT(11) UNSIGNED DEFAULT 0 NOT NULL,
     pri_prefix VARCHAR(16) DEFAULT NULL,
+    attrs VARCHAR(256) DEFAULT NULL,
     description VARCHAR(128) DEFAULT '' NOT NULL
 ) ENGINE=MyISAM;
 
@@ -16,6 +17,13 @@ CREATE TABLE dr_rules (
     timerec VARCHAR(255) NOT NULL,
     priority INT(11) DEFAULT 0 NOT NULL,
     routeid INT(11) UNSIGNED DEFAULT 0 NOT NULL,
+    gwlist VARCHAR(255) NOT NULL,
+    description VARCHAR(128) DEFAULT '' NOT NULL
+) ENGINE=MyISAM;
+
+INSERT INTO version (table_name, table_version) values ('dr_gw_lists','1');
+CREATE TABLE dr_gw_lists (
+    id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     gwlist VARCHAR(255) NOT NULL,
     description VARCHAR(128) DEFAULT '' NOT NULL
 ) ENGINE=MyISAM;
--- scripts/postgres/drouting-create.sql
+++ scripts/postgres/drouting-create.sql
@@ -1,10 +1,11 @@
-INSERT INTO version (table_name, table_version) values ('dr_gateways','1');
+INSERT INTO version (table_name, table_version) values ('dr_gateways','2');
 CREATE TABLE dr_gateways (
     gwid SERIAL PRIMARY KEY NOT NULL,
     type INTEGER DEFAULT 0 NOT NULL,
     address VARCHAR(128) NOT NULL,
     strip INTEGER DEFAULT 0 NOT NULL,
     pri_prefix VARCHAR(16) DEFAULT NULL,
+    attrs VARCHAR(256) DEFAULT NULL,
     description VARCHAR(128) DEFAULT '' NOT NULL
 );
 
@@ -16,6 +17,13 @@ CREATE TABLE dr_rules (
     timerec VARCHAR(255) NOT NULL,
     priority INTEGER DEFAULT 0 NOT NULL,
     routeid INTEGER DEFAULT 0 NOT NULL,
+    gwlist VARCHAR(255) NOT NULL,
+    description VARCHAR(128) DEFAULT '' NOT NULL
+);
+
+INSERT INTO version (table_name, table_version) values ('dr_gw_lists','1');
+CREATE TABLE dr_gw_lists (
+    id SERIAL PRIMARY KEY NOT NULL,
     gwlist VARCHAR(255) NOT NULL,
     description VARCHAR(128) DEFAULT '' NOT NULL
 );
