857555a4bd4a70750aa7f8fd53f5eb5c01b5b7de
Sergio Gutierrez <saguti@gmail.com>
2009-03-16 13:24:01 +0000

    - Backport from Kamailio Rev 5711. Missing newline in log.   Credits go to Henning Westherholt.
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@5450 689a6050-402a-0410-94f2-e92a70836424

--- db/db.c
+++ db/db.c
@@ -271,7 +271,7 @@ db_con_t* db_do_init(const str* url, void* (*new_connection)())
 		/* Not in the pool yet */
 		con = new_connection(id);
 		if (!con) {
-			LM_ERR("could not add connection to the pool");
+			LM_ERR("could not add connection to the pool\n");
 			goto err;
 		}
 		pool_insert((struct pool_con*)con);
