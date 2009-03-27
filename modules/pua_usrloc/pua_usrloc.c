c90089c12a04e8249c7dd9771a31a5b90cbd59df
Anca Vamanu <anca@opensips.org>
2009-03-27 12:57:15 +0000

    - rolled back file to the last version
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@5550 689a6050-402a-0410-94f2-e92a70836424

--- modules/pua_usrloc/pua_usrloc.c
+++ modules/pua_usrloc/pua_usrloc.c
@@ -31,9 +31,7 @@
 #include <string.h>
 #include <libxml/parser.h>
 #include <time.h>
-#include <sys/stat.h>
 
-#include "../../mi/mi.h" // ibc
 #include "../../script_cb.h"
 #include "../../sr_module.h"
 #include "../../parser/parse_expires.h"
@@ -42,7 +40,6 @@
 #include "../../parser/msg_parser.h"
 #include "../../str.h"
 #include "../../mem/mem.h"
-#include "../../locking.h"
 #include "../../pt.h"
 #include "../usrloc/ul_mod.h"
 #include "../usrloc/usrloc.h"
@@ -52,23 +49,14 @@
 
 MODULE_VERSION
 
-#define BL_MAX_LINE 400
-
-
-/** parameters */
-
 str default_domain= {NULL, 0};
 int pua_ul_publish= 0;
 pua_api_t pua;
 str pres_prefix= {0, 0};
 str presence_server= {0, 0};
-char *blfile = NULL;
-regex_t *blregex = NULL;
-gen_lock_t *bl_lock;
 
 /* Structure containing pointers to usrloc functions */
 usrloc_api_t ul;
-
 
 /** module functions */
 
@@ -76,33 +64,20 @@ static int mod_init(void);
 static int child_init(int);
 static void destroy(void);
 
-static int load_bl(int);
-static int pua_check_bl(struct sip_msg* _msg, char* _s1, char* _s2);
-static int it_list_fixup(void** param, int param_no);
-
-static struct mi_root* mi_bl_reload(struct mi_root* cmd, void* param);
-int pua_set_publish(struct sip_msg* msg , char* s1, char* s2);
+int pua_set_publish(struct sip_msg* , char*, char*);
 
 
 static cmd_export_t cmds[]=
 {
-	{"pua_set_publish", (cmd_function)pua_set_publish, 0, 0, 0, REQUEST_ROUTE},
-	{"pua_check_bl", (cmd_function)pua_check_bl, 1, it_list_fixup, 0,
-	 REQUEST_ROUTE|FAILURE_ROUTE|ONREPLY_ROUTE|BRANCH_ROUTE|LOCAL_ROUTE},
-	{0, 0, 0, 0, 0, 0}
+	{"pua_set_publish", (cmd_function)pua_set_publish, 0, 0, 0, REQUEST_ROUTE}, 	
+	{0, 0, 0, 0, 0, 0} 
 };
 
 static param_export_t params[]={
 	{"default_domain",	 STR_PARAM, &default_domain.s	},
 	{"entity_prefix",	 STR_PARAM, &pres_prefix.s		},
 	{"presence_server",	 STR_PARAM, &presence_server.s	},
-	{"blacklist_file",	 STR_PARAM, &blfile				},
 	{0,							 0,			0			}
-};
-
-static mi_export_t mi_cmds[]={
-	{ "pua_usrloc_bl_reload",   mi_bl_reload,   MI_NO_INPUT_FLAG, 0, 0 },
-	{ 0, 0, 0, 0, 0}
 };
 
 struct module_exports exports= {
@@ -111,7 +86,7 @@ struct module_exports exports= {
 	cmds,						/* exported functions */
 	params,						/* exported parameters */
 	0,							/* exported statistics */
-	mi_cmds,					/* exported MI functions */
+	0,							/* exported MI functions */
 	0,							/* exported pseudo-variables */
 	0,							/* extra processes */
 	mod_init,					/* module initialization function */
@@ -119,8 +94,7 @@ struct module_exports exports= {
 	destroy,					/* destroy function */
 	child_init                  /* per-child init function */
 };
-
-
+	
 /**
  * init module function
  */
@@ -129,8 +103,8 @@ static int mod_init(void)
 	bind_usrloc_t bind_usrloc;
 	bind_pua_t bind_pua;
 
-	LM_INFO("initializing module...\n");
-
+	LM_DBG("initializing module ...\n");
+	
 	if(default_domain.s == NULL )
 	{	
 		LM_ERR("default domain parameter not set\n");
@@ -225,31 +199,6 @@ static int mod_init(void)
 		return -1;
 	}
 
-	// If blacklist file parameter is set load and parse it.
-	if(blfile!=NULL)
-	{
-		// Create the bl_lock and init it.
-		bl_lock = lock_alloc();
-		if(bl_lock == NULL)
-		{
-			LM_ERR("cannot allocate the bl_lock\n");
-			return E_OUT_OF_MEM;
-		}
-		if(lock_init(bl_lock) == NULL)
-		{
-			LM_ERR("cannot init the bl_lock\n");
-			lock_dealloc(bl_lock);
-			return -1;
-		}
-		
-		// Load the blacklist regex.
-		LM_NOTICE("loading blacklist regex...\n");
-		if (load_bl(0))
-		{
-			LM_ERR("failed to load blacklist\n");
-			return -1;
-		}
-	}
 
 	return 0;
 }
@@ -262,181 +211,10 @@ static int child_init(int rank)
 
 static void destroy(void)
 {	
-	LM_DBG("destroying module...\n");
+	LM_DBG("destroying module ...\n");
 
-	if(blregex)
-		shm_free(blregex);
-
-	return;
+	return ;
 }
 
-// Convert the blfile content into a regular expresion and store it in blregex.
-static int load_bl(int reload)
-{
-	FILE *f = NULL;
-	struct stat file_status;
-	char line[BL_MAX_LINE], *pattern;
 
-	f = fopen(blfile, "r");
-	if(f==NULL)
-	{
-		LM_ERR("can't open blacklist file [%s]\n", blfile);
-		return -1;
-	}
 
-	// Get the file size.
-	if(stat(blfile, &file_status) != 0)
-	{
-		LM_ERR("can't stat blacklist file [%s]\n", blfile);
-		fclose(f);
-		return -1;
-	}
-	
-	// pattern size = blfile size + '(' + ')' + '\0'.
-	if ((pattern = malloc(sizeof(char)*(file_status.st_size+3))) == 0)
-	{
-		LM_ERR("no more memory to store pattern\n");
-		fclose(f);
-		return E_OUT_OF_MEM;
-	}
-
-	// Fill pattern with '\0'.
-	memset(pattern, '\0', file_status.st_size+3);
-
-	// Start pattern with '('.
-	pattern[0] = '(';
-	
-	while (fgets(line, BL_MAX_LINE, f) != NULL)
-	{
-		// Ignore comments or empty lines.
-		if (line[0]=='\n' || line[0]=='#' || line[0]==' ' || line[0]=='\t') {
-			continue;
-		}
-
-		// Replace '\n' with '|' (except when it's the last char of file).
-		if (line[strlen(line)-1] == '\n')
-			line[strlen(line)-1]='|';
-
-		// Append the line to pattern.
-		memcpy(pattern+strlen(pattern), line, strlen(line));
-	}
-	fclose(f);
-	
-	// Delete last '|' if there is nothing else after it.
-	if (pattern[strlen(pattern)-1] == '|')
-		pattern[strlen(pattern)-1] = '\0';
-
-	// End pattern with ')'.
-	pattern[strlen(pattern)] = ')';
-	
-	LM_NOTICE("<PATTERN>%s</PATTERN>\n", pattern);
-	
-	// Get the lock.
-	lock_get(bl_lock);
-
-	// Init.
-	if (reload == 0) {
-		// Creating blregex object.
-		if ((blregex=(regex_t*)shm_malloc(sizeof(regex_t)))==0)
-		{
-			LM_ERR("no more shm memory for blregex\n");
-			lock_release(bl_lock);
-			free(pattern);
-			return E_OUT_OF_MEM;
-		}
-	}
-	// Reload (from MI command).
-	else {
-		// FIXME: When running this, OpenSIPs fails (sometimes segmentation
-		// fault, sometimes it doesn't reply to SIP requests...). Also, the
-		// new compiled regcomp doesn't work.
-		//   http://dev.sipdoc.net/issues/show/11
-	}
-
-	// Compile the regular expression.
-	if (regcomp(blregex, pattern, REG_NOSUB|REG_EXTENDED|REG_ICASE|REG_NEWLINE))
-	{
-		LM_ERR("regcomp: bad pattern\n");
-		shm_free(blregex);
-		lock_release(bl_lock);
-		free(pattern);
-		return E_BAD_RE;
-	}
-
-	// Release the lock.
-	lock_release(bl_lock);
-
-	free(pattern);
-
-	return 0;
-}
-
-// Return true if the argument matches blregex.
-static int pua_check_bl(struct sip_msg *msg, char *key, char *foo)
-{
-	str s;
-	int match=0;
-
-	if(blregex==NULL) {
-		LM_ERR("blregex is NULL, 'pua_usrloc_blacklist' parameter must be set for this function to work!\n");
-		return -1;
-	}
-
-	if (pv_printf_s(msg, (pv_elem_t*)key, &s)<0) {
-		LM_ERR("failed to print the format\n");
-		return -1;
-	}
-
-	//LM_NOTICE("matching '%s' against blregex...\n", s.s);  // TODO: Poner DBG
-	
-	lock_get(bl_lock);
-	// FIXME: Cuando da coredump al hacer el MI es justo aquí.
-	match = regexec(blregex, s.s, 0, NULL, 0);
-	lock_release(bl_lock);
-	
-	if (!match) {
-		LM_NOTICE("%s matches blregex\n", s.s);  // TODO: Poner DBG
-		return 1;
-	}
-	else {
-		LM_NOTICE("%s doesn't match blregex\n", s.s);  // TODO: Poner DBG
-		return -1;
-	}
-}
-
-static int it_list_fixup(void** param, int param_no)
-{
-	pv_elem_t *model;
-	str s;
-	if(*param)
-	{
-		s.s = (char*)(*param); s.len = strlen(s.s);
-		if(pv_parse_format(&s, &model)<0)
-		{
-			LM_ERR("wrong format[%s]\n",(char*)(*param));
-			return E_UNSPEC;
-		}
-		*param = (void*)model;
-	}
-	return 0;
-}
-
-// MI functions.
-static struct mi_root* mi_bl_reload(struct mi_root* cmd_tree, void* param)
-{
-	if(blfile!=NULL)
-	{
-		LM_NOTICE("reloading blacklist regex...\n");
-		// Call load_bl() with parameter 1 (reload)
-		if (load_bl(1))
-		{
-			LM_ERR("failed to load blacklist\n");
-			return init_mi_tree(500, MI_INTERNAL_ERR_S, MI_INTERNAL_ERR_LEN);
-		}
-		return init_mi_tree(200, MI_OK_S, MI_OK_LEN);
-	}
-	else
-	{
-		return init_mi_tree(403, "Feature not enabled", 19);
-	}
-}
