8aab38095bc9e4cd305864c63d0e9ff1b23a63f3
Bogdan-Andrei Iancu <bogdan@opensips.org>
2009-05-18 13:39:20 +0000

    - definition of the dispatcher table updated to include "weight" and "attrs" fields
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@5716 689a6050-402a-0410-94f2-e92a70836424

--- scripts/mysql/dispatcher-create.sql
+++ scripts/mysql/dispatcher-create.sql
@@ -1,9 +1,11 @@
-INSERT INTO version (table_name, table_version) values ('dispatcher','3');
+INSERT INTO version (table_name, table_version) values ('dispatcher','4');
 CREATE TABLE dispatcher (
     id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     setid INT DEFAULT 0 NOT NULL,
     destination CHAR(192) DEFAULT '' NOT NULL,
     flags INT DEFAULT 0 NOT NULL,
+    weight INT DEFAULT 1 NOT NULL,
+    attrs CHAR(128) DEFAULT '' NOT NULL,
     description CHAR(64) DEFAULT '' NOT NULL
 ) ENGINE=MyISAM;
 
