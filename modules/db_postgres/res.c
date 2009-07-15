e6ee05e382952fa12e50c9a7cfa250051981efbe
Bogdan-Andrei Iancu <bogdan@opensips.org>
2009-07-15 19:14:10 +0000

    backport from trunk (rev #5844): - fixed how the NULL values are handled - use Postgres dedicated fucntions for this.   Based on a patch sent by Ruslan Bukin.   Closes patch #2817560.
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/branches/1.5@5877 689a6050-402a-0410-94f2-e92a70836424

--- modules/db_postgres/res.c
+++ modules/db_postgres/res.c
@@ -225,26 +225,30 @@ int db_postgres_convert_rows(const db_con_t* _h, db_res_t* _r)
 			 * null-terminated. The fix for this is to check what does the 
 			 * DB think about the length and use that as a correction.
 			 */
-			if ( (len=PQgetlength(CON_RESULT(_h), row, col))==0 ) {
-				s="";
-				LM_DBG("PQgetvalue(%p,%d,%d)=[], zero len\n", _h, row,col);
-			} else {
-				s = PQgetvalue(CON_RESULT(_h), row, col);
-				LM_DBG("PQgetvalue(%p,%d,%d)=[%.*s]\n", _h, row,col,len,s);
-			}
-			row_buf[col] = pkg_malloc(len+1);
-			if (!row_buf[col]) {
-				LM_ERR("no private memory left\n");
-				return -1;
-			}
-			memset(row_buf[col], 0, len+1);
-			LM_DBG("allocated %d bytes for row_buf[%d] at %p\n",
-				len, col, row_buf[col]);
+			if (PQgetisnull(CON_RESULT(_h), row, col) == 0) {
+				/* not null value */
+				if ( (len=PQgetlength(CON_RESULT(_h), row, col))==0 ) {
+					s="";
+					LM_DBG("PQgetvalue(%p,%d,%d)=[], zero len\n", _h, row,col);
+				} else {
+					s = PQgetvalue(CON_RESULT(_h), row, col);
+					LM_DBG("PQgetvalue(%p,%d,%d)=[%.*s]\n", _h, row,col,len,s);
+					
+					row_buf[col] = pkg_malloc(len+1);
+					if (!row_buf[col]) {
+						LM_ERR("no private memory left\n");
+						return -1;
+					}
+					memset(row_buf[col], 0, len+1);
+					LM_DBG("allocated %d bytes for row_buf[%d] at %p\n",
+						len, col, row_buf[col]);
 
-			strncpy(row_buf[col], s, len);
-			LM_DBG("[%d][%d] Column[%.*s]=[%s]\n",
-				row, col, RES_NAMES(_r)[col]->len,
-				RES_NAMES(_r)[col]->s, row_buf[col]);
+					strncpy(row_buf[col], s, len);
+					LM_DBG("[%d][%d] Column[%.*s]=[%s]\n",
+						row, col, RES_NAMES(_r)[col]->len,
+						RES_NAMES(_r)[col]->s, row_buf[col]);
+				}
+			}
 		}
 
 		/* ASSERT: row_buf contains an entire row in strings */
@@ -253,7 +257,7 @@ int db_postgres_convert_rows(const db_con_t* _h, db_res_t* _r)
 			RES_ROW_N(_r) = row - RES_LAST_ROW(_r);
 			for (col = 0; col < RES_COL_N(_r); col++) {
 				LM_DBG("freeing row_buf[%d] at %p\n", col, row_buf[col]);
-				pkg_free(row_buf[col]);
+				if (row_buf[col] &&  !row_buf[col][0]) pkg_free(row_buf[col]);
 			}
 			LM_DBG("freeing row buffer at %p\n", row_buf);
 			pkg_free(row_buf);
@@ -313,7 +317,7 @@ int db_postgres_convert_rows(const db_con_t* _h, db_res_t* _r)
 int db_postgres_convert_row(const db_con_t* _h, db_res_t* _r, db_row_t* _row,
 		char **row_buf)
 {
-	int col;
+	int col, len;
 
 	if (!_h || !_r || !_row)  {
 		LM_ERR("invalid parameter value\n");
@@ -325,9 +329,15 @@ int db_postgres_convert_row(const db_con_t* _h, db_res_t* _r, db_row_t* _row,
 
 	/* For each column in the row */
 	for(col = 0; col < ROW_N(_row); col++) {
+		/* compute the len of the value */
+		if ( row_buf[col]==NULL || row_buf[col][0]=='\0')
+			len = 0;
+		else
+			len =  strlen(row_buf[col]);
+
 		/* Convert the string representation into the value representation */
 		if (db_postgres_str2val(RES_TYPES(_r)[col], &(ROW_VALUES(_row)[col]),
-		row_buf[col], strlen(row_buf[col])) < 0) {
+		row_buf[col], len) < 0) {
 			LM_ERR("failed to convert value\n");
 			LM_DBG("free row at %pn", _row);
 			db_free_row(_row);
