bef5bfc0a63788b8720d6d1dba782583c2710fc7
Bogdan-Andrei Iancu <bogdan@opensips.org>
2009-12-11 19:02:19 +0000

    The change adds the following to the error messages produced:
    1. The size of the message that caused the overflow to happen
    2. The size of the receiving buffer
    
    Closes patch #2911665
    Credits go to Norm Brandinger
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@6418 689a6050-402a-0410-94f2-e92a70836424

--- pvar.c
+++ pvar.c
@@ -3803,7 +3803,7 @@ int pv_printf(struct sip_msg* msg, pv_elem_p list, char *buf, int *len)
 				n += it->text.len;
 				cur += it->text.len;
 			} else {
-				LM_ERR("no more space for text [%d]\n", it->text.len);
+				LM_ERR("no more space for text [%d][%d]\n", n, it->text.len);
 				goto overflow;
 			}
 		}
@@ -3822,7 +3822,8 @@ int pv_printf(struct sip_msg* msg, pv_elem_p list, char *buf, int *len)
 					cur += tok.rs.len;
 				}
 			} else {
-				LM_ERR("no more space for spec value\n");
+				LM_ERR("no more space for spec value  [%d][%d]\n",
+					n, tok.rs.len);
 				goto overflow;
 			}
 		}
@@ -3831,7 +3832,7 @@ int pv_printf(struct sip_msg* msg, pv_elem_p list, char *buf, int *len)
 	goto done;
 	
 overflow:
-	LM_ERR("buffer overflow -- increase the buffer size...\n");
+	LM_ERR("buffer overflow -- increase the buffer size from [%d]...\n",*len);
 	return -1;
 
 done:
