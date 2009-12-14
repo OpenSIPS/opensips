d6f40068bca710c61d4200765da3e0d54e226adf
Andrei Dragus <andreidragus@yahoo.com>
2009-12-14 15:32:50 +0000

    Backported map used for usrloc
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/branches/1.6@6436 689a6050-402a-0410-94f2-e92a70836424

--- map.h
+++ map.h
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Copyright (C) 2008 Voice System SRL
+ * Copyright (C) 2009 Voice System SRL
  *
  * This file is part of opensips, a free SIP server.
  *
@@ -38,18 +38,36 @@
 #define AVL_MAX_HEIGHT 128
 #endif
 
+/* Flags that can be passed on when creating a map */
+enum
+{
+	MAP_SHARED = 1,		/* determines if the map is to be allocated in
+				shared or private memory */
+	MAP_NO_DUPLICATE = 2	/* determines if the map will duplicate added keys*/
+
+};
+
 /* Tree data structure. */
 typedef struct avl_table {
-	int shared;			/* Shared memory or private memory */
+	int flags;			/* Shared memory or private memory */
 	struct avl_node *avl_root;	/* Tree's root. */
 	size_t avl_count;		/* Number of items in tree. */
 	int ret_code;
 	
 } *map_t;
 
+/* Iterator data structure. */
+typedef struct avl_iterator {
+	struct avl_node * node;		/* Current node. */
+	map_t map;			/* The map that this iterator points to*/
+} map_iterator_t;
+
+
+
 /* An AVL tree node. */
 struct avl_node {
 	struct avl_node * avl_link[2];	/* Subtrees. */
+	struct avl_node * avl_parent;  /* Parent node. */
 	str key;			/* Key */
 	void * val;			/* Value */
 	signed char avl_balance;	/* Balance factor. */
@@ -77,13 +95,15 @@ typedef  int (* process_each_func )(void * param, str key, void * value);
 
 /*
  * Allocates and initializes a map.
- * It can be in shared memory or in private memory.
+ * Flags can be set to determine the map behaviour. Several flags can be OR-ed
+ * together.
  *
- * shared = 0 -> private memory
- * shared = 1 -> shared memory
+ * MAP_SHARED -> flag for shared memory
+ * MAP_NO_DUPLICATE -> flag for key duplication
+ * 
  */
 
-map_t	map_create ( int shared );
+map_t map_create ( int flags );
 
 
 /*
@@ -93,7 +113,7 @@ map_t	map_create ( int shared );
  *
  */
 
-void	map_destroy( map_t, value_destroy_func );
+void map_destroy( map_t, value_destroy_func );
 
 /*
  * Searches for a given key in the map.
@@ -121,7 +141,7 @@ void **	map_get  ( map_t, str );
  *
  */
 
-void *	map_put ( map_t, str, void *);
+void* map_put ( map_t, str, void *);
 
 /*
  * Deletes a key from the map.
@@ -130,14 +150,14 @@ void *	map_put ( map_t, str, void *);
  *
  */
 
-void *	map_remove ( map_t, str);
+void* map_remove ( map_t, str);
 
 /*
  * Returns the size of the map.
  *
  */
 
-int	map_size   ( map_t );
+int map_size   ( map_t );
 
 
 /*
@@ -147,7 +167,93 @@ int	map_size   ( map_t );
  *
  */
 
-int	map_for_each   ( map_t map, process_each_func f, void * param );
+int map_for_each   ( map_t map, process_each_func f, void * param );
+
+/*
+ * Function that initializes an iterator to the first element in the map.
+ * If the map is empty it will initialize an invalid iterator.
+ *
+ * Returns 0 on success and -1 on error.
+ *
+ */
+
+int map_first( map_t map, map_iterator_t * it);
+
+
+/*
+ * Function that initializes an iterator to the last element in the map.
+ * If the map is empty it will initialize an invalid iterator.
+ *
+ * Returns 0 on success and -1 on error.
+ *
+ */
+
+int map_last( map_t map, map_iterator_t * it);
+
+/*
+ * Returns a pointer to the location where the key is stored.
+ * Users should copy the key if they want to modify it.
+ *
+ * Returns NULL on error.
+ *
+ */
+
+str *	iterator_key( map_iterator_t * it );
+
+/*
+ * Returns a pointer to the location where the value is stored.
+ * Returns NULL on error.
+ *
+ */
+void ** iterator_val( map_iterator_t * it );
+
+/*
+ * Advances the iterator to the next element in alphabetical
+ * order in the map.
+ * If the end is reached an invalid iterator is set.
+ *
+ * Returns 0 on success and -1 on error.
+ *
+ */
+
+int iterator_next( map_iterator_t * it  );
+
+/*
+ * Advances the iterator to the previous element in alphabetical
+ * order in the map.
+ * If the end is reached an invalid iterator is set.
+ *
+ * Returns 0 on success and -1 on error.
+ *
+ */
+
+int iterator_prev( map_iterator_t * it  );
+
+/*
+ * Checks if an iterator is valid.
+ * Can be used to check if the end of the map has been reached.
+ * Invalid iterators may be returned from all functions that initialize
+ * iterators.
+ *
+ * Returns 1 for valid and 0 for invalid.
+ *
+ */
+int iterator_is_valid( map_iterator_t * it );
+
+/*
+ *
+ * Deletes the node pointed by the iterator.
+ *
+ * The iterator itself will become invalid, and all
+ * iterators that pointed to the same node should no longer be used
+ * as they point to inexisting nodes.
+ *
+ * Returns the value stored in the node so the user can free it.
+ * Returns NULL if no deletion occured.
+ *
+ *
+ */
+void* iterator_delete( map_iterator_t * it  );
 
 
 #endif
