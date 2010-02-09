c04f96d741cf62cbd9dae8acb7dd2ee7907bb851
Bogdan-Andrei Iancu <bogdan@opensips.org>
2010-02-09 13:15:35 +0000

    fixed bogus free
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/branches/1.4@6574 689a6050-402a-0410-94f2-e92a70836424

--- modules/nathelper/nathelper.c
+++ modules/nathelper/nathelper.c
@@ -1213,9 +1213,6 @@ static void mod_destroy(void)
 	if (natping_state)
 		shm_free(natping_state);
 
-	if (default_rtpp_set)
-		shm_free(default_rtpp_set);
-
 	if(rtpp_set_list == NULL)
 		return;
 
