d227c12b60e1affc899ef1680568e57648d72b5d
Bogdan-Andrei Iancu <bogdan@opensips.org>
2010-08-01 22:09:04 +0000

    - fixed un-init var warning
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/branches/1.6@7087 689a6050-402a-0410-94f2-e92a70836424

--- modules/uri/db_checks.c
+++ modules/uri/db_checks.c
@@ -274,8 +274,8 @@ int get_auth_id(struct sip_msg* _msg, char* _uri, char* _auth_user, char* _auth_
 
 	// retrieve the string value of the given uri (pseudo variables will also be
 	// already substituted with their proper values)
-	if (_uri != NULL) {
-		if (pv_printf_s(_msg, (pv_elem_t *) _uri, &uri) != 0 || uri.len == 0 || uri.s == NULL) {
+	if (_uri == NULL || pv_printf_s(_msg, (pv_elem_t *) _uri, &uri) != 0 ||
+	uri.len == 0 || uri.s == NULL) {
 			LM_WARN("cannot get string for value\n");
 			return -1;
 		}
