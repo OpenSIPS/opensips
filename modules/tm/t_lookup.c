1657874a1df4b0cd74db9cad0fba665e88ec451f
Bogdan-Andrei Iancu <bogdan@opensips.org>
2010-10-06 15:29:54 +0000

    backport from trunk (rev #7254): - avoid running RESPONSE_IN callbacks from an INVITE transaction when the reply of the CANCEL (to that INVITE) is received
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/branches/1.5@7256 689a6050-402a-0410-94f2-e92a70836424

--- modules/tm/t_lookup.c
+++ modules/tm/t_lookup.c
@@ -837,7 +837,8 @@ int t_reply_matching( struct sip_msg *p_msg , int *p_branch )
 				LM_ERR("to parsing failed\n");
 			}
 		}
-		if (!is_local(p_cell)) {
+		if (!is_local(p_cell) &&
+		!(cseq->method_id==METHOD_CANCEL && is_invite(p_cell)) ) {
 			run_trans_callbacks( TMCB_RESPONSE_IN, T, T->uas.request, p_msg,
 				p_msg->REPLY_STATUS);
 		}
