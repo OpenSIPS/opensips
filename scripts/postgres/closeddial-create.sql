80d0a26f3079fd5570f3c3a9482d961d627f1414
Bogdan-Andrei Iancu <bogdan@opensips.org>
2010-10-07 17:45:43 +0000

    - added missing tables   Thaks to Ovidiu Sas to report it
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@7274 689a6050-402a-0410-94f2-e92a70836424

---
+++ scripts/postgres/closeddial-create.sql
@@ -0,0 +1,16 @@
+INSERT INTO version (table_name, table_version) values ('closeddial','1');
+CREATE TABLE closeddial (
+    id SERIAL PRIMARY KEY NOT NULL,
+    username VARCHAR(64) DEFAULT '' NOT NULL,
+    domain VARCHAR(64) DEFAULT '' NOT NULL,
+    cd_username VARCHAR(64) DEFAULT '' NOT NULL,
+    cd_domain VARCHAR(64) DEFAULT '' NOT NULL,
+    group_id VARCHAR(64) DEFAULT '' NOT NULL,
+    new_uri VARCHAR(128) DEFAULT '' NOT NULL,
+    CONSTRAINT closeddial_cd_idx1 UNIQUE (username, domain, cd_domain, cd_username, group_id)
+);
+
+CREATE INDEX closeddial_cd_idx2 ON closeddial (group_id);
+CREATE INDEX closeddial_cd_idx3 ON closeddial (cd_username);
+CREATE INDEX closeddial_cd_idx4 ON closeddial (username);
+
