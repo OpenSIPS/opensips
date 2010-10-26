dbee559ff88307bb18de9f8983920a43687f9bb4
Anca Vamanu <anca@opensips.org>
2010-10-26 07:52:40 +0000

    - add new file (forgot to add it in the last commit)
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@7324 689a6050-402a-0410-94f2-e92a70836424

---
+++ modules/b2b_logic/b2b_load.h
@@ -0,0 +1,90 @@
+
+#ifndef B2BUA_LOGIC_LOAD_
+#define B2BUA_LOGIC_LOAD_
+
+#include "../../sr_module.h"
+#include "../b2b_entities/dlg.h"
+
+typedef struct b2bl_dlg_stat
+{
+	str key;
+	int start_time;
+	int setup_time;
+	int call_time;
+}b2bl_dlg_stat_t;
+
+enum b2b_event {B2B_BYE_E1=0, B2B_BYE_E2, B2B_REJECT_E2, B2B_DESTROY};
+
+
+typedef int (*b2bl_cback_f)(void* param, b2bl_dlg_stat_t* stat, int curr_state,
+		enum b2b_event);
+/* curr_state: The current scenario state, before the rules are applied
+ * event    - B2B_BYE_E1,  bye from entity 1
+ *            B2B_BYE_E2,  bye from entity 2
+ *            B2B_DESTROY, destroy the tuple
+ * Return: -1 - error
+ *           0 - drop the BYE
+ *           1 - send the BYE on the other side 
+ *           2 - do what the scenario tells, if no rule defined send the BYE on the other side
+ **/
+
+
+typedef str* (*b2bl_init_f)(struct sip_msg* msg, str* name, str* args[5],
+		b2bl_cback_f, void* param);
+
+typedef int (*b2bl_bridge_f)(str* key, str* new_uri, str* new_from_dname,int entity_type);
+/* key - the string returned by b2bl_init_f 
+ * entity_type - 0, the server entity
+ *               1, the client entity
+ */
+typedef int (*b2bl_set_state_f)(str* key, int state);
+
+typedef str* (*b2bl_bridge_extern_f)(str* scenario_name, str* args[5],
+		b2bl_cback_f cbf, void* cb_param);
+
+int b2bl_terminate_call(str* key);
+typedef int (*b2bl_terminate_call_t)(str* key);
+
+int b2bl_bridge(str* key,str* new_uri,str* new_from_dname,int entity_no);
+int b2bl_set_state(str* key, int state);
+
+int b2bl_bridge_2calls(str* key1, str* key2);
+typedef int (*b2bl_bridge_2calls_t)(str* key1, str* key2);
+
+typedef struct b2bl_api
+{
+	b2bl_init_f init;
+	b2bl_bridge_f bridge;
+	b2bl_bridge_extern_f bridge_extern;
+	b2bl_set_state_f set_state;
+	b2bl_bridge_2calls_t bridge_2calls;
+	b2bl_terminate_call_t terminate_call;
+}b2bl_api_t;
+
+str* internal_init_scenario(struct sip_msg* msg, str* name, str* args[5],
+		b2bl_cback_f, void* param);
+
+typedef int(*load_b2bl_f)( b2bl_api_t *api );
+int b2b_logic_bind(b2bl_api_t* api);
+
+str* b2bl_bridge_extern(str* scenario_name, str* args[5], b2bl_cback_f cbf, void* cb_param);
+
+static inline int load_b2b_logic_api( b2bl_api_t *api)
+{
+	load_b2bl_f load_b2b;
+
+	/* import the b2b logic auto-loading function */
+	if ( !(load_b2b=(load_b2bl_f)find_export("b2b_logic_bind", 1, 0))) {
+		LM_ERR("failed to import b2b_logic_bind\n");
+		return -1;
+	}
+	/* let the auto-loading function load all B2B stuff */
+	if (load_b2b( api )==-1)
+		return -1;
+
+	return 0;
+}
+
+
+#endif
+
