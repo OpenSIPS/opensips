056e2439a6e0128f91b40eddd93658016ee07ba5
Bogdan-Andrei Iancu <bogdan@opensips.org>
2010-12-03 12:36:34 +0000

    - fixed typos in log messages
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@7454 689a6050-402a-0410-94f2-e92a70836424

--- modules/acc/acc.c
+++ modules/acc/acc.c
@@ -143,7 +143,7 @@ static inline int core2strar( struct sip_msg *req, str *c_vals)
 
 	/* from/to URI and TAG */
 	if (req->msg_flags&FL_REQ_UPSTREAM) {
-		LM_DBG("the flag UPSTREAM is set -> swproto F/T\n"); \
+		LM_DBG("the flag UPSTREAM is set -> swap F/T\n"); \
 		from = acc_env.to;
 		to = req->from;
 	} else {
@@ -434,7 +434,7 @@ int acc_db_init(const str* db_url)
 		return -1;
 	}
 
-	/* Check database cprotoabilities */
+	/* Check database cabilities */
 	if (!DB_CAPABILITY(acc_dbf, DB_CAP_INSERT)) {
 		LM_ERR("database module does not implement insert function\n");
 		return -1;
