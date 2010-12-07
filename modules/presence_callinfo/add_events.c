c3a804e622424700522f31618b3536fb77afc7c5
Ovidiu Sas <osas@voipembedded.com>
2010-12-07 19:12:15 +0000

    presence_callinfo: new module
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@7482 689a6050-402a-0410-94f2-e92a70836424

---
+++ modules/presence_callinfo/add_events.c
@@ -0,0 +1,138 @@
+/*
+ * Add "call-info" event to presence module
+ *
+ * Copyright (C) 2010 Ovidiu Sas
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * History:
+ * --------
+ *  2010-03-11  initial version (osas)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../presence/event_list.h"
+#include "presence_callinfo.h"
+
+static str extra_hdrs[] = {
+	str_init("Call-Info"),
+	{NULL,0},
+};
+
+static str dummy_header=str_init("Call-Info: <sip:localhost>;appearance-index=*;appearance-state=idle\r\n");
+
+/*
+ * event specific publish handling - check if body format is ok
+ */
+int callinfo_publ_handl(struct sip_msg* msg, int* sent_reply)
+{
+    if ( parse_headers(msg,HDR_EOH_F, 0)==-1 ) {
+	LM_ERR("parsing headers\n");
+	return -1;
+    }
+
+    if (!msg->call_info)
+    {
+	LM_ERR("No 'Call-Info' header\n");
+	return -1;
+    }
+
+    return 1;
+}
+
+/*
+ * event specific publish handling - check if body format is ok
+ */
+int lineseize_publ_handl(struct sip_msg* msg, int* sent_reply)
+{       
+    if ( parse_headers(msg,HDR_EOH_F, 0)==-1 ) {
+	LM_ERR("parsing headers\n");
+	return -1;
+    }
+
+    if (!msg->call_info)
+    {
+	LM_ERR("No 'Call-Info' header\n");
+	return -1;
+    }
+
+    return 1;
+}
+
+/*
+ * event specific extra headers builder - for empty notifications
+ */
+str* build_callinfo_dumy_header(str* pres_uri, str* extra_hdrs)
+{
+	if (extra_hdrs->s == NULL)
+	{
+		extra_hdrs->s = (char*)pkg_malloc(dummy_header.len);
+		if (extra_hdrs->s == NULL)
+		{
+			LM_ERR("oom: no dummy header\n");
+			return NULL;
+		}
+		memcpy(extra_hdrs->s, dummy_header.s, dummy_header.len);
+		extra_hdrs->len = dummy_header.len;
+	}
+	return NULL;
+}
+
+int callinfo_add_events(void)
+{
+	pres_ev_t event;
+
+	/* constructing call-info event */
+	memset(&event, 0, sizeof(pres_ev_t));
+	event.name.s = "call-info";
+	event.name.len = 9;
+
+	event.extra_hdrs = extra_hdrs;
+
+	event.default_expires= 3600;
+	event.type = PUBL_TYPE;
+	event.evs_publ_handl = callinfo_publ_handl;
+
+	/* register the dummy Call-Info header builder */
+	event.build_empty_pres_info = build_callinfo_dumy_header;
+
+
+	if (pres_add_event(&event) < 0) {
+		LM_ERR("failed to add event \"call-info\"\n");
+		return -1;
+	}
+
+	/* constructing line-seize-info event */
+	memset(&event, 0, sizeof(pres_ev_t));
+	event.name.s = "line-seize";
+	event.name.len = 10;
+
+	event.default_expires= 15;
+	event.type = PUBL_TYPE;
+	event.evs_publ_handl = lineseize_publ_handl;
+
+	if (pres_add_event(&event) < 0) {
+		LM_ERR("failed to add event \"line-seize\"\n");
+		return -1;
+	}
+
+	return 0;
+}
+
---
+++ modules/presence_callinfo/presence_callinfo.c
@@ -0,0 +1,126 @@
+/*
+ * presence_callinfo module - Presence Handling of call-info events
+ *
+ * Copyright (C) 2010 Ovidiu Sas
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * History:
+ * --------
+ *  2010-03-11  initial version (osas)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "../../sr_module.h"
+#include "../../dprint.h"
+#include "../../str.h"
+#include "../../parser/msg_parser.h"
+#include "../../mem/mem.h"
+#include "../presence/bind_presence.h"
+#include "add_events.h"
+#include "presence_callinfo.h"
+
+
+
+/* module functions */
+static int mod_init(void);
+static int child_init(int);
+static void destroy(void);
+
+/* module variables */
+add_event_t pres_add_event;
+
+/* module exported commands */
+static cmd_export_t cmds[] =
+{
+    {0,	0, 0, 0, 0, 0}
+};
+
+/* module exported paramaters */
+static param_export_t params[] = {
+    {0, 0, 0}
+};
+
+/* module exports */
+struct module_exports exports= {
+    "presence_callinfo",	/* module name */
+    MODULE_VERSION,		/* module version */
+    DEFAULT_DLFLAGS,		/* dlopen flags */
+    cmds,			/* exported functions */
+    params,			/* exported parameters */
+    0,				/* exported statistics */
+    0,				/* exported MI functions */
+    0,				/* exported pseudo-variables */
+    0,				/* extra processes */
+    mod_init,			/* module initialization function */
+    (response_function) 0,	/* response handling function */
+    destroy,			/* destroy function */
+    child_init			/* per-child init function */
+};
+	
+/*
+ * init module function
+ */
+static int mod_init(void)
+{
+	presence_api_t pres;
+    LM_INFO("initializing...\n");
+
+    bind_presence_t bind_presence;
+
+    bind_presence= (bind_presence_t)find_export("bind_presence", 1,0);
+    if (!bind_presence) {
+	LM_ERR("can't bind presence\n");
+	return -1;
+    }
+    if (bind_presence(&pres) < 0) {
+	LM_ERR("can't bind pua\n");
+	return -1;
+    }
+
+    pres_add_event = pres.add_event;
+    if (add_event == NULL) {
+	LM_ERR("could not import add_event\n");
+	return -1;
+    }
+    if(callinfo_add_events() < 0) {
+	LM_ERR("failed to add call-info events\n");
+	return -1;		
+    }	
+    
+    return 0;
+}
+
+static int child_init(int rank)
+{
+	LM_DBG("[%d] pid [%d]\n", rank, getpid());
+
+	return 0;
+}	
+
+static void destroy(void)
+{	
+    LM_DBG("destroying module ...\n");
+
+    return;
+}
