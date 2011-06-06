54d247a4ce13fe2de925e3a027bcd5706bf150e3
Di-Shi Sun <di-shi@transnexus.com>
2011-06-06 03:36:24 +0000

    Added support for calling number invalid.
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/branches/1.6@7996 689a6050-402a-0410-94f2-e92a70836424

--- modules/osp/orig_transaction.c
+++ modules/osp/orig_transaction.c
@@ -555,6 +555,9 @@ int ospRequestRouting(
                 case OSPC_ERR_TRAN_ROUTE_NOT_FOUND:
                     result = -404;
                     break;
+                case OSPC_ERR_TRAN_CALLING_INVALID:
+                	result = -428;
+                	break;
                 case OSPC_ERR_TRAN_CALLED_FILTERING:
                     result = -484;
                     break;
