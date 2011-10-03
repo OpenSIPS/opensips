17d92c4487b85b17bdb474830ee6538cbf226218
Razvan Crainea <razvan@opensips.org>
2011-10-03 08:57:56 +0000

    Deleted ".d" files from the new event_rabbitmq module
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@8433 689a6050-402a-0410-94f2-e92a70836424

--- modules/event_rabbitmq/rabbitmq_send.d
+++
@@ -1,8 +0,0 @@
-rabbitmq_send.o rabbitmq_send.d : rabbitmq_send.c ../../evi/evi_transport.h \
- ../../evi/../mi/mi.h ../../evi/../mi/../str.h ../../evi/../mi/tree.h \
- ../../evi/../mi/attr.h ../../evi/../str.h ../../evi/../ip_addr.h \
- ../../evi/../str.h ../../evi/../dprint.h ../../evi/evi_params.h \
- ../../mem/shm_mem.h ../../mem/../statistics.h ../../mem/../hash_func.h \
- ../../mem/../str.h ../../mem/../atomic.h ../../mem/../dprint.h \
- ../../mem/../lock_ops.h ../../mem/../fastlock.h ../../mem/q_malloc.h \
- ../../mem/meminfo.h rabbitmq_send.h event_rabbitmq.h
