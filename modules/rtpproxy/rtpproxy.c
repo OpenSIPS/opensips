35d7cbe54d601b2d08d767a1385439c5b63e70a4
Razvan Crainea <razvan@opensips.org>
2011-11-14 09:54:07 +0000

    fixed memory corruption when rtpproxy autobridging is used. * Reported by Sergey Lavrov * Credits for patch go to Walter Doekes
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@8543 689a6050-402a-0410-94f2-e92a70836424

--- modules/rtpproxy/rtpproxy.c
+++ modules/rtpproxy/rtpproxy.c
@@ -2741,7 +2741,7 @@ append_opts_str(struct options *op, str *s)
 {
 	void *p;
 
-	if (op->s.len <= op->oidx + s->len) {
+	if (op->s.len < op->oidx + s->len) {
 		p = pkg_realloc(op->s.s, op->oidx + s->len + 32);
 		if (p == NULL) {
 			 return (-1);
@@ -3259,7 +3259,7 @@ force_rtp_proxy_body(struct sip_msg* msg, struct force_rtpp_args *args)
 						v[2].iov_len = 0;
 					} else {
 						v[2].iov_base = rep_opts.s.s;
-						v[2].iov_len += rep_opts.oidx;
+						v[2].iov_len = rep_opts.oidx;
 					}
 				}
 				if (payload_types.len > 0 && args->node->rn_ptl_supported != 0) {
