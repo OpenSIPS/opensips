4b8573fc7609310f798677090523f7aca2034b52
Saúl Ibarra Corretgé <saul@ag-projects.com>
2012-01-10 12:45:18 +0000

    Fixed compilation error due to a wrong define
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@8650 689a6050-402a-0410-94f2-e92a70836424

--- statistics.c
+++ statistics.c
@@ -131,7 +131,7 @@ unsigned int calc_udp_load(void *val)
 	return ( get_stat_val((stat_var*)val) * 100)/children_no;
 }
 
-#ifdef TCP
+#ifdef USE_TCP
 unsigned int calc_tcp_load(void *val)
 {
 	return ( get_stat_val((stat_var*)val) * 100)/tcp_children_no;
@@ -166,7 +166,7 @@ int register_udp_load_stat(str *name, stat_var **s)
 	return 0;
 }
 
-#ifdef TCP
+#ifdef USE_TCP
 int register_tcp_load_stat(stat_var **s)
 {
 	*s = shm_malloc(sizeof(stat_var));
