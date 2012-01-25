bfaf389596d7e0006028194c1e7381f97e56a0a4
Ovidiu Sas <osas@voipembedded.com>
2012-01-25 18:52:05 +0000

    httpd/mi_http: fix/update copyright year
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@8688 689a6050-402a-0410-94f2-e92a70836424

--- modules/mi_http/http_fnc.c
+++ modules/mi_http/http_fnc.c
@@ -223,7 +223,7 @@ static const str MI_HTTP_Response_Foot = str_init(\
 "\n</center>\n<div align=\"center\" class=\"foot\" style=\"margin:20px auto\">"\
 	"<span style='margin-left:5px;'></span>"\
 	"<a href=\"http://opensips.org\">OpenSIPS web site</a><br/>"\
-	"Copyright &copy; 2011 <a href=\"http://www.voipembedded.com/\">VoIP Embedded</a>"\
+	"Copyright &copy; 2011-2012 <a href=\"http://www.voipembedded.com/\">VoIP Embedded</a>"\
 								". All rights reserved."\
 "</div></body></html>");
 
