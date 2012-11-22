f406fdd0c452be58100eb2c87a7f15966a1cc6ed
Vladut Paiu <vladpaiu@opensips.org>
2012-11-22 18:14:30 +0000

    backport from trunk (rev #9366)
    - bug fixed: if no interface explicitly set, select as outbound interface the interface the message was received rather than the interface served by the process.
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/branches/1.8@9447 689a6050-402a-0410-94f2-e92a70836424

--- forward.c
+++ forward.c
@@ -283,7 +283,7 @@ struct socket_info* get_send_socket(struct sip_msg *msg,
 					default:	LM_ERR("don't know how to forward to af %d\n",
 										to->s.sa_family);
 				}
-			}else send_sock=bind_address;
+			}else send_sock = msg ? msg->rcv.bind_address : bind_address;
 			break;
 		default:
 			LM_CRIT("unknown proto %d\n", proto);
