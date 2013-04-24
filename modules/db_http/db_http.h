608f6786d90bfb896e59997119c137dc4ea1a482
Vladut Paiu <vladpaiu@opensips.org>
2013-04-24 09:41:40 +0000

    completed previous timeout commit
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensips/svn/trunk@9979 689a6050-402a-0410-94f2-e92a70836424

---
+++ modules/db_http/db_http.h
@@ -0,0 +1,33 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2012 Guillaume Bour (Orange-Vallee)
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * History:
+ * ---------
+ *  2012-02-25  first version (gbour)
+ */
+
+#ifndef DB_HTTP_H
+#define DB_HTTP_H
+
+extern unsigned int db_http_timeout;
+
+#endif /* DB_HTTP_H */
