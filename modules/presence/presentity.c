6f555c69d561e6d4ab4e0657f2f1ba864a329637
Damien Sandras <dsandras@beip.be>
2013-07-10 18:04:13 +0200

    Fixed presentity naming.
    'Calling' is disturbing especially when the given peer is _receiving_ a
    call.

--- modules/presence/presentity.c
+++ modules/presence/presentity.c
@@ -54,8 +54,8 @@ char* dialog_states[]= {  "trying",
                            "early",
                        "confirmed",
                      "terminated"};
-char* presence_notes[]={ "Calling",
-                         "Calling",
+char* presence_notes[]={ "Proceeding",
+                         "Proceeding",
                     "On the phone",
                                ""};
 
