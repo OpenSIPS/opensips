OpenSIPS/opensips

commit bde8c0a0451d7e5bac7d3cb555738ab57205c730

Added a new module for performing voice transcoding using Sangoma D-series cards. (libsngtc_node dependency)

parser/sdp/sdp.c
@@ -350,7 +350,7 @@ sdp_payload_attr_t* get_sdp_payload4index(sdp_stream_cell_t *stream, int index)
 /**
  * SDP parser method.
  */
-static int parse_sdp_session(str *sdp_body, int session_num, str *cnt_disp, sdp_info_t* _sdp)
+int parse_sdp_session(str *sdp_body, int session_num, str *cnt_disp, sdp_info_t* _sdp)
 {
 	str body = *sdp_body;
 	str sdp_ip = {NULL,0};
@@ -749,14 +749,20 @@ int parse_sdp(struct sip_msg* _m)
 /**
  * Free all memory.
  */
-void free_sdp(sdp_info_t** _sdp)
+void free_sdp(sdp_info_t** sdp)
 {
-	sdp_info_t *sdp = *_sdp;
+	__free_sdp(*sdp);
+	pkg_free(sdp);
+	*sdp = NULL;
+}
+
+void __free_sdp(sdp_info_t* sdp)
+{
 	sdp_session_cell_t *session, *l_session;
 	sdp_stream_cell_t *stream, *l_stream;
 	sdp_payload_attr_t *payload, *l_payload;
 
-	LM_DBG("_sdp = %p\n", _sdp);
+	LM_DBG("sdp = %p\n", sdp);
 	if (sdp == NULL) return;
 	LM_DBG("sdp = %p\n", sdp);
 	session = sdp->sessions;
@@ -781,10 +787,7 @@ void free_sdp(sdp_info_t** _sdp)
 		}
 		pkg_free(l_session);
 	}
-	pkg_free(sdp);
-	*_sdp = NULL;
 }
-
 
 void print_sdp_stream(sdp_stream_cell_t *stream, int log_level)
 {

Makefile.conf
@@ -44,12 +44,13 @@
 #python= Easily implement your own OpenSIPS extensions in Python | Shared Python runtime library, libpython
 #rest_client= Simple HTTP client | CURL library - libcurl
 #rls= Resource List Server implementation following the specification in RFC 4662 and RFC 4826 | parsing/building XML library, tipically libxml-dev
+#sngtc= Voice Transcoding using the D-series Sangoma transcoding cards | libsngtc_node
 #snmpstats= Provides an SNMP management interface to OpenSIPS | NetSNMP v5.3 
 #xcap= XCAP utility functions for OpenSIPS. | libxml-dev
 #xcap_client= XCAP client for OpenSIPS.It fetches XCAP elements, either documents or part of them, by sending HTTP GET requests | libxml-dev and libcurl-dev
 #xmpp= Gateway between OpenSIPS and a jabber server. It enables the exchange of IMs between SIP clients and XMPP(jabber) clients. | parsing/building XML files, tipically libexpat1-devel
 
-exclude_modules?= aaa_radius b2b_logic cachedb_couchbase cachedb_cassandra cachedb_memcached cachedb_mongodb cachedb_redis carrierroute cpl-c db_berkeley db_http db_mysql db_oracle db_perlvdb db_postgres db_unixodbc dialplan event_rabbitmq h350 regex identity jabber json ldap lua httpd pi_http mi_http mi_xmlrpc mi_xmlrpc_ng mmgeoip osp perl presence presence_dialoginfo presence_mwi presence_xml pua pua_bla pua_dialoginfo pua_mi pua_usrloc pua_xmpp python rest_client rls snmpstats xcap xcap_client xmpp
+exclude_modules?= aaa_radius b2b_logic cachedb_couchbase cachedb_cassandra cachedb_memcached cachedb_mongodb cachedb_redis carrierroute cpl-c db_berkeley db_http db_mysql db_oracle db_perlvdb db_postgres db_unixodbc dialplan event_rabbitmq h350 regex identity jabber json ldap lua httpd pi_http mi_http mi_xmlrpc mi_xmlrpc_ng mmgeoip osp perl presence presence_dialoginfo presence_mwi presence_xml pua pua_bla pua_dialoginfo pua_mi pua_usrloc pua_xmpp python rest_client rls sngtc snmpstats xcap xcap_client xmpp
 
 include_modules?= 
 

modules/sngtc/Makefile
@@ -1,0 +1,8 @@
+# WARNING: do not run this directly, it should be run by the master Makefile
+
+include ../../Makefile.defs
+auto_gen =
+NAME     = sngtc.so
+LIBS     = -lsngtc_node
+
+include ../../Makefile.modules

modules/sngtc/README
@@ -1,0 +1,171 @@
+sngtc Module
+
+Liviu Chircu
+
+   OpenSIPS Solutions
+
+Edited by
+
+Liviu Chircu
+
+   Copyright © 2013 www.opensips-solutions.com
+     __________________________________________________________
+
+   Table of Contents
+
+   1. Admin Guide
+
+        1.1. Overview
+        1.2. How it works
+        1.3. Dependencies
+
+              1.3.1. OpenSIPS Modules
+              1.3.2. External Libraries or Applications
+
+        1.4. Exported Functions
+
+              1.4.1. sngtc_offer()
+              1.4.2. sngtc_callee_answer([listen_if_A[,
+                      listen_if_B]])
+
+              1.4.3. sngtc_caller_answer()
+
+   List of Examples
+
+   1.1. sngtc_offer usage
+   1.2. sngtc_callee_answer usage
+   1.3. sngtc_caller_answer usage
+
+Chapter 1. Admin Guide
+
+1.1. Overview
+
+   The Sangoma transcoding module offers the possibility of
+   performing voice transcoding with the D-series transcoding
+   cards manufactured by Sangoma. The module makes use of the
+   Sangoma Transcoding API in order to manage transcoding sessions
+   on the dedicated equipment. For the cards in the network to be
+   detected, the Sangoma SOAP server must be up and running
+   (sngtc_server daemon).
+
+1.2. How it works
+
+   The module performs several modifications in the SDP body of
+   SIP INVITE, 200 OK and ACK messages. In all transcoding
+   scenarios, the UAC performs early SDP negotiation, while the
+   UAS does late negotiation. This way, OpenSIPS becomes
+   responsible for intersecting the codec offer and answer,
+   together with the management of transcoding sessions on the
+   Sangoma cards.
+
+   This scenario brings about a couple of restrictions:
+     * UACs MUST only perform early SDP negotiation
+     * UASs MUST support late SDP negotiation (rfc 3261
+       requirement)
+
+   Since the sngtc_node library performs several memory
+   allocations with each newly created transcoding session, the
+   module uses a dedicated process, responsible for the management
+   of the above-mentioned sessions. The sangoma_worker process
+   communicates with the OpenSIPS UDP receivers through a series
+   of pipes.
+
+1.3. Dependencies
+
+1.3.1. OpenSIPS Modules
+
+   The following modules must be loaded before this module:
+     * dialog.
+
+1.3.2. External Libraries or Applications
+
+   The following libraries or applications must be installed
+   before running OpenSIPS with this module loaded:
+     * sngtc_node library.
+     * sngtc_server up and running.
+
+1.4. Exported Functions
+
+1.4.1.  sngtc_offer()
+
+   The function strips off the SDP offer from a SIP INVITE, thus
+   asking for another SDP offer from the opposite endpoint (late
+   negotiation).
+
+   The following error codes may be returned:
+     * -1 - SDP parsing error
+     * -3 - internal error / no more memory
+
+   The function can be used from REQUEST_ROUTE, ONREPLY_ROUTE.
+
+   Example 1.1. sngtc_offer usage
+...
+        if (is_method("INVITE")) {
+                t_newtran();
+                create_dialog();
+                sngtc_offer();
+        }
+...
+
+1.4.2.  sngtc_callee_answer([listen_if_A[, listen_if_B]])
+
+   Handles the SDP offer from 200 OK responses, intersects both
+   offers with the capabilities of the transcoding card and
+   creates a new transcoding session on the card only if
+   necessary. It then rewrites the 200 OK SDP so that it contains
+   the information resulted from the codec intersection.
+
+   Parameters explained:
+
+   Since the D-series transcoding cards are connected through
+   either a PCI slot or simply an Ethernet connector, they cannot
+   be assigned global IPs. Consequently, the module will write the
+   local, private IP of the card in the SDP answers sent to each
+   of the endpoints. Since this will not work with non-local UAs,
+   the optional parameters force the RTP listen interface for each
+   UA. This way, the script writer can enforce a global IP for the
+   incoming RTP (which can be port forwarded to a transcoding
+   card).
+     * listen_if_A - the interface where the UAC (the caller) will
+       send RTP after the call is established (IP from the 'c='
+       SDP line(s))
+     * listen_if_B - the interface where the UAS (the callee) will
+       send RTP after the call is established (IP from the 'c='
+       SDP line(s))
+
+   The following error codes may be returned:
+     * -1 - SDP parsing error
+     * -2 - failed to create transcoding session
+     * -3 - internal error / no more memory
+
+   This function can be used from REQUEST_ROUTE, ONREPLY_ROUTE.
+
+   Example 1.2. sngtc_callee_answer usage
+...
+onreply_route[1] {
+        if ($rs == 200)
+                sngtc_callee_answer("11.12.13.14", "11.12.13.14");
+}
+...
+
+1.4.3.  sngtc_caller_answer()
+
+   Attaches an SDP body to the caller's ACK request, so that it
+   matches the late SDP negotiation done by the UAS.
+
+   The following error codes may be returned:
+     * -3 - internal error / no more memory
+
+   This function can be used from REQUEST_ROUTE, ONREPLY_ROUTE.
+
+   Example 1.3. sngtc_caller_answer usage
+...
+        if (has_totag()) {
+                if (loose_route()) {
+                        ...
+                        if (is_method("ACK"))
+                                sngtc_caller_answer();
+                }
+                ...
+        }
+...

modules/sngtc/doc/sngtc.xml
@@ -1,0 +1,44 @@
+<?xml version="1.0" encoding='ISO-8859-1'?>
+<!DOCTYPE book PUBLIC "-//OASIS//DTD DocBook XML V4.4//EN"
+"http://www.oasis-open.org/docbook/xml/4.4/docbookx.dtd" [
+
+
+<!ENTITY admin SYSTEM "sngtc_admin.xml">
+
+<!-- Include general documentation entities -->
+<!ENTITY % docentities SYSTEM "../../../doc/entities.xml">
+%docentities;
+
+]>
+
+<book>
+    <bookinfo>
+	<title>sngtc Module</title>
+	<productname class="trade">&osipsname;</productname>
+	<authorgroup>
+	    <author>
+		<firstname>Liviu</firstname>
+		<surname>Chircu</surname>
+		<affiliation><orgname>OpenSIPS Solutions</orgname></affiliation>
+		<address>
+		    <email>liviu@opensips.org</email>
+		</address>
+	    </author>
+	    <editor>
+		<firstname>Liviu</firstname>
+		<surname>Chircu</surname>
+		<address>
+		    <email>liviu@opensips.org</email>
+		</address>
+	    </editor>
+	</authorgroup>
+	<copyright>
+	    <year>2013</year>
+		<holder>&osipssol;</holder>
+	</copyright>
+    </bookinfo>
+    <toc></toc>
+    
+    &admin;
+    
+</book>

modules/sngtc/doc/sngtc_admin.xml
@@ -1,0 +1,243 @@
+<!-- Module User's Guide -->
+
+<chapter>
+
+	<title>&adminguide;</title>
+
+	<section>
+	<title>Overview</title>
+	<para>
+		The <emphasis role='bold'>Sangoma transcoding module</emphasis> offers the possibility of performing
+	voice transcoding with the D-series transcoding cards manufactured by
+	Sangoma. The module makes use of the Sangoma Transcoding API in order to 
+	manage transcoding sessions on the dedicated equipment. For the cards
+	in the network to be detected, the Sangoma SOAP server must be up and
+	running (<emphasis>sngtc_server</emphasis> daemon).
+	</para>
+	</section>
+
+	<section>
+	<title>How it works</title>
+	<para>
+		The module performs several modifications in the SDP body of SIP INVITE,
+	200 OK and ACK messages. In all transcoding scenarios, the UAC performs early
+	SDP negotiation, while the UAS does late negotiation. This way, OpenSIPS
+	becomes responsible for intersecting the codec offer and answer, together with
+	the management of transcoding sessions on the Sangoma cards.
+	</para>
+
+	<para>
+		This scenario brings about a couple of
+	<emphasis role='bold'>restrictions</emphasis>:
+	<itemizedlist>
+		<listitem>
+			<para>
+				UACs MUST only perform early SDP negotiation
+			</para>
+		</listitem>
+		<listitem>
+			<para>
+				UASs MUST support late SDP negotiation (rfc 3261 requirement)
+			</para>
+		</listitem>
+	</itemizedlist>
+	</para>
+
+	<para>
+		Since the <emphasis>sngtc_node</emphasis> library performs several memory
+	allocations with each newly created transcoding session, the module uses a
+	dedicated process, responsible for the management of the above-mentioned sessions. The
+	<emphasis>sangoma_worker</emphasis> process communicates with the OpenSIPS
+	UDP receivers through a series of pipes.
+	</para>
+	</section>
+
+	<section>
+	<title>Dependencies</title>
+		<section>
+		<title>&osips; Modules</title>
+		<para>
+		The following  modules must be loaded before this module:
+			<itemizedlist>
+			<listitem>
+			<para>
+				<emphasis>dialog</emphasis>.
+			</para>
+			</listitem>
+			</itemizedlist>
+		</para>
+		</section>
+
+		<section>
+		<title>External Libraries or Applications</title>
+		<para>
+		The following libraries or applications must be installed before running
+		&osips; with this module loaded:
+			<itemizedlist>
+			<listitem>
+				<para>
+				<emphasis>sngtc_node library</emphasis>.
+				</para>
+			</listitem>
+			<listitem>
+				<para>
+				<emphasis>sngtc_server up and running</emphasis>.
+				</para>
+			</listitem>
+			</itemizedlist>
+		</para>
+		</section>
+	</section>
+
+	<section>
+	<title>Exported Functions</title>
+	<section>
+		<title>
+		<function moreinfo="none">sngtc_offer()</function>
+		</title>
+		<para>
+			The function strips off the SDP offer from a SIP INVITE, thus
+		asking for another SDP offer from the opposite endpoint (late negotiation).
+		<para>
+
+		</para>
+		The following <emphasis role='bold'>error codes</emphasis> may be returned:
+		<itemizedlist>
+		<listitem>
+			<para><emphasis>-1</emphasis> - SDP parsing error
+			</para>
+		</listitem>
+		<listitem>
+			<para><emphasis>-3</emphasis> - internal error / no more memory
+			</para>
+		</listitem>
+		</itemizedlist>
+		</para>
+
+		<para>
+		The function can be used from REQUEST_ROUTE, ONREPLY_ROUTE.
+		</para>
+		<example>
+		<title><function moreinfo="none">sngtc_offer</function> usage</title>
+		<programlisting format="linespecific">
+...
+	if (is_method("INVITE")) {
+		t_newtran();
+		create_dialog();
+		sngtc_offer();
+	}
+...
+</programlisting>
+		</example>
+	</section>
+
+	<section>
+		<title>
+		<function moreinfo="none">sngtc_callee_answer([listen_if_A[, listen_if_B]])
+		</function>
+		</title>
+		<para>
+			Handles the SDP offer from 200 OK responses, intersects both offers with
+		the capabilities of the transcoding card and creates a new transcoding
+		session on the card <emphasis role='bold'>only if</emphasis> necessary. It then rewrites the 200 OK SDP so that it 
+		contains the information resulted from the codec intersection.
+		</para>
+
+		<para><emphasis role='bold'>Parameters</emphasis> explained:</para>
+		<para>
+			Since the D-series transcoding cards are connected through either a
+		PCI slot or simply an Ethernet connector, they cannot be assigned
+		global IPs. Consequently, the module will write the local, private IP of the
+		card in the SDP answers sent to each of the endpoints. Since this will not
+		work with non-local UAs, the optional parameters force the RTP listen
+		interface for each UA. This way, the script writer can enforce a global IP
+		for the incoming RTP (which can be port forwarded to a transcoding card).
+		<itemizedlist>
+		<listitem>
+			<para><emphasis>listen_if_A</emphasis> - the interface where the UAC (the caller) will send RTP after the call is established (IP from the 'c=' SDP line(s))
+			</para>
+		</listitem>
+		<listitem>
+			<para><emphasis>listen_if_B</emphasis> - the interface where the UAS (the callee) will send RTP after the call is established (IP from the 'c=' SDP line(s))
+			</para>
+		</listitem>
+		</itemizedlist>
+		</para>
+
+		<para>
+		The following <emphasis role='bold'>error codes</emphasis> may be returned:
+		<itemizedlist>
+		<listitem>
+			<para><emphasis>-1</emphasis> - SDP parsing error
+			</para>
+		</listitem>
+		<listitem>
+			<para><emphasis>-2</emphasis> - failed to create transcoding session
+			</para>
+		</listitem>
+		<listitem>
+			<para><emphasis>-3</emphasis> - internal error / no more memory
+			</para>
+		</listitem>
+		</itemizedlist>
+		</para>
+
+
+		<para>
+		This function can be used from REQUEST_ROUTE, ONREPLY_ROUTE.
+		</para>
+		<example>
+		<title><function moreinfo="none">sngtc_callee_answer</function> usage</title>
+		<programlisting format="linespecific">
+...
+onreply_route[1] {
+	if ($rs == 200)
+		sngtc_callee_answer("11.12.13.14", "11.12.13.14");
+}
+...
+</programlisting>
+		</example>
+	</section>
+
+	<section>
+		<title>
+		<function moreinfo="none">sngtc_caller_answer()</function>
+		</title>
+		<para>
+			Attaches an SDP body to the caller's ACK request, so that it matches
+		the late SDP negotiation done by the UAS.
+		</para>
+
+		<para>
+		The following <emphasis role='bold'>error codes</emphasis> may be returned:
+		<itemizedlist>
+		<listitem>
+			<para><emphasis>-3</emphasis> - internal error / no more memory
+			</para>
+		</listitem>
+		</itemizedlist>
+		</para>
+
+		<para>
+		This function can be used from REQUEST_ROUTE, ONREPLY_ROUTE.
+		</para>
+		<example>
+		<title><function moreinfo="none">sngtc_caller_answer</function> usage</title>
+		<programlisting format="linespecific">
+...
+	if (has_totag()) {
+		if (loose_route()) {
+			...
+			if (is_method("ACK"))
+				sngtc_caller_answer();
+		}
+		...
+	}
+...
+</programlisting>
+		</example>
+	</section>
+
+	</section>
+</chapter>
+

modules/sngtc/sngtc.h
@@ -1,0 +1,147 @@
+/**
+ * Copyright (C) 2013 OpenSIPS Solutions
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * History
+ * -------
+ * 2013-06-05  created (liviu)
+ *
+ */
+
+#ifndef __SNGTC_H__
+#define __SNGTC_H__
+
+#include <sng_tc/sngtc_node.h>
+
+#define SDP_CONTENT_TYPE_LEN (sizeof("Content-Type: application/sdp\r\n") - 1)
+#define A_LINE_PREFIX_LEN    (sizeof("a=rtpmap:" - 1))
+#define C_LINE_LEN           (sizeof("c=IN IP4 xxx.xxx.xxx.xxx\r\n") - 1)
+#define SDP_BUFFER_SIZE      4096
+#define MAX_STREAMS          30
+#define CONTENT_LEN_DIGITS   5
+
+#define PROCESSED_FLAG       (1 << 0)
+
+#define READ_END  0
+#define WRITE_END 1
+
+#define is_processed(_info) (_info->flags & PROCESSED_FLAG)
+
+/**
+ * @_level:    OpenSIPS debug level
+ * @_l:        struct sngtc_codec_request_leg
+ * @_start:    string identifier of the leg
+ */
+#define sngtc_print_request_leg(_level, _l, _start) \
+	do { \
+		LM_GEN1(_level, "%s: [Codec: %d][ms: %d][ip: %d][nm: %d][port: %d]\n", \
+		        _start, _l.codec_id, _l.ms, _l.host_ip, _l.host_netmask, \
+		        _l.host_udp_port); \
+	} while (0)
+
+/**
+ * @_level:    OpenSIPS debug level
+ * @_l:        struct sngtc_codec_reply_leg
+ * @_start:    string identifier of the leg
+ */
+#define sngtc_print_reply_leg(_level, _l, _start) \
+	do { \
+		LM_GEN1(_level, "%s: [IP: %d][nm: %d][port: %d]\n", \
+		        _start, _l.codec_ip, _l.codec_netmask, _l.codec_udp_port); \
+		LM_GEN1(_level, "%s: [Host IP: %d][nm: %d][port: %d][iana: %d]\n", \
+		        _start, _l.host_ip, _l.host_netmask, _l.host_udp_port, \
+		        _l.iana_code); \
+	} while (0)
+
+/**
+ * @_level: OpenSIPS debug level
+ * @_r:     struct sngtc_codec_request
+ */
+#define sngtc_print_request(_level, _r) \
+	do { \
+		LM_GEN1(_level, "sngtc_codec_request with rtcp: %d\n", _r.rtcp_enable); \
+		sngtc_print_request_leg(_level, (_r).a, "A"); \
+		sngtc_print_request_leg(_level, (_r).b, "B"); \
+	} while (0)
+
+/**
+ * @_level: OpenSIPS debug level
+ * @_r:     struct sngtc_codec_reply *
+ */
+#define sngtc_print_reply(_level, _r) \
+	do { \
+		LM_GEN1(_level, "sngtc_codec_reply with [mod_session: %d]" \
+		        "[rtp_session: %d]\n", (_r)->codec_module_session_idx, \
+		        (_r)->codec_rtp_session_idx); \
+		sngtc_print_reply_leg(_level, (_r)->a, "A"); \
+		sngtc_print_reply_leg(_level, (_r)->b, "B"); \
+	} while (0)
+
+#define SNGTC_SDP_ERR -1
+#define SNGTC_TC_ERR  -2
+#define SNGTC_ERR     -3
+
+enum sng_module_status {
+	SNGTC_UNSUP_CODECS = -2,
+	SNGTC_BAD_SDP,
+	SNGTC_OFF,
+	SNGTC_ON,
+};
+
+struct sngtc_session_list {
+	struct sngtc_codec_reply *reply;
+	struct sngtc_session_list *next;
+};
+
+struct sngtc_info {
+	str caller_sdp;
+	str modified_caller_sdp;
+
+	gen_lock_t lock;
+
+	/* various session-related flags */
+	int flags;
+
+	/* optional, used if transcoding is needed */
+	struct sngtc_session_list *sessions;
+	struct sngtc_session_list *last_session;
+};
+
+struct codec_pair {
+	struct sdp_payload_attr *att1;
+	struct sdp_payload_attr *att2;
+
+	enum sngtc_codec_definition tc1;
+	enum sngtc_codec_definition tc2;
+
+	enum sng_module_status status;
+
+	struct sngtc_codec_reply *reply;
+};
+
+struct codec_mapping {
+	str name;
+	int bitrate;
+
+	int mode;
+
+	int sng_codec;
+};
+
+#endif /* __SNGTC_H__ */
+

commit 48c893f74fc65d790e491a15bad25af1ef15f000

Fix free_sdp() : don't try to free part of a sip_msg structure (bug introduced by commit bde8c0a045 )

parser/sdp/sdp.c
@@ -752,7 +752,6 @@ int parse_sdp(struct sip_msg* _m)
 void free_sdp(sdp_info_t** sdp)
 {
 	__free_sdp(*sdp);
-	pkg_free(sdp);
 	*sdp = NULL;
 }
 