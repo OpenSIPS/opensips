f153a9adfddfd2b6ae0751d54111f3f97c41b4b9
Bogdan-Andrei Iancu <bogdan@opensips.org>
2013-08-26 19:33:37 +0300

    - BYE requests received in confirmed state should terminate the dialog only if they match the TO-tag from 200 OK INVITE.   This affects parallel forking scenarios where rejected branches are terminated via BYE by caller.   Thanks for reporting and testing to Marco Hierl.

--- modules/dialog/dlg_hash.c
+++ modules/dialog/dlg_hash.c
@@ -856,6 +856,10 @@ void next_state_dlg(struct dlg_cell *dlg, int event,
 			switch (dlg->state) {
 				case DLG_STATE_CONFIRMED_NA:
 				case DLG_STATE_CONFIRMED:
+					if ( last_dst_leg!=dlg->legs_no[DLG_LEG_200OK] )
+						/* to end the call, the BYE must be received 
+						 * on the same leg as the 200 OK for INVITE */
+						break;
 					dlg->flags |= DLG_FLAG_HASBYE;
 					dlg->state = DLG_STATE_DELETED;
 					*unref = 1; /* unref from hash -> dialog ended */
