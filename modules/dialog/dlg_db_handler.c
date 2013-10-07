ec0d05aaef41b0a4f30419bbab1295eb4bce7e49
Vlad Paiu <vladpaiu@opensips.org>
2013-10-07 13:40:21 +0300

    Fixed the re-loading of dlg vars after restart Credits to Nick Altmann

--- modules/dialog/dlg_db_handler.c
+++ modules/dialog/dlg_db_handler.c
@@ -621,7 +621,7 @@ static int load_dialog_info_from_db(int dlg_hash_size)
 			/* script variables */
 			if (!VAL_NULL(values+17))
 				read_dialog_vars( VAL_STR(values+17).s,
-					VAL_STR(values+17).len, dlg);
+					strlen(VAL_STR(values+17).s), dlg);
 
 			/* profiles */
 			if (!VAL_NULL(values+18))
