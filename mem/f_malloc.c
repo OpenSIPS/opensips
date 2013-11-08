5852895cb4bdc9c53369ddc231546753de9e7d49
Razvan Crainea <razvan@opensips.org>
2013-11-08 15:18:57 +0100

    when joining two frags, only the used memory changes

--- mem/f_malloc.c
+++ mem/f_malloc.c
@@ -355,7 +355,8 @@ void* fm_malloc(struct fm_block* qm, unsigned long size)
 				frag->size += n->size + FRAG_OVERHEAD;
 
 				#if defined(DBG_F_MALLOC) || defined(STATISTICS)
-				qm->real_used -= FRAG_OVERHEAD;
+				//qm->real_used -= FRAG_OVERHEAD;
+				qm->used += FRAG_OVERHEAD;
 				#endif
 
 				if( frag->size >size )
@@ -457,7 +458,8 @@ join:
 		f->size += n->size + FRAG_OVERHEAD;
 
 		#if defined(DBG_F_MALLOC) || defined(STATISTICS)
-		qm->real_used -= FRAG_OVERHEAD;
+		//qm->real_used -= FRAG_OVERHEAD;
+		qm->used += FRAG_OVERHEAD;
 		#endif
 
 		goto join;
@@ -542,7 +544,8 @@ void* fm_realloc(struct fm_block* qm, void* p, unsigned long size)
 			f->size += n->size + FRAG_OVERHEAD;
 
 			#if defined(DBG_F_MALLOC) || defined(STATISTICS)
-			qm->real_used -= FRAG_OVERHEAD;
+			//qm->real_used -= FRAG_OVERHEAD;
+			qm->used += FRAG_OVERHEAD;
 			#endif
 
 			/* split it if necessary */
