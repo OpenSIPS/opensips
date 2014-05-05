f1fec17d012a54790ce9be47d20cd52daf5a1cbe
Vlad Paiu <vladpaiu@opensips.org>
2014-05-05 14:35:36 +0300

    Fixed the connection handling - some modules would want to run cachedb type ops in the mod init Fixed the DB schema & increased version - the keyname must be a primary key for the counter ops to properly work

--- scripts/postgres/cachedb_sql-create.sql
+++ scripts/postgres/cachedb_sql-create.sql
@@ -1,6 +1,6 @@
-INSERT INTO version (table_name, table_version) values ('cachedb','1');
+INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
-    keyname VARCHAR(255) NOT NULL,
+    keyname VARCHAR(255) PRIMARY KEY NOT NULL,
     value TEXT NOT NULL,
     counter INTEGER DEFAULT 0 NOT NULL,
     expires INTEGER DEFAULT 0 NOT NULL
