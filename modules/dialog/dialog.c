7b4a00d011f398b21156d4866d918e41cc87e1bb
Vlad Paiu <vladpaiu@opensips.org>
2014-05-07 23:33:23 +0300

    When loading new dialogs at runtime ( either via dlg_db_sync or via the bin interface ), also run the load callback ( eg. used by the acc module to write proper CDRs ) (cherry picked from commit afee8fe8aaa3d8bc5bec54868404e6a4ab49fa3d)

--- modules/dialog/dialog.c
+++ modules/dialog/dialog.c
@@ -830,7 +830,9 @@ static int mod_init(void)
 
 	/* if profiles should be kept in cachedb's */
 
+	/* do not destroy callbacks - will need them later for syncing new dialogs
 	destroy_dlg_callbacks( DLGCB_LOADED );
+	*/
 	destroy_cachedb(0);
 
 	return 0;
