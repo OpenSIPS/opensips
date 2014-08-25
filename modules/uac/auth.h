6dea2eb9dc02481be69e6c3675c77b21e0489b03
Vlad Paiu <vladpaiu@opensips.org>
2014-08-25 17:37:31 +0300

    Updated License

--- modules/uac/auth.h
+++ modules/uac/auth.h
@@ -1,16 +1,14 @@
 /*
- * $Id$
- *
- * Copyright (C) 2005-2009 Voice Sistem SRL
+ * Copyright (C) 2014 OpenSIPS Solutions
  *
  * This file is part of opensips, a free SIP server.
  *
- * UAC OpenSIPS-module is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * UAC OpenSIPS-module is distributed in the hope that it will be useful,
+ * opensips is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -20,8 +18,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  *
- * History:
+ * history:
  * ---------
+ *  2014-08-xx  created (vlad-paiu)
  */
 
 #ifndef _UAC_UAC_AUTH_H_
@@ -31,6 +30,5 @@
 #include "../rr/api.h"
 
 void rr_uac_auth_checker(struct sip_msg *msg, str *r_param, void *cb_param);
-int uac_fix_avp_definitions(void);
 
 #endif
