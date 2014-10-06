66799186fb0e04f54afae99cd13a425c8b80eb3a
Bogdan-Andrei Iancu <bogdan@opensips.org>
2014-10-06 13:41:59 +0300

    Fix how the contact URI is build.
    Do not add transport info to URI when storing it, but rather when using it - avoid double "transport" param.
    
    (cherry picked from commit bef1f938b8e6479b563c1164e0110c76753b7b02)

--- modules/presence/utils_func.h
+++ modules/presence/utils_func.h
@@ -71,31 +71,16 @@ static inline int uandd_to_uri(str user,  str domain, str *out)
 	return 0;
 }
 
+/* Build an contact URI but without the "transport" param - this is to be
+ * added when a send is done, depending on the used interface.
+ */
 static inline int get_local_contact(struct socket_info *sock, str* contact)
 {
 	static char buf[LCONTACT_BUF_SIZE];
-	char* proto;
-	int proto_len;
 
 	contact->s = buf;
 	contact->len= 0;
 	memset(buf, 0, LCONTACT_BUF_SIZE);
-
-	switch (sock->proto)
-	{
-		case PROTO_NONE:
-		case PROTO_UDP:
-			proto = NULL; proto_len=0; break;
-		case PROTO_TCP:
-			proto= "tcp"; proto_len=3; break;
-		case PROTO_TLS:
-			proto= "tls"; proto_len=3; break;
-		case PROTO_SCTP:
-			proto= "sctp";proto_len=4; break;
-		default:
-			LM_CRIT("unsupported %d proto\n",sock->proto);
-			return -1;
-	}
 
 	/* write "sip:ip" */
 	memcpy(contact->s+contact->len, "sip:", 4);
@@ -129,14 +114,6 @@ static inline int get_local_contact(struct socket_info *sock, str* contact)
 		*(contact->s+(contact->len++)) = ':';
 		memcpy(contact->s+contact->len, sock->port_no_str.s, sock->port_no_str.len);
 		contact->len += sock->port_no_str.len;
-	}
-
-	/* write ";transport=" if non-UDP */
-	if (proto) {
-		memcpy(contact->s+contact->len, ";transport=", 11);
-		contact->len += 11;
-		memcpy(contact->s+contact->len, proto, proto_len);
-		contact->len += proto_len;
 	}
 
 	return 0;
