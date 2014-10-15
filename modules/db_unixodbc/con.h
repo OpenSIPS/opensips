e0c1fea5145906a25ca89fc49d3c361543972410
Liviu Chircu <liviu@opensips.org>
2014-10-15 18:57:17 +0300

    db_unixodbc: fix query result handling and improve performance
    Until now, query rows were incrementally stored in a list with
    separately allocated length and string buffers, along with a great deal
    of useless pointer dereferencing, effectively wasting CPU cycles and pkg
    mem usage.
    
    This patch eliminates the linked list logic with a global buffer of
    pointers (doubling in size when required) meant to hold each result column.
    Each column is transparently freed by the DB backend. This completes
    commit d63ee19a8.
    (cherry picked from commit 683dfaa63df0af317f7485042539bdacf8646eab)

--- modules/db_unixodbc/con.h
+++ modules/db_unixodbc/con.h
@@ -60,7 +60,6 @@ struct my_con
 	SQLHSTMT stmt_handle;			  /* Actual result */
 	SQLHDBC dbc;					  /* Connection representation */
 	char** row;						  /* Actual row in the result */
-	void *data;
 	time_t timestamp;				  /* Timestamp of last query */
 };
 
@@ -73,7 +72,6 @@ struct my_con
 #define CON_TIMESTAMP(db_con)  (((struct my_con*)((db_con)->tail))->timestamp)
 #define CON_ID(db_con) 		(((struct my_con*)((db_con)->tail))->id)
 #define CON_ENV(db_con)		(((struct my_con*)((db_con)->tail))->env)
-#define CON_DATA(db_con)	(((struct my_con*)((db_con)->tail))->data)
 
 #define MAX_CONN_STR_LEN 2048
 
