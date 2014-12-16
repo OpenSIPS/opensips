4500848d6371a1c53f3ccb6bba92239a7710cdb7
davesidwell <davesidwell@users.noreply.github.com>
2014-12-16 10:32:15 -0500

    Update rtpproxy.c
    When moving stored parameters from Branch AVP to Dialog variable, make sure that the integer value "setid" is handled correctly so that it remains valid for the remainder of the dialog.

--- modules/rtpproxy/rtpproxy.c
+++ modules/rtpproxy/rtpproxy.c
@@ -2782,7 +2782,11 @@ static int move_bavp2dlg(struct sip_msg *msg, struct dlg_cell *dlg, str *rval1, 
 		}
 
 		if (set_found) {
-			if (dlg_api.store_dlg_value(dlg, &param3_name, &val3.rs) < 0) {
+			/* Store Set ID INT value correcty in dlg */
+			str param3_val;
+			param3_val.s = (char*)&val3.ri;
+			param3_val.len = sizeof(unsigned int);
+			if (dlg_api.store_dlg_value(dlg, &param3_name, &param3_val) < 0) {
 				LM_ERR("cannot store setid value\n");
 				goto error;
 			}
