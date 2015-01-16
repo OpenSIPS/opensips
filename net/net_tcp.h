390cf7927723e7da3d9eb2e948d7de31aaf6e3cc
Razvan Crainea <razvan@opensips.org>
2015-01-16 18:36:52 +0200

    Add network API headers

---
+++ net/net_tcp.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2015 OpenSIPS Project
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * history:
+ * ---------
+ *  2015-01-xx  created (razvanc)
+ */
+
+#ifndef _NET_TCP_H_
+#define _NET_TCP_H_
+
+/* XXX: dummy placeholder until the connection is defined */
+typedef void struct connection;
+
+// initializes the TCP structures
+int tcp_init(void);
+
+// destroys the TCP data
+void tcp_destroy(void);
+
+// looks for the transport protocol that knows how to handle the
+// proto and calls the corresponding add_listener() function
+int tcp_add_listener(char* host, int port, int type, void *ctx);
+
+// used to return a listener
+struct socket_info* tcp_find_listener(union sockaddr_union* to, int proto);
+
+// initializes the TCP listeners
+int tcp_init_listeners(void);
+
+// returns the connection identified by either the id or the destination to
+struct connection* tcp_conn_get(int id, union sockaddr_union* to);
+
+// used to tune the connection attributes
+int tcp_conn_fcntl(struct connection *conn, int attr, void *value);
+
+#endif /* _NET_TCP_H_ */
---
+++ net/net_udp.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2015 OpenSIPS Project
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * history:
+ * ---------
+ *  2015-01-xx  created (razvanc)
+ */
+
+#ifndef _NET_UDP_H_
+#define _NET_UDP_H_
+
+// initializes the udp structures
+int udp_init(void);
+
+// destroys the udp data
+void udp_destroy(void);
+
+// looks for the transport protocol that knows how to handle the
+// proto and calls the corresponding add_listener() function
+int udp_add_listener(char* host, int port, int type, void *ctx);
+
+// used to return a listener
+struct socket_info* udp_find_listener(union sockaddr_union* to, int proto);
+
+// initializes the udp listeners
+int udp_init_listeners(void);
+
+#endif /* _NET_UDP_H_ */
