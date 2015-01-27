378dae93313565798b7b47d7e417d4e2acadf665
Bogdan-Andrei Iancu <bogdan@opensips.org>
2015-01-27 18:53:43 +0200

    Fix missing dlg reference when copying from TM.

--- modules/dialog/dialog.c
+++ modules/dialog/dialog.c
@@ -822,7 +822,7 @@ static int mod_init(void)
 		return -1;
 	}
 
-	if (register_script_cb( dialog_cleanup, POST_SCRIPT_CB|REQ_TYPE_CB,0)<0) {
+	if (register_script_cb( dialog_cleanup, POST_SCRIPT_CB|REQ_TYPE_CB|RPL_TYPE_CB,0)<0) {
 		LM_ERR("cannot register script callback");
 		return -1;
 	}
