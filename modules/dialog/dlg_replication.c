85d4820eb04c922731156156329dae37e26ec804
Vlad Paiu <vladpaiu@opensips.org>
2015-02-12 10:26:46 +0000

    Fix dialog race condition on BIN replication
    The writing of dialog profiles needs to be done under the dialog lock ( in case there's a race condition with the BYE freeing the profile linkers )

--- modules/dialog/dlg_replication.c
+++ modules/dialog/dlg_replication.c
@@ -478,7 +478,9 @@ void replicate_dialog_created(struct dlg_cell *dlg)
 
 	/* XXX: on shutdown only? */
 	vars = write_dialog_vars(dlg->vals);
+	dlg_lock_dlg(dlg);
 	profiles = write_dialog_profiles(dlg->profile_links);
+	dlg_unlock_dlg(dlg);
 
 	bin_push_str(vars);
 	bin_push_str(profiles);
@@ -543,7 +545,9 @@ void replicate_dialog_updated(struct dlg_cell *dlg)
 
 	/* XXX: on shutdown only? */
 	vars = write_dialog_vars(dlg->vals);
+	dlg_lock_dlg(dlg);
 	profiles = write_dialog_profiles(dlg->profile_links);
+	dlg_unlock_dlg(dlg);
 
 	bin_push_str(vars);
 	bin_push_str(profiles);
