31ff8e5fa4ba6111aed6a97eca57c0a4e3b9d828
Bogdan-Andrei Iancu <bogdan@opensips.org>
2015-02-17 00:50:52 +0200

    One more split in defs and funcs hdr files.

--- net/proto_tcp/tcp_common.h
+++ net/proto_tcp/tcp_common.h
@@ -30,41 +30,7 @@
 #include "../../pt.h"
 #include "../../receive.h"
 
-
-/*!< the max number of chunks that a child accepts until the message
- * is read completely - anything above will lead to the connection being
- * closed - considered an attack */
-#define TCP_CHILD_MAX_MSG_CHUNK  4
-
-#define TCP_BUF_SIZE 65535			/*!< TCP buffer size */
-
-
-enum tcp_req_errors {	TCP_REQ_INIT, TCP_REQ_OK, TCP_READ_ERROR,
-		TCP_REQ_OVERRUN, TCP_REQ_BAD_LEN };
-enum tcp_req_states {	H_SKIP_EMPTY, H_SKIP, H_LF, H_LFCR,  H_BODY, H_STARTWS,
-		H_CONT_LEN1, H_CONT_LEN2, H_CONT_LEN3, H_CONT_LEN4, H_CONT_LEN5,
-		H_CONT_LEN6, H_CONT_LEN7, H_CONT_LEN8, H_CONT_LEN9, H_CONT_LEN10,
-		H_CONT_LEN11, H_CONT_LEN12, H_CONT_LEN13, H_L_COLON,
-		H_CONT_LEN_BODY, H_CONT_LEN_BODY_PARSE , H_PING_CRLFCRLF,
-		H_SKIP_EMPTY_CR_FOUND, H_SKIP_EMPTY_CRLF_FOUND,
-		H_SKIP_EMPTY_CRLFCR_FOUND
-	};
-
-
-struct tcp_req{
-	/* sockaddr ? */
-	char buf[TCP_BUF_SIZE+1];		/*!< bytes read so far (+0-terminator)*/
-	char* start;					/*!< where the message starts, after all the empty lines are skipped*/
-	char* pos;						/*!< current position in buf */
-	char* parsed;					/*!< last parsed position */
-	char* body;						/*!< body position */
-	unsigned int   content_len;
-	unsigned short has_content_len;	/*!< 1 if content_length was parsed ok*/
-	unsigned short complete;		/*!< 1 if one req has been fully read, 0 otherwise*/
-	unsigned int   bytes_to_go;		/*!< how many bytes we have still to read from the body*/
-	enum tcp_req_errors error;
-	enum tcp_req_states state;
-};
+#include "tcp_common_defs.h"
 
 
 /*! \brief
