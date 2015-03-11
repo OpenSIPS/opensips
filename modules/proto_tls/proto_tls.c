55d835512be077503a18d8b497d308c92361e414
ionutrazvanionita <ionutionita@opensips.org>
2015-03-11 15:43:18 +0200

    merged tlsops module in proto_tls

--- modules/proto_tls/proto_tls.c
+++ modules/proto_tls/proto_tls.c
@@ -44,12 +44,18 @@
 #include "../../tsend.h"
 #include "../../receive.h"
 #include "../../pt.h"
+#include "../../parser/msg_parser.h"
+#include "../../pvar.h"
 
 #include "../../net/proto_tcp/tcp_common_defs.h"
 #include "tls_config.h"
 #include "tls_domain.h"
 #include "tls_server.h"
 #include "tls_params.h"
+#include "tls_select.h"
+
+/* definition of exported functions */
+static int is_peer_verified(struct sip_msg*, char*, char*);
 
 static int tls_port_no = SIPS_PORT;
 static char *tls_domain_avp = NULL;
@@ -81,6 +87,8 @@ static void tls_conn_clean(struct tcp_connection* c);
 
 static cmd_export_t cmds[] = {
 	{"proto_init", (cmd_function)proto_tls_init, 0, 0, 0, 0},
+	{"is_peer_verified", (cmd_function)is_peer_verified,   0, 0, 0,
+			REQUEST_ROUTE},
 	{0,0,0,0,0,0}
 };
 
@@ -106,6 +114,172 @@ static param_export_t params[] = {
 };
 
 
+/*
+ *  pseudo variables
+ */
+static pv_export_t mod_items[] = {
+	/* TLS session parameters */
+	{{"tls_version", sizeof("tls_version")-1},
+		850, tlsops_version, 0,
+		0, 0, 0, 0 },
+	{{"tls_description", sizeof("tls_description")-1},
+		850, tlsops_desc, 0,
+		0, 0, 0, 0 },
+	{{"tls_cipher_info", sizeof("tls_cipher_info")-1},
+		850, tlsops_cipher, 0,
+		0, 0, 0, 0 },
+	{{"tls_cipher_bits", sizeof("tls_cipher_bits")-1},
+		850,  tlsops_bits, 0,
+		0, 0, 0, 0 },
+	/* general certificate parameters for peer and local */
+	{{"tls_peer_version", sizeof("tls_peer_version")-1},
+		850, tlsops_cert_version, 0,
+		0, 0, pv_init_iname, CERT_PEER  },
+	{{"tls_my_version", sizeof("tls_my_version")-1},
+		850, tlsops_cert_version, 0,
+		0, 0, pv_init_iname, CERT_LOCAL },
+	{{"tls_peer_serial", sizeof("tls_peer_serial")-1},
+		850, tlsops_sn, 0,
+		0, 0, pv_init_iname, CERT_PEER  },
+	{{"tls_my_serial", sizeof("tls_my_serial")-1},
+		850, tlsops_sn,0,
+		0, 0, pv_init_iname, CERT_LOCAL },
+	/* certificate parameters for peer and local, for subject and issuer*/
+	{{"tls_peer_subject", sizeof("tls_peer_subject")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_SUBJECT },
+	{{"tls_peer_issuer", sizeof("tls_peer_issuer")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_ISSUER  },
+	{{"tls_my_subject", sizeof("tls_my_subject")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_SUBJECT },
+	{{"tls_my_issuer", sizeof("tls_my_issuer")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_ISSUER  },
+	{{"tls_peer_subject_cn", sizeof("tls_peer_subject_cn")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_SUBJECT | COMP_CN },
+	{{"tls_peer_issuer_cn", sizeof("tls_peer_issuer_cn")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_ISSUER  | COMP_CN },
+	{{"tls_my_subject_cn", sizeof("tls_my_subject_cn")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_SUBJECT | COMP_CN },
+	{{"tls_my_issuer_cn", sizeof("tls_my_issuer_cn")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_ISSUER  | COMP_CN },
+	{{"tls_peer_subject_locality", sizeof("tls_peer_subject_locality")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_SUBJECT | COMP_L },
+	{{"tls_peer_issuer_locality", sizeof("tls_peer_issuer_locality")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_ISSUER  | COMP_L },
+	{{"tls_my_subject_locality", sizeof("tls_my_subject_locality")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_SUBJECT | COMP_L },
+	{{"tls_my_issuer_locality", sizeof("tls_my_issuer_locality")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_ISSUER  | COMP_L },
+	{{"tls_peer_subject_country", sizeof("tls_peer_subject_country")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_SUBJECT | COMP_C },
+	{{"tls_peer_issuer_country", sizeof("tls_peer_issuer_country")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_ISSUER  | COMP_C },
+	{{"tls_my_subject_country", sizeof("tls_my_subject_country")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_SUBJECT | COMP_C },
+	{{"tls_my_issuer_country", sizeof("tls_my_issuer_country")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_ISSUER  | COMP_C },
+	{{"tls_peer_subject_state", sizeof("tls_peer_subject_state")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_SUBJECT | COMP_ST },
+	{{"tls_peer_issuer_state", sizeof("tls_peer_issuer_state")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_ISSUER  | COMP_ST },
+	{{"tls_my_subject_state", sizeof("tls_my_subject_state")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_SUBJECT | COMP_ST },
+	{{"tls_my_issuer_state", sizeof("tls_my_issuer_state")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_ISSUER  | COMP_ST },
+	{{"tls_peer_subject_organization", sizeof("tls_peer_subject_organization")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_SUBJECT | COMP_O },
+	{{"tls_peer_issuer_organization", sizeof("tls_peer_issuer_organization")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_ISSUER  | COMP_O },
+	{{"tls_my_subject_organization", sizeof("tls_my_subject_organization")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_SUBJECT | COMP_O },
+	{{"tls_my_issuer_organization", sizeof("tls_my_issuer_organization")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_ISSUER  | COMP_O },
+	{{"tls_peer_subject_unit", sizeof("tls_peer_subject_unit")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_SUBJECT | COMP_OU },
+	{{"tls_peer_issuer_unit", sizeof("tls_peer_issuer_unit")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_PEER  | CERT_ISSUER  | COMP_OU },
+	{{"tls_my_subject_unit", sizeof("tls_my_subject_unit")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_SUBJECT | COMP_OU },
+	{{"tls_my_issuer_unit", sizeof("tls_my_issuer_unit")-1},
+		850, tlsops_comp, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | CERT_ISSUER  | COMP_OU },
+	/* subject alternative name parameters for peer and local */
+	{{"tls_peer_san_email", sizeof("tls_peer_san_email")-1},
+		850, tlsops_alt, 0,
+		0, 0, pv_init_iname, CERT_PEER  | COMP_E },
+	{{"tls_my_san_email", sizeof("tls_my_san_email")-1},
+		850, tlsops_alt, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | COMP_E },
+	{{"tls_peer_san_hostname", sizeof("tls_peer_san_hostname")-1},
+		850, tlsops_alt, 0,
+		0, 0, pv_init_iname, CERT_PEER  | COMP_HOST },
+	{{"tls_my_san_hostname", sizeof("tls_my_san_hostname")-1},
+		850, tlsops_alt, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | COMP_HOST },
+	{{"tls_peer_san_uri", sizeof("tls_peer_san_uri")-1},
+		850, tlsops_alt, 0,
+		0, 0, pv_init_iname, CERT_PEER  | COMP_URI },
+	{{"tls_my_san_uri", sizeof("tls_my_san_uri")-1},
+		850, tlsops_alt, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | COMP_URI },
+	{{"tls_peer_san_ip", sizeof("tls_peer_san_ip")-1},
+		850, tlsops_alt, 0,
+		0, 0, pv_init_iname, CERT_PEER  | COMP_IP },
+	{{"tls_my_san_ip", sizeof("tls_my_san_ip")-1},
+		850, tlsops_alt, 0,
+		0, 0, pv_init_iname, CERT_LOCAL | COMP_IP },
+	/* peer certificate validation parameters */
+	{{"tls_peer_verified", sizeof("tls_peer_verified")-1},
+		850, tlsops_check_cert, 0,
+		0, 0, pv_init_iname, CERT_VERIFIED },
+	{{"tls_peer_revoked", sizeof("tls_peer_revoked")-1},
+		850, tlsops_check_cert, 0,
+		0, 0, pv_init_iname, CERT_REVOKED },
+	{{"tls_peer_expired", sizeof("tls_peer_expired")-1},
+		850, tlsops_check_cert, 0,
+		0, 0, pv_init_iname, CERT_EXPIRED },
+	{{"tls_peer_selfsigned", sizeof("tls_peer_selfsigned")-1},
+		850, tlsops_check_cert, 0,
+		0, 0, pv_init_iname, CERT_SELFSIGNED },
+	{{"tls_peer_notBefore", sizeof("tls_peer_notBefore")-1},
+		850, tlsops_validity, 0,
+		0, 0, pv_init_iname, CERT_NOTBEFORE },
+	{{"tls_peer_notAfter", sizeof("tls_peer_notAfter")-1},
+		850, tlsops_validity, 0,
+		0, 0, pv_init_iname, CERT_NOTAFTER },
+
+	{ {0, 0}, 0, 0, 0, 0, 0, 0, 0 }
+
+};
+
+
+
 struct module_exports exports = {
 	PROTO_PREFIX "tls",  /* module name*/
 	MOD_TYPE_DEFAULT,    /* class of this module */
@@ -117,7 +291,7 @@ struct module_exports exports = {
 	params,     /* module parameters */
 	0,          /* exported statistics */
 	0,          /* exported MI functions */
-	0,          /* exported pseudo-variables */
+	mod_items,          /* exported pseudo-variables */
 	0,          /* extra processes */
 	mod_init,   /* module initialization function */
 	0,          /* response function */
@@ -1315,3 +1489,68 @@ error:
 	return -1;
 }
 
+
+
+
+static int is_peer_verified(struct sip_msg* msg, char* foo, char* foo2)
+{
+	struct tcp_connection *c;
+	SSL *ssl;
+	long ssl_verify;
+	X509 *x509_cert;
+
+	LM_DBG("started...\n");
+	if (msg->rcv.proto != PROTO_TLS) {
+		LM_ERR("proto != TLS --> peer can't be verified, return -1\n");
+		return -1;
+	}
+
+	LM_DBG("trying to find TCP connection of received message...\n");
+	/* what if we have multiple connections to the same remote socket? e.g. we can have
+	     connection 1: localIP1:localPort1 <--> remoteIP:remotePort
+	     connection 2: localIP2:localPort2 <--> remoteIP:remotePort
+	   but I think the is very unrealistic */
+	tcp_conn_get(0, &(msg->rcv.src_ip), msg->rcv.src_port, &c, NULL/*fd*/);
+	if (c==NULL) {
+		LM_ERR("no corresponding TLS/TCP connection found."
+				" This should not happen... return -1\n");
+		return -1;
+	}
+	LM_DBG("corresponding TLS/TCP connection found. s=%d, fd=%d, id=%d\n",
+			c->s, c->fd, c->id);
+
+	if (!c->extra_data) {
+		LM_ERR("no extra_data specified in TLS/TCP connection found."
+				" This should not happen... return -1\n");
+		goto error;
+	}
+
+	ssl = (SSL *) c->extra_data;
+
+	ssl_verify = SSL_get_verify_result(ssl);
+	if ( ssl_verify != X509_V_OK ) {
+		LM_WARN("verification of presented certificate failed... return -1\n");
+		goto error;
+	}
+
+	/* now, we have only valid peer certificates or peers without certificates.
+	 * Thus we have to check for the existence of a peer certificate
+	 */
+	x509_cert = SSL_get_peer_certificate(ssl);
+	if ( x509_cert == NULL ) {
+		LM_WARN("tlsops:is_peer_verified: WARNING: peer did not presented "
+			"a certificate. Thus it could not be verified... return -1\n");
+		goto error;
+	}
+
+	X509_free(x509_cert);
+
+	tcp_conn_release(c, 0);
+
+	LM_DBG("tlsops:is_peer_verified: peer is successfuly verified"
+		"...done\n");
+	return 1;
+error:
+	tcp_conn_release(c, 0);
+	return -1;
+}
