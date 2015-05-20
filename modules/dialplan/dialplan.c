52554da7698af974a721525d8c64c1943f07a4ab
ionutrazvanionita <ionutionita@opensips.org>
2015-05-20 15:30:27 +0300

    move db data initialization from mi_child_init to child init
    (cherry picked from commit fa14431830bde1c8ede30bf77a609197e7d002fa)

--- modules/dialplan/dialplan.c
+++ modules/dialplan/dialplan.c
@@ -61,7 +61,6 @@
 static int mod_init(void);
 static int child_init(int rank);
 static void mod_destroy();
-static int mi_child_init();
 
 static struct mi_root * mi_reload_rules(struct mi_root *cmd_tree,void *param);
 static struct mi_root * mi_translate(struct mi_root *cmd_tree, void *param);
@@ -94,7 +93,7 @@ static param_export_t mod_params[]={
 };
 
 static mi_export_t mi_cmds[] = {
-	{ "dp_reload",  0, mi_reload_rules,   0,       0,  mi_child_init},
+	{ "dp_reload",  0, mi_reload_rules,   0,       0,  0},
 	{ "dp_translate",  0, mi_translate,   0,       0,              0},
 	{ 0, 0, 0, 0, 0, 0}
 };
@@ -519,6 +518,22 @@ static int mod_init(void)
 
 static int child_init(int rank)
 {
+	dp_connection_list_p el;
+
+	/* only process with rank 0 loads data */
+	if (rank != 1)
+		return 0;
+
+	/*Connect to DB s and get rules*/
+	for(el = dp_conns; el; el = el->next){
+		if (init_db_data(el) != 0) {
+			LM_ERR("Unable to init db data\n");
+			shm_free(el);
+			return -1;
+		}
+	}
+
+
 	return 0;
 }
 
@@ -542,24 +557,6 @@ static void mod_destroy(void)
 
 
 	destroy_data();
-}
-
-
-static int mi_child_init(void)
-{
-
-	dp_connection_list_p el;
-
-	/*Connect to DB s and get rules*/
-	for(el = dp_conns; el; el = el->next){
-		if (init_db_data(el) != 0) {
-			LM_ERR("Unable to init db data\n");
-			shm_free(el);
-			return -1;
-		}
-	}
-
-	return 0;
 }
 
 
