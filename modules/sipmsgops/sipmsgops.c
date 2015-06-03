31ddc4942d624f4d4cb559c0daa078fcebe527fe
Bogdan-Andrei Iancu <bogdan@opensips.org>
2015-06-03 19:37:19 +0300

    Fix parsing for Diversion header.
    As Diversion header may be a multi-body header, do not use the parse_to() function as it does not accept multiple bodies. Use parse_rr() function.
    Reported by Nick Altmann.
    
    (cherry picked from commit 36384927a5d2fd1d900ac36adc2ee15b2acad0c9)

--- modules/sipmsgops/sipmsgops.c
+++ modules/sipmsgops/sipmsgops.c
@@ -1588,7 +1588,6 @@ static int sip_validate_hdrs(struct sip_msg *msg)
 			case HDR_PAI_T:
 			case HDR_RPID_T:
 			case HDR_REFER_TO_T:
-			case HDR_DIVERSION_T:
 				/* these are similar */
 				if (!(to = pkg_malloc(sizeof(struct to_body)))) {
 					LM_ERR("out of pkg_memory\n");
@@ -1635,6 +1634,7 @@ static int sip_validate_hdrs(struct sip_msg *msg)
 			case HDR_PATH_T:
 			case HDR_ROUTE_T:
 			case HDR_RECORDROUTE_T:
+			case HDR_DIVERSION_T:
 				CHECK_HDR_FUNC(parse_rr, hf);
 				break;
 
