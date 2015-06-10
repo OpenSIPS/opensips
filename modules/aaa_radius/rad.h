769cae813ca8b1f523d5b3c4b1cfb5541d8b8ed8
ionutrazvanionita <ionutionita@opensips.org>
2015-06-10 11:21:36 +0300

    async support for radius module

--- modules/aaa_radius/rad.h
+++ modules/aaa_radius/rad.h
@@ -29,6 +29,12 @@
 
 #include "../../aaa/aaa.h"
 
+struct rad_ctx {
+	int index2;
+	VALUE_PAIR *send;
+	SEND_CONTEXT *ctx;
+};
+
 aaa_conn* rad_init_prot(str* aaa_url);
 
 aaa_message* rad_create_message(aaa_conn* rh, int flag);
