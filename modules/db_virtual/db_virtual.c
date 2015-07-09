576fada75152f3d708d074e4ae0dafa41cececb3
Liviu Chircu <liviu@opensips.org>
2015-07-09 15:10:13 +0300

    db_virtual: Do not crash if no DB URLs are defined

--- modules/db_virtual/db_virtual.c
+++ modules/db_virtual/db_virtual.c
@@ -260,8 +260,13 @@ int init_global(void){//str *info_set_mapping){
                 add_url(count, s);
             }
         }
-
     }
+
+	if (!global) {
+		LM_ERR("db_virtual module cannot start with no DB URLs defined!\n");
+		return -1;
+	}
+
     for(i = 0; i< global->size; i++)
         for(j=0; j<global->set_list[i].size; j++){
 
@@ -351,9 +356,8 @@ int virtual_mod_init(void){
 
         if(!global){
             int i,j;
-            int rc;
-            rc = init_global();
-            rc |= init_private_handles();
+            if (init_global() || init_private_handles())
+				return -1;
 
             //print structure
             for(i = 0; i< global->size; i++){
@@ -374,8 +378,6 @@ int virtual_mod_init(void){
                     LM_ERR("failed to register keepalive timer\n");
                 }
             }
-
-            return rc;
         }
 
         return 0;
