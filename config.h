a2e73ba9741c6304625e4120b30f45fce48356a4
Razvan Crainea <razvan@opensips.org>
2015-09-02 15:17:31 +0300

    fix WS SRV scheme according to the draft

--- config.h
+++ config.h
@@ -115,7 +115,7 @@
 #define SRV_TLS_PREFIX "_sips._tcp."
 #define SRV_TLS_PREFIX_LEN (sizeof(SRV_TLS_PREFIX) - 1)
 
-#define SRV_WS_PREFIX "_sip._ws."
+#define SRV_WS_PREFIX "_ws._tcp."
 #define SRV_WS_PREFIX_LEN (sizeof(SRV_WS_PREFIX) - 1)
 
 #define SRV_MAX_PREFIX_LEN SRV_TLS_PREFIX_LEN
