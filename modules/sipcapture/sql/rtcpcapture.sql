acd0a9c26477fb21a5f969c3a5d3b55e71bc7819
ionutrazvanionita <ionutionita@opensips.org>
2016-04-18 19:03:11 +0300

    [sipcapture][HOMERv5 incompatibility fix]generic capturing function
          Added new function called report_capture which allows
    capturing hep packets in a generic format. It will be very
    effective for the third version of the hep protocol for capturing
    packets other than SIP. Also added a new table called rtcp_capture
    for storing everything capture with report capture.
    
    (cherry picked from commit c5700cce4a37e484869941502e495733b8138b98)

---
+++ modules/sipcapture/sql/rtcpcapture.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS `rtcp_capture` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `micro_ts` bigint(18) NOT NULL DEFAULT '0',
+  `correlation_id` varchar(256) NOT NULL DEFAULT '',
+  `source_ip` varchar(60) NOT NULL DEFAULT '',
+  `source_port` int(10) NOT NULL DEFAULT 0,
+  `destination_ip` varchar(60) NOT NULL DEFAULT '',
+  `destination_port` int(10) NOT NULL DEFAULT 0,
+  `proto` int(5) NOT NULL DEFAULT 0,
+  `family` int(1) DEFAULT NULL,
+  `type` int(2) NOT NULL DEFAULT 0,
+  `node` varchar(125) NOT NULL DEFAULT '',
+  `msg` varchar(1500) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`,`date`),
+  KEY `date` (`date`),
+  KEY `correlationid` (`correlation_id`(255))
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
