ccf58c432aec2fa7b31ee82f45ae1d975f644217
Bogdan-Andrei Iancu <bogdan@opensips.org>
2016-05-19 18:22:41 +0300

    Fix bogus timer ref counter during expire update.
    Many thanks to Trevor Francis for reporting and helping with troubleshooting and testing.
    
    (cherry picked from commit 047b1b3e32a9af30dd44f2487460975ed7b3a815)

--- modules/dialog/dialog.c
+++ modules/dialog/dialog.c
@@ -1591,10 +1591,20 @@ int pv_set_dlg_timeout(struct sip_msg *msg, pv_param_t *param,
 		dlg_tmp_timeout = -1;
 		dlg_tmp_timeout_id = -1;
 
-		if (timer_update && update_dlg_timer(&dlg->tl, timeout) < 0) {
-			LM_ERR("failed to update timer\n");
-			return -1;
+		if (timer_update) {
+			switch ( update_dlg_timer(&dlg->tl, timeout) ) {
+			case -1:
+				LM_ERR("failed to update timer\n");
+				return -1;
+			case 1:
+				/* dlg inserted in timer list with new expire (reference it)*/
+				ref_dlg(dlg,1);
+			case 0:
+				/* timeout value was updated */
+				break;
+			}
 		}
+
 	} else {
 		/* store it until we match the dialog */
 		dlg_tmp_timeout = timeout;
