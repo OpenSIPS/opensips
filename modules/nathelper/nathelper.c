8ea0ab181e616a017fb66ba0854fbbda034f9ce7
Bogdan-Andrei Iancu <bogdan@opensips.org>
2016-06-16 16:29:55 +0300

    Fixed failure to enable NAT pinging.
    Commit 8f0c677 introduces a bug in testing is usrloc domains are available, and if not, to disable the NAT pinging in nathelper. The test is wrong as it is done in mod_init, while registrar module registerd the usrloc domain via fixup function, after the mod_init section.
    
    Reported by Nick Altmann
    
    (cherry picked from commit f150fb75611a6e61eabc134f266064bd23f1c850)

--- modules/nathelper/nathelper.c
+++ modules/nathelper/nathelper.c
@@ -485,9 +485,7 @@ mod_init(void)
 			nortpproxy_str.s = NULL;
 	}
 
-	/* enable all the pinging stuff only if pinging interval is set and
-	 * if we have at least one domain in the usrloc (otherwise there is
-	 * nothing to ping) */
+	/* enable all the pinging stuff only if pinging interval is set */
 	if (natping_interval > 0) {
 		bind_usrloc = (bind_usrloc_t)find_export("ul_bind_usrloc", 1, 0);
 		if (!bind_usrloc) {
@@ -497,12 +495,6 @@ mod_init(void)
 
 		if (bind_usrloc(&ul) < 0) {
 			return -1;
-		}
-
-		if ( ul.get_next_udomain(NULL)==NULL ) {
-			/* no usrloc domains, nothing to ping, so abort enabling
-			 * the pinging support */
-			goto after_pinging;
 		}
 
 		if (force_socket_str) {
@@ -565,8 +557,6 @@ mod_init(void)
 			}
 		}
 	}
-
-after_pinging:
 
 	/* Prepare 1918/6598 networks list */
 	for (i = 0; nets_1918[i].cnetaddr != NULL; i++) {
