d8357217342a63b36616fe0c7657c92c1388a8ee
ionutrazvanionita <ionutionita@opensips.org>
2016-06-27 13:26:20 +0300

    [siptrace][bugfix] fix bad macro in siptrace(trace local ip logic)
    This commit closes #916
    Thanks to @Tinet-AaronAn for reporting the issue
    
    (cherry picked from commit 2a4f686d4997b814eeaa425dc23fe822cb8ef91a)

--- modules/siptrace/siptrace.c
+++ modules/siptrace/siptrace.c
@@ -1571,9 +1571,9 @@ do { \
 
 #define set_columns_to_trace_local_ip( _col_proto, _col_ip, _col_port) \
 	do { \
-		db_vals[4].val.str_val = trace_local_proto; \
-		db_vals[5].val.str_val = trace_local_ip; \
-		db_vals[6].val.int_val = trace_local_port; \
+		_col_proto.val.str_val = trace_local_proto; \
+		_col_ip.val.str_val = trace_local_ip; \
+		_col_port.val.int_val = trace_local_port; \
 	} while (0)
 
 static int sip_trace(struct sip_msg *msg, trace_info_p info)
