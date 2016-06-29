OpenSIPS/opensips

commit 03398fbf59b485a566afc9f3654afb7b7d185e6e

Refactor commit 58a944cd

modules/usrloc/ucontact.c
@@ -118,12 +118,7 @@ new_ucontact(str* _dom, str* _aor, str* _contact, ucontact_info_t* _ci)
 	if (shm_str_dup( &c->callid, _ci->callid) < 0) goto mem_error;
 
 	/* an additional null byte may be needed by "regexec" later on */
-	c->user_agent.s = shm_malloc(_ci->user_agent->len + 1);
-	if (!c->user_agent.s)
-		goto mem_error;
-	c->user_agent.len = _ci->user_agent->len;
-	memcpy(c->user_agent.s, _ci->user_agent->s, c->user_agent.len);
-	c->user_agent.s[c->user_agent.len] = '\0';
+	if (shm_nt_str_dup( &c->user_agent, _ci->user_agent) < 0) goto mem_error;
 
 	if (_ci->received.s && _ci->received.len) {
 		if (shm_str_dup( &c->received, &_ci->received) < 0) goto mem_error;
@@ -258,10 +253,10 @@ void print_ucontact(FILE* _f, ucontact_t* _c)
 int mem_update_ucontact(ucontact_t* _c, ucontact_info_t* _ci)
 {
 	/* "user_agent" must be null-terminated (see e5cb9805b) */
-#define update_str(_old,_new) \
+#define update_str(_old,_new,_nt) \
 	do{\
 		if ((_old)->len < (_new)->len) { \
-			ptr = (char*)shm_malloc((_new)->len + 1); \
+			ptr = (char*)shm_malloc((_new)->len + _nt?1:0); \
 			if (ptr == 0) { \
 				LM_ERR("no more shm memory\n"); \
 				return -1; \
@@ -273,7 +268,8 @@ int mem_update_ucontact(ucontact_t* _c, ucontact_info_t* _ci)
 			memcpy((_old)->s, (_new)->s, (_new)->len);\
 		}\
 		(_old)->len = (_new)->len;\
-		(_old)->s[(_old)->len] = '\0'; \
+		if (_nt) \
+			(_old)->s[(_old)->len] = '\0'; \
 	} while(0)
 
 	char* ptr;
@@ -282,12 +278,12 @@ int mem_update_ucontact(ucontact_t* _c, ucontact_info_t* _ci)
 	 * the same Call-ID header field value for registrations sent
 	 * to a particular registrar.', but it is not a 'MUST'. So
 	 * always update the call ID to be safe. */
-	update_str( &_c->callid, _ci->callid);
+	update_str( &_c->callid, _ci->callid, 0);
 
-	update_str( &_c->user_agent, _ci->user_agent);
+	update_str( &_c->user_agent, _ci->user_agent, 1);
 
 	if (_ci->received.s && _ci->received.len) {
-		update_str( &_c->received, &_ci->received);
+		update_str( &_c->received, &_ci->received, 0);
 	} else {
 		if (_c->received.s) shm_free(_c->received.s);
 		_c->received.s = 0;
@@ -295,7 +291,7 @@ int mem_update_ucontact(ucontact_t* _c, ucontact_info_t* _ci)
 	}
 
 	if (_ci->path) {
-		update_str( &_c->path, _ci->path);
+		update_str( &_c->path, _ci->path, 0);
 	} else {
 		if (_c->path.s) shm_free(_c->path.s);
 		_c->path.s = 0;
@@ -303,7 +299,7 @@ int mem_update_ucontact(ucontact_t* _c, ucontact_info_t* _ci)
 	}
 
 	if (_ci->attr && _ci->attr->s && _ci->attr->len) {
-		update_str( &_c->attr, _ci->attr);
+		update_str( &_c->attr, _ci->attr, 0);
 	} else {
 		if (_c->attr.s) shm_free(_c->attr.s);
 		_c->attr.s = 0;

ut.h
@@ -578,6 +578,24 @@ static inline int shm_str_dup(str* dst, const str* src)
 }
 
 /*
+ * Make a copy of an str structure using shm_malloc
+ *	  + an additional '\0' byte, so you can make use of dst->s
+ */
+static inline int shm_nt_str_dup(str* dst, const str* src)
+{
+	dst->s = shm_malloc(src->len + 1);
+	if (!dst->s) {
+		LM_ERR("no shared memory left\n");
+		return -1;
+	}
+
+	memcpy(dst->s, src->s, src->len);
+	dst->len = src->len;
+	dst->s[dst->len] = '\0';
+	return 0;
+}
+
+/*
  * Make a copy of a str structure using pkg_malloc
  */
 static inline int pkg_str_dup(str* dst, const str* src)

commit e16abb4b2a97c7a3130895eca9adbba64e660874

usrloc: Fix an operator precedence bug
Introduced in commit 03398fbf. Oops!

modules/usrloc/ucontact.c
@@ -256,7 +256,7 @@ int mem_update_ucontact(ucontact_t* _c, ucontact_info_t* _ci)
 #define update_str(_old,_new,_nt) \
 	do{\
 		if ((_old)->len < (_new)->len) { \
-			ptr = (char*)shm_malloc((_new)->len + _nt?1:0); \
+			ptr = shm_malloc((_new)->len + ((_nt) ? 1 : 0)); \
 			if (ptr == 0) { \
 				LM_ERR("no more shm memory\n"); \
 				return -1; \