0eecff942dc3700e008a9941ac406a565325ef89
Razvan Crainea <razvan@opensips.org>
2016-07-18 10:33:54 +0300

    permissions: allow any supported protocol
    Before this commit, the protocol check was hardcoded in the module. This
    commit allows to add in the permissions table any transport protocol
    supported.
    
    Thanks go to Kirill Galinurov for reporting this!

--- modules/permissions/address.c
+++ modules/permissions/address.c
@@ -35,6 +35,7 @@
 #include "../../config.h"
 #include "../../db/db.h"
 #include "../../ip_addr.h"
+#include "../../socket_info.h"
 #include "../../mem/shm_mem.h"
 #include "../../parser/msg_parser.h"
 #include "../../parser/parse_from.h"
@@ -50,18 +51,13 @@
 
 str def_part = str_init("default");
 
-static int proto_char2int(str *proto) {
+static inline int proto_char2int(str *proto) {
+	int ret_proto;
 	if (proto->len==0 || (proto->len==3 && !strcasecmp(proto->s, "any")))
 		return PROTO_NONE;
-	if (proto->len==3 && !strcasecmp(proto->s, "udp"))
-		return PROTO_UDP;
-	if (proto->len==3 && !strcasecmp(proto->s, "tcp"))
-		return PROTO_TCP;
-	if (proto->len==3 && !strcasecmp(proto->s, "tls"))
-		return PROTO_TLS;
-	if (proto->len==4 && !strcasecmp(proto->s, "sctp"))
-		return PROTO_SCTP;
-	return -1;
+	if (parse_proto((unsigned char*)proto->s, proto->len, &ret_proto) < 0)
+		return -1;
+	return ret_proto;
 }
 
 /*
@@ -534,17 +530,7 @@ int check_addr_6(struct sip_msg* msg,
 		str_proto.len = strlen(str_proto.s);
 	}
 
-	if (!strncasecmp(str_proto.s, "UDP", str_proto.len))
-	    proto = PROTO_UDP;
-	else if (!strncasecmp(str_proto.s, "TCP", str_proto.len))
-	    proto = PROTO_TCP;
-    else if (!strncasecmp(str_proto.s, "TLS", str_proto.len))
-	    proto = PROTO_TLS;
-    else if (!strncasecmp(str_proto.s, "SCTP", str_proto.len))
-	    proto = PROTO_SCTP;
-	else if (!strncasecmp(str_proto.s, "ANY", str_proto.len))
-	    proto = PROTO_NONE;
-	else {
+	if ((proto = proto_char2int(&str_proto)) < 0) {
 		LM_ERR("unknown protocol %.*s\n", str_proto.len, str_proto.s);
 		return -1;
 	}
