e57328e1b40518ceb33cc500cc9bf0556f91f651
ionutrazvanionita <ionutionita@opensips.org>
2016-07-25 18:47:19 +0300

    [acc] introducing new extra/leg engine; module redesign
    	Redesigning accounting module extra values engine raised
    new problems which in the end were solved by redesigning the
    whole accounting module.
    	First of all an accounting module context was created
    which is available for the whole dialog lifetime. Having
    this mechanism all values that were stored in avps or dialog
    values are now being held in this context.
    	Using the new dialog callback that is triggered at shutdown,
    extra and leg values are not abusing anymore of dialog values.
    We store them there only when OpenSIPS closes by any means. This
    made the code look a lot cleaner and the program faster by not
    creating a dialog value for all INVITEs and restoring it for each
    BYE.
    	Accounting module extra values new engine are the main purpose
    of this commit. There are only two variables now, called
    $acc_extra and $acc_leg. They are visible during the whole lifetime
    of a dialog and provide a much cleaner and easier way to use this
    powerful engine.
    	New parameters have been defined, extra_fields and leg_fields
    through which one can define tags and log values. The tags are
    used to reference $acc_extra and $acc_leg variables, whereas the
    leg_fields will be the value that you will see in the logs.
    	As concerning the acc_leg variable, new legs can be created
    using acc_new_leg() function. The value of the current leg can
    be retrieved using $acc_current_leg. It's the script writer the
    one who has to decide when is the moment to jump to a new leg.
    	Also from this commit diameter support from acc module has been
    removed. Diameter didn't have any support and it wasn't
    compatible with a diameter server.

---
+++ modules/acc/acc_vars.h
@@ -0,0 +1,48 @@
+/**
+ *
+ * Copyright (C) 2016 OpenSIPS Foundation
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ * History
+ * -------
+ *  2016-06-23  initial version (Ionut Ionita)
+*/
+
+#ifndef _ACC_VARS_H
+#define _ACC_VARS_H
+
+/* $acc_extra */
+int pv_parse_acc_extra_name(pv_spec_p sp, str *in);
+int pv_get_acc_extra(struct sip_msg *msg, pv_param_t *param,
+		pv_value_t *val);
+int pv_set_acc_extra(struct sip_msg *msg, pv_param_t *param, int flag,
+		pv_value_t *val);
+int set_value_shm(pv_value_t* pvt, extra_value_t* values);
+/* $acc_current_leg */
+int pv_get_acc_current_leg(struct sip_msg *msg, pv_param_t *param,
+		pv_value_t *val);
+/* $acc_leg */
+int pv_parse_acc_leg_index(pv_spec_p sp, str* in);
+int pv_parse_acc_leg_name(pv_spec_p sp, str *in);
+int pv_get_acc_leg(struct sip_msg *msg, pv_param_t *param,
+		pv_value_t *val);
+
+int pv_set_acc_leg(struct sip_msg *msg, pv_param_t *param, int flag,
+		pv_value_t *val);
+
+#endif
