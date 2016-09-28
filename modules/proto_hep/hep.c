OpenSIPS/opensips

commit 7190a2d89f54998a88a2b0b208bba50c9bb4027d

[proto_hep] fix extra chunks memory leak
	Extra chunks (the ones that are not defined in the internal
hep structure) were not freed, causing a memory leak for each hep
packet that had such chunks.

modules/proto_hep/hep.c
@@ -779,3 +779,25 @@ safe_exit:
 	return 0;
 }
 
+void free_extra_chunks(struct hep_desc* h)
+{
+	generic_chunk_t *it, *foo=NULL;
+	if (h == NULL)
+		return;
+
+	if (h->version < 3 || h->u.hepv3.chunk_list == NULL)
+		return;
+
+	for (it=h->u.hepv3.chunk_list; it; foo=it, it=it->next) {
+		if (foo) {
+			shm_free(foo->data);
+			shm_free(foo);
+		}
+	}
+
+	if (foo) {
+		shm_free(foo->data);
+		shm_free(foo);
+	}
+
+}

modules/proto_hep/proto_hep.c
@@ -941,6 +941,8 @@ static inline int hep_handle_req(struct tcp_req *req,
 				receive_msg(msg_buf, msg_len, &local_rcv, ctx) <0)
 				LM_ERR("receive_msg failed \n");
 
+		free_extra_chunks(&hep_ctx->h);
+
 		if (!size && req != &hep_current_req) {
 			/* if we no longer need this tcp_req
 			 * we can free it now */
@@ -1160,7 +1162,6 @@ static int hep_udp_read_req(struct socket_info *si, int* bytes_read)
 #else
 	static char buf [BUF_SIZE+1];
 #endif
-	char *tmp;
 	unsigned int fromlen;
 	str msg;
 
@@ -1280,6 +1281,8 @@ static int hep_udp_read_req(struct socket_info *si, int* bytes_read)
 		/* receive_msg must free buf too!*/
 		receive_msg( msg.s, msg.len, &ri, ctx);
 	}
+
+	free_extra_chunks(&hep_ctx->h);
 
 	return 0;
 

modules/proto_hep/hep.h
@@ -236,6 +236,7 @@ int pack_hep(union sockaddr_union* from_su, union sockaddr_union* to_su,
 int unpack_hepv12(char *buf, int len, struct hep_desc* h);
 int unpack_hepv3(char *buf, int len, struct hep_desc *h);
 int unpack_hep(char *buf, int len, int version, struct hep_desc* h);
+void free_extra_chunks(struct hep_desc* h);
 
 
 typedef int (*pack_hep_t)(union sockaddr_union* from_su, union sockaddr_union* to_su,

commit 63b2a52c2c2d453ccf889cc5e74991a2c81aaccd

[proto_hep] fix bug introduced in 7190a2d
	Commit 7190a2d was freeing the extra chunks twice when
the message was processed as SIP. Now the original hep context
free function is not registered as a context free function, but
called manually each time all the hep processing is done. This
way we avoid the memory leaks that appeared when the message was
not a SIP message, and the chunks were not freed.

modules/proto_hep/hep.c
@@ -778,26 +778,3 @@ safe_exit:
 
 	return 0;
 }
-
-void free_extra_chunks(struct hep_desc* h)
-{
-	generic_chunk_t *it, *foo=NULL;
-	if (h == NULL)
-		return;
-
-	if (h->version < 3 || h->u.hepv3.chunk_list == NULL)
-		return;
-
-	for (it=h->u.hepv3.chunk_list; it; foo=it, it=it->next) {
-		if (foo) {
-			shm_free(foo->data);
-			shm_free(foo);
-		}
-	}
-
-	if (foo) {
-		shm_free(foo->data);
-		shm_free(foo);
-	}
-
-}

modules/proto_hep/proto_hep.c
@@ -192,7 +192,7 @@ static int mod_init(void)
 		}
 	}
 
-	hep_ctx_idx = context_register_ptr(CONTEXT_GLOBAL, free_hep_context);
+	hep_ctx_idx = context_register_ptr(CONTEXT_GLOBAL, 0);
 
 	return 0;
 }
@@ -941,7 +941,7 @@ static inline int hep_handle_req(struct tcp_req *req,
 				receive_msg(msg_buf, msg_len, &local_rcv, ctx) <0)
 				LM_ERR("receive_msg failed \n");
 
-		free_extra_chunks(&hep_ctx->h);
+		free_hep_context(hep_ctx);
 
 		if (!size && req != &hep_current_req) {
 			/* if we no longer need this tcp_req
@@ -1282,7 +1282,7 @@ static int hep_udp_read_req(struct socket_info *si, int* bytes_read)
 		receive_msg( msg.s, msg.len, &ri, ctx);
 	}
 
-	free_extra_chunks(&hep_ctx->h);
+	free_hep_context(hep_ctx);
 
 	return 0;
 