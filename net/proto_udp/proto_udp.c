f4ac7aa14a78833810dc21c435c1a8c84a92ec10
Razvan Crainea <razvan@opensips.org>
2016-11-03 16:41:03 +0200

    udp: print IP:port when there is a sendto error
    This can ease debugging in case of errors

--- net/proto_udp/proto_udp.c
+++ net/proto_udp/proto_udp.c
@@ -213,8 +213,9 @@ static int proto_udp_send(struct socket_info* source,
 again:
 	n=sendto(source->socket, buf, len, 0, &to->s, tolen);
 	if (n==-1){
-		LM_ERR("sendto(sock,%p,%d,0,%p,%d): %s(%d)\n", buf,len,to,tolen,
-				strerror(errno),errno);
+		LM_ERR("sendto(sock,%p,%d,0,%p,%d): %s(%d) [%s:%hu]\n", buf,len,to,
+				tolen,strerror(errno),errno,inet_ntoa(to->sin.sin_addr),
+				ntohs(to->sin.sin_port));
 		if (errno==EINTR || errno==EAGAIN) goto again;
 		if (errno==EINVAL) {
 			LM_CRIT("invalid sendtoparameters\n"
