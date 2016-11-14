138b2f752edf771c390bb71832cdb0b13bab799c
Razvan Crainea <razvan@opensips.org>
2016-11-14 11:37:23 +0200

    snmpstats: free space in case of error
    Fixes Coverity CID 40799, 40800, 40801, 40804, 40811, 40814, 40817, 40818
    
    (cherry picked from commit 9b64078ada10287848d6f5853cc5c4cc3dc1b0fb)

--- modules/snmpstats/openserSIPStatusCodesTable.c
+++ modules/snmpstats/openserSIPStatusCodesTable.c
@@ -684,6 +684,8 @@ void initialize_table_openserSIPStatusCodesTable(void)
 	if (!my_handler || !table_info) {
 		snmp_log(LOG_ERR, "malloc failed in initialize_table_openserSIP"
 				"StatusCodesTable_handler\n");
+		if (table_info)
+			SNMP_FREE(table_info);
 		return; /** mallocs failed */
 	}
 
