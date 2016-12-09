490298f38db7c50100962f231dd47fcc8dca049f
rvlad-patrascu <vladp@opensips.org>
2016-12-09 13:42:34 +0200

    event_virtual: fix copyright
    (cherry picked from commit 79ebb90726b8184b2908804046ad2bc5f9092e0d)

--- modules/event_virtual/event_virtual.h
+++ modules/event_virtual/event_virtual.h
@@ -1,3 +1,28 @@
+/*
+ * Copyright (C) 2015 OpenSIPS Project
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *
+ * history:
+ * ---------
+ *  2015-07-xx created (rvlad-patrascu)
+ */
+
 #ifndef _EV_VIRTUAL_H_
 #define _EV_VIRTUAL_H_
 
