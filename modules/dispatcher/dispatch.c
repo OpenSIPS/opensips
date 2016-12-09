5116cc84360dee3488f2b6f5e0fd6c78379b0a05
Bogdan-Andrei Iancu <bogdan@opensips.org>
2016-12-09 19:01:10 +0200

    Fix recalculating Active nodes and weight sums after reload
    When inheriting the state from in-mem old data set, the state of a node may change, so we need to recalculate the number of active nodes and the running sums (for weights) in the set.
    
    Fixes #972, but we need @hydrosine confirmation on this.
    
    (cherry picked from commit c5c1f40ea54f0908421ece09bdc5e4b70ceba6cb)

--- modules/dispatcher/dispatch.c
+++ modules/dispatcher/dispatch.c
@@ -626,6 +626,7 @@ static void ds_inherit_state( ds_data_t *old_data , ds_data_t *new_data)
 {
 	ds_set_p new_set, old_set;
 	ds_dest_p new_ds, old_ds;
+	int changed;
 
 	/* search the new sets through the old sets */
 	for ( new_set=new_data->sets ; new_set ; new_set=new_set->next ) {
@@ -638,6 +639,7 @@ static void ds_inherit_state( ds_data_t *old_data , ds_data_t *new_data)
 			continue;
 		}
 		LM_DBG("set id %d found in old sets\n",new_set->id);
+		changed = 0;
 
 		/* sets are matching, try to match the destinations, one by one */
 		for ( new_ds=new_set->dlist ; new_ds ; new_ds=new_ds->next ) {
@@ -646,7 +648,10 @@ static void ds_inherit_state( ds_data_t *old_data , ds_data_t *new_data)
 				strncasecmp(new_ds->uri.s, old_ds->uri.s, old_ds->uri.len)==0 ) {
 					LM_DBG("DST <%.*s> found in old set, copying state\n",
 						new_ds->uri.len,new_ds->uri.s);
-					new_ds->flags = old_ds->flags;
+					if (new_ds->flags != old_ds->flags) {
+						new_ds->flags = old_ds->flags;
+						changed = 1;
+					}
 					break;
 				}
 			}
@@ -654,6 +659,8 @@ static void ds_inherit_state( ds_data_t *old_data , ds_data_t *new_data)
 				LM_DBG("DST <%.*s> not found in old set\n",
 					new_ds->uri.len,new_ds->uri.s);
 		}
+		if (changed)
+			re_calculate_active_dsts(new_set);
 	}
 }
 
