98cd04642db6bfa70a154086258ed810f7275da5
Ovidiu Sas <osas@voipembedded.com>
2017-01-13 12:17:06 -0500

    db_text: downgrade an init INFO probe to DBG

--- modules/db_text/dbt_base.c
+++ modules/db_text/dbt_base.c
@@ -93,7 +93,7 @@ db_con_t* dbt_init(const str* _sqlurl)
 	memset(_res, 0, sizeof(db_con_t) + sizeof(dbt_con_t));
 	_res->tail = (unsigned long)((char*)_res+sizeof(db_con_t));
 
-	LM_INFO("using database at: %.*s\n", _s.len, _s.s);
+	LM_DBG("using database at: %.*s\n", _s.len, _s.s);
 	DBT_CON_CONNECTION(_res) = dbt_cache_get_db(&_s);
 	if (!DBT_CON_CONNECTION(_res))
 	{
