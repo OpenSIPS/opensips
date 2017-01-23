524db38c906ea473f4b09080dd397e3d04b4b97a
Liviu Chircu <liviu@opensips.org>
2017-01-23 18:05:32 +0200

    freeswitch ESL connector: First draft

---
+++ modules/freeswitch/fs_api.h
@@ -0,0 +1,41 @@
+/*
+ * FreeSWITCH API
+ *
+ * This module is intended to be used as a middle layer SIP component in
+ * environments where a large proportion of SIP UAs (e.g. mobile devices)
+ * register at high enough frequencies that they actually degrade the
+ * performance of their registrars.
+ *
+ * Copyright (C) 2016 OpenSIPS Solutions
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ * History:
+ * --------
+ *  2017-01-19 initial version (liviu)
+ */
+
+#ifndef __FREESWITCH_API__
+#define __FREESWITCH_API__
+
+typedef struct _fs_api_t fs_api_t;
+struct _fs_api_t {
+	int a;
+};
+
+
+#endif /* __FREESWITCH_API__ */
---
+++ modules/freeswitch/fs_mod.c
@@ -0,0 +1,88 @@
+/*
+ * Client for the FreeSWITCH ESL (Event Socket Layer)
+ *
+ * This module is intended to be used as a middle layer SIP component in
+ * environments where a large proportion of SIP UAs (e.g. mobile devices)
+ * register at high enough frequencies that they actually degrade the
+ * performance of their registrars.
+ *
+ * Copyright (C) 2016 OpenSIPS Solutions
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ * History:
+ * --------
+ *  2017-01-19 initial version (liviu)
+ */
+
+#include "../../sr_module.h"
+#include "../../ut.h"
+#include "../../timer.h"
+#include "../../mod_fix.h"
+#include "../../parser/msg_parser.h"
+
+#include "fs_api.h"
+
+static int mod_init(void);
+int fs_bind(fs_api_t *fapi);
+
+static cmd_export_t cmds[] = {
+	{ "fs_bind", (cmd_function)fs_bind, 1, NULL, NULL, 0 },
+	{ NULL, NULL, 0, NULL, NULL, 0 }
+};
+
+static param_export_t mod_params[] = {
+	{ 0,0,0 }
+};
+
+static dep_export_t deps = {
+	{ /* OpenSIPS module dependencies */
+		{ MOD_TYPE_NULL, NULL, 0 },
+	},
+	{ /* modparam dependencies */
+		{ NULL, NULL },
+	},
+};
+
+struct module_exports exports= {
+	"mid_registrar",        /* module's name */
+	MOD_TYPE_DEFAULT,/* class of this module */
+	MODULE_VERSION,
+	DEFAULT_DLFLAGS, /* dlopen flags */
+	&deps,           /* OpenSIPS module dependencies */
+	cmds,            /* exported functions */
+	NULL,               /* exported async functions */
+	mod_params,      /* param exports */
+	NULL,       /* exported statistics */
+	NULL,         /* exported MI functions */
+	NULL,       /* exported pseudo-variables */
+	NULL,               /* extra processes */
+	mod_init,        /* module initialization function */
+	NULL,               /* reply processing function */
+	NULL,
+	NULL       /* per-child init function */
+};
+
+static int mod_init(void)
+{
+	return 0;
+}
+
+int fs_bind(fs_api_t *fapi)
+{
+	return 0;
+}
