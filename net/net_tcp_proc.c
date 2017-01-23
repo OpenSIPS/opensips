26a5453fbf7296858ac35a96e4d42a4373e2206b
Razvan Crainea <razvan@opensips.org>
2017-01-23 12:52:05 +0200

    net/tcp: increase info for error reports

--- net/net_tcp_proc.c
+++ net/net_tcp_proc.c
@@ -64,7 +64,7 @@ static void tcpconn_release(struct tcp_connection* c, long state,int writer)
 
 	if (send_all((state==ASYNC_WRITE)?unix_tcp_sock:tcpmain_sock, response,
 	sizeof(response))<=0)
-		LM_ERR("send_all failed\n");
+		LM_ERR("send_all failed state=%ld con=%p\n", state, c);
 }
 
 
