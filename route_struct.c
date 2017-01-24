79c4006cb1b2e770a0d16c113016b17511c936d3
Bogdan-Andrei Iancu <bogdan@opensips.org>
2017-01-24 20:20:03 +0200

    Fix finding the script functions in assigments.
    Do an aggressive search by digging through all possible ACTIONS or EXPRessions to see if a function is used in the script.
    Fix for #1022
    
    (cherry picked from commit 87a3a412aa720e858b126a5c2dc0ffe7c6c28530)

--- route_struct.c
+++ route_struct.c
@@ -667,22 +667,34 @@ int is_mod_func_used(struct action *a, char *name, int param_no)
 				return 1;
 		}
 
-		if (a->type==IF_T || a->type==WHILE_T)
-			if (is_mod_func_in_expr((struct expr*)a->elem[0].u.data,name,param_no)==1)
+		/* follow all leads from actions/expressions than may have 
+		 * sub-blocks of instructions */
+		if (a->elem[0].type==ACTIONS_ST)
+			if (is_mod_func_used((struct action*)a->elem[0].u.data,
+			name,param_no)==1)
+				return 1;
+		if (a->elem[0].type==EXPR_ST)
+			if (is_mod_func_in_expr((struct expr*)a->elem[0].u.data,
+			name,param_no)==1)
 				return 1;
 
-		/* follow all leads from actions than may have sub-blocks of instructions */
-		if (a->elem[0].type==ACTIONS_ST)
-				if (is_mod_func_used((struct action*)a->elem[0].u.data,name,param_no)==1)
-					return 1;
-
 		if (a->elem[1].type==ACTIONS_ST)
-				if (is_mod_func_used((struct action*)a->elem[1].u.data,name,param_no)==1)
-					return 1;
+			if (is_mod_func_used((struct action*)a->elem[1].u.data,
+			name,param_no)==1)
+				return 1;
+		if (a->elem[1].type==EXPR_ST)
+			if (is_mod_func_in_expr((struct expr*)a->elem[1].u.data,
+			name,param_no)==1)
+				return 1;
 
 		if (a->elem[2].type==ACTIONS_ST)
-				if (is_mod_func_used((struct action*)a->elem[2].u.data,name,param_no)==1)
-					return 1;
+			if (is_mod_func_used((struct action*)a->elem[2].u.data,
+			name,param_no)==1)
+				return 1;
+		if (a->elem[2].type==EXPR_ST)
+			if (is_mod_func_in_expr((struct expr*)a->elem[2].u.data,
+			name,param_no)==1)
+				return 1;
 
 		a = a->next;
 	}
