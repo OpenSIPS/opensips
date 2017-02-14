076c78cbf3acb63842a924c9535fc687cbe40f1d
Razvan Crainea <razvan@opensips.org>
2017-02-14 12:36:23 +0200

    load_balancer: rename internal used AVPs
    This prevents using them from script by mistake

--- modules/load_balancer/load_balancer.c
+++ modules/load_balancer/load_balancer.c
@@ -71,11 +71,11 @@ static void mod_destroy(void);
 static int mi_child_init();
 
 /* failover stuff */
-static str group_avp_name_s = str_init("lb_grp");
-static str flags_avp_name_s = str_init("lb_flg");
-static str mask_avp_name_s = str_init("lb_mask");
-static str id_avp_name_s = str_init("lb_id");
-static str res_avp_name_s = str_init("lb_res");
+static str group_avp_name_s = str_init("__lb_grp");
+static str flags_avp_name_s = str_init("__lb_flg");
+static str mask_avp_name_s = str_init("__lb_mask");
+static str id_avp_name_s = str_init("__lb_id");
+static str res_avp_name_s = str_init("__lb_res");
 int group_avp_name;
 int flags_avp_name;
 int mask_avp_name;
