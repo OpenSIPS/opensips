OpenSIPS/opensips

commit 4089d44ca7b1a0a3fbda64dd1c5978d59011d443

move init_shm before the script parsing
shared memory is now initialized before script parsing
removed uid and git script variables (can now be set only from commad line)
removed pending statistics list from statistics.c as dynamic statistics can now
be allocated directly in shared memory

main.c
@@ -1011,6 +1011,35 @@ try_again:
 		goto error;
 	}
 
+		/* get uid/gid */
+	if (user){
+		if (user2uid(&uid, &gid, user)<0){
+			LM_ERR("bad user name/uid number: -u %s\n", user);
+			goto error00;
+		}
+	}
+	if (group){
+		if (group2gid(&gid, group)<0){
+			LM_ERR("bad group name/gid number: -u %s\n", group);
+			goto error00;
+		}
+	}
+
+		/*init shm mallocs
+	 *  this must be here
+	 *     -to allow setting shm mem size from the command line
+	 *     -it must be also before init_timer and init_tcp
+	 *     -it must be after we know uid (so that in the SYSV sems case,
+	 *        the sems will have the correct euid)
+	 * --andrei */
+	if (init_shm_mallocs()==-1)
+		goto error;
+
+	if (init_stats_collector() < 0) {
+		LM_ERR("failed to initialize statistics\n");
+		goto error;
+	}
+
 	/* parse the config file, prior to this only default values
 	   e.g. for debugging settings will be used */
 	yyin=cfg_stream;
@@ -1058,19 +1087,6 @@ try_again:
 	/* fix parameters */
 	if (working_dir==0) working_dir="/";
 
-	/* get uid/gid */
-	if (user){
-		if (user2uid(&uid, &gid, user)<0){
-			LM_ERR("bad user name/uid number: -u %s\n", user);
-			goto error00;
-		}
-	}
-	if (group){
-		if (group2gid(&gid, group)<0){
-			LM_ERR("bad group name/gid number: -u %s\n", group);
-			goto error00;
-		}
-	}
 	if (fix_all_socket_lists()!=0){
 		LM_ERR("failed to initialize list addresses\n");
 		goto error00;
@@ -1097,24 +1113,9 @@ try_again:
 
 	time(&startup_time);
 
-	/*init shm mallocs
-	 *  this must be here
-	 *     -to allow setting shm mem size from the command line
-	 *       => if shm_mem should be settable from the cfg file move
-	 *       everything after
-	 *     -it must be also before init_timer and init_tcp
-	 *     -it must be after we know uid (so that in the SYSV sems case,
-	 *        the sems will have the correct euid)
-	 * --andrei */
-	if (init_shm_mallocs()==-1)
-		goto error;
+
 
 	/* Init statistics */
-	if (init_stats_collector()<0) {
-		LM_ERR("failed to initialize statistics\n");
-		goto error;
-	}
-
 	init_shm_statistics();
 
 	/*init UDP networking layer*/

cfg.lex
@@ -326,8 +326,6 @@ SIP_WARNING sip_warning
 SERVER_SIGNATURE server_signature
 SERVER_HEADER server_header
 USER_AGENT_HEADER user_agent_header
-USER		"user"|"uid"
-GROUP		"group"|"gid"
 CHROOT		"chroot"
 WDIR		"workdir"|"wdir"
 MHOMED		mhomed
@@ -603,8 +601,6 @@ IMPORTFILE      "import_file"
 <INITIAL>{QUERYBUFFERSIZE}	{ count(); yylval.strval=yytext; return QUERYBUFFERSIZE; }
 <INITIAL>{QUERYFLUSHTIME}	{ count(); yylval.strval=yytext; return QUERYFLUSHTIME; }
 <INITIAL>{SIP_WARNING}	{ count(); yylval.strval=yytext; return SIP_WARNING; }
-<INITIAL>{USER}		{ count(); yylval.strval=yytext; return USER; }
-<INITIAL>{GROUP}	{ count(); yylval.strval=yytext; return GROUP; }
 <INITIAL>{CHROOT}	{ count(); yylval.strval=yytext; return CHROOT; }
 <INITIAL>{WDIR}	{ count(); yylval.strval=yytext; return WDIR; }
 <INITIAL>{MHOMED}	{ count(); yylval.strval=yytext; return MHOMED; }

cfg.y
@@ -363,8 +363,6 @@ static struct multi_str *tmp_mod;
 %token MPATH
 %token MODPARAM
 %token MAXBUFFER
-%token USER
-%token GROUP
 %token CHROOT
 %token WDIR
 %token MHOMED
@@ -771,12 +769,6 @@ assign_stm: DEBUG EQUAL snumber {
 		| QUERYFLUSHTIME EQUAL error { yyerror("int value expected"); }
 		| SIP_WARNING EQUAL NUMBER { sip_warning=$3; }
 		| SIP_WARNING EQUAL error { yyerror("boolean value expected"); }
-		| USER EQUAL STRING     { user=$3; }
-		| USER EQUAL ID         { user=$3; }
-		| USER EQUAL error      { yyerror("string value expected"); }
-		| GROUP EQUAL STRING     { group=$3; }
-		| GROUP EQUAL ID         { group=$3; }
-		| GROUP EQUAL error      { yyerror("string value expected"); }
 		| CHROOT EQUAL STRING     { chroot_dir=$3; }
 		| CHROOT EQUAL ID         { chroot_dir=$3; }
 		| CHROOT EQUAL error      { yyerror("string value expected"); }

statistics.c
@@ -258,39 +258,16 @@ static inline module_stats* __add_stat_module( char *module, int unsafe)
 
 /***************** Init / Destroy STATS support functions *******************/
 
-struct pending_stat_name {
-	str* name;
-	struct pending_stat_name *next;
-};
-struct pending_stat_name *pending_name_list = NULL;
 
 int clone_pv_stat_name(str *name, str *clone)
 {
-	struct pending_stat_name *psn;
-
-	if (collector) {
-		/* if collector init'ed, simply clone it in shm */
-		clone->s = (char*)shm_malloc(name->len);
-		if (clone->s==NULL) {
-			LM_ERR("failed to allocated more shm mem (%d)\n",name->len);
-			return -1;
-		}
-		clone->len = name->len;
-		memcpy(clone->s,name->s,name->len);
-	} else {
-		/* just link it, we will move it to shm when callector available */
-		*clone = *name;
-		/* add to pending list */
-		psn = (struct pending_stat_name *)pkg_malloc( sizeof(struct pending_stat_name) );
-		if (psn==NULL) {
-			LM_ERR("failed to allocated more shm mem (%u)\n",
-				(unsigned int)sizeof(struct pending_stat_name));
-			return -1;
-		}
-		psn->name = clone;
-		psn->next = pending_name_list;
-		pending_name_list = psn;
+	clone->s = (char*)shm_malloc(name->len);
+	if (clone->s==NULL) {
+		LM_ERR("failed to allocated more shm mem (%d)\n",name->len);
+		return -1;
 	}
+	clone->len = name->len;
+	memcpy(clone->s,name->s,name->len);
 
 	return 0;
 }
@@ -298,9 +275,7 @@ int clone_pv_stat_name(str *name, str *clone)
 
 int init_stats_collector(void)
 {
-	struct pending_stat_name *psn, *next;
 	module_stats *dy_mod;
-	char *s;
 
 	/* init the collector */
 	collector = (stats_collector*)shm_malloc_unsafe(sizeof(stats_collector));
@@ -309,21 +284,6 @@ int init_stats_collector(void)
 		goto error;
 	}
 	memset( collector, 0 , sizeof(stats_collector));
-
-	/* move pending stat names in shm */
-	for ( psn=pending_name_list ; psn ; psn=next ) {
-		next = psn->next;
-
-		s = (char*)shm_malloc_unsafe( psn->name->len );
-		if (s==NULL) {
-			LM_ERR("no more shm mem (%d)\n", psn->name->len);
-			goto error;
-		}
-		memcpy( s, psn->name->s, psn->name->len);
-		psn->name->s = s;
-
-		pkg_free(psn);
-	}
 
 	/*
 	 * register shm statistics in an unsafe manner, as some allocators

commit cc63f2de47f59bd01b7663413a825323eb100e92

HP_MALLOC: Fix a startup ordering issue
Memory warming would never be run at all, due to the following
dependency (issue introduced by 4089d44ca):

       mem warming         -> "mem_warming_enabled" -> yyparse()
  (in init_shm_mallocs())

But init_shm_mallocs() runs _before_ yyparse()!

This patch moves the mem warming code after yyparse().

main.c
@@ -1020,6 +1020,9 @@ try_again:
 		goto error00;
 	}
 
+	/* shm statistics, module stat groups, memory warming */
+	init_shm_post_yyparse();
+
 	if (config_check>1 && check_rls()!=0) {
 		LM_ERR("bad function call in config file\n");
 		return ret;
@@ -1073,9 +1076,6 @@ try_again:
 	}
 
 	time(&startup_time);
-
-	/* Init statistics */
-	init_shm_statistics();
 
 	/*init UDP networking layer*/
 	if (udp_init()<0){

mem/hp_malloc.c
@@ -51,14 +51,14 @@ extern unsigned long *mem_hash_usage;
  * adaptive image of OpenSIPS's memory usage during runtime
  * used to fragment the shared memory pool at daemon startup
  */
-char *mem_warming_pattern_file;
+char *mem_warming_pattern_file = MEM_WARMING_DEFAULT_PATTERN_FILE;
 int mem_warming_enabled;
 
 /*
  * percentage of shared memory which will be fragmented at startup
  * common values are between [0, 75]
  */
-int mem_warming_percentage = -1;
+int mem_warming_percentage = MEM_WARMING_DEFAULT_PERCENTAGE;
 
 #if defined(HP_MALLOC) && !defined(HP_MALLOC_FAST_STATS)
 stat_var *shm_used;
@@ -460,6 +460,7 @@ write_error:
 
 /**
  * on-demand memory fragmentation, based on an input pattern file
+ *    (pre-populates the hash with free fragments)
  */
 int hp_mem_warming(struct hp_block *hpb)
 {
@@ -754,18 +755,6 @@ struct hp_block *hp_shm_malloc_init(char *address, unsigned long size,
 	} else {
 		hpb->real_used += FRAG_OVERHEAD;
 		hpb->total_fragments++;
-	}
-
-	/* if memory warming is on, pre-populate the hash with free fragments */
-	if (mem_warming_enabled) {
-		if (!mem_warming_pattern_file)
-			mem_warming_pattern_file = MEM_WARMING_DEFAULT_PATTERN_FILE;
-
-		if (mem_warming_percentage == -1)
-			mem_warming_percentage = MEM_WARMING_DEFAULT_PERCENTAGE;
-
-		if (hp_mem_warming(hpb) != 0)
-			LM_INFO("skipped memory warming\n");
 	}
 
 #ifdef DBG_MALLOC

mem/shm_mem.c
@@ -462,11 +462,14 @@ struct mi_root *mi_shm_check(struct mi_root *cmd, void *param)
 	return NULL;
 }
 
-void init_shm_statistics(void)
+void init_shm_post_yyparse(void)
 {
-	#ifdef HP_MALLOC
+#ifdef HP_MALLOC
+	if (mem_warming_enabled && hp_mem_warming(shm_block) != 0) {
+		LM_INFO("skipped memory warming\n");
+	}
 	hp_init_shm_statistics(shm_block);
-	#endif
+#endif
 
 #ifdef SHM_EXTRA_STATS
 	struct multi_str *mod_name;

mem/shm_mem.h
@@ -137,10 +137,11 @@ extern gen_lock_t* mem_lock;
 int shm_mem_init(); /* calls shm_getmem & shm_mem_init_mallocs */
 
 /*
- * should be called after the statistics engine is initialized
- * updates the atomic shm statistics with proper values
+ * must be called after the statistics engine is initialized
+ *	- updates the atomic shm statistics with proper values
+ *	- performs memory warming with HP_MALLOC
  */
-void init_shm_statistics(void);
+void init_shm_post_yyparse(void);
 
 int shm_getmem();   /* allocates the memory (mmap or sysv shmap) */
 int shm_mem_init_mallocs(void* mempool, unsigned long size); /* initialize