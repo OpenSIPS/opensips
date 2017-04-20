4e3ae9214b83722c22f0fd2c800ab1525cd9ee1f
Bogdan-Andrei Iancu <bogdan@opensips.org>
2017-04-20 14:08:24 +0300

    Fixed t_check_trans() not to report end-2-end ACKs
    end-2-end ACKs are sequential requests (in dialog) forming a separate transaction than the INVITE; In terms of ACK matching, the t_check_trans() reports only hop-by-hop ACKs (to negative replies) which are part of the INVITE transaction.
    
    (cherry picked from commit dee4d12f8e99adbb6e46476eaeaf2b80130cb5a9)

--- modules/tm/tm.c
+++ modules/tm/tm.c
@@ -959,7 +959,7 @@ inline static int t_check_trans(struct sip_msg* msg)
 				return 0;
 			case -2:
 				/* e2e ACK found */
-				return 1;
+				return -1;
 			default:
 				/* notfound */
 				return -1;
