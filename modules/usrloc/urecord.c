f114dd2918b4fb53b5858b06b7110211d221e5ee
ionutrazvanionita <ionutionita@opensips.org>
2017-04-26 13:29:25 +0300

    [usrloc] fix contact removal in DB_ONLY mode
    (cherry picked from commit b807576180ef4ae0e85176bab364703f4af5b419)

--- modules/usrloc/urecord.c
+++ modules/usrloc/urecord.c
@@ -384,6 +384,33 @@ static inline int wb_timer(urecord_t* _r,query_list_t **ins_list)
 	return ins_done;
 }
 
+static inline int db_only_wb_timer(urecord_t* _r) {
+
+	if (db_mode != DB_ONLY)
+		return 0;
+
+	if (!_r) {
+		LM_ERR("no urecord!\n");
+		return -1;
+	}
+
+	if (wb_timer(_r, 0) < 0) {
+		LM_ERR("failed to sync with db\n");
+		return -1;
+	}
+
+	/* delete all the contacts left pending in the "to-be-delete" buffer */
+	if (cid_len &&
+	db_multiple_ucontact_delete(_r->domain, cid_keys, cid_vals, cid_len) < 0) {
+		LM_ERR("failed to delete contacts from database\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+
+
 
 
 int timer_urecord(urecord_t* _r,query_list_t **ins_list)
@@ -450,7 +477,7 @@ void release_urecord(urecord_t* _r, char is_replicated)
 {
 	if (db_mode==DB_ONLY) {
 		/* force flushing to DB*/
-		if (wb_timer(_r, 0) < 0)
+		if (db_only_wb_timer(_r) < 0)
 			LM_ERR("failed to sync with db\n");
 		/* now simply free everything */
 		free_urecord(_r);
@@ -533,6 +560,12 @@ int delete_ucontact(urecord_t* _r, struct ucontact* _c, char is_replicated)
 		}
 
 		mem_delete_ucontact(_r, _c);
+
+		if (db_mode == DB_ONLY) {
+			/* force flushing to DB*/
+			if (db_only_wb_timer(_r) < 0)
+				LM_ERR("failed to sync with db\n");
+		}
 	}
 
 	return 0;
