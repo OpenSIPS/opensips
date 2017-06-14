af584bc8f4667e8b6eb6b5aaf4923c8103fcf628
Bogdan-Andrei Iancu <bogdan@opensips.org>
2017-06-14 19:14:07 +0300

    Fix memory leak in do_routing()
    (cherry picked from commit 3c0df80d3b5ec8dfb5e54e78cd84153266fbf0f7)
    (cherry picked from commit df9c76f216b190e12d2eccd57e3eb1be1196094c)

--- modules/drouting/drouting.c
+++ modules/drouting/drouting.c
@@ -240,8 +240,6 @@ static int dr_disable_w_part(struct sip_msg *req,
 		struct head_db *current_partition);
 static int to_partition(struct sip_msg*, dr_partition_t *,
 		struct head_db **);
-static inline int init_part_grp(dr_part_group_t **, struct head_db *,
-		gparam_t*);
 
 
 /* reader-writers lock for reloading the data */
@@ -2051,28 +2049,22 @@ static inline str* build_ruri(struct sip_uri *uri, int strip, str *pri,
 }
 
 
-static inline int init_part_grp(dr_part_group_t ** part_w_no_grp,
-							struct head_db * current_partition, gparam_t * grp)
+static inline void pack_part_grp(dr_part_group_t ** part_w_no_grp,
+						struct head_db * current_partition, gparam_t * grp)
 {
-	dr_partition_t * part;
-	*part_w_no_grp = pkg_malloc(sizeof(dr_part_group_t));
-	if(*part_w_no_grp == NULL) {
-		LM_ERR("No more pkg memory.\n");
-		return -1;
-	}
+	static dr_part_group_t  part_grp;
+	static dr_partition_t   part;
 
-	part = pkg_malloc(sizeof(dr_partition_t));
-	if(part == NULL) {
-		LM_ERR("No more pkg memory.\n");
-		return -1;
-	}
-	memset(part, 0, sizeof(dr_partition_t));
-	part->type = DR_PTR_PART;
-	part->v.part = current_partition;
+	memset( &part_grp, 0, sizeof(dr_part_group_t));
+	memset( &part, 0, sizeof(dr_partition_t));
 
-	(*part_w_no_grp)->gid = grp;
-	(*part_w_no_grp)->dr_part = part;
-	return 0;
+	part.type = DR_PTR_PART;
+	part.v.part = current_partition;
+
+	part_grp.gid = grp;
+	part_grp.dr_part = &part;
+
+	*part_w_no_grp = &part_grp;
 }
 
 static int do_routing_0(struct sip_msg* msg)
@@ -2084,8 +2076,7 @@ static int do_routing_0(struct sip_msg* msg)
 			LM_ERR("Error while loading configuration\n");
 			return -1;
 		}
-		if(init_part_grp(&part_w_no_grp, head_db_start, 0) < 0)
-			return -1;
+		pack_part_grp(&part_w_no_grp, head_db_start, 0);
 		return do_routing(msg, part_w_no_grp, (int)0, NULL);
 	} else {
 		LM_ERR("Partition name is mandatory");
@@ -2412,7 +2403,7 @@ rule_fallback:
 		return -1;
 	}
 
-	init_part_grp(&part_grp, current_partition, &grp);
+	pack_part_grp(&part_grp, current_partition, &grp);
 	if (do_routing( msg, part_grp, flags, wl_list.type?&wl_list:NULL)==1) {
 		return 1;
 	}
