b06f4fb4e50c3e302ec5b1d32d0a026f5984c189
Bogdan-Andrei Iancu <bogdan@opensips.org>
2017-06-27 19:13:48 +0300

    Set proper UPDATED flag upon cloning.
    IF the cloning is full (with the updatable part too), set the UPDATED flag too
    
    (cherry picked from commit 0f9e65fef33d2846eaf0d3f17a469c321796cee5)

--- modules/tm/sip_msg.c
+++ modules/tm/sip_msg.c
@@ -1001,7 +1001,7 @@ struct sip_msg*  sip_msg_cloner( struct sip_msg *org_msg, int *sip_msg_len,
 		CLONE_RPL_LUMP_LIST( p, &(new_msg->reply_lump), org_msg->reply_lump);
 
 	case 1: /* updatable and cloning now */
-		new_msg->msg_flags |= FL_SHM_UPDATABLE;
+		new_msg->msg_flags |= FL_SHM_UPDATABLE|FL_SHM_UPDATED;
 		/* msg is updatable -> the fields that can be updated are allocated in 
 		 * separate memory chunks */
 		tm_shm_lock();
