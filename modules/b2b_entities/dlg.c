43afb8c85cd144159fc5ef44760c749e5c0a8ab7
Bogdan-Andrei Iancu <bogdan@opensips.org>
2017-06-27 19:58:36 +0300

    Do not expose the transaction when t_newtran() detects a retransmission
    It is much safer and consistent to avoid exposing the transaction when retransmision is detected; t_newtran() anyhow is automatically taking care of the retransmission handling, so makes no sense to expose the transaction further.
    
    (cherry picked from commit 51859e0455595b445e60f687592bdbc0a6cf64c6)

--- modules/b2b_entities/dlg.c
+++ modules/b2b_entities/dlg.c
@@ -676,7 +676,6 @@ search_dialog:
 			if(ret== 0)
 			{
 				LM_DBG("It is a retransmission, drop\n");
-				tmb.unref_cell(tmb.t_gett());
 			}
 			else
 				LM_DBG("Error when creating tm transaction\n");
@@ -838,7 +837,6 @@ logic_notify:
 			if(ret== 0)
 			{
 				LM_DBG("It is a retransmission, drop\n");
-				tmb.unref_cell(tmb.t_gett());
 			}
 			else
 				LM_DBG("Error when creating tm transaction\n");
