b10b142378a18a302360f56de495efead509f3c0
Liviu Chircu <liviu@opensips.org>
2017-10-10 11:49:05 +0300

    drouting: Fix verbose log line

--- modules/drouting/drouting.c
+++ modules/drouting/drouting.c
@@ -633,7 +633,7 @@ static int dr_disable_w_part(struct sip_msg *req, struct head_db *current_partit
 
 	gw = get_gw_by_id( (*current_partition->rdata)->pgw_tree, &id_val.s );
 	if (gw!=NULL && (gw->flags&DR_DST_STAT_DSBL_FLAG)==0) {
-		LM_INFO(" partition : %.*s\n", current_partition->partition.len,
+		LM_DBG("partition : %.*s\n", current_partition->partition.len,
 				current_partition->partition.s);
 		gw->flags |= DR_DST_STAT_DSBL_FLAG|DR_DST_STAT_DIRT_FLAG;
 		dr_gw_status_changed( current_partition, gw);
