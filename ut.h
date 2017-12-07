595c91590cbbb3eed3ba789d689281c7088f0ffe
Razvan Crainea <razvan@opensips.org>
2017-12-07 16:07:43 +0200

    ut: fix *_strdup() functions

--- ut.h
+++ ut.h
@@ -639,11 +639,12 @@ static inline char *shm_strdup(const char *str)
 	if (!str)
 		return NULL;
 
-	len = strlen(str) + 1;
-	rval = shm_malloc(len);
+	len = strlen(str);
+	rval = shm_malloc(len + 1);
 	if (!rval)
 		return NULL;
 	memcpy(rval, str, len);
+	rval[len] = '\0';
 	return rval;
 }
 
@@ -691,11 +692,12 @@ static inline char *pkg_strdup(const char *str)
 	if (!str)
 		return NULL;
 
-	len = strlen(str) + 1;
-	rval = pkg_malloc(len);
+	len = strlen(str);
+	rval = pkg_malloc(len + 1);
 	if (!rval)
 		return NULL;
 	memcpy(rval, str, len);
+	rval[len] = '\0';
 	return rval;
 }
 
