8b6884e98e8b4cbabcd3218f8b7a73864a07fc95
Liviu Chircu <liviu@opensips.org>
2017-12-19 17:31:28 +0200

    db: Fix a minor mem leak

--- db/db.c
+++ db/db.c
@@ -317,6 +317,7 @@ db_con_t* db_do_init(const str* url, void* (*new_connection)())
 		LM_DBG("connection %p inserted in pool as %p\n", id,con);
 	} else {
 		LM_DBG("connection %p found in pool as %p\n", id,con);
+		free_db_id(id);
 	}
 
 	if (!con->transfers) {
