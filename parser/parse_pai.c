908b9f190bff3b356f76ce8ca75d43a2c149a412
Bogdan-Andrei Iancu <bogdan@opensips.org>
2017-12-19 13:13:14 +0200

    Fix hdr parsing to accept multi-value bodies.
    The parser for Diversion, PAI and PPI use now the parse_multi_to() in order to handle multi name-addr/addr-spec hdr bodies.

--- parser/parse_pai.c
+++ parser/parse_pai.c
@@ -33,49 +33,47 @@
 /*
  * This method is used to parse P-Asserted-Identity header (RFC 3325).
  *
- * Currently only one name-addr / addr-spec is supported in the header
- * and it must contain a sip or sips URI.
- *
  * params: msg : sip msg
  * returns 0 on success,
  *        -1 on failure.
  */
 int parse_pai_header( struct sip_msg *msg )
 {
-    struct to_body* pai_b;
+	struct to_body* pai_b;
 
-    if ( !msg->pai &&
-	 (parse_headers(msg, HDR_PAI_F,0)==-1 || !msg->pai)) {
-	goto error;
-    }
+	if ( !msg->pai &&
+		(parse_headers(msg, HDR_PAI_F,0)==-1 || !msg->pai)) {
+		goto error;
+	}
 
-    /* maybe the header is already parsed! */
-    if (msg->pai->parsed)
+	/* maybe the header is already parsed! */
+	if (msg->pai->parsed)
+		return 0;
+
+	/* bad luck! :-( - we have to parse it */
+	/* first, get some memory */
+	pai_b = pkg_malloc(sizeof(struct to_body));
+	if (pai_b == NULL) {
+		LM_ERR("out of pkg memory\n");
+		goto error;
+	}
+
+	/* now parse it as a multi name-addr/addr-spec instance header !! */
+	parse_multi_to( msg->pai->body.s, msg->pai->body.s + msg->pai->body.len+1,
+		pai_b);
+	if (pai_b->error == PARSE_ERROR) {
+		LM_ERR("failed to parse P-Asserted-Identity header\n");
+		pkg_free(pai_b);
+		set_err_info(OSER_EC_PARSER, OSER_EL_MEDIUM,
+			"error parsing PAI header");
+		set_err_reply(400, "bad header");
+		goto error;
+	}
+
+	/* check header validity */
+	msg->pai->parsed = pai_b;
+
 	return 0;
-
-    /* bad luck! :-( - we have to parse it */
-    /* first, get some memory */
-    pai_b = pkg_malloc(sizeof(struct to_body));
-    if (pai_b == 0) {
-	LM_ERR("out of pkg_memory\n");
-	goto error;
-    }
-
-    /* now parse it!! */
-    parse_to(msg->pai->body.s,
-	     msg->pai->body.s + msg->pai->body.len+1,
-	     pai_b);
-    if (pai_b->error == PARSE_ERROR) {
-	LM_ERR("bad P-Asserted-Identity header\n");
-	pkg_free(pai_b);
-	set_err_info(OSER_EC_PARSER, OSER_EL_MEDIUM,
-		"error parsing PAI header");
-	set_err_reply(400, "bad header");
-	goto error;
-    }
- 	msg->pai->parsed = pai_b;
-
- 	return 0;
- error:
- 	return -1;
+error:
+	return -1;
 }
