3c39167e333a8801772f49f8561a015bfa1836f1
Ovidiu Sas <osas@voipembedded.com>
2018-02-05 09:53:54 -0500

    nathelper: fix test condition for updateing IP for fix_nated_sdp()

--- modules/nathelper/nathelper.c
+++ modules/nathelper/nathelper.c
@@ -989,7 +989,7 @@ alter_mediaip(struct sip_msg *msg, str *body, str *oldip, int oldpf,
 	str omip, nip, oip;
 
 	/* check that updating mediaip is really necessary */
-	if (oldpf == newpf && isnulladdr(oldip, oldpf) && !forcenulladdr)
+	if (oldpf == newpf || (isnulladdr(oldip, oldpf) && !forcenulladdr))
 		return 0;
 	if (newip->len == oldip->len &&
 	    memcmp(newip->s, oldip->s, newip->len) == 0)
