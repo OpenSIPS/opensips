40083e99dfb7591dfc8b68d6e2c20d8a265fab8f
Norm Brandinger <Norman.Brandinger@vonage.com>
2018-02-15 10:48:48 -0500

    Cherry pick updated code from OpenSIPS master that fixes the looping while draining an rtpproxy

--- modules/rtpproxy/rtpproxy.c
+++ modules/rtpproxy/rtpproxy.c
@@ -2277,13 +2277,15 @@ send_rtpp_command(struct rtpp_node *node, struct iovec *v, int vcnt)
 		/* Drain input buffer */
 		while ((poll(fds, 1, 0) == 1) &&
 		    ((fds[0].revents & POLLIN) != 0)) {
-			do {
-			    len = recv(rtpp_socks[node->idx], buf, sizeof(buf)-1, 0);
-				fds[0].revents = 0;
-			} while (len == -1 && errno == EINTR);
-			if (len <= 0) {
-				LM_ERR("can't read reply from a RTP proxy\n");
-				goto badproxy;
+			if (fds[0].revents & (POLLERR|POLLNVAL)) {
+				LM_ERR("error on rtpproxy socket %d!\n", rtpp_socks[node->idx]);
+				break;
+			}
+			fds[0].revents = 0;
+			if (recv(rtpp_socks[node->idx], buf, sizeof(buf) - 1, 0) < 0 &&
+				errno != EINTR) {
+				LM_ERR("error while draining rtpproxy socket %d!\n", errno);
+				break;
 			}
 		}
 		v[0].iov_base = gencookie();
