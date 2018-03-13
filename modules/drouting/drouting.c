164f0732df91a2fbe5c04df762c4c2953ac0178f
Vlad Paiu <vladpaiu@opensips.org>
2018-03-13 12:38:29 +0200

    Also flush carrier id AVPs when initiating a new routing
    Fixed cases where do_routing() followed by route_to_carrier() would report the wrong carrier id
    
    (cherry picked from commit 7844aaba9400e6480d341c18ac41dcc17b403070)

--- modules/drouting/drouting.c
+++ modules/drouting/drouting.c
@@ -2650,6 +2650,7 @@ static int do_routing(struct sip_msg* msg, dr_part_group_t * part_group,
 	if ((flags & DR_PARAM_ONLY_CHECK) == 0) {
 		destroy_avps( 0, current_partition->ruri_avp, 1);
 		destroy_avps( 0, current_partition->gw_id_avp, 1);
+		destroy_avps( 0, current_partition->carrier_id_avp, 1);
 		destroy_avps( 0, current_partition->gw_sock_avp, 1);
 		destroy_avps( 0, current_partition->rule_attrs_avp, 1);
 		destroy_avps( 0, current_partition->gw_attrs_avp, 1);
@@ -3144,6 +3145,7 @@ static int route2_carrier(struct sip_msg* msg, char* part_carrier,
 	/* do some cleanup first */
 	destroy_avps( 0, current_partition->ruri_avp, 1);
 	destroy_avps( 0, current_partition->gw_id_avp, 1);
+	destroy_avps( 0, current_partition->carrier_id_avp, 1);
 	destroy_avps( 0, current_partition->gw_sock_avp, 1);
 	destroy_avps( 0, current_partition->gw_attrs_avp, 1);
 	destroy_avps( 0, current_partition->rule_attrs_avp, 1);
