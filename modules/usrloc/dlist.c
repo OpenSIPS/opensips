OpenSIPS/opensips

commit 02e6a641292eb377b3afe9942ce4a78a4848855f

usrloc/nathelper: Implement pinging/timeouts in cachedb-only mode

modules/usrloc/dlist.c
@@ -321,7 +321,6 @@ static int get_domain_db_ucontacts(udomain_t *d, void *buf, int *len,
 			/* write the next hop */
 			memcpy(buf, &next_hop, sizeof next_hop);
 			buf += sizeof next_hop;
-			memcpy(&next_hop, (char *)buf - sizeof next_hop, sizeof next_hop);
 
 			*len -= needed;
 			if (!pack_coords)
@@ -345,7 +344,7 @@ static int get_domain_db_ucontacts(udomain_t *d, void *buf, int *len,
 	ul_dbf.free_result(ul_dbh, res);
 
 	/* len < 0 is possible, if size of the buffer < sizeof c->c.len */
-	if (zero_end && *len >= 0)
+	if (zero_end && *len >= (int)sizeof p_len)
 		memset(buf, 0, sizeof p_len);
 
 	/* Shouldn't happen */
@@ -362,6 +361,269 @@ error:
 	return -1;
 }
 
+static int
+cdb_pack_ping_data(const str *aor, const cdb_pair_t *contact,
+                   unsigned int ct_match_cflags, char **cpos,
+                   int *len, int pack_coords)
+{
+	enum {
+		COL_CONTACT = 1 << 0,
+		COL_RECEIVED = 1 << 1,
+		COL_PATH = 1 << 2,
+		COL_CFLAGS = 1 << 3,
+	};
+
+	ucontact_sip_coords *coords = NULL;
+	cdb_dict_t *ct_fields = (cdb_dict_t *)&contact->val.val.dict;
+	cdb_pair_t *pair;
+	struct sip_uri puri;
+	struct list_head *_;
+	unsigned int cflags;
+	struct socket_info *sock = NULL;
+	struct proxy_l next_hop;
+	str ct_uri, received, path, next_hop_uri;
+	int needed;
+	char *cp = *cpos;
+	int cols_needed = COL_CONTACT | COL_RECEIVED | COL_PATH | COL_CFLAGS;
+
+	if (!pack_coords)
+		goto skip_coords;
+
+	coords = shm_malloc(sizeof *coords + aor->len + contact->key.name.len);
+	if (!coords) {
+		LM_ERR("oom\n");
+		return 0;
+	}
+
+	coords->aor.s = (char *)(coords + 1);
+	str_cpy(&coords->aor, aor);
+
+	coords->ct_key.s = coords->aor.s + aor->len;
+	str_cpy(&coords->ct_key, &contact->key.name);
+
+skip_coords:
+	list_for_each (_, ct_fields) {
+		if (!cols_needed)
+			break;
+
+		pair = list_entry(_, cdb_pair_t, list);
+		switch (pair->key.name.s[0]) {
+		case 'c':
+			switch (pair->key.name.s[1]) {
+			case 'o':
+				ct_uri = pair->val.val.st;
+				cols_needed &= ~COL_CONTACT;
+				break;
+
+			case 'f':
+				cflags = flag_list_to_bitmask(&pair->val.val.st,
+				                              FLAG_TYPE_BRANCH, FLAG_DELIM);
+				cols_needed &= ~COL_CFLAGS;
+				break;
+
+			default:
+				continue;
+			}
+			break;
+
+		case 'p':
+			path = pair->val.val.st;
+			cols_needed &= ~COL_PATH;
+			break;
+
+		case 'r':
+			received = pair->val.val.st;
+			cols_needed &= ~COL_RECEIVED;
+			break;
+
+		default:
+			continue;
+		}
+	}
+
+	if (cols_needed) {
+		LM_BUG("missing contact columns in AoR %.*s\n", aor->len, aor->s);
+		goto out_free;
+	}
+
+	if ((cflags & ct_match_cflags) != ct_match_cflags)
+		goto out_free;
+
+	if (!ZSTR(received))
+		ct_uri = received;
+
+	needed = (int)(sizeof ct_uri.len + ct_uri.len + sizeof path.len + path.len
+	               + sizeof sock + sizeof cflags + sizeof next_hop);
+
+	if (pack_coords)
+		needed += sizeof(ucontact_coords);
+
+	if (*len < needed)
+		return needed;
+
+	/* determine the next hop towards this contact */
+	if (ZSTR(path)) {
+		next_hop_uri = ct_uri;
+	} else {
+		if (get_path_dst_uri(&path, &next_hop_uri) < 0) {
+			LM_ERR("failed to get dst_uri for Path\n");
+			goto out_free;
+		}
+	}
+
+	if (parse_uri(next_hop_uri.s, next_hop_uri.len, &puri) < 0) {
+		LM_ERR("failed to parse URI of next hop: '%.*s'\n",
+		       next_hop_uri.len, next_hop_uri.s);
+		goto out_free;
+	}
+
+	memcpy(cp, &ct_uri.len, sizeof ct_uri.len);
+	cp += sizeof ct_uri.len;
+	memcpy(cp, ct_uri.s, ct_uri.len);
+	cp += ct_uri.len;
+
+	memcpy(cp, &path.len, sizeof path.len);
+	cp += sizeof path.len;
+	memcpy(cp, path.s, path.len);
+	cp += path.len;
+
+	memcpy(cp, &sock, sizeof sock);
+	cp += sizeof sock;
+
+	memcpy(cp, &cflags, sizeof cflags);
+	cp += sizeof cflags;
+
+	memset(&next_hop, 0, sizeof next_hop);
+	next_hop.port  = puri.port_no;
+	next_hop.proto = puri.proto;
+	next_hop.name  = puri.host;
+	memcpy(cp, &next_hop, sizeof next_hop);
+	cp += sizeof next_hop;
+
+	*len -= needed;
+	if (pack_coords) {
+		memcpy(cp, &coords, sizeof(ucontact_coords));
+		cp += sizeof(ucontact_coords);
+	}
+
+	*cpos = cp;
+	return 0;
+
+out_free:
+	shm_free(coords);
+	return 0;
+}
+
+static int
+get_domain_cdb_ucontacts(udomain_t *d, void *buf, int *len,
+                         unsigned int flags, unsigned int part_idx,
+                         unsigned int part_max, char zero_end, int pack_coords)
+{
+	static const cdb_key_t aorhash_key = {str_init("aorhash"), 0}; /* TODO */
+	struct list_head *_, *__;
+	int cur_node_idx, nr_nodes, min, max;
+	char *cpos;
+	double unit;
+	cdb_filter_t *aorh_filter;
+	cdb_res_t res;
+	cdb_row_t *row;
+	int_str_t val;
+	cdb_pair_t *pair;
+	cdb_dict_t *contacts;
+	str contacts_key = str_init("contacts"); /* TODO */
+	str *aor;
+	enum cdb_filter_op rhs_op;
+	int shortage;
+
+	cur_node_idx = clusterer_api.get_my_index(
+	         ul_replication_cluster, &contact_repl_cap, &nr_nodes);
+
+	unit = MAX_DB_AOR_HASH / (double)(part_max * nr_nodes);
+	min = (int)(unit * part_max * cur_node_idx + unit * part_idx);
+	max = (int)(unit * part_max * cur_node_idx + unit * (part_idx + 1));
+
+	val.is_str = 0;
+	val.i = min;
+	aorh_filter = cdb_append_filter(NULL, &aorhash_key, CDB_OP_GTE, &val);
+	if (!aorh_filter) {
+		LM_ERR("oom\n");
+		return -1;
+	}
+
+	rhs_op = (max == MAX_DB_AOR_HASH) ? CDB_OP_LTE : CDB_OP_LT;
+
+	val.i = max;
+	aorh_filter = cdb_append_filter(aorh_filter, &aorhash_key, rhs_op, &val);
+	if (!aorh_filter) {
+		LM_ERR("oom\n");
+		return -1;
+	}
+
+	LM_DBG("idx=%d/max=%d, node=%d/nr_nodes=%d, "
+	       "filter: %d <= aorhash <%s %d\n", part_idx, part_max, cur_node_idx,
+	       nr_nodes, min, max == MAX_DB_AOR_HASH ? "=" : "", max);
+
+	/* spread ping workload evenly across pinging interval second ticks,
+	 * CPU cores and current number of cluster nodes, all in one query! */
+	if (cdbf.query(cdbc, aorh_filter, &res) != 0) {
+		LM_ERR("failed to fetch contacts to ping\n");
+		return -1;
+	}
+
+	LM_DBG("fetched %d results\n", res.count);
+
+	/* Reserve space for terminating 0000 */
+	if (zero_end)
+		*len -= sizeof ((ucontact_t *)0)->c.len;
+
+	cpos = buf;
+	shortage = 0;
+
+	list_for_each (_, &res.rows) {
+		row = list_entry(_, cdb_row_t, list);
+		aor = NULL;
+		contacts = NULL;
+
+		/* locate the 'aor' and 'contacts' fields */
+		list_for_each (__, &row->dict) {
+			pair = list_entry(__, cdb_pair_t, list);
+			if (pair->key.is_pk) {
+				aor = &pair->val.val.st;
+				if (contacts)
+					goto pack_data;
+			} else {
+				if (!str_strcmp(&pair->key.name, &contacts_key)) {
+					contacts = &pair->val.val.dict;
+					if (aor)
+						goto pack_data;
+				}
+			}
+		}
+
+		LM_BUG("found entry with missing 'contacts' or 'aor' field!\n");
+		continue;
+
+pack_data:
+		list_for_each (__, contacts) {
+			pair = list_entry(__, cdb_pair_t, list);
+			shortage += cdb_pack_ping_data(aor, pair, flags, &cpos, len,
+			                               pack_coords);
+		}
+	}
+
+	cdb_free_rows(&res);
+	cdb_free_filters(aorh_filter);
+
+	if (zero_end && *len >= (int)sizeof ((ucontact_t *)0)->c.len) {
+		memset(cpos, 0, sizeof ((ucontact_t *)0)->c.len);
+		*len -= sizeof ((ucontact_t *)0)->c.len;
+	}
+
+	if (shortage)
+		return shortage - *len;
+
+	return 0;
+}
 
 static inline int
 get_domain_mem_ucontacts(udomain_t *d,void *buf, int *len, unsigned int flags,
@@ -508,8 +770,10 @@ get_domain_mem_ucontacts(udomain_t *d,void *buf, int *len, unsigned int flags,
 		unlock_ulslot(d, i);
 	}
 	/* len < 0 is possible, if size of the buffer < sizeof(c->c.len) */
-	if (zero_end && *len >= 0)
+	if (zero_end && *len >= (int)sizeof(c->c.len)) {
 		memset(cp, 0, sizeof(c->c.len));
+		*len -= sizeof(c->c.len);
+	}
 
 	/* Shouldn't happen */
 	if (shortage > 0 && *len > shortage) {
@@ -623,7 +887,8 @@ int get_domain_ucontacts(udomain_t *d, void *buf, int len, unsigned int flags,
 		return get_domain_db_ucontacts(d, buf, &len,
 							flags, part_idx, part_max, 1, pack_coords);
 	else if (cluster_mode == CM_CORE_CACHEDB_ONLY)
-		abort(); /* TODO */
+		return get_domain_cdb_ucontacts(d, buf, &len,
+		                    flags, part_idx, part_max, 1, pack_coords);
 	else
 		return get_domain_mem_ucontacts(d, buf, &len, flags,
 											part_idx, part_max, 1, pack_coords);
@@ -874,6 +1139,48 @@ ucontact_t* get_ucontact_from_id(udomain_t *d, uint64_t contact_id, urecord_t **
 	return NULL;
 }
 
+int cdb_delete_ucontact_coords(ucontact_sip_coords *sip_key)
+{
+	static const cdb_key_t aor_key = {{"aor", 3}, 1}; /* TODO */
+	int_str_t val;
+	cdb_filter_t *aor_filter;
+	cdb_pair_t *pair;
+	cdb_dict_t updates;
+	cdb_key_t contacts_key;
+	int rc = 0;
+
+	val.is_str = 1;
+	val.s = sip_key->aor;
+	aor_filter = cdb_append_filter(NULL, &aor_key, CDB_OP_EQ, &val);
+	if (!aor_filter) {
+		LM_ERR("oom\n");
+		return -1;
+	}
+
+	cdb_dict_init(&updates);
+	cdb_key_init(&contacts_key, "contacts"); /* TODO */
+	pair = cdb_mk_pair(&contacts_key, &sip_key->ct_key);
+	if (!pair) {
+		cdb_free_filters(aor_filter);
+		LM_ERR("oom\n");
+		return -1;
+	}
+
+	pair->unset = 1;
+
+	cdb_dict_add(pair, &updates);
+	if (cdbf.update(cdbc, aor_filter, &updates) < 0) {
+		LM_ERR("failed to delete AoR %.*s, ct: %.*s\n",
+		       sip_key->aor.len, sip_key->aor.s,
+		       sip_key->ct_key.len, sip_key->ct_key.s);
+		rc = -1;
+	}
+
+	cdb_free_filters(aor_filter);
+	cdb_free_entries(&updates, NULL);
+	return rc;
+}
+
 int delete_ucontact_from_coords(udomain_t *d, ucontact_coords ct_coords,
                                 char is_replicated)
 {
@@ -892,6 +1199,13 @@ int delete_ucontact_from_coords(udomain_t *d, ucontact_coords ct_coords,
 			LM_ERR("failed to remove contact from db\n");
 			return -1;
 		}
+		return 0;
+	} else if (cluster_mode == CM_CORE_CACHEDB_ONLY) {
+		if (cdb_delete_ucontact_coords((ucontact_sip_coords *)ct_coords)) {
+			LM_ERR("failed to remove contact from cache\n");
+			return -1;
+		}
+
 		return 0;
 	}
 

modules/nathelper/nathelper.c
@@ -526,11 +526,6 @@ mod_init(void)
 		if (bind_usrloc(&ul) < 0)
 			return -1;
 
-		if (ul.cluster_mode == CM_CORE_CACHEDB_ONLY) {
-			LM_ERR("support for cachedb core usrloc not available yet\n");
-			return -1;
-		}
-
 		if (force_socket_str) {
 			socket_str.s=force_socket_str;
 			socket_str.len=strlen(socket_str.s);
@@ -1753,9 +1748,9 @@ ping_checker_timer(unsigned int ticks, void *timer_idx)
 		lock_hash(cell->hash_id);
 
 		/* for these cells threshold has been exceeded */
+		cell->not_responded++;
 		LM_DBG("cell with ucoords %llu has %d unresponded pings\n",
 		       (unsigned long long)cell->ct_coords, cell->not_responded);
-		cell->not_responded++;
 
 		if (cell->not_responded >= max_pings_lost) {
 			LM_DBG("cell with ucoords %llu exceeded max failed pings! "

modules/nathelper/nh_table.c
@@ -135,7 +135,7 @@ struct ping_cell *get_cell(int hash_id, ucontact_coords coords)
 	cell = entry->first;
 
 	for (cell=entry->first; cell; cell = cell->next) {
-		if (cell->ct_coords == coords)
+		if (!ul.ucontact_coords_cmp(cell->ct_coords, coords))
 			return cell;
 	}
 

modules/usrloc/dlist.h
@@ -110,6 +110,11 @@ int find_domain(str* _d, udomain_t** _p);
 typedef udomain_t* (*get_next_udomain_t) (udomain_t* _d);
 udomain_t* get_next_udomain(udomain_t *_d);
 
+/* when using various DBs (SQL/NoSQL) in order to store AoR hashes, it's best
+ * to drop the MSB and assume that none of them support unsigned integers */
+#define DB_AOR_HASH_MASK (1U << 31)
+#define MAX_DB_AOR_HASH (DB_AOR_HASH_MASK - 1U)
+
 /*contact label may not be higher than 14 bits*/
 #define CLABEL_MASK ((1<<14)-1)
 #define CLABEL_INC_AND_TEST(_clabel_) ((_clabel_+1)&CLABEL_MASK)

modules/usrloc/ucontact.c
@@ -958,6 +958,23 @@ int update_ucontact(struct urecord* _r, ucontact_t* _c, ucontact_info_t* _ci,
 	return 0;
 }
 
+int ucontact_coords_cmp(ucontact_coords _a, ucontact_coords _b)
+{
+	ucontact_sip_coords *a, *b;
+
+	if (cluster_mode != CM_CORE_CACHEDB_ONLY)
+		return _a == _b ? 0 : -1;
+
+	a = (ucontact_sip_coords *)_a;
+	b = (ucontact_sip_coords *)_b;
+
+	if (a->aor.len != b->aor.len || a->ct_key.len != b->ct_key.len ||
+		  str_strcmp(&a->aor, &b->aor) || str_strcmp(&a->ct_key, &b->ct_key))
+		return -1;
+
+	return 0;
+}
+
 void free_ucontact_coords(ucontact_coords coords)
 {
 	if (cluster_mode == CM_CORE_CACHEDB_ONLY)

modules/usrloc/ucontact.h
@@ -139,7 +139,7 @@ typedef struct ucontact_info {
 /*! \brief
  * The representation (lookup coordinates) of a contact:
  *	- ucontact_id: suitable for in-memory storage or when
- *        running in CM_SQL_ONLY, which benefits from auto-generated ids
+ *        running in CM_SQL_ONLY, which benefits from auto-generated keys
  *	- ucontact_sip_coords *: a more verbose way of locating a contact.
  *        This is required when the user location is held inside distributed
  *        NoSQL databases, which have limited support for primary keys, hence
@@ -153,7 +153,7 @@ typedef struct {
 	str ct_key;
 } ucontact_sip_coords;
 
-typedef void (*free_ucontact_coords_t) (ucontact_coords coords);
+int ucontact_coords_cmp(ucontact_coords a, ucontact_coords b);
 void free_ucontact_coords(ucontact_coords coords);
 
 /*! \brief

modules/usrloc/udomain.c
@@ -451,7 +451,7 @@ get_static_urecord(const udomain_t* _d, const str* _aor, struct urecord** _r)
 	memset( &r, 0, sizeof(struct urecord) );
 	r.aor = *_aor;
 	r.domain = _d->name;
-	r.aorhash = core_hash(_aor, 0, 0)&(_d->size-1);
+	r.aorhash = core_hash(_aor, 0, DB_AOR_HASH_MASK);
 
 	*_r = &r;
 }
@@ -1142,7 +1142,7 @@ urecord_t* db_load_urecord(db_con_t* _c, udomain_t* _d, str *_aor)
 urecord_t* cdb_load_urecord(db_con_t* _c, const udomain_t* _d,
                                 const str *_aor)
 {
-	static const cdb_key_t aor_key = {{"aor", 3}, 1};
+	static const cdb_key_t aor_key = {{"aor", 3}, 1}; /* TODO */
 	struct list_head *_;
 	ucontact_info_t *ci;
 	cdb_filter_t *aor_filter;
@@ -1150,7 +1150,7 @@ urecord_t* cdb_load_urecord(db_con_t* _c, const udomain_t* _d,
 	cdb_res_t res;
 	cdb_row_t *row;
 	cdb_pair_t *contacts, *pair;
-	str contact, contacts_key = str_init("contacts");
+	str contact, contacts_key = str_init("contacts"); /* TODO */
 
 	urecord_t *r;
 	ucontact_t *c;

modules/usrloc/urecord.c
@@ -685,6 +685,11 @@ int cdb_flush_urecord(urecord_t *_r)
 	if (cdb_dict_empty(&ct_changes))
 		goto out;
 
+	if (CDB_DICT_ADD_INT32(&ct_changes, "aorhash", _r->aorhash) != 0) {
+		LM_ERR("oom\n");
+		goto err_free;
+	}
+
 	val.is_str = 1;
 	val.s = *ct->aor;
 	aor_filter = cdb_append_filter(NULL, &aor_key, CDB_OP_EQ, &val);

commit 2bb11c599095fbef90706bad9e314feba80d107f

usrloc: Fix possible crash during contact pinging
This patch fixes a bug introduced in commit 02e6a641292, where the
output buffer does not contain a leading "0000" marker despite the
"ul.get_all_contacts()" function returning a 0 ("all contacts have been
dumped, the buffer is properly formatted").

Reported by @thesipguy

Credits to Răzvan Crainea for spotting the bug

Fixes #1352

modules/usrloc/dlist.c
@@ -134,7 +134,7 @@ static int get_domain_db_ucontacts(udomain_t *d, void *buf, int *len,
 
 	/* Reserve space for terminating 0000 */
 	if (zero_end)
-		*len -= sizeof p_len;
+		*len -= (int)sizeof p_len;
 
 	/* get the current time in DB format */
 	now_len = 25;
@@ -345,7 +345,7 @@ static int get_domain_db_ucontacts(udomain_t *d, void *buf, int *len,
 	ul_dbf.free_result(ul_dbh, res);
 
 	/* len < 0 is possible, if size of the buffer < sizeof c->c.len */
-	if (zero_end && *len >= (int)sizeof p_len)
+	if (zero_end && *len >= 0)
 		memset(buf, 0, sizeof p_len);
 
 	/* Shouldn't happen */
@@ -575,7 +575,7 @@ get_domain_cdb_ucontacts(udomain_t *d, void *buf, int *len,
 
 	/* Reserve space for terminating 0000 */
 	if (zero_end)
-		*len -= sizeof ((ucontact_t *)0)->c.len;
+		*len -= (int)sizeof ((ucontact_t *)0)->c.len;
 
 	cpos = buf;
 	shortage = 0;
@@ -615,10 +615,8 @@ pack_data:
 	cdb_free_rows(&res);
 	cdb_free_filters(aorh_filter);
 
-	if (zero_end && *len >= (int)sizeof ((ucontact_t *)0)->c.len) {
+	if (zero_end && *len >= 0)
 		memset(cpos, 0, sizeof ((ucontact_t *)0)->c.len);
-		*len -= sizeof ((ucontact_t *)0)->c.len;
-	}
 
 	if (shortage)
 		return shortage - *len;
@@ -646,7 +644,7 @@ get_domain_mem_ucontacts(udomain_t *d,void *buf, int *len, unsigned int flags,
 	shortage = 0;
 	/* Reserve space for terminating 0000 */
 	if (zero_end)
-		*len -= sizeof(c->c.len);
+		*len -= (int)sizeof(c->c.len);
 
 	if (cluster_mode == CM_FULL_SHARING
 	    || cluster_mode == CM_FULL_SHARING_CACHEDB) {
@@ -773,10 +771,8 @@ get_domain_mem_ucontacts(udomain_t *d,void *buf, int *len, unsigned int flags,
 		unlock_ulslot(d, i);
 	}
 	/* len < 0 is possible, if size of the buffer < sizeof(c->c.len) */
-	if (zero_end && *len >= (int)sizeof(c->c.len)) {
+	if (zero_end && *len >= 0)
 		memset(cp, 0, sizeof(c->c.len));
-		*len -= sizeof(c->c.len);
-	}
 
 	/* Shouldn't happen */
 	if (shortage > 0 && *len > shortage) {