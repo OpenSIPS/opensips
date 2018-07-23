9234a417ab8a4fefeef370300e7f0a76cd2c025d
Razvan Crainea <razvan@opensips.org>
2018-07-23 13:39:25 +0300

    proto_tcp/bin/hep: fix deadlock when delaying writes
    (cherry picked from commit 058cc22cb55dce9b890308b9f83a42a88691f2c8)

--- modules/proto_bin/proto_bin.c
+++ modules/proto_bin/proto_bin.c
@@ -495,8 +495,9 @@ inline static int _bin_write_on_socket(struct tcp_connection *c, int fd,
 		 * to be sent, otherwise we will completely break the messages' order
 		 */
 		if (((struct bin_data*)c->proto_data)->async_chunks_no)
-			return add_write_chunk(c, buf, len, 0);
-		n=async_tsend_stream(c,fd,buf,len, bin_async_local_write_timeout);
+			n = add_write_chunk(c, buf, len, 0);
+		else
+			n = async_tsend_stream(c,fd,buf,len, bin_async_local_write_timeout);
 	} else {
 		n = tsend_stream(fd, buf, len, bin_send_timeout);
 	}
