791af6f6e4c2a09d7a54314cba793b3e536330f6
Dan Pascu <dan@ag-projects.com>
2018-09-12 12:05:37 +0300

    Adjust the reactor size in accordance to the type of memory being used

--- reactor.c
+++ reactor.c
@@ -40,56 +40,67 @@ int init_reactor_size(void)
 {
 	struct rlimit lim;
 	int n, pc;
+	char *mem_type;
+	unsigned long mem_size;
+
+#if defined(PKG_MALLOC)
+	mem_type = "pkg";
+	mem_size = pkg_mem_size;
+#elif defined(USE_SHM_MEM)
+	mem_type = "shm";
+	mem_size = shm_mem_size;
+#else
+	mem_type = "system";
+	mem_size = 2*1024*1024*1024UL;  // assume 2Gb per process to avoid reaching limits below
+#endif
 
 	n = sizeof(struct fd_map) + sizeof(struct pollfd);
 
-	if (open_files_limit>0) {
-
-		/* the fd limit was explicitly set, so just follow but only warn 
+	if (open_files_limit > 0) {
+		/* the fd limit was explicitly set, so just follow but only warn
 		 * if too much memory is to consumed by reactor */
-		pc = 100*n*open_files_limit / pkg_mem_size;
-		if (pc>=80) {
-			LM_ERR("required memory for a %d files reactor is over 80%% of"
-				" the configured pkg mem (%luMb)\n",
-				open_files_limit, pkg_mem_size/1024/1024);
-			LM_ERR("Please consider increasing the pkg memory or reduce the"
-				" limit of open files...Exiting\n");
+		pc = 100 * n * open_files_limit / mem_size;
+		if (pc >= 80) {
+			LM_ERR("required memory for a %d files reactor is over 80%% of "
+				   "the configured %s mem (%luMb)\n",
+				   open_files_limit, mem_type, mem_size/1024/1024);
+			LM_ERR("Please consider increasing the %s memory or reduce the "
+				   "limit of open files...Exiting\n", mem_type);
 			return -1;
-		} else if (pc>=50) {
-			LM_WARN("required memory for a %d files reactor is over 50%% of"
-				" the configured pkg mem (%luMb)\n",
-				open_files_limit, pkg_mem_size/1024/1024);
-			LM_WARN("PKG memory may not be enough at runtime (consider "
-				"increasing it), still continuing\n");
+		} else if (pc >= 50) {
+			LM_WARN("required memory for a %d files reactor is over 50%% of "
+					"the configured %s mem (%luMb)\n",
+					open_files_limit, mem_type, mem_size/1024/1024);
+			LM_WARN("%s memory may not be enough at runtime (consider "
+					"increasing it), still continuing\n", mem_type);
 		}
 		/* seems to have enough mem -> size the reactor based on open files */
 		reactor_size = open_files_limit;
-
 	} else {
-
 		/* auto detect the limit of open files */
 		if (getrlimit(RLIMIT_NOFILE, &lim)<0){
 			LM_ERR("cannot get the maximum number of file descriptors: %s\n",
-				strerror(errno));
+				   strerror(errno));
 			return -1;
 		}
 
-		/* calculate the size to fit into 10% PKG mem */
-		reactor_size = pkg_mem_size * FD_MEM_PERCENT / (100*n);
+		/* calculate the size to fit into 10% of mem */
+		reactor_size = mem_size / n * FD_MEM_PERCENT / 100;
 
-		if (reactor_size<lim.rlim_cur) {
-			LM_WARN("shrinking reactor size from %lu (autodetected via rlimit)"
-				" to %d (limited by memory of %d%% from %luMb)\n",
-				lim.rlim_cur,reactor_size,FD_MEM_PERCENT,pkg_mem_size/1024/1024);
+		if (reactor_size < lim.rlim_cur) {
+			LM_WARN("shrinking reactor size from %lu (autodetected via rlimit) "
+					"to %d (limited by memory of %d%% from %luMb)\n",
+					lim.rlim_cur, reactor_size, FD_MEM_PERCENT, mem_size/1024/1024);
 			LM_WARN("use 'open_files_limit' to enforce other limit or "
-				"increase PKG memory\n");
+					"increase %s memory\n", mem_type);
 		} else {
 			/* enouhg memory, use as limit the fd limit */
 			reactor_size = lim.rlim_cur;
 		}
 	}
 
-	LM_DBG("using reactor size %d\n",reactor_size);
+	LM_INFO("reactor size %d (can use up to %.2fMb of memory)\n",
+			reactor_size, 1.0 * n * reactor_size / 1024 / 1024);
 
 	return 0;
 }
