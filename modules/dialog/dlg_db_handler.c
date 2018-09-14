6fcbcc816b6d8224ed97f14cec74181c0fe6423f
Razvan Crainea <razvan@opensips.org>
2018-09-14 16:37:08 +0300

    dialog: fix deadlock when freeing a cell on timer
    When freeing a dialog on timer, sequential destroy callbacks might try
    to fetch the dialog lock, resulting in a deadlock. The spotted backtrace
    is dialog_update_db -> destroy_dlg -> free_dlg_dlg ->
    destroy_dlg_callbacks_list -> unref_acc_ctx -> free_acc_ctx ->
    store_dlg_value
    
    Thanks go to Jonathan Hulme for spotting this and offering testing

--- modules/dialog/dlg_db_handler.c
+++ modules/dialog/dlg_db_handler.c
@@ -1435,8 +1435,10 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 
 		/* lock the whole entry */
 		entry = &((d_table->entries)[index]);
-		if (do_lock)
+		if (do_lock) {
 			dlg_lock( d_table, entry);
+			cell->locked_by = process_no;
+		}
 
 		for (cell = entry->first; cell != NULL; ) {
 			callee_leg = callee_idx(cell);
@@ -1589,8 +1591,10 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 			}
 			cell = cell->next;
 		}
-		if (do_lock)
+		if (do_lock) {
+			cell->locked_by = 0;
 			dlg_unlock( d_table, entry);
+		}
 
 	}
 
