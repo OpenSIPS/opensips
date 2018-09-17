113973a7b70c9a89fb0f0e730fcb2eef0459c706
Bogdan-Andrei Iancu <bogdan@opensips.org>
2018-09-17 10:58:35 +0300

    Rebuild DB schema

--- scripts/sqlite/registrant-create.sql
+++ scripts/sqlite/registrant-create.sql
@@ -1,13 +1,13 @@
 INSERT INTO version (table_name, table_version) values ('registrant','1');
 CREATE TABLE registrant (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    registrar CHAR(256) DEFAULT '' NOT NULL,
-    proxy CHAR(256) DEFAULT NULL,
-    aor CHAR(256) DEFAULT '' NOT NULL,
-    third_party_registrant CHAR(256) DEFAULT NULL,
+    registrar CHAR(255) DEFAULT '' NOT NULL,
+    proxy CHAR(255) DEFAULT NULL,
+    aor CHAR(255) DEFAULT '' NOT NULL,
+    third_party_registrant CHAR(255) DEFAULT NULL,
     username CHAR(64) DEFAULT NULL,
     password CHAR(64) DEFAULT NULL,
-    binding_URI CHAR(256) DEFAULT '' NOT NULL,
+    binding_URI CHAR(255) DEFAULT '' NOT NULL,
     binding_params CHAR(64) DEFAULT NULL,
     expiry INTEGER DEFAULT NULL,
     forced_socket CHAR(64) DEFAULT NULL,
