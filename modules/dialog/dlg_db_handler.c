OpenSIPS/opensips

commit 6fcbcc816b6d8224ed97f14cec74181c0fe6423f

dialog: fix deadlock when freeing a cell on timer
When freeing a dialog on timer, sequential destroy callbacks might try
to fetch the dialog lock, resulting in a deadlock. The spotted backtrace
is dialog_update_db -> destroy_dlg -> free_dlg_dlg ->
destroy_dlg_callbacks_list -> unref_acc_ctx -> free_acc_ctx ->
store_dlg_value

Thanks go to Jonathan Hulme for spotting this and offering testing

modules/dialog/dlg_db_handler.c
@@ -1435,8 +1435,10 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 
 		/* lock the whole entry */
 		entry = &((d_table->entries)[index]);
-		if (do_lock)
+		if (do_lock) {
 			dlg_lock( d_table, entry);
+			cell->locked_by = process_no;
+		}
 
 		for (cell = entry->first; cell != NULL; ) {
 			callee_leg = callee_idx(cell);
@@ -1589,8 +1591,10 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 			}
 			cell = cell->next;
 		}
-		if (do_lock)
+		if (do_lock) {
+			cell->locked_by = 0;
 			dlg_unlock( d_table, entry);
+		}
 
 	}
 

commit af98f58a4c30f19af2b2f7da99f389b9f9c252d7

fix possible crash introduced in 6fcbcc8

modules/dialog/dlg_db_handler.c
@@ -1435,10 +1435,8 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 
 		/* lock the whole entry */
 		entry = &((d_table->entries)[index]);
-		if (do_lock) {
+		if (do_lock)
 			dlg_lock( d_table, entry);
-			cell->locked_by = process_no;
-		}
 
 		for (cell = entry->first; cell != NULL; ) {
 			callee_leg = callee_idx(cell);
@@ -1452,7 +1450,9 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 						/* mark it as deleted so as we don't deal with it later */
 						cell->flags |= DLG_FLAG_DB_DELETED;
 						/* timer is done with this dialog */
+						cell->locked_by = process_no;
 						unref_dlg_unsafe(cell,1,entry);
+						cell->locked_by = 0;
 						cell=next_cell;
 						continue;
 					}
@@ -1524,7 +1524,9 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 					ins_done=1;
 
 				/* dialog saved */
+				cell->locked_by = process_no;
 				run_dlg_callbacks( DLGCB_DB_SAVED, cell, 0, DLG_DIR_NONE, NULL,1);
+				cell->locked_by = 0;
 
 				cell->flags &= ~(DLG_FLAG_NEW |DLG_FLAG_CHANGED|DLG_FLAG_VP_CHANGED);
 
@@ -1566,7 +1568,9 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 				}
 
 				/* dialog saved */
+				cell->locked_by = process_no;
 				run_dlg_callbacks( DLGCB_DB_SAVED, cell, 0, DLG_DIR_NONE, NULL,1);
+				cell->locked_by = 0;
 
 				cell->flags &= ~(DLG_FLAG_CHANGED|DLG_FLAG_VP_CHANGED);
 			} else if (db_flush_vp && (cell->flags & DLG_FLAG_VP_CHANGED)) {
@@ -1585,17 +1589,16 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 					continue;
 				}
 
+				cell->locked_by = process_no;
 				run_dlg_callbacks( DLGCB_DB_SAVED, cell, 0, DLG_DIR_NONE, NULL,1);
+				cell->locked_by = 0;
 
 				cell->flags &= ~DLG_FLAG_VP_CHANGED;
 			}
 			cell = cell->next;
 		}
-		if (do_lock) {
-			cell->locked_by = 0;
+		if (do_lock)
 			dlg_unlock( d_table, entry);
-		}
-
 	}
 
 	if (ins_done) {