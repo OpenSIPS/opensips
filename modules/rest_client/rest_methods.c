d72409d6633514d9c155ab8f2c22fcde2032ab9d
Razvan Crainea <razvan@opensips.org>
2018-09-26 12:56:51 +0300

    rest_client: prevent overflow in Content-Type
    (cherry picked from commit c92794e1899ca5fbfc8e9ad488026cc5f344e68a)

--- modules/rest_client/rest_methods.c
+++ modules/rest_client/rest_methods.c
@@ -201,7 +201,7 @@ int start_async_http_req(struct sip_msg *msg, enum rest_client_method method,
 		w_curl_easy_setopt(handle, CURLOPT_POSTFIELDS, req_body);
 
 		if (req_ctype) {
-			sprintf(print_buff, "Content-Type: %s", req_ctype);
+			snprintf(print_buff, MAX_CONTENT_TYPE_LEN, "Content-Type: %s", req_ctype);
 			header_list = curl_slist_append(header_list, print_buff);
 			w_curl_easy_setopt(handle, CURLOPT_HTTPHEADER, header_list);
 		}
@@ -606,7 +606,7 @@ int rest_post_method(struct sip_msg *msg, char *url, char *body, char *ctype,
 	}
 
 	if (ctype) {
-		sprintf(print_buff, "Content-Type: %s", ctype);
+		snprintf(print_buff, MAX_CONTENT_TYPE_LEN, "Content-Type: %s", ctype);
 		header_list = curl_slist_append(header_list, print_buff);
 	}
 
