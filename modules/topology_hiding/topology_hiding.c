baae077c7a2224c7eda807caba9781e3a0825877
Liviu Chircu <liviu@opensips.org>
2018-09-27 17:50:40 +0300

    topology_hiding: Fix possible startup crash
    How to reproduce:
        loadmodule "topology_hiding.so"
        loadmodule "dialog.so"
    ... without enabling "force_dialog".
    
    Alternative, temporary fix (until you update your binaries):
        loadmodule "dialog.so"
        loadmodule "topology_hiding.so"
    
    (cherry picked from commit d837021b1558da7a147197e60e2d6d61570dc978)

--- modules/topology_hiding/topology_hiding.c
+++ modules/topology_hiding/topology_hiding.c
@@ -91,6 +91,7 @@ static module_dependency_t *get_deps_dialog(param_export_t *param)
 static dep_export_t deps = {
 	{ /* OpenSIPS module dependencies */
 		{ MOD_TYPE_DEFAULT, "tm", DEP_ABORT },
+		{ MOD_TYPE_DEFAULT, "dialog", DEP_SILENT },
 		{ MOD_TYPE_NULL, NULL, 0 },
 	},
 	{ /* modparam dependencies */
