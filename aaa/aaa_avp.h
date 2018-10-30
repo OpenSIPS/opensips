a04bf76bbbdd5be3e3f3d9565655e14f6f133467
Bogdan-Andrei Iancu <bogdan@opensips.org>
2018-10-30 12:21:27 +0200

    Simple formating

--- aaa/aaa_avp.h
+++ aaa/aaa_avp.h
@@ -25,38 +25,38 @@
 #ifndef _AAA_AVP_H
 #define _AAA_AVP_H
 
-#define	A_USER_NAME					0
+#define	A_USER_NAME						0
 #define	A_SERVICE_TYPE					1
 #define	A_CALLED_STATION_ID				2
-#define	A_CALLING_STATION_ID			        3
+#define	A_CALLING_STATION_ID			3
 #define	A_ACCT_STATUS_TYPE				4
 #define	A_ACCT_SESSION_ID				5
 #define	A_SIP_METHOD					6
 #define	A_SIP_RESPONSE_CODE				7
-#define	A_SIP_CSEQ					8
+#define	A_SIP_CSEQ						8
 #define	A_SIP_TO_TAG					9
 #define	A_SIP_FROM_TAG					10
 #define	A_DIGEST_RESPONSE				11
 #define	A_DIGEST_ATTRIBUTES				12
 #define	A_SIP_URI_USER					13
-#define	A_SIP_URI_HOST                                  14
+#define	A_SIP_URI_HOST					14
 #define	A_DIGEST_REALM					15
 #define	A_DIGEST_NONCE					16
 #define	A_DIGEST_METHOD					17
 #define	A_DIGEST_URI					18
 #define	A_DIGEST_QOP					19
 #define	A_DIGEST_ALGORITHM				20
-#define	A_DIGEST_BODY_DIGEST			        21
+#define	A_DIGEST_BODY_DIGEST			21
 #define	A_DIGEST_CNONCE					22
-#define	A_DIGEST_NONCE_COUNT			        23
+#define	A_DIGEST_NONCE_COUNT			23
 #define	A_DIGEST_USER_NAME				24
-#define	A_SIP_GROUP					25
+#define	A_SIP_GROUP						25
 #define	A_CISCO_AVPAIR					26
-#define	A_SIP_AVP					27
+#define	A_SIP_AVP						27
 #define	A_TIME_STAMP					28
 #define	A_SIP_CALL_ID					29
 #define	A_SIP_REQUEST_HASH				30
-#define	A_MAX						31
+#define	A_MAX							31
 
 #define	V_STATUS_START			0
 #define	V_STATUS_STOP			1
