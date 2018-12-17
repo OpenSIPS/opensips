OpenSIPS/opensips

commit be3b60366763c3c0390f20be590420440c834204

compression: fix leaks when message was rebuilt
When the initial buffer had to be changed, the previous one was no
longer released, resulting in a memleak.

Credits go to Jock McKechnie for reporting and providing testing env!

modules/compression/compression.c
@@ -943,6 +943,7 @@ again:
 
 	switch (type) {
 		case TM_CB:
+			shm_free(*buf_p);
 			*buf_p = shm_malloc(new_buf.len);
 			if (*buf_p == NULL) {
 				LM_ERR("no more sh mem\n");
@@ -950,6 +951,7 @@ again:
 			}
 			break;
 		case PROCESSING_CB:
+			pkg_free(*buf_p);
 			*buf_p = pkg_malloc(new_buf.len);
 			if (*buf_p == NULL) {
 				LM_ERR("no more pkg mem\n");
@@ -1650,6 +1652,7 @@ only_body:
 			}
 			break;
 		case PROCESSING_CB:
+			pkg_free(*buf_p);
 			*buf_p = pkg_malloc(buf2send.len+1);
 			if (*buf_p == NULL) {
 				LM_ERR("no more pkg mem\n");

commit 57f142bdeda5dd7ad4406cab184ab7a2668e32a1

compression: do not free buf for stateless
The callback frees them for you, resulting in a double free.
Bug introduced in be3b6036.

Reported by Ryan Bullock on GitHub.

modules/compression/compression.c
@@ -951,7 +951,6 @@ again:
 			}
 			break;
 		case PROCESSING_CB:
-			pkg_free(*buf_p);
 			*buf_p = pkg_malloc(new_buf.len);
 			if (*buf_p == NULL) {
 				LM_ERR("no more pkg mem\n");
@@ -1652,7 +1651,6 @@ only_body:
 			}
 			break;
 		case PROCESSING_CB:
-			pkg_free(*buf_p);
 			*buf_p = pkg_malloc(buf2send.len+1);
 			if (*buf_p == NULL) {
 				LM_ERR("no more pkg mem\n");