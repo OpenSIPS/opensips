8904e6a3941aa4fd8b55ed4ecd89450b7273d960
Bogdan-Andrei Iancu <bogdan@opensips.org>
2019-01-08 11:19:16 +0200

    httpd: use newer API of libmicrohttpd
     For new versions of libmicrohttpd, use MHD_create_response_from_buffer instead
     of deprecated MHD_create_response_from_data.
     Also MHD_HTTP_NOT_ACCEPTABLE is replacing MHD_HTTP_METHOD_NOT_ACCEPTABLE
    
     cherry-pick from 621bf99a45fe22502cbed7541f1343dfb1314583

--- modules/httpd/httpd_proc.c
+++ modules/httpd/httpd_proc.c
@@ -614,16 +614,27 @@ int answer_to_connection (void *cls, struct MHD_Connection *connection,
 		}
 	}else{
 		page = MI_HTTP_U_METHOD;
+#ifdef MHD_HTTP_NOT_ACCEPTABLE
+		ret_code = MHD_HTTP_NOT_ACCEPTABLE;
+#else
 		ret_code = MHD_HTTP_METHOD_NOT_ACCEPTABLE;
+#endif
 	}
 
 send_response:
 	if (page.s) {
 		LM_DBG("MHD_create_response_from_data [%p:%d]\n",
 			page.s, page.len);
+#if defined MHD_VERSION && MHD_VERSION >= 0x00090000
+		response = MHD_create_response_from_buffer(page.len,
+							(void*)page.s,
+							MHD_RESPMEM_MUST_COPY);
+#else
+		/* use old constructor */
 		response = MHD_create_response_from_data(page.len,
 							(void*)page.s,
 							0, 1);
+#endif
 	} else {
 		LM_DBG("MHD_create_response_from_callback\n");
 		response = MHD_create_response_from_callback (MHD_SIZE_UNKNOWN,
