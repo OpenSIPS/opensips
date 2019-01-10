c748145b2975df9c17bbef5c17fd4311f9534cf0
Razvan Crainea <razvan@opensips.org>
2019-01-10 14:33:26 +0200

    mi_fifo: adhere mi_fifo to the new JSONRPC mechanism
    TODO: handle async responses

--- modules/mi_fifo/mi_fifo.h
+++ modules/mi_fifo/mi_fifo.h
@@ -29,16 +29,10 @@
 
 #define DEFAULT_MI_REPLY_DIR "/tmp/"
 
-#define DEFAULT_MI_REPLY_IDENT "\t"
-
 #define MI_CMD_SEPARATOR       ':'
 
-/* the 2-chars separator between name and value */
-#define MI_ATTR_VAL_SEP1 ':'
-#define MI_ATTR_VAL_SEP2 ':'
-
-/* maximum length of a FIFO line */
-#define MAX_MI_FIFO_BUFFER    1024
+/* size of the fifo read buffer */
+#define MAX_MI_FIFO_BUFFER    4096
 
 /* how patient is ser with FIFO clients not awaiting a reply?
 	4 x 80ms = 0.32 sec */
@@ -46,9 +40,6 @@
 
 /* maximum size for the composed fifo reply name */
 #define MAX_MI_FILENAME 128
-
-/* size of buffer used by parser to read and build the MI tree */
-#define MAX_MI_FIFO_READ 8192
 
 /* we are waiting for a while between fifo file checks */
 #define FIFO_CHECK_WAIT 30
