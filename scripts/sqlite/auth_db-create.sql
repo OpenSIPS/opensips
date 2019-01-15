fecf154f47443100bf968e5d8c8f8b55b3b09e5d
Ovidiu Sas <osas@voipembedded.com>
2019-01-15 12:13:26 -0500

    scripts: update scripts for uri table (uri->auth_db)

--- scripts/sqlite/auth_db-create.sql
+++ scripts/sqlite/auth_db-create.sql
@@ -13,3 +13,13 @@ CREATE TABLE subscriber (
 
 CREATE INDEX subscriber_username_idx  ON subscriber (username);
 
+INSERT INTO version (table_name, table_version) values ('uri','2');
+CREATE TABLE uri (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    username CHAR(64) DEFAULT '' NOT NULL,
+    domain CHAR(64) DEFAULT '' NOT NULL,
+    uri_user CHAR(64) DEFAULT '' NOT NULL,
+    last_modified DATETIME DEFAULT '1900-01-01 00:00:01' NOT NULL,
+    CONSTRAINT uri_account_idx  UNIQUE (username, domain, uri_user)
+);
+
