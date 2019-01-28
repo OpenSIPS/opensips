3f8f5d0c01b8e6e6c95377cb91b84fe6b2a2bd93
Ryan Bullock <rrb3942@gmail.com>
2019-01-28 10:06:17 -0800

    Remove double free on bad input

--- modules/mi_json/http_fnc.c
+++ modules/mi_json/http_fnc.c
@@ -278,7 +278,6 @@ struct mi_root* mi_json_run_mi_cmd(struct mi_cmd *f, const str* miCmd,
           node = &mi_cmd->node;
           if(!add_mi_node_child(node,0,NULL,0,val.s,val.len)){
             LM_ERR("cannot add the child node to the tree\n");
-            free_mi_tree(mi_cmd);
             goto error;
           }
           j = i+1;
@@ -291,7 +290,6 @@ struct mi_root* mi_json_run_mi_cmd(struct mi_cmd *f, const str* miCmd,
         node = &mi_cmd->node;
         if(!add_mi_node_child(node,0,NULL,0,val.s,val.len)){
           LM_ERR("cannot add the child node to the tree\n");
-          free_mi_tree(mi_cmd);
           goto error;
         }
       }
