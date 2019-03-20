OpenSIPS/opensips

commit d8fe587cef17421ebbbd4cc0d4c688bcf83b3f5a

usrloc: Also binary-replicate the k/v storage
This allows us to also cluster the mid-registrar (not only registrar!).

modules/mid_registrar/save.c
@@ -1212,7 +1212,8 @@ update_usrloc:
 			LM_DBG("UPDATING .....\n");
 
 			if (reg_mode == MID_REG_THROTTLE_CT &&
-			    update_ucontact_data(c, ctmap->expires, e_out, ci->cseq) != 0) {
+				store_ucontact_data(c, mri, &_c->uri, ctmap->expires, e_out,
+				                    get_act_time(), ci->cseq) != 0) {
 				LM_ERR("failed to update ucontact data - oom?\n");
 				goto error;
 			}
@@ -1315,7 +1316,10 @@ static inline int save_restore_req_contacts(struct sip_msg *req, struct sip_msg*
 			LM_ERR("failed to insert new record structure\n");
 			goto out_err;
 		}
+	}
 
+	/* replicated AoRs will have an empty k/v store */
+	if (!ul_api.get_urecord_key(r, &ul_key_callid)) {
 		if (store_urecord_data(r, mri, &_c->uri, e_out, get_act_time(),
 		                       cseq) != 0) {
 			LM_ERR("failed to attach urecord data - oom?\n");

modules/usrloc/ucontact.c
@@ -288,6 +288,17 @@ int mem_update_ucontact(ucontact_t* _c, ucontact_info_t* _ci)
 	_c->flags = _ci->flags;
 	_c->cflags = _ci->cflags;
 
+	if (have_mem_storage()) {
+		if (!ZSTRP(_ci->packed_kv_storage)) {
+			if (_c->kv_storage)
+				store_destroy(_c->kv_storage);
+
+			_c->kv_storage = store_deserialize(_ci->packed_kv_storage);
+			if (!_c->kv_storage)
+				LM_ERR("oom\n");
+		}
+	}
+
 	if (compute_next_hop(_c) != 0)
 		LM_ERR("failed to resolve next hop. keeping old one - '%.*s'\n",
 		        _c->next_hop.name.len, _c->next_hop.name.s);
@@ -926,7 +937,7 @@ static inline void update_contact_pos(struct urecord* _r, ucontact_t* _c)
 int update_ucontact(struct urecord* _r, ucontact_t* _c, ucontact_info_t* _ci,
 															char is_replicated)
 {
-	int ret;
+	int ret, persist_kv_store = 1;
 
 	/* we have to update memory in any case, but database directly
 	 * only in sql_wmode SQL_WRITE_THROUGH */
@@ -935,8 +946,17 @@ int update_ucontact(struct urecord* _r, ucontact_t* _c, ucontact_info_t* _ci,
 		return -1;
 	}
 
-	if (!is_replicated && have_data_replication())
-		replicate_ucontact_update(_r, &_c->c, _ci);
+	if (is_replicated && _c->kv_storage)
+		restore_urecord_kv_store(_r, _c);
+
+	if (!is_replicated && have_data_replication()) {
+		if (persist_urecord_kv_store(_r) != 0)
+			LM_ERR("failed to persist latest urecord K/V storage\n");
+		else
+			persist_kv_store = 0;
+
+		replicate_ucontact_update(_r, _c);
+	}
 
 	/* run callbacks for UPDATE event */
 	if (exists_ulcb_type(UL_CONTACT_UPDATE))
@@ -951,7 +971,7 @@ int update_ucontact(struct urecord* _r, ucontact_t* _c, ucontact_info_t* _ci,
 	st_update_ucontact(_c);
 
 	if (sql_wmode == SQL_WRITE_THROUGH) {
-		if (persist_urecord_kv_store(_r) != 0)
+		if (persist_kv_store && persist_urecord_kv_store(_r) != 0)
 			LM_ERR("failed to persist latest urecord K/V storage\n");
 
 		ret = db_update_ucontact(_c) ;

modules/usrloc/urecord.c
@@ -124,6 +124,16 @@ void free_urecord(urecord_t* _r)
 	}
 }
 
+void restore_urecord_kv_store(urecord_t *_r, ucontact_t *_c)
+{
+	int_str_t **urec_kv_store;
+
+	urec_kv_store = (int_str_t **)map_find(_c->kv_storage, urec_store_key);
+	if (urec_kv_store) {
+		store_destroy(_r->kv_storage);
+		_r->kv_storage = store_deserialize(&(*urec_kv_store)->s);
+	}
+}
 
 /*! \brief
  * Add a new contact
@@ -136,7 +146,6 @@ ucontact_t* mem_insert_ucontact(urecord_t* _r, str* _c, ucontact_info_t* _ci)
 {
 	ucontact_t* ptr, *prev = 0;
 	ucontact_t* c;
-	int_str_t **urec_kv_store;
 
 	if ( (c=new_ucontact(_r->domain, &_r->aor, _c, _ci)) == 0) {
 		LM_ERR("failed to create new contact\n");
@@ -145,16 +154,8 @@ ucontact_t* mem_insert_ucontact(urecord_t* _r, str* _c, ucontact_info_t* _ci)
 
 	if_update_stat( _r->slot, _r->slot->d->contacts, 1);
 
-	if (c->kv_storage) {
-		urec_kv_store = (int_str_t **)map_find(c->kv_storage, urec_store_key);
-		if (urec_kv_store) {
-			if (map_size(_r->kv_storage) != 0)
-				LM_BUG("urec key in 2 contacts");
-
-			store_destroy(_r->kv_storage);
-			_r->kv_storage = store_deserialize(&(*urec_kv_store)->s);
-		}
-	}
+	if (c->kv_storage)
+		restore_urecord_kv_store(_r, c);
 
 	ptr = _r->contacts;
 

modules/usrloc/urecord.h
@@ -158,9 +158,14 @@ uint64_t next_contact_id(urecord_t* _r);
 /*
  * Prepares the K/V store of an urecord_t to be persisted to DB by serializing
  * it and storing it in one of the contact's K/V store using an internal key
- * ("_urec_kvs").
+ * ("urec_store_key").
  */
 int persist_urecord_kv_store(urecord_t* _r);
+/*
+ * Attempts to set the K/V store of an urecord_t using the contact's K/V store
+ * data under the "urec_store_key" key.
+ */
+void restore_urecord_kv_store(urecord_t *_r, ucontact_t *_c);
 
 /*! \brief
  * Fetch a key from the record-level storage

modules/usrloc/ureplication.c
@@ -29,6 +29,7 @@
 #include "ureplication.h"
 #include "ul_mod.h"
 #include "dlist.h"
+#include "kv_store.h"
 
 str contact_repl_cap = str_init("usrloc-contact-repl");
 
@@ -128,6 +129,11 @@ void replicate_ucontact_insert(urecord_t *r, str *contact, ucontact_info_t *ci)
 	bin_push_str(&packet, r->domain);
 	bin_push_str(&packet, &r->aor);
 	bin_push_str(&packet, contact);
+
+	st.s = (char *)&ci->contact_id;
+	st.len = sizeof ci->contact_id;
+	bin_push_str(&packet, &st);
+
 	bin_push_str(&packet, ci->callid);
 	bin_push_str(&packet, ci->user_agent);
 	bin_push_str(&packet, ci->path);
@@ -179,7 +185,7 @@ error:
 	bin_free_packet(&packet);
 }
 
-void replicate_ucontact_update(urecord_t *r, str *contact, ucontact_info_t *ci)
+void replicate_ucontact_update(urecord_t *r, ucontact_t *ct)
 {
 	str st;
 	int rc;
@@ -192,31 +198,37 @@ void replicate_ucontact_update(urecord_t *r, str *contact, ucontact_info_t *ci)
 
 	bin_push_str(&packet, r->domain);
 	bin_push_str(&packet, &r->aor);
-	bin_push_str(&packet, contact);
-	bin_push_str(&packet, ci->callid);
-	bin_push_str(&packet, ci->user_agent);
-	bin_push_str(&packet, ci->path);
-	bin_push_str(&packet, ci->attr);
-	bin_push_str(&packet, &ci->received);
-	bin_push_str(&packet, &ci->instance);
+	bin_push_str(&packet, &ct->c);
+	bin_push_str(&packet, &ct->callid);
+	bin_push_str(&packet, &ct->user_agent);
+	bin_push_str(&packet, &ct->path);
+	bin_push_str(&packet, &ct->attr);
+	bin_push_str(&packet, &ct->received);
+	bin_push_str(&packet, &ct->instance);
 
-	st.s = (char *) &ci->expires;
-	st.len = sizeof ci->expires;
+	st.s = (char *) &ct->expires;
+	st.len = sizeof ct->expires;
 	bin_push_str(&packet, &st);
 
-	st.s = (char *) &ci->q;
-	st.len = sizeof ci->q;
+	st.s = (char *) &ct->q;
+	st.len = sizeof ct->q;
 	bin_push_str(&packet, &st);
 
-	bin_push_str(&packet, ci->sock?&ci->sock->sock_str:NULL);
-	bin_push_int(&packet, ci->cseq);
-	bin_push_int(&packet, ci->flags);
-	bin_push_int(&packet, ci->cflags);
-	bin_push_int(&packet, ci->methods);
+	bin_push_str(&packet, ct->sock?&ct->sock->sock_str:NULL);
+	bin_push_int(&packet, ct->cseq);
+	bin_push_int(&packet, ct->flags);
+	bin_push_int(&packet, ct->cflags);
+	bin_push_int(&packet, ct->methods);
 
-	st.s   = (char *)&ci->last_modified;
-	st.len = sizeof ci->last_modified;
+	st.s   = (char *)&ct->last_modified;
+	st.len = sizeof ct->last_modified;
 	bin_push_str(&packet, &st);
+
+	st = store_serialize(ct->kv_storage);
+	if (ZSTR(st))
+		LM_ERR("oom\n");
+	bin_push_str(&packet, &st);
+	store_free_buffer(&st);
 
 	if (cluster_mode == CM_FEDERATION_CACHEDB)
 		rc = clusterer_api.send_all_having(&packet, location_cluster,
@@ -377,6 +389,9 @@ static int receive_ucontact_insert(bin_packet_t *packet)
 	}
 
 	bin_pop_str(packet, &contact_str);
+
+	bin_pop_str(packet, &st);
+	memcpy(&ci.contact_id, st.s, sizeof ci.contact_id);
 
 	bin_pop_str(packet, &callid);
 	ci.callid = &callid;
@@ -541,6 +556,9 @@ static int receive_ucontact_update(bin_packet_t *packet)
 
 	bin_pop_str(packet, &st);
 	memcpy(&ci.last_modified, st.s, sizeof ci.last_modified);
+
+	bin_pop_str(packet, &st);
+	ci.packed_kv_storage = &st;
 
 	if (skip_replicated_db_ops)
 		ci.flags |= FL_MEM;

modules/usrloc/ureplication.h
@@ -52,7 +52,7 @@ extern str contact_repl_cap;
 void replicate_urecord_insert(urecord_t *r);
 void replicate_urecord_delete(urecord_t *r);
 void replicate_ucontact_insert(urecord_t *r, str *contact, ucontact_info_t *ci);
-void replicate_ucontact_update(urecord_t *r, str *contact, ucontact_info_t *ci);
+void replicate_ucontact_update(urecord_t *r, ucontact_t *ct);
 void replicate_ucontact_delete(urecord_t *r, ucontact_t *c);
 
 void receive_binary_packets(bin_packet_t *packet);

commit d4f023e8da8fc6bda92b4d4aa8e58cde0282ee67

mid_registrar: Fix possible crash in mode == 2
It seems commit d8fe587cef17 broke the handling for non-star
de-registrations which remove the last standing contact, causing replies
from the main registrar to not include a "Contact" header field.  The
latter, in itself, is perfectly fine -- however, it must be handled
properly.

modules/mid_registrar/save.c
@@ -1344,7 +1344,7 @@ static inline int save_restore_req_contacts(struct sip_msg *req, struct sip_msg*
 	}
 
 	/* replicated AoRs will have an empty k/v store */
-	if (!ul_api.get_urecord_key(r, &ul_key_callid)) {
+	if (!ul_api.get_urecord_key(r, &ul_key_callid) && _c) {
 		if (store_urecord_data(r, mri, &_c->uri, e_out, get_act_time(),
 		                       cseq) != 0) {
 			LM_ERR("failed to attach urecord data - oom?\n");