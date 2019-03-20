d87169c7a1f22e06a2569c72535a561575937daa
Liviu Chircu <liviu@opensips.org>
2019-03-20 12:01:56 +0200

    pua_dialoginfo: Fix pkg memleak

--- modules/pua_dialoginfo/pua_dialoginfo.c
+++ modules/pua_dialoginfo/pua_dialoginfo.c
@@ -417,7 +417,7 @@ static void
 __dialog_loaded(struct dlg_cell *dlg, int type, struct dlg_cb_params *_params)
 {
 	str peer_uri= {0, 0};
-	if(dlg_api.fetch_dlg_value(dlg, &peer_dlg_var, &peer_uri, 1)==0 && peer_uri.len!=0) {
+	if(dlg_api.fetch_dlg_value(dlg, &peer_dlg_var, &peer_uri, 0)==0 && peer_uri.len!=0) {
 		/* register dialog callbacks which triggers sending PUBLISH */
 		if (dlg_api.register_dlgcb(dlg,
 			DLGCB_FAILED| DLGCB_CONFIRMED | DLGCB_TERMINATED | DLGCB_EXPIRED |
