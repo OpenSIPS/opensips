487429bb077f54271030ba50e7a31832662c29df
Vlad Patrascu <vladp@opensips.org>
2019-04-02 13:26:11 +0300

    mediaproxy: port script functions to new param interface

--- modules/mediaproxy/mediaproxy.c
+++ modules/mediaproxy/mediaproxy.c
@@ -199,10 +199,13 @@ static AVP_Param media_relay_avp = {str_init(MEDIA_RELAY_AVP_SPEC), -1, 0};
 static AVP_Param ice_candidate_avp = {str_init(ICE_CANDIDATE_AVP_SPEC), -1, 0};
 
 static cmd_export_t commands[] = {
-    {"engage_media_proxy", (cmd_function)EngageMediaProxy, 0, 0, 0, REQUEST_ROUTE},
-    {"use_media_proxy",    (cmd_function)UseMediaProxy,    0, 0, 0, REQUEST_ROUTE | ONREPLY_ROUTE | FAILURE_ROUTE | BRANCH_ROUTE | LOCAL_ROUTE},
-    {"end_media_session",  (cmd_function)EndMediaSession,  0, 0, 0, REQUEST_ROUTE | ONREPLY_ROUTE | FAILURE_ROUTE | BRANCH_ROUTE | LOCAL_ROUTE},
-    {0, 0, 0, 0, 0, 0}
+    {"engage_media_proxy", (cmd_function)EngageMediaProxy, {{0,0,0}},
+        REQUEST_ROUTE},
+    {"use_media_proxy",    (cmd_function)UseMediaProxy, {{0,0,0}},
+        REQUEST_ROUTE | ONREPLY_ROUTE | FAILURE_ROUTE | BRANCH_ROUTE | LOCAL_ROUTE},
+    {"end_media_session",  (cmd_function)EndMediaSession, {{0,0,0}},
+        REQUEST_ROUTE | ONREPLY_ROUTE | FAILURE_ROUTE | BRANCH_ROUTE | LOCAL_ROUTE},
+    {0,0,{{0,0,0}},0}
 };
 
 static param_export_t parameters[] = {
