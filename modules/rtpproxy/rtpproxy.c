ce93ed5b80082953662b22e0e5467b59f56cf26b
Razvan Crainea <razvan@opensips.org>
2019-05-22 17:03:52 +0300

    rtpproxy: engage late negociation even after call is established
    Thanks go to spacetourist(GitHub)/Callum Guy for reporting this
    Close #1702
    
    (cherry picked from commit 295a5842dee444a1f26b8eb5af539e58e4db8d44)

--- modules/rtpproxy/rtpproxy.c
+++ modules/rtpproxy/rtpproxy.c
@@ -2908,13 +2908,24 @@ static int engage_force_rtpproxy(struct dlg_cell *dlg, struct sip_msg *msg)
 		if (!offer && !has_sdp)
 			goto done;
 		/* late negotiation */
+		/* delete the value, to make sure we don't re-engage late again */
+		dlg_api.store_dlg_value(dlg, &late_name, NULL);
 	} else {
 		/* sequential request without SDP */
 		if (!has_sdp) {
+			if (msg->first_line.type == SIP_REQUEST &&
+					(method_id == METHOD_INVITE ||  method_id == METHOD_UPDATE)) {
+				/* indicate there's an ongoing late negociation happening */
+				if (dlg_api.store_dlg_value(dlg, &late_name, &late_name) < 0) {
+					LM_ERR("cannot store late_negotiation param into dialog\n");
+					goto error;
+				}
+			}
 			goto done;
 		}
 		/* if it is not an 200OK */
-		LM_DBG("handling 200 OK? - %d\n", msg->first_line.u.reply.statuscode);
+		if (msg->first_line.type == SIP_REPLY)
+			LM_DBG("handling 200 OK? - %d\n", msg->first_line.u.reply.statuscode);
 	}
 
 	/* try to move values */
