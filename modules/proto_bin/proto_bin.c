0bb6fb8a20a65177586b62cef58d9278eb32b1bb
Bogdan-Andrei Iancu <bogdan@opensips.org>
2019-05-23 19:57:09 +0300

    Expose the real ports (src/dst) used by TC-based write op
    As src/dst ports may be ephemeral, expose (via holders in socket_info struct) the real used ports during the last write operation
    
    Related to #1692
    
    (cherry picked from commit 9dde6d5f00626ac78cec948c84443b2caea33013)

--- modules/proto_bin/proto_bin.c
+++ modules/proto_bin/proto_bin.c
@@ -557,6 +557,9 @@ static int proto_bin_send(struct socket_info* send_sock,
 			if (n==0) {
 				/* mark the ID of the used connection (tracing purposes) */
 				last_outgoing_tcp_id = c->id;
+				send_sock->last_local_real_port = c->rcv.dst_port;
+				send_sock->last_remote_real_port = c->rcv.src_port;
+
 				/* connect is still in progress, break the sending
 				 * flow now (the actual write will be done when 
 				 * connect will be completed */
@@ -595,6 +598,8 @@ static int proto_bin_send(struct socket_info* send_sock,
 
 			/* mark the ID of the used connection (tracing purposes) */
 			last_outgoing_tcp_id = c->id;
+			send_sock->last_local_real_port = c->rcv.dst_port;
+			send_sock->last_remote_real_port = c->rcv.src_port;
 
 			/* we successfully added our write chunk - success */
 			tcp_conn_release(c, 0);
@@ -632,6 +637,8 @@ send_it:
 
 	/* mark the ID of the used connection (tracing purposes) */
 	last_outgoing_tcp_id = c->id;
+	send_sock->last_local_real_port = c->rcv.dst_port;
+	send_sock->last_remote_real_port = c->rcv.src_port;
 
 	tcp_conn_release(c, (n<len)?1:0/*pending data in async mode?*/ );
 	return n;
