a9e0a9619b151ba42782be2a11e07df25c424a62
Razvan Crainea <razvan@opensips.org>
2019-05-28 09:15:54 +0000

    tls_mgm: make sure pthread locking has SHARED support
    Prevent system lock when using openssl 1.1, that is using phreads as
    syncrhonizing mechanisms.
    
    Close #1590
    
    (cherry picked from commit 149e69e3f67013884623e7986c47742aad787ece)

--- modules/tls_mgm/tls_mgm.c
+++ modules/tls_mgm/tls_mgm.c
@@ -34,6 +34,9 @@
  *
  */
 
+#define _GNU_SOURCE
+#include <dlfcn.h>
+
 #include <openssl/ui.h>
 #include <openssl/ssl.h>
 #include <openssl/opensslv.h>
@@ -1538,6 +1541,70 @@ static int tls_init_multithread(void)
 	CRYPTO_set_dynlock_destroy_callback(tls_dyn_lock_destroy);
 
 	return 0;
+}
+#else
+int pthread_mutex_init (pthread_mutex_t *__mutex,
+		const pthread_mutexattr_t *__mutexattr)
+{
+	int ret;
+	pthread_mutexattr_t *attr;
+	pthread_mutexattr_t local_attr;
+	int (*real_pthread_mutex_init)(pthread_mutex_t *,
+			const pthread_mutexattr_t *);
+
+	real_pthread_mutex_init = dlsym(RTLD_NEXT, "pthread_mutex_init");
+	if (!real_pthread_mutex_init)
+		return -1;
+
+	if (__mutexattr)
+		attr = (pthread_mutexattr_t *)__mutexattr;
+	else {
+		ret = pthread_mutexattr_init(&local_attr);
+		if (ret != 0)
+			return ret;
+
+		attr = &local_attr;
+	}
+	ret = pthread_mutexattr_setpshared(attr, PTHREAD_PROCESS_SHARED);
+	if (ret != 0)
+		goto destroy;
+	ret = real_pthread_mutex_init(__mutex, attr);
+destroy:
+	if (attr != __mutexattr)
+		pthread_mutexattr_destroy(attr);
+	return ret;
+}
+
+int pthread_rwlock_init (pthread_rwlock_t *__restrict __rwlock,
+		                const pthread_rwlockattr_t *__restrict __attr)
+{
+	int ret;
+	pthread_rwlockattr_t *attr;
+	pthread_rwlockattr_t local_attr;
+	int (*real_pthread_rwlock_init)(pthread_rwlock_t *,
+			const pthread_rwlockattr_t *);
+
+	real_pthread_rwlock_init = dlsym(RTLD_NEXT, "pthread_rwlock_init");
+	if (!real_pthread_rwlock_init)
+		return -1;
+
+	if (__attr)
+		attr = (pthread_rwlockattr_t *)__attr;
+	else {
+		ret = pthread_rwlockattr_init(&local_attr);
+		if (ret != 0)
+			return ret;
+
+		attr = &local_attr;
+	}
+	ret = pthread_rwlockattr_setpshared(attr, PTHREAD_PROCESS_SHARED);
+	if (ret != 0)
+		goto destroy;
+	ret = real_pthread_rwlock_init(__rwlock, attr);
+destroy:
+	if (attr != __attr)
+		pthread_rwlockattr_destroy(attr);
+	return ret;
 }
 #endif
 
