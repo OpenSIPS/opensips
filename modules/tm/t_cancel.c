b97015920775af29a0aca31eb0141d2a61317603
Bogdan-Andrei Iancu <bogdan@opensips.org>
2019-08-07 12:33:41 +0300

    [tm] fixed regression on cancelling empty branches upon timeout
    While adding support for Content-Disposition: no-cancel (see 4747da559f4df161441be8373488dee9fd16c282), a regression was made, allowing TM to send out cancel (upon internal 408 timeout) to branches with no incoming replies.
    Only this cancelling scenaio was affected (internal 408 timeout). The cancelling upon 200OK or incoming cancel were not affected.
    
    Thanks to Richard Revels for spotting and reporting this.
    
    (cherry picked from commit f1a6d0d8e46c4aff9f203f2eb7e85a2b1e40cf92)

--- modules/tm/t_cancel.c
+++ modules/tm/t_cancel.c
@@ -72,7 +72,11 @@ void cancel_uacs( struct cell *t, branch_bm_t cancel_bm )
 				/* send a cancel out */
 				cancel_branch(t, i);
 			} else {
-				/* set flag to catch the delaied replies */
+				/* if no reply received on this branch, do not send out
+				 * a CANCEL as it is against RFC3261. We will eventually send
+				 * one out IF we receive later a reply on this branch, so let's
+				 * flag it for catching (and cancelling) such delaied replies
+				 */
 				t->uac[i].flags |= T_UAC_TO_CANCEL_FLAG;
 			}
 		}
