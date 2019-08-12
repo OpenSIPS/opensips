OpenSIPS/opensips

commit 65a9f51f1ee43e500d6fbe34c3a0e07722bd75b2

dialog: Fix broken re-INVITE pinging after failover
The mandatory re-INVITE pinging data (SDP1, SDP2, ct1, ct2) was not
included in the BIN replication packets, so the pinging would stop
working once we'd failover to the backup box, in an active/backup
HA scenario.

modules/dialog/dlg_replication.c
@@ -231,6 +231,14 @@ int dlg_replicated_create(bin_packet_t *packet, struct dlg_cell *cell, str *ftag
 
 	dlg->lifetime = 0;
 
+	if (vars.s && vars.len != 0) {
+		read_dialog_vars(vars.s, vars.len, dlg);
+		run_dlg_callbacks(DLGCB_PROCESS_VARS, dlg,
+				NULL, DLG_DIR_NONE, NULL, 1, 0);
+	}
+
+	dlg_unlock(d_table, d_entry);
+
 	if (dlg->flags & DLG_FLAG_PING_CALLER || dlg->flags & DLG_FLAG_PING_CALLEE) {
 		if (insert_ping_timer(dlg) != 0)
 			LM_CRIT("Unable to insert dlg %p into ping timer\n",dlg);
@@ -239,18 +247,23 @@ int dlg_replicated_create(bin_packet_t *packet, struct dlg_cell *cell, str *ftag
 		}
 	}
 
+	if (dlg_has_reinvite_pinging(dlg)) {
+		/* re-populate Re-INVITE pinging fields */
+		if (restore_reinvite_pinging(dlg) != 0)
+			LM_ERR("failed to fetch some Re-INVITE pinging data\n");
+		else if (0 != insert_reinvite_ping_timer(dlg))
+			LM_CRIT("Unable to insert dlg %p into reinvite"
+			        "ping timer\n", dlg);
+		else {
+			/* reference dialog as kept in reinvite ping timer list */
+			ref_dlg_unsafe(dlg, 1);
+		}
+	}
+
 	if (dlg_db_mode == DB_MODE_DELAYED) {
 		/* to be later removed by timer */
 		ref_dlg_unsafe(dlg, 1);
 	}
-
-	if (vars.s && vars.len != 0) {
-		read_dialog_vars(vars.s, vars.len, dlg);
-		run_dlg_callbacks(DLGCB_PROCESS_VARS, dlg,
-				NULL, DLG_DIR_NONE, NULL, 1, 0);
-	}
-
-	dlg_unlock(d_table, d_entry);
 
 	if (profiles.s && profiles.len != 0)
 		read_dialog_profiles(profiles.s, profiles.len, dlg, 0, 1);
@@ -578,6 +591,9 @@ void replicate_dialog_created(struct dlg_cell *dlg)
 	if (bin_init(&packet, &dlg_repl_cap, REPLICATION_DLG_CREATED, BIN_VERSION, 0) != 0)
 		goto init_error;
 
+	if (dlg_has_reinvite_pinging(dlg) && persist_reinvite_pinging(dlg))
+		LM_ERR("failed to persist Re-INVITE pinging info\n");
+
 	bin_push_dlg(&packet, dlg);
 
 	dlg->replicated = 1;
@@ -638,6 +654,9 @@ void replicate_dialog_updated(struct dlg_cell *dlg)
 
 	if (bin_init(&packet, &dlg_repl_cap, REPLICATION_DLG_UPDATED, BIN_VERSION, 0) != 0)
 		goto init_error;
+
+	if (dlg_has_reinvite_pinging(dlg) && persist_reinvite_pinging(dlg))
+		LM_ERR("failed to persist Re-INVITE pinging info\n");
 
 	bin_push_dlg(&packet, dlg);
 

modules/dialog/dlg_db_handler.c
@@ -80,8 +80,6 @@ static db_func_t dialog_dbf;
 extern int dlg_bulk_del_no;
 
 static inline void set_final_update_cols(db_val_t *, struct dlg_cell *, int);
-static int persist_reinvite_pinging(struct dlg_cell *dlg);
-static int restore_reinvite_pinging(struct dlg_cell *dlg);
 
 #define SET_BIGINT_VALUE(_val, _bigint)\
 	do{\
@@ -694,6 +692,8 @@ static int load_dialog_info_from_db(int dlg_hash_size)
 			dlg->legs[callee_idx(dlg)].last_gen_cseq =
 				(unsigned int)(VAL_INT(values+21));
 
+			dlg_unlock(d_table, d_entry);
+
 			if (dlg->flags & DLG_FLAG_PING_CALLER || dlg->flags & DLG_FLAG_PING_CALLEE) {
 				if (0 != insert_ping_timer(dlg))
 					LM_CRIT("Unable to insert dlg %p into ping timer\n",dlg);
@@ -702,6 +702,7 @@ static int load_dialog_info_from_db(int dlg_hash_size)
 					ref_dlg_unsafe(dlg, 1);
 				}
 			}
+
 
 			if (dlg_has_reinvite_pinging(dlg)) {
 				/* re-populate Re-INVITE pinging fields */
@@ -723,7 +724,6 @@ static int load_dialog_info_from_db(int dlg_hash_size)
 
 			update_dlg_stats(dlg, +1);
 
-			dlg_unlock(d_table, d_entry);
 			run_load_callback_per_dlg(dlg);
 
 			next_dialog:;
@@ -1258,7 +1258,7 @@ str* write_dialog_profiles( struct dlg_profile_link *links)
 }
 
 /* duplicate the SDPs/Contacts of caller/callee(s) into dlg val storage */
-static int persist_reinvite_pinging(struct dlg_cell *dlg)
+int persist_reinvite_pinging(struct dlg_cell *dlg)
 {
 	str caller_adv_sdp = str_init("CSDP"), callee_adv_sdp = str_init("cSDP");
 	str caller_adv_ct = str_init("Cct"), callee_adv_ct = str_init("cct");
@@ -1296,7 +1296,7 @@ static int persist_reinvite_pinging(struct dlg_cell *dlg)
 }
 
 /* re-populate the SDPs/Contacts of caller/callee(s) from dlg val storage */
-static int restore_reinvite_pinging(struct dlg_cell *dlg)
+int restore_reinvite_pinging(struct dlg_cell *dlg)
 {
 	str caller_adv_sdp = str_init("CSDP"), callee_adv_sdp = str_init("cSDP");
 	str caller_adv_ct = str_init("Cct"), callee_adv_ct = str_init("cct");

modules/dialog/dlg_db_handler.h
@@ -125,5 +125,7 @@ struct mi_root* mi_restore_dlg_db(struct mi_root *cmd, void *param);
 
 void dlg_setup_reinvite_callbacks(struct cell *t, struct sip_msg *req,
 		struct dlg_cell *dlg);
+int persist_reinvite_pinging(struct dlg_cell *dlg);
+int restore_reinvite_pinging(struct dlg_cell *dlg);
 
 #endif

commit 0e85dd69f9d9ead1363e2afe8f6b4f0d3293f4db

dialog: Fix two referencing issues
    * an unsafe referencing bug, introduced in 65a9f51f1ee.  As long as
      the dialog is not locked, we _must not_ call ref_dlg_unsafe().
      This unsafe increment operation could lead to occasionally missing
      references, possibly even a crash.

    * a long-standing bug, where read_dialog_profiles() is invoked on
      replicated create/update events with a dialog that has no extra
      reference, so it could very well be a lingering/freed pointer.
      Under the right conditions, this too could lead to a crash.

modules/dialog/dlg_replication.c
@@ -223,8 +223,8 @@ int dlg_replicated_create(bin_packet_t *packet, struct dlg_cell *cell, str *ftag
 		goto error;
 	}
 
-	/* reference the dialog as kept in the timer list */
-	ref_dlg_unsafe(dlg, 1);
+	/* timer list + this ref */
+	ref_dlg_unsafe(dlg, 2);
 
 	LM_DBG("Received initial timeout of %d for dialog %.*s, safe = %d\n",
 		dlg->tl.timeout, callid.len, callid.s, safe);
@@ -237,8 +237,6 @@ int dlg_replicated_create(bin_packet_t *packet, struct dlg_cell *cell, str *ftag
 				NULL, DLG_DIR_NONE, NULL, 1, 0);
 	}
 
-	dlg_unlock(d_table, d_entry);
-
 	if (dlg->flags & DLG_FLAG_PING_CALLER || dlg->flags & DLG_FLAG_PING_CALLEE) {
 		if (insert_ping_timer(dlg) != 0)
 			LM_CRIT("Unable to insert dlg %p into ping timer\n",dlg);
@@ -246,6 +244,13 @@ int dlg_replicated_create(bin_packet_t *packet, struct dlg_cell *cell, str *ftag
 			ref_dlg_unsafe(dlg, 1);
 		}
 	}
+
+	if (dlg_db_mode == DB_MODE_DELAYED) {
+		/* to be later removed by timer */
+		ref_dlg_unsafe(dlg, 1);
+	}
+
+	dlg_unlock(d_table, d_entry);
 
 	if (dlg_has_reinvite_pinging(dlg)) {
 		/* re-populate Re-INVITE pinging fields */
@@ -256,13 +261,8 @@ int dlg_replicated_create(bin_packet_t *packet, struct dlg_cell *cell, str *ftag
 			        "ping timer\n", dlg);
 		else {
 			/* reference dialog as kept in reinvite ping timer list */
-			ref_dlg_unsafe(dlg, 1);
+			ref_dlg(dlg, 1);
 		}
-	}
-
-	if (dlg_db_mode == DB_MODE_DELAYED) {
-		/* to be later removed by timer */
-		ref_dlg_unsafe(dlg, 1);
 	}
 
 	if (profiles.s && profiles.len != 0)
@@ -272,6 +272,7 @@ int dlg_replicated_create(bin_packet_t *packet, struct dlg_cell *cell, str *ftag
 
 	run_load_callback_per_dlg(dlg);
 
+	unref_dlg(dlg, 1);
 	return 0;
 
 pre_linking_error:
@@ -389,11 +390,13 @@ int dlg_replicated_update(bin_packet_t *packet)
 
 	dlg->flags |= DLG_FLAG_VP_CHANGED;
 
+	ref_dlg_unsafe(dlg, 1);
 	dlg_unlock(d_table, d_entry);
 
 	if (profiles.s && profiles.len != 0)
 		read_dialog_profiles(profiles.s, profiles.len, dlg, 1, 1);
 
+	unref_dlg(dlg, 1);
 	return 0;
 
 error: