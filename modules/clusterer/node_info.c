OpenSIPS/opensips

commit 66ae3ac13b6f3e996147d33b74d4ad1d4006ad74

clusterer: fix bad parsing of node specification modparams
Multiple string properties in 'my_node_info' and 'neighbor_node_info'
modparams would not be parsed correctly.

Fixes #1775

(cherry picked from commit c26bea063ec7e86c6552857180eba06861d6ad50)

modules/clusterer/node_info.c
@@ -52,7 +52,7 @@ extern str sip_addr_col;
 extern str flags_col;
 extern str description_col;
 
-int parse_param_node_info(str *descr, int *int_vals, char **str_vals);
+int parse_param_node_info(str *descr, int *int_vals, str *str_vals);
 
 db_con_t *db_hdl;
 db_func_t dr_dbf;
@@ -67,7 +67,7 @@ rw_lock_t *cl_list_lock;
 cluster_info_t **cluster_list;
 
 int add_node_info(node_info_t **new_info, cluster_info_t **cl_list, int *int_vals,
-					char **str_vals)
+					str *str_vals)
 {
 	char *host;
 	int hlen, port;
@@ -130,53 +130,53 @@ int add_node_info(node_info_t **new_info, cluster_info_t **cl_list, int *int_val
 	else
 		(*new_info)->link_state = LS_UP;
 
-	if (str_vals[STR_VALS_DESCRIPTION_COL] &&
-		strlen(str_vals[STR_VALS_DESCRIPTION_COL]) != 0) {
-		(*new_info)->description.len = strlen(str_vals[STR_VALS_DESCRIPTION_COL]);
+	if (str_vals[STR_VALS_DESCRIPTION_COL].s &&
+		str_vals[STR_VALS_DESCRIPTION_COL].len) {
+		(*new_info)->description.len = str_vals[STR_VALS_DESCRIPTION_COL].len;
 		(*new_info)->description.s =
 			shm_malloc((*new_info)->description.len * sizeof(char));
 		if ((*new_info)->description.s == NULL) {
 			LM_ERR("no more shm memory\n");
 			goto error;
 		}
-		memcpy((*new_info)->description.s, str_vals[STR_VALS_DESCRIPTION_COL],
+		memcpy((*new_info)->description.s, str_vals[STR_VALS_DESCRIPTION_COL].s,
 			(*new_info)->description.len);
 	} else {
 		(*new_info)->description.s = NULL;
 		(*new_info)->description.len = 0;
 	}
 
-	if (str_vals[STR_VALS_SIP_ADDR_COL] &&
-		strlen(str_vals[STR_VALS_SIP_ADDR_COL]) != 0) {
-		(*new_info)->sip_addr.len = strlen(str_vals[STR_VALS_SIP_ADDR_COL]);
+	if (str_vals[STR_VALS_SIP_ADDR_COL].s &&
+		str_vals[STR_VALS_SIP_ADDR_COL].len) {
+		(*new_info)->sip_addr.len = str_vals[STR_VALS_SIP_ADDR_COL].len;
 		(*new_info)->sip_addr.s = shm_malloc((*new_info)->sip_addr.len * sizeof(char));
 		if ((*new_info)->sip_addr.s == NULL) {
 			LM_ERR("no more shm memory\n");
 			goto error;
 		}
-		memcpy((*new_info)->sip_addr.s, str_vals[STR_VALS_SIP_ADDR_COL],
+		memcpy((*new_info)->sip_addr.s, str_vals[STR_VALS_SIP_ADDR_COL].s,
 			(*new_info)->sip_addr.len);
 	} else {
 		(*new_info)->sip_addr.s = NULL;
 		(*new_info)->sip_addr.len = 0;
 	}
 
-	if (str_vals[STR_VALS_FLAGS_COL] &&
-		strlen(str_vals[STR_VALS_FLAGS_COL]) != 0)
-		if (memcmp(str_vals[STR_VALS_FLAGS_COL], seed_flag.s, seed_flag.len) == 0)
+	if (str_vals[STR_VALS_FLAGS_COL].s &&
+		str_vals[STR_VALS_FLAGS_COL].len)
+		if (memcmp(str_vals[STR_VALS_FLAGS_COL].s, seed_flag.s, seed_flag.len) == 0)
 			(*new_info)->flags |= NODE_IS_SEED;
 
-	if (str_vals[STR_VALS_URL_COL] == NULL) {
+	if (str_vals[STR_VALS_URL_COL].s == NULL) {
 		LM_ERR("no url specified in DB\n");
 		return 1;
 	}
-	(*new_info)->url.len = strlen(str_vals[STR_VALS_URL_COL]);
-	(*new_info)->url.s = shm_malloc(strlen(str_vals[STR_VALS_URL_COL]) * sizeof(char));
+	(*new_info)->url.len = str_vals[STR_VALS_URL_COL].len;
+	(*new_info)->url.s = shm_malloc(str_vals[STR_VALS_URL_COL].len);
 	if (!(*new_info)->url.s) {
 		LM_ERR("no more shm memory\n");
 		goto error;
 	}
-	memcpy((*new_info)->url.s, str_vals[STR_VALS_URL_COL], (*new_info)->url.len);
+	memcpy((*new_info)->url.s, str_vals[STR_VALS_URL_COL].s, (*new_info)->url.len);
 
 	if (int_vals[INT_VALS_NODE_ID_COL] != current_id) {
 		if (parse_phostport((*new_info)->url.s, (*new_info)->url.len, &host, &hlen,
@@ -310,7 +310,7 @@ int load_db_info(db_func_t *dr_dbf, db_con_t* db_hdl, str *db_table,
 					cluster_info_t **cl_list)
 {
 	int int_vals[NO_DB_INT_VALS];
-	char *str_vals[NO_DB_STR_VALS];
+	str str_vals[NO_DB_STR_VALS];
 	int no_clusters;
 	int i;
 	int rc;
@@ -438,7 +438,8 @@ int load_db_info(db_func_t *dr_dbf, db_con_t* db_hdl, str *db_table,
 		int_vals[INT_VALS_NODE_ID_COL] = VAL_INT(ROW_VALUES(row) + 2);
 
 		check_val(url_col, ROW_VALUES(row) + 3, DB_STRING, 1, 1);
-		str_vals[STR_VALS_URL_COL] = (char*) VAL_STRING(ROW_VALUES(row) + 3);
+		str_vals[STR_VALS_URL_COL].s = (char*) VAL_STRING(ROW_VALUES(row) + 3);
+		str_vals[STR_VALS_URL_COL].len = strlen(str_vals[STR_VALS_URL_COL].s);
 
 		check_val(state_col, ROW_VALUES(row) + 4, DB_INT, 1, 0);
 		int_vals[INT_VALS_STATE_COL] = VAL_INT(ROW_VALUES(row) + 4);
@@ -450,13 +451,16 @@ int load_db_info(db_func_t *dr_dbf, db_con_t* db_hdl, str *db_table,
 		int_vals[INT_VALS_PRIORITY_COL] = VAL_INT(ROW_VALUES(row) + 6);
 
 		check_val(sip_addr_col, ROW_VALUES(row) + 7, DB_STRING, 0, 0);
-		str_vals[STR_VALS_SIP_ADDR_COL] = (char*) VAL_STRING(ROW_VALUES(row) + 7);
+		str_vals[STR_VALS_SIP_ADDR_COL].s = (char*) VAL_STRING(ROW_VALUES(row) + 7);
+		str_vals[STR_VALS_SIP_ADDR_COL].len = strlen(str_vals[STR_VALS_SIP_ADDR_COL].s);
 
 		check_val(flags_col, ROW_VALUES(row) + 8, DB_STRING, 0, 0);
-		str_vals[STR_VALS_FLAGS_COL] = (char*) VAL_STRING(ROW_VALUES(row) + 8);
+		str_vals[STR_VALS_FLAGS_COL].s = (char*) VAL_STRING(ROW_VALUES(row) + 8);
+		str_vals[STR_VALS_FLAGS_COL].len = strlen(str_vals[STR_VALS_FLAGS_COL].s);
 
 		check_val(description_col, ROW_VALUES(row) + 9, DB_STRING, 0, 0);
-		str_vals[STR_VALS_DESCRIPTION_COL] = (char*) VAL_STRING(ROW_VALUES(row) + 9);
+		str_vals[STR_VALS_DESCRIPTION_COL].s = (char*) VAL_STRING(ROW_VALUES(row) + 9);
+		str_vals[STR_VALS_DESCRIPTION_COL].len = strlen(str_vals[STR_VALS_DESCRIPTION_COL].s);
 
 		/* add info to backing list */
 		if ((rc = add_node_info(&new_info, cl_list, int_vals, str_vals)) != 0) {
@@ -489,7 +493,7 @@ error:
 int provision_neighbor(modparam_t type, void *val)
 {
 	int int_vals[NO_DB_INT_VALS];
-	char *str_vals[NO_DB_STR_VALS];
+	str str_vals[NO_DB_STR_VALS];
 	str prov_str;
 	node_info_t *new_info;
 
@@ -508,7 +512,7 @@ int provision_neighbor(modparam_t type, void *val)
 
 	if (int_vals[INT_VALS_CLUSTER_ID_COL] == -1 ||
 		int_vals[INT_VALS_NODE_ID_COL] == -1 ||
-		str_vals[STR_VALS_URL_COL] == NULL) {
+		str_vals[STR_VALS_URL_COL].s == NULL) {
 		LM_ERR("At least the cluster id, node id and url are required for a neighbor node\n");
 		return -1;
 	}
@@ -518,7 +522,7 @@ int provision_neighbor(modparam_t type, void *val)
 	if (int_vals[INT_VALS_PRIORITY_COL] == -1)
 		int_vals[INT_VALS_PRIORITY_COL] = DEFAULT_NO_PING_RETRIES;
 
-	str_vals[STR_VALS_DESCRIPTION_COL] = NULL;
+	str_vals[STR_VALS_DESCRIPTION_COL].s = NULL;
 	int_vals[INT_VALS_ID_COL] = -1;
 
 	if (cluster_list == NULL) {
@@ -541,7 +545,7 @@ int provision_neighbor(modparam_t type, void *val)
 int provision_current(modparam_t type, void *val)
 {
 	int int_vals[NO_DB_INT_VALS];
-	char *str_vals[NO_DB_STR_VALS];
+	str str_vals[NO_DB_STR_VALS];
 	node_info_t *new_info;
 	str prov_str;
 
@@ -558,7 +562,7 @@ int provision_current(modparam_t type, void *val)
 		return -1;
 	}
 
-	if (int_vals[INT_VALS_CLUSTER_ID_COL] == -1 || str_vals[STR_VALS_URL_COL] == NULL) {
+	if (int_vals[INT_VALS_CLUSTER_ID_COL] == -1 || str_vals[STR_VALS_URL_COL].s == NULL) {
 		LM_ERR("At least the cluster id and url are required for the current node\n");
 		return -1;
 	}
@@ -585,7 +589,7 @@ int provision_current(modparam_t type, void *val)
 	if (int_vals[INT_VALS_PRIORITY_COL] == -1)
 		int_vals[INT_VALS_PRIORITY_COL] = DEFAULT_NO_PING_RETRIES;
 
-	str_vals[STR_VALS_DESCRIPTION_COL] = NULL;
+	str_vals[STR_VALS_DESCRIPTION_COL].s = NULL;
 	int_vals[INT_VALS_ID_COL] = -1;
 
 	if (cluster_list == NULL) {

modules/clusterer/clusterer.c
@@ -1043,23 +1043,11 @@ static inline int validate_update(int seq_no, int msg_seq_no, int timestamp,
 static node_info_t *add_node(bin_packet_t *received, cluster_info_t *cl,
 								int src_node_id, str *str_vals, int *int_vals)
 {
-	char *char_str_vals[NO_DB_STR_VALS];
 	node_info_t *new_node = NULL;
 	int lock_old_flag;
 
-	char_str_vals[STR_VALS_URL_COL] = shm_malloc(str_vals[STR_VALS_URL_COL].len+1);
-	memcpy(char_str_vals[STR_VALS_URL_COL],
-		str_vals[STR_VALS_URL_COL].s, str_vals[STR_VALS_URL_COL].len);
-	char_str_vals[STR_VALS_URL_COL][str_vals[STR_VALS_URL_COL].len] = 0;
-
-	char_str_vals[STR_VALS_SIP_ADDR_COL] = shm_malloc(str_vals[STR_VALS_SIP_ADDR_COL].len+1);
-	memcpy(char_str_vals[STR_VALS_SIP_ADDR_COL],
-		str_vals[STR_VALS_SIP_ADDR_COL].s, str_vals[STR_VALS_SIP_ADDR_COL].len);
-	char_str_vals[STR_VALS_SIP_ADDR_COL][str_vals[STR_VALS_SIP_ADDR_COL].len] = 0;
-
-	char_str_vals[STR_VALS_FLAGS_COL] = 0;
-	char_str_vals[STR_VALS_DESCRIPTION_COL] = 0;
-
+	str_vals[STR_VALS_FLAGS_COL].s = 0;
+	str_vals[STR_VALS_DESCRIPTION_COL].s = 0;
 	int_vals[INT_VALS_ID_COL] = -1;	/* no DB id */
 	int_vals[INT_VALS_CLUSTER_ID_COL] = cl->cluster_id;
 	int_vals[INT_VALS_NODE_ID_COL] = src_node_id;
@@ -1067,7 +1055,7 @@ static node_info_t *add_node(bin_packet_t *received, cluster_info_t *cl,
 
 	lock_switch_write(cl_list_lock, lock_old_flag);
 
-	if (add_node_info(&new_node, &cl, int_vals, char_str_vals) != 0) {
+	if (add_node_info(&new_node, &cl, int_vals, str_vals) != 0) {
 		LM_ERR("Unable to add node info to backing list\n");
 		lock_switch_read(cl_list_lock, lock_old_flag);
 		return NULL;
@@ -1077,8 +1065,6 @@ static node_info_t *add_node(bin_packet_t *received, cluster_info_t *cl,
 		lock_switch_read(cl_list_lock, lock_old_flag);
 		return NULL;
 	}
-	shm_free(char_str_vals[STR_VALS_URL_COL]);
-	shm_free(char_str_vals[STR_VALS_SIP_ADDR_COL]);
 
 	lock_switch_read(cl_list_lock, lock_old_flag);
 

modules/clusterer/clusterer_mod.c
@@ -234,10 +234,11 @@ do { \
 				col_name.s);	\
 			return -1;	\
 		}	\
-		pe = q_memchr(p + 1, ',', descr->s + descr->len - p - 1);	\
-		aux.s = p + 1;	\
-		aux.len = pe ? pe - p - 1 : descr->s + descr->len - p - 1;	\
-		if (aux.s >= descr->s + descr->len || !aux.len) {	\
+		p++;	\
+		pe = q_memchr(p, ',', descr->s + descr->len - p);	\
+		aux.s = p;	\
+		aux.len = pe ? pe - p : descr->s + descr->len - p;	\
+		if (aux.len == 0) {	\
 			LM_ERR("<%.*s> value expected\n", col_name.len,	\
 				col_name.s);	\
 			return -1;	\
@@ -249,17 +250,17 @@ do { \
 					col_name.s);	\
 				return -1;	\
 			}	\
-		} else \
-			str_vals[(_col_idx)] = aux.len ? aux.s : NULL;	\
+		} else	\
+			str_vals[(_col_idx)] = aux;	\
 	} else {	\
 		if ((_type) == 0)	\
 			int_vals[(_col_idx)] = -1;	\
 		else	\
-			str_vals[(_col_idx)] = NULL;	\
+			str_vals[(_col_idx)].s = NULL;	\
 	}	\
 } while(0)
 
-int parse_param_node_info(str *descr, int *int_vals, char **str_vals)
+int parse_param_node_info(str *descr, int *int_vals, str *str_vals)
 {
 	char *p, *pe;
 	str aux;

modules/clusterer/node_info.h
@@ -128,7 +128,7 @@ int load_db_info(db_func_t *dr_dbf, db_con_t* db_hdl, str *db_table, cluster_inf
 void free_info(cluster_info_t *cl_list);
 
 int add_node_info(node_info_t **new_info, cluster_info_t **cl_list, int *int_vals,
-					char **str_vals);
+					str *str_vals);
 
 int provision_neighbor(modparam_t type, void* val);
 int provision_current(modparam_t type, void *val);

commit e8255c83a655e8e6e8fb8136344599155da01e65

clusterer: fix crash when loading null string columns from DB
The bug was introduced in commit: 66ae3ac

modules/clusterer/node_info.c
@@ -452,15 +452,18 @@ int load_db_info(db_func_t *dr_dbf, db_con_t* db_hdl, str *db_table,
 
 		check_val(sip_addr_col, ROW_VALUES(row) + 7, DB_STRING, 0, 0);
 		str_vals[STR_VALS_SIP_ADDR_COL].s = (char*) VAL_STRING(ROW_VALUES(row) + 7);
-		str_vals[STR_VALS_SIP_ADDR_COL].len = strlen(str_vals[STR_VALS_SIP_ADDR_COL].s);
+		str_vals[STR_VALS_SIP_ADDR_COL].len = str_vals[STR_VALS_SIP_ADDR_COL].s ?
+			strlen(str_vals[STR_VALS_SIP_ADDR_COL].s) : 0;
 
 		check_val(flags_col, ROW_VALUES(row) + 8, DB_STRING, 0, 0);
 		str_vals[STR_VALS_FLAGS_COL].s = (char*) VAL_STRING(ROW_VALUES(row) + 8);
-		str_vals[STR_VALS_FLAGS_COL].len = strlen(str_vals[STR_VALS_FLAGS_COL].s);
+		str_vals[STR_VALS_FLAGS_COL].len = str_vals[STR_VALS_FLAGS_COL].s ?
+			strlen(str_vals[STR_VALS_FLAGS_COL].s) : 0;
 
 		check_val(description_col, ROW_VALUES(row) + 9, DB_STRING, 0, 0);
 		str_vals[STR_VALS_DESCRIPTION_COL].s = (char*) VAL_STRING(ROW_VALUES(row) + 9);
-		str_vals[STR_VALS_DESCRIPTION_COL].len = strlen(str_vals[STR_VALS_DESCRIPTION_COL].s);
+		str_vals[STR_VALS_DESCRIPTION_COL].len = str_vals[STR_VALS_DESCRIPTION_COL].s ?
+			strlen(str_vals[STR_VALS_DESCRIPTION_COL].s) : 0;
 
 		/* add info to backing list */
 		if ((rc = add_node_info(&new_info, cl_list, int_vals, str_vals)) != 0) {