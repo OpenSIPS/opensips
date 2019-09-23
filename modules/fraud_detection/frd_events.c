676289827d6931a8c070a095259fa9370e0fafb6
Liviu Chircu <liviu@opensips.org>
2019-09-23 20:08:24 +0300

    fraud_detection: Fix a dangling SHM access bug
    This patch fixes a race condition where a fraud_detection reload could
    free the global SHM "threshold" objects which are still being referenced
    N times (once for each ongoing dialog -- callback is invoked on
    termination).
    
    The solution is to simply store the integer values of the thresholds
    into the callback data, and avoid storing any SHM pointers which have no
    lifetime guarantees.
    
    (cherry picked from commit 04704dc05e48e1773252d0e038db89e2183ccb64)

--- modules/fraud_detection/frd_events.c
+++ modules/fraud_detection/frd_events.c
@@ -145,23 +145,22 @@ void raise_critical_event(str *param, unsigned int *val, unsigned int *thr,
  * For confirmed dialogs, it checks the duration against the
  * thresholds (sent through the params) and raises the appropriate event
  */
-void dialog_terminate_CB(struct dlg_cell *dlgc, int type,
+void dialog_terminate_CB(struct dlg_cell *dlg, int type,
 		struct dlg_cb_params *params)
 {
 	static str call_dur_name = str_init ("call_duration");
 	frd_dlg_param *frdparam = (frd_dlg_param*) *(params->param);
-	extern unsigned int frd_data_rev;
 
-	if (type == DLGCB_TERMINATED && frd_data_rev == frdparam->data_rev) {
-		unsigned int duration = time(NULL) - dlgc->start_ts;
-		if ( duration >= frdparam->thr->call_duration_thr.critical)
+	if (type & (DLGCB_TERMINATED|DLGCB_EXPIRED)) {
+		unsigned int duration = time(NULL) - dlg->start_ts;
+		if (duration >= frdparam->calldur_crit)
 			raise_critical_event(&call_dur_name, &duration,
-					&frdparam->thr->call_duration_thr.critical,
+					&frdparam->calldur_crit,
 					&frdparam->user, &frdparam->number, &frdparam->ruleid);
 
-		else if ( duration >= frdparam->thr->call_duration_thr.warning)
+		else if (duration >= frdparam->calldur_warn)
 			raise_warning_event(&call_dur_name, &duration,
-					&frdparam->thr->call_duration_thr.warning,
+					&frdparam->calldur_warn,
 					&frdparam->user, &frdparam->number, &frdparam->ruleid);
 	}
 
