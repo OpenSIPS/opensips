OpenSIPS/opensips

commit a74fff149a19c51ad333342c9a33357d3385f05b

struct hist debugging: Enhance the API
    * add the possibility to disable auto-logging.  Previously, each
      debugged entity would periodically dump logs to disk as it
      accumulated logs.   Disabling auto logging is especially useful
      when troubleshooting code in high-throughput environments.

    * sh_log() is now a macro, which will additionally print the
      FILE / FUNC / LINE of the calling code, offering more
      troubleshooting insights and reducing the amount of code written

    * simplify sh_unref(), the 2nd parameter was unnecessary

(cherry picked from commit deb03fe86a8d0134526c86e62183cb80887b09c9)

lib/dbg/struct_hist.c
@@ -42,10 +42,11 @@ static inline const char *verb2str(enum struct_hist_verb verb)
 	return sh_verb_strs[verb];
 }
 
-static void sh_unref_unsafe(struct struct_hist *sh, struct struct_hist_list *list);
+static void sh_unref_unsafe(struct struct_hist *sh);
 static void sh_free(struct struct_hist *sh);
 
-struct struct_hist_list *shl_init(char *obj_name, int window_size)
+struct struct_hist_list *shl_init(char *obj_name, int window_size,
+			int auto_logging)
 {
 	struct struct_hist_list *shl;
 
@@ -60,6 +61,7 @@ struct struct_hist_list *shl_init(char *obj_name, int window_size)
 	lock_init(&shl->wlock);
 	shl->win_sz = window_size;
 	shl->obj_name = obj_name;
+	shl->auto_logging = !!auto_logging;
 
 	return shl;
 }
@@ -101,9 +103,11 @@ struct struct_hist *sh_push(void *obj, struct struct_hist_list *list)
 
 	sh->obj = obj;
 	sh->obj_name = list->obj_name;
+	sh->shlist = list;
 	sh->created = get_uticks();
 	sh->ref = 2; /* one for "list", one for "return sh;" */
 	sh->max_len = ACTIONS_SIZE;
+	sh->auto_logging = list->auto_logging;
 	lock_init(&sh->wlock);
 
 	lock_get(&list->wlock);
@@ -116,7 +120,7 @@ struct struct_hist *sh_push(void *obj, struct struct_hist_list *list)
 		list_del(&last->list);
 		INIT_LIST_HEAD(&last->list);
 		list->len--;
-		sh_unref_unsafe(last, list);
+		sh_unref_unsafe(last);
 	}
 	lock_release(&list->wlock);
 
@@ -129,54 +133,64 @@ static void sh_free(struct struct_hist *sh)
 	shm_free(sh);
 }
 
-void sh_unref(struct struct_hist *sh, struct struct_hist_list *list)
+void sh_unref(struct struct_hist *sh)
 {
-	lock_get(&list->wlock);
-	sh_unref_unsafe(sh, list);
-	lock_release(&list->wlock);
+	lock_get(&sh->shlist->wlock);
+	sh_unref_unsafe(sh);
+	lock_release(&sh->shlist->wlock);
 }
 
-static void flush_sh(struct struct_hist *sh)
+static void _sh_flush(struct struct_hist *sh, int do_logging)
 {
-#ifdef ENABLE_SH_LOGGING
 	int i;
 
-	for (i = 0; i < sh->len; i++) {
-		LM_INFO("%5d. %p-%lld | %-15s | %-12lld | %-5d | %s |\n",
-		        i + 1 + sh->flush_offset,
-				sh->obj,
-				sh->created,
-		        verb2str(sh->actions[i].verb),
-		        sh->actions[i].t,
-		        sh->actions[i].pid,
-		        sh->actions[i].log);
+	if (do_logging) {
+		for (i = 0; i < sh->len; i++) {
+			LM_INFO("%5d. %p-%lld | %-15s | %-12lld | %-5d | %s |\n",
+			        i + 1 + sh->flush_offset,
+					sh->obj,
+					sh->created,
+			        verb2str(sh->actions[i].verb),
+			        sh->actions[i].t,
+			        sh->actions[i].pid,
+			        sh->actions[i].log);
+		}
 	}
-#endif
+
 	sh->flush_offset += sh->len;
 	sh->len = 0;
 }
 
-static void sh_unref_unsafe(struct struct_hist *sh, struct struct_hist_list *list)
+void sh_flush(struct struct_hist *sh)
+{
+	lock_get(&sh->wlock);
+	_sh_flush(sh, 1);
+	lock_release(&sh->wlock);
+}
+
+static void sh_unref_unsafe(struct struct_hist *sh)
 {
 	sh->ref--;
 	if (sh->ref != 0)
 		return;
-#ifdef ENABLE_SH_LOGGING
-	lock_get(&sh->wlock);
 
-	LM_INFO("%s %p free, %d actions follow\n", sh->obj_name, sh->obj, sh->len);
-	LM_INFO("=====================================\n");
-	flush_sh(sh);
+	if (sh->auto_logging) {
+		lock_get(&sh->wlock);
 
-	lock_release(&sh->wlock);
-#endif
-	if (!list_empty(&sh->list)) {
-		list_del(&sh->list);
+		LM_INFO("%s %p free, %d actions follow\n", sh->obj_name, sh->obj, sh->len);
+		LM_INFO("=====================================\n");
+		_sh_flush(sh, 1);
+
+		lock_release(&sh->wlock);
 	}
+
+	if (!list_empty(&sh->list))
+		list_del(&sh->list);
+
 	sh_free(sh);
 }
 
-int sh_log(struct struct_hist *sh, enum struct_hist_verb verb, char *fmt, ...)
+int _sh_log(struct struct_hist *sh, enum struct_hist_verb verb, char *fmt, ...)
 {
 	va_list ap;
 	int n;
@@ -189,11 +203,12 @@ int sh_log(struct struct_hist *sh, enum struct_hist_verb verb, char *fmt, ...)
 	lock_get(&sh->wlock);
 
 	if (flushable(sh)) {
-#ifdef ENABLE_SH_LOGGING
-		LM_INFO("%s %p flush, %d actions follow\n", sh->obj_name, sh->obj, sh->len);
-		LM_INFO("=====================================\n");
-#endif
-		flush_sh(sh);
+		if (sh->auto_logging) {
+			LM_INFO("%s %p flush, %d actions follow\n", sh->obj_name, sh->obj, sh->len);
+			LM_INFO("=====================================\n");
+		}
+
+		_sh_flush(sh, sh->auto_logging);
 	} else if (sh->len == sh->max_len) {
 		new = shm_realloc(sh->actions, sh->max_len * 2 * sizeof *sh->actions);
 		if (!new) {

lib/dbg/struct_hist.h
@@ -45,9 +45,7 @@
  *    remain available inside the global history list for a while
  */
 
-#define ENABLE_SH_LOGGING
-
-#define MAX_SHLOG_SIZE 50 /* longer log lines will get truncated */
+#define MAX_SHLOG_SIZE 100 /* longer log lines will get truncated */
 
 /**
  * To be freely extended by any piece of OpenSIPS code which makes use of
@@ -79,6 +77,7 @@ struct struct_hist {
 	void *obj;
 	char *obj_name;
 	utime_t created;
+	struct struct_hist_list *shlist;
 
 	int ref;
 
@@ -88,6 +87,7 @@ struct struct_hist {
 	int flush_offset;
 
 	gen_lock_t wlock;
+	int auto_logging;
 
 	struct list_head list;
 };
@@ -102,6 +102,7 @@ struct struct_hist_list {
 	int len;
 	int win_sz;
 	long long total_obj;
+	int auto_logging;
 
 	gen_lock_t wlock;
 };
@@ -111,7 +112,9 @@ struct struct_hist_list {
 #define shl_destroy(...)
 #define sh_push(...) NULL
 #define sh_unref(...)
-#define sh_log(...) ({0;})
+#define _sh_log(...) ({0;})
+#define sh_log _sh_log
+#define sh_flush(...)
 #else
 
 /**
@@ -120,11 +123,13 @@ struct struct_hist_list {
  *
  * @obj_name: A name for the structs which will be troubleshooted
  * @window_size: (gliding window) - the max number of retained histories
+ * @auto_logging: if true, each struct_hist object will log info as it grows
  *
  * WARNING: a "window_size" of 0 (infinite) is essentially a memory leak,
  * use with caution!
  */
-struct struct_hist_list *shl_init(char *obj_name, int window_size);
+struct struct_hist_list *shl_init(char *obj_name, int window_size,
+			int auto_logging);
 
 /**
  * Frees up the global history holder, along with all of its content
@@ -145,9 +150,8 @@ struct struct_hist *sh_push(void *obj, struct struct_hist_list *list);
  * gliding window or not, it may also get freed immediately.
  *
  * @sh: a struct history tracker
- * @list: the global holder of histories
  */
-void sh_unref(struct struct_hist *sh, struct struct_hist_list *list);
+void sh_unref(struct struct_hist *sh);
 
 /**
  * Record a log line to the history of a struct. The max length of a line is
@@ -157,7 +161,22 @@ void sh_unref(struct struct_hist *sh, struct struct_hist_list *list);
  * @verb: the type of the log line recorded (taken from SH_ALL_VERBS)
  * @fmt: C format string
  */
-int sh_log(struct struct_hist *sh, enum struct_hist_verb verb, char *fmt, ...);
+int _sh_log(struct struct_hist *sh, enum struct_hist_verb verb, char *fmt, ...);
+#define sh_log(sh, verb, fmt, args...) \
+	do { \
+		_sh_log(sh, verb, "%s:%s:%d: "fmt, \
+		        __FILE__, __FUNCTION__, __LINE__, ##args); \
+	} while (0)
+
+/**
+ * Force the contents of a struct hist to be flushed to the log.  Useful in
+ * high-volume traffic conditions, where only certain (dubious) objects
+ * must be logged.
+ *
+ * @sh: a struct history tracker
+ */
+void sh_flush(struct struct_hist *sh);
+
 #endif
 
 #endif /* __STRUCT_HIST_H__ */

net/net_tcp.c
@@ -741,7 +741,7 @@ static void _tcpconn_rm(struct tcp_connection* c)
 		protos[c->type].net.conn_clean(c);
 
 	sh_log(c->hist, TCP_DESTROY, "type=%d", c->type);
-	sh_unref(c->hist, con_hist);
+	sh_unref(c->hist);
 	c->hist = NULL;
 
 	shm_free(c);
@@ -1723,7 +1723,7 @@ int tcp_init(void)
 		return 0;
 
 #ifdef DBG_TCPCON
-	con_hist = shl_init("TCP con", 10000);
+	con_hist = shl_init("TCP con", 10000, 1);
 	if (!con_hist) {
 		LM_ERR("oom con hist\n");
 		goto error;

commit 0db946724e9eaa364f54425f8664d96529e848a1

struct hist API:  Fix possible crashes; Improve API
Commit a74fff149a introduced a race condition on this logic:

    lock_get(&sh->shlist->wlock);
    sh_unref_unsafe(sh);
    lock_release(&sh->shlist->wlock);

, where "sh" must no longer be read following the unref operation.
This commit fixes this issue, along with:

    * fix crash with -DSTRUCT_HIST but no -DDBG_TCPCON
    * speed optimizations: eliminate memset() operations (not needed)
    * make sh_push() more flexible (extra ref counts from outside)
    * code: hide structs, so importing struct_hist.h doesn't conflict
       with mysql.h's own "struct list_head"

lib/dbg/struct_hist.c
@@ -22,10 +22,44 @@
 #include <stdarg.h>
 
 #include "struct_hist.h"
+
 #include "../../mem/shm_mem.h"
 #include "../../dprint.h"
 #include "../../locking.h"
 #include "../../pt.h"
+#include "../list.h"
+
+struct struct_hist {
+	void *obj;
+	char *obj_name;
+	utime_t created;
+	struct struct_hist_list *shlist;
+
+	int ref;
+
+	struct struct_hist_action *actions;
+	int len;
+	int max_len;
+	int flush_offset;
+
+	gen_lock_t wlock;
+	int auto_logging;
+
+	struct list_head list;
+};
+
+struct struct_hist_list {
+	char *obj_name;
+
+	struct list_head objects;
+	int len;
+	int win_sz;
+	long long total_obj;
+	int auto_logging;
+	int init_actions_sz;
+
+	gen_lock_t wlock;
+};
 
 static inline const char *verb2str(enum struct_hist_verb verb)
 {
@@ -45,8 +79,8 @@ static inline const char *verb2str(enum struct_hist_verb verb)
 static void sh_unref_unsafe(struct struct_hist *sh);
 static void sh_free(struct struct_hist *sh);
 
-struct struct_hist_list *shl_init(char *obj_name, int window_size,
-			int auto_logging)
+struct struct_hist_list *_shl_init(char *obj_name, int window_size,
+			int auto_logging, int init_actions_sz)
 {
 	struct struct_hist_list *shl;
 
@@ -62,6 +96,7 @@ struct struct_hist_list *shl_init(char *obj_name, int window_size,
 	shl->win_sz = window_size;
 	shl->obj_name = obj_name;
 	shl->auto_logging = !!auto_logging;
+	shl->init_actions_sz = init_actions_sz;
 
 	return shl;
 }
@@ -71,6 +106,9 @@ void shl_destroy(struct struct_hist_list *shl)
 	struct list_head *el, *next;
 	struct struct_hist *sh;
 
+	if (!shl)
+		return;
+
 	list_for_each_safe(el, next, &shl->objects) {
 		sh = list_entry(el, struct struct_hist, list);
 		sh_free(sh);
@@ -79,11 +117,11 @@ void shl_destroy(struct struct_hist_list *shl)
 	shm_free(shl);
 }
 
-struct struct_hist *sh_push(void *obj, struct struct_hist_list *list)
+struct struct_hist *_sh_push(void *obj, struct struct_hist_list *list, int refs)
 {
 	struct struct_hist *sh, *last;
 
-	if (!obj)
+	if (!obj || !list)
 		return NULL;
 
 	sh = shm_malloc(sizeof *sh);
@@ -91,23 +129,26 @@ struct struct_hist *sh_push(void *obj, struct struct_hist_list *list)
 		LM_ERR("oom\n");
 		return NULL;
 	}
-	memset(sh, 0, sizeof *sh);
+	/* CAREFUL: sh is not memset, for speed reasons! */
 
-	sh->actions = shm_malloc(ACTIONS_SIZE * sizeof *sh->actions);
+	sh->actions = shm_malloc(list->init_actions_sz * sizeof *sh->actions);
 	if (!sh->actions) {
 		LM_ERR("oom2\n");
 		shm_free(sh);
 		return NULL;
 	}
-	memset(sh->actions, 0, ACTIONS_SIZE * sizeof *sh->actions);
+	/* CAREFUL: sh->actions is not memset, for speed reasons! */
 
 	sh->obj = obj;
 	sh->obj_name = list->obj_name;
-	sh->shlist = list;
 	sh->created = get_uticks();
-	sh->ref = 2; /* one for "list", one for "return sh;" */
-	sh->max_len = ACTIONS_SIZE;
+	sh->shlist = list;
+	sh->ref = 1 + refs; /* one for "list", the rest are for the caller */
+	sh->len = 0;
+	sh->max_len = list->init_actions_sz;
+	sh->flush_offset = 0;
 	sh->auto_logging = list->auto_logging;
+
 	lock_init(&sh->wlock);
 
 	lock_get(&list->wlock);
@@ -135,9 +176,11 @@ static void sh_free(struct struct_hist *sh)
 
 void sh_unref(struct struct_hist *sh)
 {
-	lock_get(&sh->shlist->wlock);
+	gen_lock_t *shl_lock = &sh->shlist->wlock;
+
+	lock_get(shl_lock);
 	sh_unref_unsafe(sh);
-	lock_release(&sh->shlist->wlock);
+	lock_release(shl_lock);
 }
 
 static void _sh_flush(struct struct_hist *sh, int do_logging)
@@ -216,7 +259,7 @@ int _sh_log(struct struct_hist *sh, enum struct_hist_verb verb, char *fmt, ...)
 			LM_ERR("oom\n");
 			return -1;
 		}
-		memset(&new[sh->max_len], 0, sh->max_len * sizeof *sh->actions);
+		/* CAREFUL: newly added actions are not memset, for speed reasons! */
 
 		sh->actions = new;
 		sh->max_len *= 2;

lib/dbg/struct_hist.h
@@ -21,10 +21,7 @@
 #ifndef __STRUCT_HIST_H__
 #define __STRUCT_HIST_H__
 
-#include "../../locking.h"
 #include "../../timer.h"
-
-#include "../list.h"
 
 /**
  * Generic struct debugging support. Especially useful for troubleshooting
@@ -75,40 +72,11 @@ struct struct_hist_action {
 	char log[MAX_SHLOG_SIZE];
 };
 
-#define ACTIONS_SIZE 5
-struct struct_hist {
-	void *obj;
-	char *obj_name;
-	utime_t created;
-	struct struct_hist_list *shlist;
-
-	int ref;
-
-	struct struct_hist_action *actions;
-	int len;
-	int max_len;
-	int flush_offset;
-
-	gen_lock_t wlock;
-	int auto_logging;
-
-	struct list_head list;
-};
+struct struct_hist;
+struct struct_hist_list;
 
 #define FLUSH_LIMIT 2000
 #define flushable(sh) (sh->len == FLUSH_LIMIT)
-
-struct struct_hist_list {
-	char *obj_name;
-
-	struct list_head objects;
-	int len;
-	int win_sz;
-	long long total_obj;
-	int auto_logging;
-
-	gen_lock_t wlock;
-};
 
 #ifndef DBG_STRUCT_HIST
 #define shl_init(...) NULL
@@ -127,12 +95,14 @@ struct struct_hist_list {
  * @obj_name: A name for the structs which will be troubleshooted
  * @window_size: (gliding window) - the max number of retained histories
  * @auto_logging: if true, each struct_hist object will log info as it grows
+ * @init_actions_sz: initial allocation size of each object's actions array
  *
  * WARNING: a "window_size" of 0 (infinite) is essentially a memory leak,
  * use with caution!
  */
-struct struct_hist_list *shl_init(char *obj_name, int window_size,
-			int auto_logging);
+struct struct_hist_list *_shl_init(char *obj_name, int window_size,
+			int auto_logging, int init_actions_sz);
+#define shl_init(nm, wsz, autolog) _shl_init(nm, wsz, autolog, 5)
 
 /**
  * Frees up the global history holder, along with all of its content
@@ -145,8 +115,10 @@ void shl_destroy(struct struct_hist_list *shl);
  * @obj: The corresponding struct address. This value will be embedded into
  *       the history object, to allow look-ups inside gdb
  * @list: global holder where this history will be pushed
+ * @refs: the amount of references to the new object kept by the calling code
  */
-struct struct_hist *sh_push(void *obj, struct struct_hist_list *list);
+struct struct_hist *_sh_push(void *obj, struct struct_hist_list *list, int refs);
+#define sh_push(obj, list) _sh_push(obj, list, 1)
 
 /**
  * Unreference a history struct. Depending on whether it is still in the

net/net_tcp.c
@@ -723,9 +723,11 @@ static void _tcpconn_rm(struct tcp_connection* c)
 	if (protos[c->type].net.conn_clean)
 		protos[c->type].net.conn_clean(c);
 
+#ifdef DBG_TCPCON
 	sh_log(c->hist, TCP_DESTROY, "type=%d", c->type);
 	sh_unref(c->hist);
 	c->hist = NULL;
+#endif
 
 	shm_free(c);
 }
@@ -876,7 +878,9 @@ static struct tcp_connection* tcpconn_new(int sock, union sockaddr_union* su,
 	/* start with the default conn lifetime */
 	c->lifetime = get_ticks()+tcp_con_lifetime;
 	c->flags|=F_CONN_REMOVED|flags;
+#ifdef DBG_TCPCON
 	c->hist = sh_push(c, con_hist);
+#endif
 
 	if (protos[si->proto].net.conn_init &&
 	protos[si->proto].net.conn_init(c)<0) {