OpenSIPS/opensips

commit 568675291ec7dab511856e4cba8fab162e6e137e

Integer fixups rework: Complete commit f37527dcf

modules/drouting/drouting.c
@@ -4304,11 +4304,11 @@ static int goes_to_gw_1(struct sip_msg* msg, char * part, char* _type, char* fla
 
 	if(use_partitions) {
 		gw_attrs_spec = (pv_spec_p)gw_att;
-		return _is_dr_uri_gw(msg, part, flags_pv, (!_type ? -1 : (int)(long)_type),
+		return _is_dr_uri_gw(msg, part, flags_pv, (!_type ? -1 : *(int *)_type),
 				GET_NEXT_HOP(msg));
 	} else {
 		gw_attrs_spec = (pv_spec_p)flags_pv;
-		return _is_dr_uri_gw(msg, NULL, flags_pv, (!_type ? -1 : (int)(long)_type),
+		return _is_dr_uri_gw(msg, NULL, flags_pv, (!_type ? -1 : *(int *)_type),
 				GET_NEXT_HOP(msg));
 	}
 }

modules/auth/challenge.c
@@ -214,7 +214,8 @@ static inline int challenge(struct sip_msg* _msg, gparam_p _realm, int _qop,
  */
 int www_challenge(struct sip_msg* _msg, char* _realm, char* _qop)
 {
-	return challenge(_msg, (gparam_p)_realm, (int)(long)_qop, 401,
+	return challenge(_msg, (gparam_p)_realm,
+			!_qop ? 0 : (int)*(unsigned int *)_qop, 401,
 			MESSAGE_401, WWW_AUTH_CHALLENGE);
 }
 
@@ -224,7 +225,8 @@ int www_challenge(struct sip_msg* _msg, char* _realm, char* _qop)
  */
 int proxy_challenge(struct sip_msg* _msg, char* _realm, char* _qop)
 {
-	return challenge(_msg, (gparam_p)_realm, (int)(long)_qop, 407,
+	return challenge(_msg, (gparam_p)_realm,
+			!_qop ? 0 : (int)*(unsigned int *)_qop, 407,
 			MESSAGE_407, PROXY_AUTH_CHALLENGE);
 }
 

modules/regex/regex_mod.c
@@ -623,7 +623,7 @@ static int w_pcre_match_group(struct sip_msg* _msg, char* _s1, char* _s2)
 	if (_s2 == NULL) {
 		num_pcre = 0;
 	} else {
-		num_pcre = (uint)(long)_s2;
+		num_pcre = *(unsigned int *)_s2;
 	}
 
 	if (num_pcre >= *num_pcres) {

commit 50e21b27be1764a269bf5361b1e0e15fa909d3bf

drouting: fix type crash for goes_to_gw()
When goes_to_gw() was not called with type parameter, OpenSIPS was
crashing, due to the fact that it was dereffering -1.
This bug was introduced in 568675291e and f37527dcf

modules/drouting/drouting.c
@@ -4324,7 +4324,8 @@ static int goes_to_gw_1(struct sip_msg* msg, char * part, char* _type, char* fla
  */
 static int goes_to_gw_0(struct sip_msg* msg)
 {
-	return goes_to_gw_1(msg, NULL, (char *)-1, NULL, NULL);
+	int type = -1;
+	return goes_to_gw_1(msg, NULL, (char *)&type, NULL, NULL);
 }
 
 