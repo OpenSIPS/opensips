040c1dcf890741624968b4ac0d6ae19a0536dd7e
Razvan Crainea <razvan@opensips.org>
2020-03-28 13:07:45 +0000

    also export tables along connectors

--- db/db_pi.c
+++ db/db_pi.c
@@ -27,6 +27,7 @@ struct pi_conns {
     str table;          /* the name of the table */
     db_con_t *con;      /* database connector */
     unsigned int flags; /* different flags */
+    str *tables;        /* NULL terminated array of tables the module exposes */
     struct pi_conns *next;
 } *pi_conns_list;
 
@@ -41,22 +42,29 @@ int db_pi_add(str *name, str *table, db_con_t *con, unsigned int flags)
 mi_response_t *w_mi_pi_list(const mi_params_t *params,
 								struct mi_handler *async_hdl)
 {
-    static str any = str_init("*");
-    static str pi_conns[] = {
-        str_init("auth_db"),
-        str_init("call_center"),
-        str_init("domain"),
-        str_init("dialplan"),
-        str_init("rtpproxy"),
+    struct {
+        str name;
+        str table[10];
+    } pi_conns[] = {
+        {str_init("acc"), {str_init("acc"), str_init("missed_calls"), {0, 0}}},
+        {str_init("auth_db"), {str_init("subscriber"), {0, 0}}},
+        {str_init("call_center"), {str_init("call_center"), {0, 0}}},
+        {str_init("domain"), {str_init("domain"), {0, 0}}},
+        {str_init("dialplan"), {str_init("dialplan"), {0, 0}}},
+        {str_init("rtpproxy"), {str_init("rtpproxy_sockets"), {0, 0}}},
     };
 
-    int i;
+    int i, j;
 	mi_item_t *resp_obj;
+	mi_item_t *arr_obj;
 	mi_response_t *resp = init_mi_result_object(&resp_obj);
 	if (!resp)
 		return 0;
     /* quick hack to return a list of provisioned connectors */
-    for (i = 0; i < sizeof(pi_conns)/sizeof(pi_conns[0]); i++)
-        add_mi_string(resp_obj, pi_conns[i].s, pi_conns[i].len, any.s, any.len);
+    for (i = 0; i < sizeof(pi_conns)/sizeof(pi_conns[0]); i++) {
+        arr_obj = add_mi_array(resp_obj, pi_conns[i].name.s, pi_conns[i].name.len);
+        for (j = 0; pi_conns[i].table[j].s; j++)
+            add_mi_string(arr_obj, 0, 0, pi_conns[i].table[j].s, pi_conns[i].table[j].len);
+    }
     return resp;
 }
