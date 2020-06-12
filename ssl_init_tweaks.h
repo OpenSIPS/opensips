3836391a0f27a37612aa9e1c22d4158c25401f00
Vlad Patrascu <vladp@opensips.org>
2020-06-13 00:26:09 +0300

    cachedb_mongodb: complete fix in commit a3e8727 for openssl < 1.1.0
    (cherry picked from commit 9f454463e42f2148990bd5f307eeff0acecb18bb)

--- ssl_init_tweaks.h
+++ ssl_init_tweaks.h
@@ -25,6 +25,7 @@
 #include "sr_module.h"
 
 #if OPENSSL_VERSION_NUMBER >= 0x10100000L
+
 int OPENSSL_init_ssl(uint64_t opts, const OPENSSL_INIT_SETTINGS *settings)
 {
 	int (*real_OPENSSL_init_ssl)(uint64_t, const OPENSSL_INIT_SETTINGS *);
@@ -42,7 +43,7 @@ int OPENSSL_init_ssl(uint64_t opts, const OPENSSL_INIT_SETTINGS *settings)
 
 		return real_OPENSSL_init_ssl(opts, settings);
 	}
-};
+}
 
 int OPENSSL_init_crypto(uint64_t opts, const OPENSSL_INIT_SETTINGS *settings)
 {
@@ -61,7 +62,7 @@ int OPENSSL_init_crypto(uint64_t opts, const OPENSSL_INIT_SETTINGS *settings)
 
 		return real_OPENSSL_init_crypto(opts, settings);
 	}
-};
+}
 
 int ERR_load_BIO_strings(void)
 {
@@ -81,7 +82,9 @@ int ERR_load_BIO_strings(void)
 		return real_ERR_load_BIO_strings();
 	}
 }
+
 #else
+
 int SSL_library_init(void)
 {
 	int (*real_SSL_library_init)(void);
@@ -119,4 +122,69 @@ void ERR_load_BIO_strings(void)
 		real_ERR_load_BIO_strings();
 	}
 }
+
+void CRYPTO_set_locking_callback(void (*func)(int mode, int type,
+                                               const char *file, int line))
+{
+	int (*real_CRYPTO_set_locking_callback)(void (*)(int, int,
+		const char *, int));
+	static int have_tls_mgm = -1;
+
+	if (have_tls_mgm == -1)
+		have_tls_mgm = module_loaded("tls_mgm");
+
+	if (have_tls_mgm) {
+		return;
+	} else {
+		real_CRYPTO_set_locking_callback = dlsym(RTLD_NEXT,
+			"CRYPTO_set_locking_callback");
+		if (!real_CRYPTO_set_locking_callback)
+			return;
+
+		real_CRYPTO_set_locking_callback(func);
+	}
+}
+
+#if OPENSSL_VERSION_NUMBER < 0x10000000L
+void CRYPTO_set_id_callback(unsigned long (*func) (void))
+{
+	int (*real_CRYPTO_set_id_callback)(unsigned long (*)(void));
+	static int have_tls_mgm = -1;
+
+	if (have_tls_mgm == -1)
+		have_tls_mgm = module_loaded("tls_mgm");
+
+	if (have_tls_mgm) {
+		return;
+	} else {
+		real_CRYPTO_set_id_callback = dlsym(RTLD_NEXT, "CRYPTO_set_id_callback");
+		if (!real_CRYPTO_set_id_callback)
+			return;
+
+		real_CRYPTO_set_id_callback(func);
+	}
+}
+
+#else /* between 1.0.0 and 1.1.0 */
+int CRYPTO_THREADID_set_callback(void (*threadid_func) (CRYPTO_THREADID *))
+{
+	int (*real_CRYPTO_THREADID_set_callback)(void (*)(CRYPTO_THREADID *));
+	static int have_tls_mgm = -1;
+
+	if (have_tls_mgm == -1)
+		have_tls_mgm = module_loaded("tls_mgm");
+
+	if (have_tls_mgm) {
+		return 0;
+	} else {
+		real_CRYPTO_THREADID_set_callback = dlsym(RTLD_NEXT,
+			"CRYPTO_THREADID_set_callback");
+		if (!real_CRYPTO_THREADID_set_callback)
+			return 0;
+
+		return real_CRYPTO_THREADID_set_callback(threadid_func);
+	}
+}
+#endif
+
 #endif
