OpenSIPS/opensips

commit c263182ee4dd692212ca310feb8a902714b93b45

Do not simply link the buffered lumps, but better clone them -> this will avoid a mixage of lump types (shm versus pkg) when using async()

modules/rr/record.c
@@ -427,10 +427,7 @@ int record_route_preset(struct sip_msg* _m, str* _data)
 		&& ap->before->u.cond==COND_FALSE) {
 			/* found our phony anchor lump */
 			/* jump over the anchor and conditional lumps */
-			lp = ap->before->before;
-			/* unlink it */
-			ap->before->before = NULL;
-			ap->type = 0;
+			lp = dup_lump_list(ap->before->before);
 			/* link the pending buffered params and go at the end of the list*/
 			for ( l2->before = lp ; l2 && l2->before ; l2=l2->before);
 			break;

commit e3ca95ecd0b3f33b7f42f3a7eda4a6e686cb241d

rr: Fix ordering issues with add_rr_param() and create_dialog()
Commits bd1719dcf and c263182ee4d introduced regressions for
record_route() and record_route_preset(), respectively, which caused
add_rr_param() to not work anymore in the following sequence of function
calls:

create_dialog() with a "dlg_match_mode" != DID_NONE
record_route() or record_route_preset()
add_rr_param() # would not do anything, simply acting as a NOP

The reason is that the "fake" lump chain which was meant to hold
all add_rr_param() operations prior to an eventual record_route() call
would no longer be masked after the record_route() and would continue to
accumulate data from subsequent add_rr_param() calls.  Since it was
meant to be a "fake" chain from the beginning (maybe the user doesn't
call record_route() at all!), the data would simply be lost.

Many thanks to John Quick for the accurate report and instructions on
how to reproduce the issue!

modules/rr/record.c
@@ -280,9 +280,12 @@ int record_route(struct sip_msg* _m, str *params)
 		if (ap->type==HDR_RECORDROUTE_T && ap->op==LUMP_NOP
 		&& ap->before && ap->before->op==LUMP_ADD_OPT
 		&& ap->before->u.cond==COND_FALSE) {
-			/* found our phony anchor lump */
+			/* found our phony anchor lump -> hide it for future searches */
+			ap->type = HDR_ERROR_T;
+
 			/* jump over the anchor and conditional lumps */
 			lp = dup_lump_list(ap->before->before);
+
 			/* if double routing, make a copy of the buffered lumps for the
 			   second route hdr. */
 			if (enable_double_rr)
@@ -425,9 +428,12 @@ int record_route_preset(struct sip_msg* _m, str* _data)
 		if (ap->type==HDR_RECORDROUTE_T && ap->op==LUMP_NOP
 		&& ap->before && ap->before->op==LUMP_ADD_OPT
 		&& ap->before->u.cond==COND_FALSE) {
-			/* found our phony anchor lump */
+			/* found our phony anchor lump -> hide it for future searches */
+			ap->type = HDR_ERROR_T;
+
 			/* jump over the anchor and conditional lumps */
 			lp = dup_lump_list(ap->before->before);
+
 			/* link the pending buffered params and go at the end of the list*/
 			for ( l2->before = lp ; l2 && l2->before ; l2=l2->before);
 			break;