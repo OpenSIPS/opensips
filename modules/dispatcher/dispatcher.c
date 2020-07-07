1f14390a4b7147a3ba7434b5dc19c6aa98184a44
Liviu Chircu <liviu@opensips.org>
2020-07-07 19:27:45 +0300

    dispatcher: Fix a bug with the 'default' partition
    This commit allows the "default" partition to be populated just like the
    other ones:
    
    modparam("dispatcher", "partition",
        "default:
                db_url = mysql://opensips:opensipsrw@127.0.0.1/opensips_3_2;
                table_name = v1_dispatcher")
    
    (cherry picked from commit cc2f4a8c49402f99502a58940d2fbb51daed6e47)
    (cherry picked from commit 419f0119400229ac54df2b8ce1943d88c31c4b5a)

--- modules/dispatcher/dispatcher.c
+++ modules/dispatcher/dispatcher.c
@@ -95,15 +95,15 @@ typedef struct _ds_db_head
 
 ds_db_head_t default_db_head = {
 	str_init(DS_DEFAULT_PARTITION_NAME),
-	{NULL, 0},
-	{NULL, 0},
+	{NULL, -1},
+	{NULL, -1},
 
 
-	{NULL, 0},
-	{NULL, 0},
-	{NULL, 0},
-	{NULL, 0},
-	{NULL, 0},
+	{NULL, -1},
+	{NULL, -1},
+	{NULL, -1},
+	{NULL, -1},
+	{NULL, -1},
 	NULL
 };
 ds_db_head_t *ds_db_heads = NULL;
@@ -705,7 +705,7 @@ void set_default_head_values(ds_db_head_t *head)
 		str *p_val = partition_params[i].getter_func(head);
 		if (p_val->s == NULL)
 			*p_val = partition_params[i].default_value;
-		else
+		else if (p_val->len == -1)
 			p_val->len = strlen(p_val -> s);
 	}
 }
