a9d3da3d18c7f0647bafdc7f2856b5753f8879d1
Bogdan-Andrei Iancu <bogdan@opensips.org>
2020-07-15 18:13:02 +0300

    [pua_dialoginfo] Fix bad test on resolving flag name
    get_flag_id_by_name() may also return other negative codes than -1
    
    Reported by coverity CID 207914
    
    (cherry picked from commit f3530c10adc1d2a5b01340a9823f84db61c98e72)

--- modules/pua_dialoginfo/pua_dialoginfo.c
+++ modules/pua_dialoginfo/pua_dialoginfo.c
@@ -663,7 +663,7 @@ static int mod_init(void)
 	pua_send_publish= pua.send_publish;
 
 	nopublish_flag = get_flag_id_by_name(FLAG_TYPE_MSG, nopublish_flag_str, 0);
-	nopublish_flag = (nopublish_flag!=-1)?(1<<nopublish_flag):0;
+	nopublish_flag = (nopublish_flag>=0)?(1<<nopublish_flag):0;
 
 	if(!osips_ps)
 		evp = dialoginfo_process_body;
