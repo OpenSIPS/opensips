13536402f7b86b81deb9fb53963a41adc5b011b5
Liviu Chircu <liviu@opensips.org>
2020-07-30 16:53:21 +0300

    fraud_detection: Add the possibility to disable stats
    A (0) value for any of the fraud detection thresholds now means: "ignore
    this threshold", instead of: "raise an event on each call for this
    threshold?!".  Although (0) values could, in theory, have been useful
    for event generation testing purposes before this patch, this is a small
    price to pay in order to gain the ability to disable a stat.  Developers
    can now add 1 more CPS and still keep testing with a new (1) value :)
    
    Although the DB schema changes slightly, due to the added "DEFAULT 0",
    it is fully backwards-compatible with the previous one, except
    fraud_detection row INSERTs will now be more quiet, generating no more
    warnings from the backend (e.g. "Column does not have a default
    value..." -> but still defaults to 0, thanks to non-strict mode, leading
    to strange warnings from the module).
    
    Fixes #890
    
    (cherry picked from commit 214f105ae52593bd53aef83f4426e1bbb0a09666)

--- scripts/postgres/fraud_detection-create.sql
+++ scripts/postgres/fraud_detection-create.sql
@@ -6,16 +6,16 @@ CREATE TABLE fraud_detection (
     start_hour VARCHAR(5) DEFAULT '00:00' NOT NULL,
     end_hour VARCHAR(5) DEFAULT '23:59' NOT NULL,
     daysoftheweek VARCHAR(64) DEFAULT 'Mon-Sun' NOT NULL,
-    cpm_warning INTEGER NOT NULL,
-    cpm_critical INTEGER NOT NULL,
-    call_duration_warning INTEGER NOT NULL,
-    call_duration_critical INTEGER NOT NULL,
-    total_calls_warning INTEGER NOT NULL,
-    total_calls_critical INTEGER NOT NULL,
-    concurrent_calls_warning INTEGER NOT NULL,
-    concurrent_calls_critical INTEGER NOT NULL,
-    sequential_calls_warning INTEGER NOT NULL,
-    sequential_calls_critical INTEGER NOT NULL
+    cpm_warning INTEGER DEFAULT 0 NOT NULL,
+    cpm_critical INTEGER DEFAULT 0 NOT NULL,
+    call_duration_warning INTEGER DEFAULT 0 NOT NULL,
+    call_duration_critical INTEGER DEFAULT 0 NOT NULL,
+    total_calls_warning INTEGER DEFAULT 0 NOT NULL,
+    total_calls_critical INTEGER DEFAULT 0 NOT NULL,
+    concurrent_calls_warning INTEGER DEFAULT 0 NOT NULL,
+    concurrent_calls_critical INTEGER DEFAULT 0 NOT NULL,
+    sequential_calls_warning INTEGER DEFAULT 0 NOT NULL,
+    sequential_calls_critical INTEGER DEFAULT 0 NOT NULL
 );
 
 ALTER SEQUENCE fraud_detection_ruleid_seq MAXVALUE 2147483647 CYCLE;
