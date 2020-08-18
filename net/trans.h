OpenSIPS/opensips

commit 899f65c8f455070b9f04d456e59a3220810ef877

Fix test of recognizing SCTP as UDP-based protocol
...and allow per-socket/listener definition of number of workers/processes

Closes #2125

(cherry picked from commit 749685c7ab6e7baa6562f9803bc52f2fc89ea82b)

socket_info.c
@@ -150,8 +150,9 @@ struct socket_info* new_sock_info(	char* name,
 		si->adv_port_str.len=snprintf(si->adv_port_str.s, 10, "%hu", adv_port);
 		si->adv_port = adv_port;
 	}
-	if ( (si->proto==PROTO_TCP || si->proto==PROTO_TLS) && children) {
-		LM_WARN("number of children per TCP/TLS listener not supported -> ignoring...\n");
+	if ( si->proto!=PROTO_UDP && si->proto!=PROTO_SCTP && children) {
+		LM_WARN("number of workers per non UDP-based <%.*s> listener not "
+			"supported -> ignoring...\n", si->name.len, si->name.s);
 	} else {
 		si->children = children;
 	}

commit 2801fae6f88bd6b4c958b76ecb96c7cd7ba80d72

hep_udp listener: Fix "use_children" regression
Commit 899f65c8f4 introduced a regression where "use_children" would be
ignored for the HEP UDP protocol, possibly leading to extra forked
workers after an OpenSIPS upgrade, failed restarts due to exceeded MySQL
max connections, etc.

net/trans.h
@@ -73,7 +73,10 @@ extern struct proto_info protos[];
  */
 
 #define is_udp_based_proto(_p) \
-	(protos[_p].net.flags&PROTO_NET_USE_UDP)
+	((_p==PROTO_HEP_UDP) || protos[_p].net.flags&PROTO_NET_USE_UDP)
+/* XXX: ^ the PROTO_NET_USE_UDP concept is currently broken, since the macro
+ * may be used well before trans_load() is invoked, thus the "extra" protocols
+ * from @protos are still uninitialized and the macro will fail to see them */
 
 #define proto_has_listeners(_p) \
 	(protos[_p].listeners != NULL)

socket_info.c
@@ -150,7 +150,7 @@ struct socket_info* new_sock_info(	char* name,
 		si->adv_port_str.len=snprintf(si->adv_port_str.s, 10, "%hu", adv_port);
 		si->adv_port = adv_port;
 	}
-	if ( si->proto!=PROTO_UDP && si->proto!=PROTO_SCTP && children) {
+	if (!is_udp_based_proto(si->proto) && children) {
 		LM_WARN("number of workers per non UDP-based <%.*s> listener not "
 			"supported -> ignoring...\n", si->name.len, si->name.s);
 	} else {