OpenSIPS/opensips

commit d883a0d72ac07353d8c84cdd4ab62265887a56b9

dialog: always represent the dialog ID as hexa
This commit normalizes the way the dialog id is represented or
interpreted by different functions to a single, hexa representation of
the hash entry and hash id of the dialog. It affects the following:
 * $DLG_did variable - changes the output representation
 * dlg_list MI command - instead of printing only the db ID, it now shows
   both the dialog ID, and the db id
 * dlg_end_dlg MI command - use the dialog id instead of the DB id to
   terminate a dialog
 * load_dlg_ctx function - the did always need to have the hexa
   representation
 * rtpproxy timeout & DTMF - the id will now have the hexa
   representation

modules/dialog/dlg_hash.c
@@ -36,6 +36,7 @@
 #include "dlg_profile.h"
 #include "dlg_replication.h"
 #include "dlg_req_within.h"
+#include "dlg_handlers.h"
 #include "dlg_db_handler.h"
 #include "../../evi/evi_params.h"
 #include "../../evi/evi_modules.h"
@@ -891,11 +892,14 @@ struct dlg_cell* get_dlg_by_callid( str *callid, int active_only)
 }
 
 
-struct dlg_cell* get_dlg_by_did(unsigned int h_entry, unsigned int h_id,
-		int active_only)
+struct dlg_cell* get_dlg_by_did(str *did, int active_only)
 {
 	struct dlg_cell *dlg;
 	struct dlg_entry *d_entry;
+	unsigned h_entry, h_id;
+
+	if (parse_dlg_did(did, &h_entry, &h_id) < 0)
+		return NULL;
 
 	if (h_entry>=d_table->size)
 		return NULL;
@@ -916,6 +920,26 @@ struct dlg_cell* get_dlg_by_did(unsigned int h_entry, unsigned int h_id,
 
 	dlg_unlock( d_table, d_entry);
 	return NULL;
+}
+
+struct dlg_cell *get_dlg_by_dialog_id(str *dialog_id)
+{
+	struct dlg_cell *dlg;
+	unsigned int h_entry, h_id;
+
+	if (parse_dlg_did(dialog_id, &h_entry, &h_id) == 0) {
+		/* we might have a dialog did */
+		LM_DBG("ID: %*s (h_entry %u h_id %u)\n",
+				dialog_id->len, dialog_id->s, h_entry, h_id);
+		dlg = lookup_dlg(h_entry, h_id);
+	}
+	if (!dlg) {
+		/* the ID is not a number, so let's consider
+		 * the value a SIP call-id */
+		LM_DBG("Call-ID: <%.*s>\n", dialog_id->len, dialog_id->s);
+		dlg = get_dlg_by_callid(dialog_id, 1);
+	}
+	return dlg;
 }
 
 
@@ -1298,10 +1322,13 @@ static inline int internal_mi_print_dlg(mi_item_t *dialog_obj,
 	int date_buf_len;
 	mi_item_t *callees_arr, *values_arr, *profiles_arr;
 	mi_item_t *context_obj, *callee_item, *values_item, *profiles_item;
+	str *did = dlg_get_did(dlg);
 
-	if (add_mi_string_fmt(dialog_obj, MI_SSTR("ID"), "%llu",
-		(((long long unsigned)dlg->h_entry)<<(8*sizeof(int)))+dlg->h_id) < 0)
+	if (add_mi_string(dialog_obj, MI_SSTR("ID"), did->s, did->len) < 0)
 		goto error;
+	if (add_mi_string_fmt(dialog_obj, MI_SSTR("db_id"), "%llu",
+			(((long long unsigned)dlg->h_entry)<<(8*sizeof(int)))+dlg->h_id) < 0)
+
 	if (add_mi_number(dialog_obj, MI_SSTR("state"), dlg->state) < 0)
 		goto error;
 	if (add_mi_number(dialog_obj, MI_SSTR("user_flags"), dlg->user_flags) < 0)
@@ -1704,10 +1731,7 @@ mi_response_t *mi_print_dlgs_cnt_ctx(const mi_params_t *params,
 mi_response_t *mi_push_dlg_var(const mi_params_t *params,
 								struct mi_handler *async_hdl)
 {
-	unsigned int h_entry, h_id;
-	unsigned long long d_id;
 	str dlg_var_name,dlg_var_value,dialog_id;
-	char bkp, *end;
 	struct dlg_cell * dlg = NULL;
 	int shtag_state = 1, db_update = 0;
 	mi_item_t *did_param_arr;
@@ -1715,8 +1739,6 @@ mi_response_t *mi_push_dlg_var(const mi_params_t *params,
 
 	if ( d_table == NULL)
 		goto not_found;
-
-	h_entry = h_id = 0;
 
 	if (get_mi_string_param(params, "dlg_val_name",
 		&dlg_var_name.s, &dlg_var_name.len) < 0)
@@ -1737,30 +1759,8 @@ mi_response_t *mi_push_dlg_var(const mi_params_t *params,
 		/* Get the dialog based of the dialog_id. This may be a
 		 * numerical DID or a string SIP Call-ID */
 
-		/* make value null terminated (in an ugly way) */
-		bkp = dialog_id.s[dialog_id.len];
-		dialog_id.s[dialog_id.len] = 0;
-
 		/* convert to unsigned long long */
-		d_id = strtoull(dialog_id.s, &end, 10);
-		dialog_id.s[dialog_id.len] = bkp;
-		if (end-dialog_id.s==dialog_id.len) {
-			/* the ID is numeric, so let's consider it DID */
-			dlg_parse_did(d_id, h_entry, h_id);
-			LM_DBG("ID: %llu (h_entry %u h_id %u)\n", d_id, h_entry, h_id);
-			dlg = lookup_dlg(h_entry, h_id);
-			if (dlg == NULL) {
-				LM_DBG("Faiure to match the ID as DLG_ID \n");
-				goto callid_lookup;
-			}
-		} else {
-callid_lookup:
-			/* the ID is not a number, so let's consider
-			 * the value a SIP call-id */
-			LM_DBG("Call-ID: <%.*s>\n", dialog_id.len, dialog_id.s);
-			dlg = get_dlg_by_callid( &dialog_id, 1 );
-		}
-
+		dlg = get_dlg_by_dialog_id(&dialog_id);
 		if (dlg == NULL) {
 			/* XXX - not_found or loop_end here ? */
 			continue;

modules/dialog/dialog.c
@@ -616,6 +616,8 @@ int load_dlg( struct dlg_binds *dlgb )
 	dlgb->dlg_unref = unref_dlg_destroy_safe;
 
 	dlgb->get_direction = get_dlg_direction;
+	dlgb->get_dlg_did = dlg_get_did;
+	dlgb->get_dlg_by_did = get_dlg_by_did;
 	dlgb->get_dlg_by_callid = get_dlg_by_callid;
 	dlgb->send_indialog_request = send_indialog_request;
 
@@ -1616,13 +1618,11 @@ int pv_get_dlg_dir(struct sip_msg *msg, pv_param_t *param,
 	return 0;
 }
 
-/* the maximum value we can have is 2 ints + ':' */
-static char buf_get_did[2 * INT2STR_MAX_LEN];
 int pv_get_dlg_did(struct sip_msg *msg, pv_param_t *param,
 		pv_value_t *res)
 {
 	struct dlg_cell *dlg;
-	str aux;
+	str *did;
 
 	if(res==NULL)
 		return -1;
@@ -1630,27 +1630,10 @@ int pv_get_dlg_did(struct sip_msg *msg, pv_param_t *param,
 	if ( (dlg=get_current_dialog())==NULL )
 		return pv_get_null( msg, param, res);
 
-	res->rs.s = buf_get_did;
-
-	aux.s = int2str( (unsigned long)dlg->h_entry, &aux.len);
-	if (!aux.s || !aux.len) {
-		LM_ERR("invalid hash entry\n");
-		return -1;
-	}
-
-	memcpy(buf_get_did, aux.s, aux.len);
-	buf_get_did[aux.len] = ':';
-	res->rs.len = aux.len + 1;
-
-	aux.s = int2str( (unsigned long)dlg->h_id, &aux.len);
-	if (!aux.s || !aux.len) {
-		LM_ERR("invalid hash id\n");
-		return -1;
-	}
-
-	memcpy(buf_get_did + res->rs.len, aux.s, aux.len);
-	res->rs.len += aux.len;
-
+	did = dlg_get_did(dlg);
+	if (!dlg)
+		return pv_get_null( msg, param, res);
+	res->rs = *did;
 	res->flags = PV_VAL_STR;
 
 	return 0;
@@ -2232,27 +2215,8 @@ static int fixup_lmode(void **param)
 }
 
 
-static inline long long parse_dlg_id(str *id)
-{
-	long long r;
-	char *e;
-	char buf[21]; /* 20 + null termination */
-	if (id->len > 20 || id->len < 0)
-		return 0;
-	memcpy(buf, id->s, id->len);
-	buf[id->len] = 0;
-	r = strtoll(buf, &e, 10);
-	if (r == LLONG_MIN || r == LLONG_MAX)
-		return 0;
-	if (*e != '\0')
-		return 0;
-	return r;
-}
-
-
 static int load_dlg_ctx(struct sip_msg *msg, str *callid, void *lmode)
 {
-	long long dlg_id;
 	struct dlg_cell *dlg;
 	int mode;
 
@@ -2274,9 +2238,7 @@ static int load_dlg_ctx(struct sip_msg *msg, str *callid, void *lmode)
 
 		case DLG_CTX_LOAD_BY_DID:
 			/* did */
-			dlg_id = parse_dlg_id( callid );
-			dlg = get_dlg_by_did( (unsigned int )(dlg_id >> 32),
-				(unsigned int)(dlg_id & 0x00000000ffffffff), 0);
+			dlg = get_dlg_by_did( callid, 0);
 			break;
 	}
 

modules/dialog/dlg_db_handler.c
@@ -567,7 +567,7 @@ static int load_dialog_info_from_db(int dlg_hash_size)
 				continue;
 			}
 
-			dlg_parse_did(VAL_BIGINT(values), hash_entry, hash_id);
+			dlg_parse_db_id(VAL_BIGINT(values), hash_entry, hash_id);
 
 			if (VAL_NULL(values+6) || VAL_NULL(values+7)) {
 				LM_ERR("columns %.*s or/and %.*s cannot be null -> skipping\n",
@@ -861,7 +861,7 @@ int dlg_timer_remove_from_db(struct dlg_cell *cell)
 	}
 
 	/* store info in del holders */
-	VAL_BIGINT(dlg_del_values+dlg_del_curr_no) = dlg_get_did(cell);
+	VAL_BIGINT(dlg_del_values+dlg_del_curr_no) = dlg_get_db_id(cell);
 	dlg_del_holder[dlg_del_curr_no]=cell;
 	/* mark is as deleted so we don't care about it later
 	 * in the timer */
@@ -934,7 +934,7 @@ int remove_dialog_from_db(struct dlg_cell * cell)
 	VAL_TYPE(values) = DB_BIGINT;
 	VAL_NULL(values) = 0;
 
-	VAL_BIGINT(values) = dlg_get_did(cell);
+	VAL_BIGINT(values) = dlg_get_db_id(cell);
 
 	CON_PS_REFERENCE(dialog_db_handle) = &my_ps;
 
@@ -974,7 +974,7 @@ int update_dialog_timeout_info(struct dlg_cell * cell)
 	entry = (d_table->entries)[cell->h_entry];
 	dlg_lock( d_table, &entry);
 
-	SET_BIGINT_VALUE(values, dlg_get_did(cell));
+	SET_BIGINT_VALUE(values, dlg_get_db_id(cell));
 	SET_INT_VALUE(values+1, (unsigned int)( (unsigned int)time(0) +
 			 cell->tl.timeout - get_ticks()) );
 
@@ -1049,7 +1049,7 @@ int update_dialog_dbinfo(struct dlg_cell * cell)
 		entry = (d_table->entries)[cell->h_entry];
 		dlg_lock( d_table, &entry);
 
-		SET_BIGINT_VALUE(values, dlg_get_did(cell));
+		SET_BIGINT_VALUE(values, dlg_get_db_id(cell));
 		/* to be later removed by timer */SET_STR_VALUE(values+1, cell->callid);
 
 		SET_STR_VALUE(values+2, cell->from_uri);
@@ -1118,7 +1118,7 @@ int update_dialog_dbinfo(struct dlg_cell * cell)
 		entry = (d_table->entries)[cell->h_entry];
 		dlg_lock( d_table, &entry);
 
-		SET_BIGINT_VALUE(values, dlg_get_did(cell));
+		SET_BIGINT_VALUE(values, dlg_get_db_id(cell));
 		SET_INT_VALUE(values+11, cell->state);
 		SET_INT_VALUE(values+12, (unsigned int)( (unsigned int)time(0) +
 				 cell->tl.timeout - get_ticks()) );
@@ -1156,7 +1156,7 @@ int update_dialog_dbinfo(struct dlg_cell * cell)
 		entry = (d_table->entries)[cell->h_entry];
 		dlg_lock( d_table, &entry);
 
-		SET_BIGINT_VALUE(values, dlg_get_did(cell));
+		SET_BIGINT_VALUE(values, dlg_get_db_id(cell));
 
 		set_final_update_cols(values+18, cell, 0);
 
@@ -1581,7 +1581,7 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 				}
 				LM_DBG("inserting new dialog %p\n",cell);
 
-				SET_BIGINT_VALUE(values, dlg_get_did(cell));
+				SET_BIGINT_VALUE(values, dlg_get_db_id(cell));
 				SET_STR_VALUE(values+1, cell->callid);
 				SET_STR_VALUE(values+2, cell->from_uri);
 
@@ -1662,7 +1662,7 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 			} else if ( (cell->flags & DLG_FLAG_CHANGED)!=0 || on_shutdown ){
 				LM_DBG("updating existing dialog %p\n",cell);
 
-				SET_BIGINT_VALUE(values, dlg_get_did(cell));
+				SET_BIGINT_VALUE(values, dlg_get_db_id(cell));
 
 				SET_STR_VALUE(values+13, cell->legs[DLG_CALLER_LEG].contact);
 				SET_STR_VALUE(values+14,
@@ -1695,7 +1695,7 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 				cell->flags &= ~(DLG_FLAG_CHANGED|DLG_FLAG_VP_CHANGED);
 			} else if (db_flush_vp && (cell->flags & DLG_FLAG_VP_CHANGED)) {
 
-				SET_BIGINT_VALUE(values, dlg_get_did(cell));
+				SET_BIGINT_VALUE(values, dlg_get_db_id(cell));
 
 				set_final_update_cols(values+21, cell, on_shutdown);
 
@@ -1773,7 +1773,7 @@ static int sync_dlg_db_mem(void)
 				continue;
 			}
 
-			dlg_parse_did(VAL_BIGINT(values), hash_entry, hash_id);
+			dlg_parse_db_id(VAL_BIGINT(values), hash_entry, hash_id);
 
 			if (VAL_NULL(values+6) || VAL_NULL(values+7)) {
 				LM_ERR("columns %.*s or/and %.*s cannot be null -> skipping\n",
@@ -2271,7 +2271,7 @@ static int restore_dlg_db(void)
 
 			callee_leg = callee_idx(cell);
 
-			SET_BIGINT_VALUE(values, dlg_get_did(cell));
+			SET_BIGINT_VALUE(values, dlg_get_db_id(cell));
 			SET_STR_VALUE(values+1, cell->callid);
 			SET_STR_VALUE(values+2, cell->from_uri);
 

modules/dialog/dlg_handlers.c
@@ -81,6 +81,38 @@ void destroy_dlg_handlers(void)
 	shutdown_done = 1;
 }
 
+static int dlg_get_did_buf(struct dlg_cell *dlg, str *buf)
+{
+	char *p;
+
+	p = buf->s;
+	if (int2reverse_hex(&p, &buf->len, dlg->h_entry) == -1)
+		return -1;
+
+	if (!buf->len)
+		return 01;
+
+	*(p++) = DLG_SEPARATOR;
+	buf->len--;
+
+	if (int2reverse_hex(&p, &buf->len, dlg->h_id) == -1)
+		return -1;
+	buf->len = p - buf->s;
+	return 0;
+}
+
+str *dlg_get_did(struct dlg_cell *dlg)
+{
+	static str did_str;
+	static char did_buf[DLG_DID_SIZE];
+
+	did_str.s = did_buf;
+	did_str.len = DLG_DID_SIZE;
+
+	if (dlg_get_did_buf(dlg, &did_str) < 0)
+		return NULL;
+	return &did_str;
+}
 
 int run_dlg_script_route(struct dlg_cell *dlg, int rt_idx)
 {
@@ -118,34 +150,25 @@ int run_dlg_script_route(struct dlg_cell *dlg, int rt_idx)
 }
 
 
-static inline int add_dlg_rr_param(struct sip_msg *req, unsigned int entry,
-													unsigned int id)
+static inline int add_dlg_rr_param(struct sip_msg *req, struct dlg_cell *dlg)
 {
 	static char buf[RR_DLG_PARAM_SIZE];
-	str s;
-	int n;
 	char *p;
+	str id;
 
-	s.s = p = buf;
+	p = buf;
 
 	*(p++) = ';';
 	memcpy(p, rr_param.s, rr_param.len);
 	p += rr_param.len;
 	*(p++) = '=';
 
-	n = RR_DLG_PARAM_SIZE - (p-buf);
-	if (int2reverse_hex( &p, &n, entry)==-1)
+	id.s = p;
+	id.len = RR_DLG_PARAM_SIZE - (p-buf);
+	if (dlg_get_did_buf(dlg, &id) < 0)
 		return -1;
 
-	*(p++) = DLG_SEPARATOR;
-
-	n = RR_DLG_PARAM_SIZE - (p-buf);
-	if (int2reverse_hex( &p, &n, id)==-1)
-		return -1;
-
-	s.len = p-buf;
-
-	if (d_rrb.add_rr_param( req, &s)<0) {
+	if (d_rrb.add_rr_param( req, &id)<0) {
 		LM_ERR("failed to add rr param\n");
 		return -1;
 	}
@@ -1485,7 +1508,7 @@ int dlg_create_dialog(struct cell* t, struct sip_msg *req,unsigned int flags)
 	link_dlg( dlg , extra_ref);
 
 	if ( seq_match_mode!=SEQ_MATCH_NO_ID && has_rr() &&
-		add_dlg_rr_param( req, dlg->h_entry, dlg->h_id)<0 ) {
+		add_dlg_rr_param( req, dlg)<0 ) {
 		LM_ERR("failed to add RR param\n");
 		goto error;
 	}
@@ -1672,8 +1695,10 @@ void dlg_onroute(struct sip_msg* req, str *route_params, void *param)
 		} else {
 			LM_DBG("route param is '%.*s' (len=%d)\n",val.len,val.s,val.len);
 
-			if ( parse_dlg_rr_param( val.s, val.s+val.len, &h_entry, &h_id)<0 )
+			if ( parse_dlg_did(&val, &h_entry, &h_id)<0 ) {
+				LM_ERR("malformed route param [%.*s]\n", val.len, val.s);
 				return;
+			}
 
 			dlg = lookup_dlg( h_entry, h_id);
 			if (dlg==0) {

modules/dialog/dlg_handlers.h
@@ -57,7 +57,8 @@ static inline int dlg_match_mode_str_to_int(const str *in)
 	return SEQ_MATCH_DEFAULT;
 }
 
-#define RR_DLG_PARAM_SIZE  (2*2*sizeof(int)+3+MAX_DLG_RR_PARAM_NAME)
+#define DLG_DID_SIZE       (2*2*sizeof(int)+1)
+#define RR_DLG_PARAM_SIZE  (DLG_DID_SIZE+2+MAX_DLG_RR_PARAM_NAME)
 #define DLG_SEPARATOR      '.'
 
 struct _dlg_cseq{
@@ -121,6 +122,8 @@ void dlg_onroute(struct sip_msg* req, str *rr_param, void *param);
 
 void dlg_ontimeout( struct dlg_tl *tl);
 
+str *dlg_get_did(struct dlg_cell *dlg);
+
 int dlg_validate_dialog( struct sip_msg* req, struct dlg_cell *dlg);
 
 int fix_route_dialog(struct sip_msg *req,struct dlg_cell *dlg);
@@ -137,25 +140,23 @@ void unreference_dialog(void *dialog);
 int run_dlg_script_route(struct dlg_cell *dlg, int rt_idx);
 
 
-
-static inline int parse_dlg_rr_param(char *p, char *end,
-								unsigned int *h_entry, unsigned int *h_id)
+static inline int parse_dlg_did(str *did, unsigned int *h_entry, unsigned int *h_id)
 {
-	char *s;
+	char *p, *end = did->s + did->len;
 
-	for ( s=p ; p<end && *p!=DLG_SEPARATOR ; p++ );
+	for ( p=did->s ; p<end && *p!=DLG_SEPARATOR ; p++ );
 	if (*p!=DLG_SEPARATOR) {
-		LM_ERR("malformed rr param '%.*s'\n", (int)(long)(end-s), s);
+		LM_DBG("malformed rr param '%.*s'\n", (int)(long)(end-did->s), did->s);
 		return -1;
 	}
 
-	if ( reverse_hex2int( s, p-s, h_entry)<0 ) {
-		LM_ERR("invalid hash entry '%.*s'\n", (int)(long)(p-s), s);
+	if ( reverse_hex2int( did->s, p-did->s, h_entry)<0 ) {
+		LM_DBG("invalid hash entry '%.*s'\n", (int)(long)(p-did->s), did->s);
 		return -1;
 	}
 
 	if ( reverse_hex2int( p+1, end-(p+1), h_id)<0 ) {
-		LM_ERR("invalid hash id '%.*s'\n", (int)(long)(end-(p+1)), p+1 );
+		LM_DBG("invalid hash id '%.*s'\n", (int)(long)(end-(p+1)), p+1 );
 		return -1;
 	}
 

modules/dialog/dlg_hash.h
@@ -397,8 +397,9 @@ struct dlg_cell* get_dlg_by_val(str *attr, str *val);
 
 struct dlg_cell* get_dlg_by_callid( str *callid, int active_only);
 
-struct dlg_cell* get_dlg_by_did(unsigned int h_entry, unsigned int h_id,
-		int active_only);
+struct dlg_cell* get_dlg_by_did(str *did, int active_only);
+
+struct dlg_cell *get_dlg_by_dialog_id(str *dialog_id);
 
 int get_dlg_direction(void);
 
@@ -642,13 +643,14 @@ static inline void init_dlg_term_reason(struct dlg_cell *dlg,char *reason,int re
 	}
 }
 
+
 int state_changed_event_init(void);
 void state_changed_event_destroy(void);
 
-#define dlg_get_did(_dlg) \
+#define dlg_get_db_id(_dlg) \
 	(((unsigned long long)(_dlg)->h_entry << 32) | ((_dlg)->h_id))
 
-#define dlg_parse_did(_did, _h_entry, _h_id) \
+#define dlg_parse_db_id(_did, _h_entry, _h_id) \
 	do { \
 		(_h_entry) = (unsigned int)((_did) >> 32); \
 		(_h_id) = (unsigned int)((_did) & 0x00000000ffffffff); \

modules/dialog/dlg_load.h
@@ -30,7 +30,9 @@
 #include "../../sr_module.h"
 
 typedef struct dlg_cell *(*get_dlg_f) (void);
+typedef str *(*get_dlg_did_f) (struct dlg_cell *dlg);
 typedef struct dlg_cell *(*get_dlg_by_callid_f) (str *, int);
+typedef struct dlg_cell *(*get_dlg_by_did_f) (str *, int);
 typedef int (*match_dialog_f) (struct sip_msg *msg, int _seq_match_mode);
 typedef int (*get_direction_f) (void);
 
@@ -58,6 +60,8 @@ struct dlg_binds {
 	unref_dlg_f          dlg_unref;
 
 	get_direction_f      get_direction;
+	get_dlg_did_f        get_dlg_did;
+	get_dlg_by_did_f     get_dlg_by_did;
 	get_dlg_by_callid_f  get_dlg_by_callid;
 	send_indialog_req_f  send_indialog_request;
 

modules/dialog/dlg_replication.c
@@ -946,7 +946,7 @@ void rcv_cluster_event(enum clusterer_event ev, int node_id)
 				ref_dlg_unsafe(dlg, 1);
 				dlg_unlock(d_table, &d_table->entries[i]);
 
-				LM_DBG("Drop DB loaded dialog ID=%llu\n", dlg_get_did(dlg));
+				LM_DBG("Drop DB loaded dialog ID=%llu\n", dlg_get_db_id(dlg));
 
 				/* simulate BYE received from caller */
 				next_state_dlg(dlg, DLG_EVENT_REQBYE, DLG_DIR_UPSTREAM, &old_state,

commit f17737985e65e88d416b102f6f3f5503f918768f

dialog: fix bug when "state" was not shown by dlg_list
Commit d883a0d72a introduced a bug in the dialog module that was no
longer printing the "state" node in the MI output; this commit fixes the
bug.

modules/dialog/dlg_hash.c
@@ -1334,6 +1334,7 @@ static inline int internal_mi_print_dlg(mi_item_t *dialog_obj,
 		goto error;
 	if (add_mi_string_fmt(dialog_obj, MI_SSTR("db_id"), "%llu",
 			(((long long unsigned)dlg->h_entry)<<(8*sizeof(int)))+dlg->h_id) < 0)
+		goto error;
 
 	if (add_mi_number(dialog_obj, MI_SSTR("state"), dlg->state) < 0)
 		goto error;