9cf4843401ceca9e11744b323f793cca4bf3abec
Maksym Sobolyev <sobomax@sippysoft.com>
2020-10-13 13:40:57 -0700

    Add immutable version of the struct str - str_const and associated helper macros (STR_NULL_const, str_const_init() and str2const()).

--- str.h
+++ str.h
@@ -56,11 +56,21 @@ struct __str {
 	int len; /**< string length, not including null-termination */
 };
 
+/* Immutable version of the struct __str */
+struct __str_const {
+	const char* s; /**< string as char array */
+	int len; /**< string length, not including null-termination */
+};
+
 typedef struct __str str;
+typedef struct __str_const str_const;
 
 /* str initialization */
 #define STR_NULL (str){NULL, 0}
+#define STR_NULL_const (str_const){NULL, 0}
 #define str_init(_string)  (str){_string, sizeof(_string) - 1}
+#define str_const_init(_string)  (str_const){_string, sizeof(_string) - 1}
+#define str2const(_sp) &(str_const){(_sp)->s, (_sp)->len}
 
 static inline void init_str(str *dest, const char *src)
 {
