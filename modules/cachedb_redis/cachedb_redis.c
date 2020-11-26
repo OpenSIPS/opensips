b82b85b6acc870c099c9411bb525a1fc09aff928
Vlad Patrascu <vladp@opensips.org>
2020-11-26 20:55:04 +0200

    cachedb_redis: add TLS support
    Closes #2264

--- modules/cachedb_redis/cachedb_redis.c
+++ modules/cachedb_redis/cachedb_redis.c
@@ -57,9 +57,27 @@ static param_export_t params[]={
 	{ "query_timeout",               INT_PARAM,                &redis_query_tout      },
 	{ "shutdown_on_error",           INT_PARAM,                &shutdown_on_error     },
 	{ "cachedb_url",                 STR_PARAM|USE_FUNC_PARAM, (void *)&set_connection},
+	{ "use_tls",                     INT_PARAM,                &use_tls},
 	{0,0,0}
 };
 
+static module_dependency_t *get_deps_use_tls(param_export_t *param)
+{
+	if (*(int *)param->param_pointer == 0)
+		return NULL;
+
+	return alloc_module_dep(MOD_TYPE_DEFAULT, "tls_mgm", DEP_ABORT);
+}
+
+static dep_export_t deps = {
+	{
+		{ MOD_TYPE_NULL, NULL, 0 },
+	},
+	{
+		{ "use_tls", get_deps_use_tls },
+		{ NULL, NULL },
+	},
+};
 
 /** module exports */
 struct module_exports exports= {
@@ -68,7 +86,7 @@ struct module_exports exports= {
 	MODULE_VERSION,
 	DEFAULT_DLFLAGS,			/* dlopen flags */
 	0,							/* load function */
-	NULL,            /* OpenSIPS module dependencies */
+	&deps,                      /* OpenSIPS module dependencies */
 	0,						/* exported functions */
 	0,						/* exported async functions */
 	params,						/* exported parameters */
@@ -112,6 +130,19 @@ static int mod_init(void)
 
 	if (register_cachedb(&cde) < 0) {
 		LM_ERR("failed to initialize cachedb_redis\n");
+		return -1;
+	}
+
+	/* check if we have TLS support, as it is not built by defult in libhiredis */
+#ifndef HAVE_REDIS_SSL
+	if (use_tls) {
+		LM_NOTICE("Unable to use TLS connections as libhiredis was not "
+			"compiled with TLS support!\n");
+		use_tls = 0;
+	}
+#endif
+	if (use_tls && load_tls_mgm_api(&tls_api) != 0) {
+		LM_ERR("failed to load tls_mgm API!\n");
 		return -1;
 	}
 
