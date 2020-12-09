OpenSIPS/opensips

commit 3aae7e652d2d39b3b600de4be452e8f948e33612

SIP PN Support: Refactor usrloc code, move to /lib/reg
Most of the PN support is better placed in the registrar module code,
since that's where we also have access to the SIP message in order to
make changes to it (e.g. append the Feature-Caps header field)

modules/usrloc/ucontact.h
@@ -115,8 +115,6 @@ typedef struct ucontact {
 	str shtag;              /*!< helps determine the logical owner node */
 	str cdb_key;            /*!< the key of the contact in cache_db; makes
 	                              sense only in full_sharing_cachedb mode */
-	str *pn_params;         /*!< Array of PN param pointers (only if all of
-	                              them are present in the Contact URI) */
 
 	map_t kv_storage;       /*!< data attached by API subscribers >*/
 
@@ -150,8 +148,8 @@ typedef struct ucontact_info {
 	str *attr;
 	str shtag;
 	str cdb_key;
-	/* info on how the contact matching should be performed.
-	 * it points to a record part of the 'save_ctx' structure */
+
+	/* contact matching algorithm - no need to free anything */
 	struct ct_match *cmatch;
 } ucontact_info_t;
 

lib/reg/pn.c
@@ -1,0 +1,128 @@
+/**
+ * SIP Push Notification support - RFC 8599
+ * Copyright (C) 2020 OpenSIPS Solutions
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
+#include "../../lib/csv.h"
+#include "../../parser/parse_uri.h"
+
+#include "pn.h"
+
+/* modparams */
+int pn_enable;
+int pn_pnsreg_interval = 130;  /* sec */
+int pn_trigger_interval = 120; /* sec */
+char *_pn_ct_params = "pn-provider, pn-prid, pn-param";
+char *_pn_providers;
+
+str_list *pn_ct_params;
+int pn_ct_params_n;
+
+struct pn_provider *pn_providers;
+
+#define MAX_PROVIDER_LEN 20
+#define MAX_PNSPURR_LEN 40
+#define MAX_FEATURE_CAPS_SIZE \
+	(sizeof("Feature-Caps: +sip.pns=\"\";" \
+			"+sip.pnsreg=\"\";+sip.pnspurr=\"\"") + \
+			MAX_PROVIDER_LEN + INT2STR_MAX_LEN + MAX_PNSPURR_LEN + CRLF_LEN)
+
+int pn_init(void)
+{
+	str_list *param;
+	csv_record *items, *pnp;
+	struct pn_provider *provider;
+
+	if (!pn_enable)
+		return 0;
+
+	if (!_pn_providers) {
+		LM_ERR("the 'pn_providers' modparam is missing\n");
+		return -1;
+	}
+
+	/* parse the list of PN params */
+	items = parse_csv_record(_str(_pn_ct_params));
+	for (pnp = items; pnp; pnp = pnp->next) {
+		if (ZSTR(pnp->s))
+			continue;
+
+		param = shm_malloc(sizeof *param + pnp->s.len + 1);
+		if (!param) {
+			LM_ERR("oom\n");
+			return -1;
+		}
+		memset(param, 0, sizeof *param);
+
+		param->s.s = (char *)(param + 1);
+		str_cpy(&param->s, &pnp->s);
+		param->s.s[pnp->s.len] = '\0';
+
+		pn_ct_params_n++;
+		add_last(param, pn_ct_params);
+
+		LM_DBG("parsed PN contact param: '%.*s'\n",
+		       param->s.len, param->s.s);
+	}
+	free_csv_record(items);
+
+	if (!pn_ct_params) {
+		LM_ERR("'pn_ct_match_params' must contain at least 1 param!\n");
+		return -1;
+	}
+
+	/* parse the list of providers */
+	items = parse_csv_record(_str(_pn_providers));
+	for (pnp = items; pnp; pnp = pnp->next) {
+		if (ZSTR(pnp->s))
+			continue;
+
+		provider = shm_malloc(sizeof *provider + pnp->s.len + 1 +
+		                      MAX_FEATURE_CAPS_SIZE);
+		if (!provider) {
+			LM_ERR("oom\n");
+			return -1;
+		}
+		memset(provider, 0, sizeof *provider);
+
+		provider->name.s = (char *)(provider + 1);
+		str_cpy(&provider->name, &pnp->s);
+		provider->name.s[provider->name.len] = '\0';
+
+		provider->feature_caps.s = (char *)(provider->name.s + pnp->s.len + 1);
+		provider->feature_caps.len = sprintf(provider->feature_caps.s,
+				"Feature-Caps: +sip.pns=\"%.*s\";+sip.pnsreg=\"%u\"\r\n",
+							  // TODO: ";+sip.pnspurr=\"",
+				pnp->s.len, pnp->s.s, pn_pnsreg_interval);
+
+		add_last(provider, pn_providers);
+		LM_DBG("parsed PN provider: '%.*s', hdr: '%.*s'\n", provider->name.len,
+		       provider->name.s, provider->feature_caps.len,
+		       provider->feature_caps.s);
+	}
+	free_csv_record(items);
+
+	return 0;
+}
+
+
+int pn_build_feature_caps(const str *provider, str *out)
+{
+	return 0;
+}

lib/reg/pn.h
@@ -1,0 +1,138 @@
+/**
+ * SIP Push Notification support - RFC 8599
+ * Copyright (C) 2020 OpenSIPS Solutions
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
+#ifndef __REG_PN_H__
+#define __REG_PN_H__
+
+#include "../../parser/msg_parser.h"
+#include "../../parser/parse_uri.h"
+#include "../../str_list.h"
+#include "../../ut.h"
+
+struct pn_provider {
+	str name;
+	str feature_caps;
+
+	struct pn_provider *next;
+};
+
+/* modparams */
+extern int pn_enable;
+extern int pn_pnsreg_interval;
+extern int pn_trigger_interval;
+extern char *_pn_ct_params;
+extern char *_pn_providers;
+
+/* useful fixups */
+extern str_list *pn_ct_params;  /* list of parsed match params */
+extern int pn_ct_params_n;      /* list size */
+extern struct pn_provider *pn_providers;
+
+
+/**
+ * initialize RFC 8599 support
+ */
+int pn_init(void);
+
+
+//TODO
+#if 0
+/**
+ * parse and extract all PN parameters from @uri into the
+ *     @val_arr array.  Also sum their lengths into @val_len
+ * @return: 0 if all parameters found, otherwise -1
+ */
+int pn_extract_params(const str *uri, str *val_arr, int *val_len);
+
+
+/**
+ * similar to the above, except it receives a parsed URI
+ * @return: 0 if all parameters found, otherwise -1
+ */
+static inline int _pn_extract_params(const struct sip_uri *uri,
+                                     str *val_arr, int *val_len);
+#endif
+
+
+/**
+ * check whether the given SIP @uri string contains all PN params
+ * @return: 1 on success, otherwise 0
+ */
+static inline int pn_uri_has_params(const str *uri);
+
+
+/**
+ * build a Feature-Caps header with all server capabilities, store it in @out
+ * @return: 0 on success, -1 otherwise
+ */
+int pn_build_feature_caps(const str *provider, str *out);
+
+
+/* ------------------------------------------------------------------------- */
+
+
+static inline int pn_uri_has_params(const str *uri)
+{
+	struct sip_uri puri;
+	str_list *pnp;
+
+	if (parse_uri(uri->s, uri->len, &puri) != 0) {
+		LM_ERR("failed to parse URI: '%.*s'\n", uri->len, uri->s);
+		return -1;
+	}
+
+	for (pnp = pn_ct_params; pnp; pnp = pnp->next) {
+		for (int i = 0; i < puri.u_params_no; i++)
+			if (str_match(&pnp->s, &puri.u_name[i]))
+				goto found_param;
+
+		return 0;
+
+found_param:;
+	}
+
+	return 1;
+}
+
+//TODO
+#if 0
+static inline int _pn_extract_params(const struct sip_uri *uri,
+                                     str *val_arr, int *val_len)
+{
+	for (int i = 0; i < pn_ct_params_n; i++) {
+		for (int j = 0; j < uri->u_params_no; j++) {
+			if (str_match(&pn_ct_params[i], &uri->u_name[j])) {
+				val_arr[i] = uri->u_val[j];
+				*val_len += uri->u_val[j].len;
+				goto found_param;
+			}
+		}
+
+		return -1;
+
+found_param:;
+	}
+
+	return 0;
+}
+#endif
+
+#endif /* __REG_PN_H__ */

lib/reg/save_flags.c
@@ -21,13 +21,13 @@
 #include <ctype.h>
 
 #include "../../dprint.h"
-#include "../../modules/usrloc/urecord.h"
 #include "config.h"
 #include "save_flags.h"
 
 
 void reg_parse_save_flags(str *flags_s, struct save_ctx *sctx)
 {
+	static str_list mp;
 	int st;
 
 	sctx->cmatch.mode = CT_MATCH_NONE;
@@ -87,20 +87,21 @@ void reg_parse_save_flags(str *flags_s, struct save_ctx *sctx)
 						sctx->cmatch.mode = CT_MATCH_CONTACT_CALLID;
 					else if (flags_s->s[st]=='<' && st<flags_s->len-3) {
 						st++;
-						sctx->cmatch.param.s = flags_s->s + st;
+						mp.s.s = flags_s->s + st;
 						while (st<flags_s->len-1 && flags_s->s[st+1]!='>')
 							st++;
 						if (st<flags_s->len-1 && flags_s->s[st+1]=='>') {
-							sctx->cmatch.param.len = flags_s->s + st + 1
-								 - sctx->cmatch.param.s;
-							sctx->cmatch.mode = CT_MATCH_PARAM;
+							mp.s.len = flags_s->s + st + 1
+								 - mp.s.s;
+
+							sctx->cmatch.match_params = &mp;
+							sctx->cmatch.mode = CT_MATCH_PARAMS;
 							st++;
 						} else {
 							LM_ERR("invalid format for MATCH 'M' param, "
 								"discarding trailing '%.*s'\n",
-								(int)(flags_s->s+st-sctx->cmatch.param.s),
-								sctx->cmatch.param.s);
-							sctx->cmatch.param.s = NULL;
+								(int)(flags_s->s + st - mp.s.s), mp.s.s);
+							mp.s.s = NULL;
 						}
 					} else {
 						LM_ERR("invalid value for MATCH 'M' param, "

lib/reg/save_flags.h
@@ -22,6 +22,7 @@
 #define __LIB_REG_SAVE_FLAGS__
 
 #include "../../str.h"
+#include "../../modules/usrloc/urecord.h"
 
 struct save_ctx {
 	unsigned int flags;

modules/mid_registrar/doc/mid_registrar.xml
@@ -5,6 +5,7 @@
 
 <!ENTITY admin SYSTEM "mid_registrar_admin.xml">
 <!ENTITY contrib SYSTEM "contributors.xml">
+<!ENTITY pn_modparams SYSTEM "../../registrar/doc/pn_modparams.xml">
 
 <!-- Include general documentation entities -->
 <!ENTITY % docentities SYSTEM "../../../doc/entities.xml">

modules/mid_registrar/doc/mid_registrar_admin.xml
@@ -781,6 +781,8 @@ modparam("mid_registrar", "gruu_secret", "my_secret")
 		</example>
 	</section>
 
+	&pn_modparams;
+
 	</section>
 
 	<section id="exported_functions" xreflabel="exported_functions">

modules/registrar/Makefile
@@ -11,7 +11,6 @@ NAME=registrar.so
 LIBS=
 
 REG_LIB_DIR = ../../lib/reg
-_SRC=ci.c regtime.c rerrno.c sip_msg.c path.c save_flags.c
-extra_sources=$(patsubst %,$(REG_LIB_DIR)/%,$(_SRC))
+extra_sources=$(patsubst %,$(REG_LIB_DIR)/%,*.c)
 
 include ../../Makefile.modules

commit dc8cfe85abd7f38e476a02a47862a034c2a272ce

usrloc: Fix `matching_mode` regression in 3aae7e652d2d
Commit 3aae7e652d did too many things at once, including the refactoring
of CT_MATCH_NONE from -1 to 0 (along with all other enum members!),
causing a mismatch of this module parameter with its documentation, and
leading to failed startup checks under ul_init_globals().

Credits to @98oi98 for the report!

Fixes #2337

modules/usrloc/ucontact.h
@@ -86,7 +86,7 @@ typedef uint64_t ucontact_id;
 
 struct ct_match {
 	enum {
-		CT_MATCH_NONE,
+		CT_MATCH_NONE=-1,
 		CT_MATCH_CONTACT_ONLY,
 		CT_MATCH_CONTACT_CALLID,
 		CT_MATCH_PARAMS,