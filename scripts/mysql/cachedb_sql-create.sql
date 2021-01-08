3f3dc8a45d44eed76204c1ebc82e8ae6be03b335
Bogdan-Andrei Iancu <bogdan@opensips.org>
2021-01-08 18:14:14 +0200

    [cachedb_sql] fixed definition of the "cachedb" table
    In postgres, the tables must have an auto-increment
    Fix for #2310

--- scripts/mysql/cachedb_sql-create.sql
+++ scripts/mysql/cachedb_sql-create.sql
@@ -1,8 +1,10 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
+    id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     keyname CHAR(255) PRIMARY KEY NOT NULL,
     value TEXT(512) NOT NULL,
     counter INT(10) DEFAULT 0 NOT NULL,
-    expires INT(10) UNSIGNED DEFAULT 0 NOT NULL
+    expires INT(10) UNSIGNED DEFAULT 0 NOT NULL,
+    CONSTRAINT cachedbsql_keyname_idx UNIQUE (keyname)
 ) ENGINE=InnoDB;
 
--- scripts/postgres/cachedb_sql-create.sql
+++ scripts/postgres/cachedb_sql-create.sql
@@ -1,8 +1,11 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
+    id SERIAL PRIMARY KEY NOT NULL,
     keyname VARCHAR(255) PRIMARY KEY NOT NULL,
     value TEXT NOT NULL,
     counter INTEGER DEFAULT 0 NOT NULL,
-    expires INTEGER DEFAULT 0 NOT NULL
+    expires INTEGER DEFAULT 0 NOT NULL,
+    CONSTRAINT cachedb_cachedbsql_keyname_idx UNIQUE (keyname)
 );
 
+ALTER SEQUENCE cachedb_id_seq MAXVALUE 2147483647 CYCLE;
