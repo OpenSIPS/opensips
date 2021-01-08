3f58031efb18a995ac1b660abf295a47338b1b63
Bogdan-Andrei Iancu <bogdan@opensips.org>
2021-01-08 18:14:14 +0200

    [cachedb_sql] fixed definition of the "cachedb" table
    In postgres, the tables must have an auto-increment
    Fix for #2310
    
    (cherry picked from commit 3f3dc8a45d44eed76204c1ebc82e8ae6be03b335)

--- scripts/oracle/cachedb_sql-create.sql
+++ scripts/oracle/cachedb_sql-create.sql
@@ -1,9 +1,11 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
+    id NUMBER(10) PRIMARY KEY,
     keyname VARCHAR2(255) PRIMARY KEY,
     value CLOB(512),
     counter NUMBER(10) DEFAULT 0 NOT NULL,
-    expires NUMBER(10) DEFAULT 0 NOT NULL
+    expires NUMBER(10) DEFAULT 0 NOT NULL,
+    CONSTRAINT cachedb_cachedbsql_keyname_idx  UNIQUE (keyname)
 );
 
 CREATE OR REPLACE TRIGGER cachedb_tr
--- scripts/sqlite/cachedb_sql-create.sql
+++ scripts/sqlite/cachedb_sql-create.sql
@@ -1,8 +1,10 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     keyname CHAR(255) PRIMARY KEY NOT NULL,
     value TEXT(512) NOT NULL,
     counter INTEGER DEFAULT 0 NOT NULL,
-    expires INTEGER DEFAULT 0 NOT NULL
+    expires INTEGER DEFAULT 0 NOT NULL,
+    CONSTRAINT cachedb_cachedbsql_keyname_idx  UNIQUE (keyname)
 );
 
