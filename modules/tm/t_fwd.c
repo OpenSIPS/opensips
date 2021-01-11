OpenSIPS/opensips

commit dddf9c253d6fb445cd1e9782f1c584f2c4ef98dc

tm: Minor improvements
    * t_forward_nonack(): Avoid redundant str copying
    * improve docs on t->nr_of_outgoings

modules/tm/t_fwd.c
@@ -248,7 +248,7 @@ static inline char *print_uac_request(struct sip_msg *i_req, unsigned int *len,
 
 
 static inline void post_print_uac_request(struct sip_msg *request,
-				str *org_uri, str *org_dst, struct sip_msg_body *body_clone)
+			const str *org_uri, str *org_dst, struct sip_msg_body *body_clone)
 {
 	reset_init_lump_flags();
 	/* delete inserted branch lumps */
@@ -383,7 +383,7 @@ static inline unsigned int count_local_rr(struct sip_msg *req)
    or error (<0); it doesn't send a message yet -- a reply to it
    might interfere with the processes of adding multiple branches
 */
-static int add_uac( struct cell *t, struct sip_msg *request, str *uri,
+static int add_uac( struct cell *t, struct sip_msg *request, const str *uri,
 		str* next_hop, unsigned int bflags, str* path, struct proxy_l *proxy)
 {
 	unsigned short branch;
@@ -754,9 +754,8 @@ int t_forward_nonack( struct cell *t, struct sip_msg* p_msg ,
 			t->first_branch--;
 	}
 
-	/* as first branch, use current uri */
-	current_uri = *GET_RURI(p_msg);
-	branch_ret = add_uac( t, p_msg, &current_uri, &backup_dst,
+	/* as first branch, use current R-URI, bflags, etc. */
+	branch_ret = add_uac( t, p_msg, GET_RURI(p_msg), &backup_dst,
 		getb0flags(p_msg), &p_msg->path_vec, proxy);
 	if (branch_ret>=0)
 		added_branches |= 1<<branch_ret;

modules/tm/h_table.h
@@ -258,8 +258,11 @@ typedef struct cell
 	/* first branch - when serial forking is performed, keeps the first
 	 * branch for each step ; it allows proper branch selection */
 	int first_branch;
-	/* number of forks */
+
+	/* total number of UAC structures (forks), regardless whether the
+	 * messages have been successfully relayed or not.  Starts at 0. */
 	int nr_of_outgoings;
+
 	/* nr of replied branch; 0..MAX_BRANCHES=branch value,
 	 * -1 no reply, -2 local reply */
 	int relaied_reply_branch;

commit 97c6bc78a34ed7ff47ea993b8bf843f700428cc5

tm: Fix t_on_branch() PKG memory leak
Commit dddf9c253d introduced a regression because add_uac() is sensible
to the addresses of the (str *) address provided.

"Premature optimization is the root of all evil"...

modules/tm/t_fwd.c
@@ -384,6 +384,9 @@ static inline unsigned int count_local_rr(struct sip_msg *req)
 /* introduce a new uac to transaction; returns its branch id (>=0)
    or error (<0); it doesn't send a message yet -- a reply to it
    might interfere with the processes of adding multiple branches
+
+   NOTICE: do NOT provide (str *) buffers pointing to @request itself, as this
+   may break the function logic!
 */
 static int add_uac( struct cell *t, struct sip_msg *request, const str *uri,
 		str* next_hop, unsigned int bflags, str* path, struct proxy_l *proxy)
@@ -693,9 +696,6 @@ int t_forward_nonack( struct cell *t, struct sip_msg* p_msg ,
 	str path;
 	str bk_path;
 
-	/* make -Wall happy */
-	current_uri.s=0;
-
 	/* before doing enything, update the t flags from msg */
 	t->uas.request->flags = p_msg->flags;
 
@@ -757,8 +757,10 @@ int t_forward_nonack( struct cell *t, struct sip_msg* p_msg ,
 			t->first_branch--;
 	}
 
+	current_uri = *GET_RURI(p_msg); /* separate storage required! */
+
 	/* as first branch, use current R-URI, bflags, etc. */
-	branch_ret = add_uac( t, p_msg, GET_RURI(p_msg), &backup_dst,
+	branch_ret = add_uac( t, p_msg, &current_uri, &backup_dst,
 		getb0flags(p_msg), &p_msg->path_vec, proxy);
 	if (branch_ret>=0)
 		added_branches |= 1<<branch_ret;