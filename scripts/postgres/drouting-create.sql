bbbff7eef6cc7710401573dabfb2db189be8e77a
Bogdan-Andrei Iancu <bogdan@opensips.org>
2021-01-11 14:43:10 +0200

    [drouting] set sort_profile column to default NULL
    If Quality Routing module is not used (on top of Dynamic Routing), this column is not used, so we do not have to explicitly provision it.
    
    (cherry picked from commit c8743de27c926bb4f85d4969956d384d3d9b19c6)

--- scripts/postgres/drouting-create.sql
+++ scripts/postgres/drouting-create.sql
@@ -25,7 +25,7 @@ CREATE TABLE dr_rules (
     routeid VARCHAR(255) DEFAULT NULL,
     gwlist VARCHAR(255) NOT NULL,
     sort_alg VARCHAR(1) DEFAULT 'N' NOT NULL,
-    sort_profile INTEGER,
+    sort_profile INTEGER DEFAULT NULL,
     attrs VARCHAR(255) DEFAULT NULL,
     description VARCHAR(128) DEFAULT NULL
 );
