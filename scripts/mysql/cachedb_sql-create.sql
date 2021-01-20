0625fa0dfd10e444b9c1ccfa36401d794ab445fb
Bogdan-Andrei Iancu <bogdan@opensips.org>
2021-01-20 13:05:32 +0200

    [dbschema] remove bogus second primary key
    Issue introduced with 7d38cb85bc18e9831d7ef507383a9388d73578a6
    Reported by @kuuse
    Closes #2379

--- scripts/mysql/cachedb_sql-create.sql
+++ scripts/mysql/cachedb_sql-create.sql
@@ -1,7 +1,7 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
     id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    keyname CHAR(255) PRIMARY KEY NOT NULL,
+    keyname CHAR(255) NOT NULL,
     value TEXT(512) NOT NULL,
     counter INT(10) DEFAULT 0 NOT NULL,
     expires INT(10) UNSIGNED DEFAULT 0 NOT NULL,
