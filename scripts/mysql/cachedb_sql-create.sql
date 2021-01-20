OpenSIPS/opensips

commit 3f3dc8a45d44eed76204c1ebc82e8ae6be03b335

[cachedb_sql] fixed definition of the "cachedb" table
In postgres, the tables must have an auto-increment
Fix for #2310

db/schema/cachedb_sql.xml
@@ -16,13 +16,24 @@
 		</db:para>
 	</description>
 
-	<column>
+	<column id="id">
+		<name>id</name>
+		<type>unsigned int</type>
+		<size>&table_id_len;</size>
+		<autoincrement/>
+		<natural/>
+		<primary/>
+		<type db="dbtext">int,auto</type>
+		<description>Table primary key, not used by module
+		</description>
+	</column>
+
+	<column id="keyname">
 		<name>keyname</name>
 		<type>string</type>
 		<size>255</size>
 		<primary/>
 		<description>The Key</description>
-		
 	</column>
 
 	<column>
@@ -47,4 +58,11 @@
 		<default>0</default>
 		<description>The unix timestamp when the key will expires</description>
 	</column>
+
+	<index>
+		<name>cachedbsql_keyname_idx</name>
+		<colref linkend="keyname"/>
+		<unique/>
+	</index>
+
 </table>

scripts/mysql/cachedb_sql-create.sql
@@ -1,8 +1,10 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
+    id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     keyname CHAR(255) PRIMARY KEY NOT NULL,
     value TEXT(512) NOT NULL,
     counter INT(10) DEFAULT 0 NOT NULL,
-    expires INT(10) UNSIGNED DEFAULT 0 NOT NULL
+    expires INT(10) UNSIGNED DEFAULT 0 NOT NULL,
+    CONSTRAINT cachedbsql_keyname_idx UNIQUE (keyname)
 ) ENGINE=InnoDB;
 

scripts/oracle/cachedb_sql-create.sql
@@ -1,9 +1,11 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
+    id NUMBER(10) PRIMARY KEY,
     keyname VARCHAR2(255) PRIMARY KEY,
     value CLOB(512),
     counter NUMBER(10) DEFAULT 0 NOT NULL,
-    expires NUMBER(10) DEFAULT 0 NOT NULL
+    expires NUMBER(10) DEFAULT 0 NOT NULL,
+    CONSTRAINT cachedb_cachedbsql_keyname_idx  UNIQUE (keyname)
 );
 
 CREATE OR REPLACE TRIGGER cachedb_tr

scripts/pi_http/cachedb_sql-mod
@@ -4,6 +4,7 @@
 			<db_table_id>cachedb</db_table_id>
 			<cmd_type>DB_QUERY</cmd_type>
 			<query_cols>
+				<col><field>id</field><link_cmd>update</link_cmd></col>
 				<col><field>keyname</field><link_cmd>update</link_cmd></col>
 				<col><field>value</field></col>
 				<col><field>counter</field></col>
@@ -24,6 +25,7 @@
 			<db_table_id>cachedb</db_table_id>
 			<cmd_type>DB_UPDATE</cmd_type>
 			<clause_cols>
+				<col><field>id</field><operator>=</operator></col>
 				<col><field>keyname</field><operator>=</operator></col>
 			</clause_cols>
 			<query_cols>
@@ -36,6 +38,7 @@
 			<db_table_id>cachedb</db_table_id>
 			<cmd_type>DB_DELETE</cmd_type>
 			<clause_cols>
+				<col><field>id</field><operator>=</operator></col>
 				<col><field>keyname</field><operator>=</operator></col>
 			</clause_cols>
 		</cmd>

scripts/pi_http/pi_framework.xml
@@ -223,6 +223,7 @@
 	<db_table id="cachedb">
 		<table_name>cachedb</table_name>
 		<db_url_id>mysql</db_url_id>
+		<column><field>id</field><type>DB_INT</type></column>
 		<column><field>keyname</field><type>DB_STR</type></column>
 		<column><field>value</field><type>DB_BLOB</type></column>
 		<column><field>counter</field><type>DB_INT</type></column>
@@ -1671,6 +1672,7 @@
 			<db_table_id>cachedb</db_table_id>
 			<cmd_type>DB_QUERY</cmd_type>
 			<query_cols>
+				<col><field>id</field><link_cmd>update</link_cmd></col>
 				<col><field>keyname</field><link_cmd>update</link_cmd></col>
 				<col><field>value</field></col>
 				<col><field>counter</field></col>
@@ -1691,6 +1693,7 @@
 			<db_table_id>cachedb</db_table_id>
 			<cmd_type>DB_UPDATE</cmd_type>
 			<clause_cols>
+				<col><field>id</field><operator>=</operator></col>
 				<col><field>keyname</field><operator>=</operator></col>
 			</clause_cols>
 			<query_cols>
@@ -1703,6 +1706,7 @@
 			<db_table_id>cachedb</db_table_id>
 			<cmd_type>DB_DELETE</cmd_type>
 			<clause_cols>
+				<col><field>id</field><operator>=</operator></col>
 				<col><field>keyname</field><operator>=</operator></col>
 			</clause_cols>
 		</cmd>

scripts/postgres/cachedb_sql-create.sql
@@ -1,8 +1,11 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
+    id SERIAL PRIMARY KEY NOT NULL,
     keyname VARCHAR(255) PRIMARY KEY NOT NULL,
     value TEXT NOT NULL,
     counter INTEGER DEFAULT 0 NOT NULL,
-    expires INTEGER DEFAULT 0 NOT NULL
+    expires INTEGER DEFAULT 0 NOT NULL,
+    CONSTRAINT cachedb_cachedbsql_keyname_idx UNIQUE (keyname)
 );
 
+ALTER SEQUENCE cachedb_id_seq MAXVALUE 2147483647 CYCLE;

scripts/sqlite/cachedb_sql-create.sql
@@ -1,8 +1,10 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     keyname CHAR(255) PRIMARY KEY NOT NULL,
     value TEXT(512) NOT NULL,
     counter INTEGER DEFAULT 0 NOT NULL,
-    expires INTEGER DEFAULT 0 NOT NULL
+    expires INTEGER DEFAULT 0 NOT NULL,
+    CONSTRAINT cachedb_cachedbsql_keyname_idx  UNIQUE (keyname)
 );
 

scripts/db_berkeley/opensips/cachedb
@@ -1,10 +1,10 @@
 METADATA_COLUMNS
-keyname(str) value(str) counter(int) expires(int)
+id(int) keyname(str) value(str) counter(int) expires(int)
 METADATA_KEY
-
+0 
 METADATA_READONLY
 0
 METADATA_LOGFLAGS
 0
 METADATA_DEFAULTS
-NIL|NIL|0|0
+NIL|NIL|NIL|0|0

commit d800f05c983f4ee31b701271bf709f0823bfbaab

[dbschema] remove bogus second primary key
Issue introduced with 3f3dc8a45d44eed76204c1ebc82e8ae6be03b335
Reported by @kuuse
Closes #2379

db/schema/cachedb_sql.xml
@@ -32,7 +32,6 @@
 		<name>keyname</name>
 		<type>string</type>
 		<size>255</size>
-		<primary/>
 		<description>The Key</description>
 	</column>
 

scripts/mysql/cachedb_sql-create.sql
@@ -1,7 +1,7 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
     id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    keyname CHAR(255) PRIMARY KEY NOT NULL,
+    keyname CHAR(255) NOT NULL,
     value TEXT(512) NOT NULL,
     counter INT(10) DEFAULT 0 NOT NULL,
     expires INT(10) UNSIGNED DEFAULT 0 NOT NULL,

scripts/oracle/cachedb_sql-create.sql
@@ -1,7 +1,7 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
     id NUMBER(10) PRIMARY KEY,
-    keyname VARCHAR2(255) PRIMARY KEY,
+    keyname VARCHAR2(255),
     value CLOB(512),
     counter NUMBER(10) DEFAULT 0 NOT NULL,
     expires NUMBER(10) DEFAULT 0 NOT NULL,

scripts/pi_http/cachedb_sql-mod
@@ -5,7 +5,7 @@
 			<cmd_type>DB_QUERY</cmd_type>
 			<query_cols>
 				<col><field>id</field><link_cmd>update</link_cmd></col>
-				<col><field>keyname</field><link_cmd>update</link_cmd></col>
+				<col><field>keyname</field></col>
 				<col><field>value</field></col>
 				<col><field>counter</field></col>
 				<col><field>expires</field></col>
@@ -26,9 +26,9 @@
 			<cmd_type>DB_UPDATE</cmd_type>
 			<clause_cols>
 				<col><field>id</field><operator>=</operator></col>
-				<col><field>keyname</field><operator>=</operator></col>
 			</clause_cols>
 			<query_cols>
+				<col><field>keyname</field></col>
 				<col><field>value</field></col>
 				<col><field>counter</field></col>
 				<col><field>expires</field></col>
@@ -39,7 +39,6 @@
 			<cmd_type>DB_DELETE</cmd_type>
 			<clause_cols>
 				<col><field>id</field><operator>=</operator></col>
-				<col><field>keyname</field><operator>=</operator></col>
 			</clause_cols>
 		</cmd>
 	</mod>

scripts/pi_http/pi_framework.xml
@@ -1673,7 +1673,7 @@
 			<cmd_type>DB_QUERY</cmd_type>
 			<query_cols>
 				<col><field>id</field><link_cmd>update</link_cmd></col>
-				<col><field>keyname</field><link_cmd>update</link_cmd></col>
+				<col><field>keyname</field></col>
 				<col><field>value</field></col>
 				<col><field>counter</field></col>
 				<col><field>expires</field></col>
@@ -1694,9 +1694,9 @@
 			<cmd_type>DB_UPDATE</cmd_type>
 			<clause_cols>
 				<col><field>id</field><operator>=</operator></col>
-				<col><field>keyname</field><operator>=</operator></col>
 			</clause_cols>
 			<query_cols>
+				<col><field>keyname</field></col>
 				<col><field>value</field></col>
 				<col><field>counter</field></col>
 				<col><field>expires</field></col>
@@ -1707,7 +1707,6 @@
 			<cmd_type>DB_DELETE</cmd_type>
 			<clause_cols>
 				<col><field>id</field><operator>=</operator></col>
-				<col><field>keyname</field><operator>=</operator></col>
 			</clause_cols>
 		</cmd>
 	</mod>

scripts/postgres/cachedb_sql-create.sql
@@ -1,7 +1,7 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
     id SERIAL PRIMARY KEY NOT NULL,
-    keyname VARCHAR(255) PRIMARY KEY NOT NULL,
+    keyname VARCHAR(255) NOT NULL,
     value TEXT NOT NULL,
     counter INTEGER DEFAULT 0 NOT NULL,
     expires INTEGER DEFAULT 0 NOT NULL,

scripts/sqlite/cachedb_sql-create.sql
@@ -1,7 +1,7 @@
 INSERT INTO version (table_name, table_version) values ('cachedb','2');
 CREATE TABLE cachedb (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    keyname CHAR(255) PRIMARY KEY NOT NULL,
+    keyname CHAR(255) NOT NULL,
     value TEXT(512) NOT NULL,
     counter INTEGER DEFAULT 0 NOT NULL,
     expires INTEGER DEFAULT 0 NOT NULL,