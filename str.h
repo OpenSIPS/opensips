81523d0c6c26c13bda3396d8807343a2e9f5db22
Maksym Sobolyev <sobomax@sippysoft.com>
2021-01-22 15:28:28 -0800

    o Simplify str2const() using the fact that struct __str and struct   __str_const have the same structure. Make sure that   (void *)str2const(p) == (void *)p.
    o Check that argument of str2const() is either str * or const str *.

--- str.h
+++ str.h
@@ -21,6 +21,7 @@
 #ifndef str_h
 #define str_h
 
+#include <assert.h>
 #include <string.h>
 
 /**
@@ -70,7 +71,12 @@ typedef struct __str_const str_const;
 #define STR_NULL_const (str_const){NULL, 0}
 #define str_init(_string)  (str){_string, sizeof(_string) - 1}
 #define str_const_init(_string)  (str_const){_string, sizeof(_string) - 1}
-#define str2const(_sp) &(str_const){(_sp)->s, (_sp)->len}
+
+#define OBJ_IS_OF_TYPE(Type, Obj) _Generic((Obj), Type: 1, const Type: 1, default: 0)
+#define str2const(_sp) ({ \
+    static_assert(OBJ_IS_OF_TYPE(str *, _sp), "str * expected"); \
+    /*return*/ (str_const *)(void *)(_sp); \
+})
 
 static inline void init_str(str *dest, const char *src)
 {
