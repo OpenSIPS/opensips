OpenSIPS/opensips

commit 52e6c425b101ce56d7eb35cb413faacbb9309f1a

dialog: update contact & SDPs for replicated updates

modules/dialog/dlg_replication.c
@@ -383,7 +383,34 @@ int dlg_replicated_update(bin_packet_t *packet)
 		goto error;
 	}
 
-	bin_skip_str(packet, 12);
+	/* route set */
+	bin_skip_str(packet, 2);
+
+	/* sync caller and callee contact */
+	bin_pop_str(packet, &st);
+	shm_str_sync(&dlg->legs[DLG_CALLER_LEG].contact, &st);
+	bin_pop_str(packet, &st);
+	shm_str_sync(&dlg->legs[callee_idx(dlg)].contact, &st);
+
+	/* from & to URIs set */
+	bin_skip_str(packet, 2);
+
+	/* sync SDPs */
+	bin_pop_str(packet, &st);
+	shm_str_sync(&dlg->legs[DLG_CALLER_LEG].in_sdp, &st);
+	bin_pop_str(packet, &st);
+	shm_str_sync(&dlg->legs[DLG_CALLER_LEG].out_sdp, &st);
+	bin_pop_str(packet, &st);
+	shm_str_sync(&dlg->legs[callee_idx(dlg)].in_sdp, &st);
+	bin_pop_str(packet, &st);
+	shm_str_sync(&dlg->legs[callee_idx(dlg)].out_sdp, &st);
+
+	/* sync advertised caller and callee contacts */
+	bin_pop_str(packet, &st);
+	shm_str_sync(&dlg->legs[DLG_CALLER_LEG].adv_contact, &st);
+	bin_pop_str(packet, &st);
+	shm_str_sync(&dlg->legs[callee_idx(dlg)].adv_contact, &st);
+
 	bin_pop_str(packet, &vars);
 	bin_pop_str(packet, &profiles);
 	bin_pop_int(packet, &dlg->user_flags);

commit d85d0b50ec635d859a33073875d7e5680fda3772

dialog: do not update advertised contacts on dlg update
When replicating a dialog update, we shall not update the advertised
contacts, as we don't have all the needed information. If we do, we
break the re-homing scenario that uses different advertises for
different instances.
Bug introduced in 52e6c42

modules/dialog/dlg_replication.c
@@ -369,6 +369,7 @@ int dlg_replicated_update(bin_packet_t *packet)
 	bin_skip_int(packet, 2);
 	bin_pop_int(packet, &dlg->state);
 
+	/* sockets */
 	bin_skip_str(packet, 2);
 
 	bin_pop_str(packet, &st);
@@ -405,11 +406,13 @@ int dlg_replicated_update(bin_packet_t *packet)
 	bin_pop_str(packet, &st);
 	shm_str_sync(&dlg->legs[callee_idx(dlg)].out_sdp, &st);
 
-	/* sync advertised caller and callee contacts */
-	bin_pop_str(packet, &st);
-	shm_str_sync(&dlg->legs[DLG_CALLER_LEG].adv_contact, &st);
-	bin_pop_str(packet, &st);
-	shm_str_sync(&dlg->legs[callee_idx(dlg)].adv_contact, &st);
+	/*
+	 * advertised caller and callee contacts
+	 * we shall skip these, as we're currently not re-learning sockets either,
+	 * so we can't figure out the advertised contact to be used by this
+	 * instance, in case it is different than the other one
+	 */
+	bin_skip_str(packet, 2);
 
 	bin_pop_str(packet, &vars);
 	bin_pop_str(packet, &profiles);