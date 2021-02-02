f3c3ed53e6c74b17151e9266d848e323c3eb54bc
Liviu Chircu <liviu@opensips.org>
2021-02-02 22:03:43 +0200

    $socket(af) variable: Fix returned value type
    Credits to Alex Kinch for the report!

--- pvar.c
+++ pvar.c
@@ -2218,7 +2218,7 @@ static inline int get_socket_field( struct socket_info *si,
 				res->rs.len = 5;
 			} else
 				return pv_get_null( NULL, NULL, res);
-			res->flags = PV_VAL_STR|PV_VAL_INT;
+			res->flags = PV_VAL_STR;
 			break;
 		default:
 			LM_CRIT("BUG - unsupported ID %d\n",pvn->u.isname.name.n);
