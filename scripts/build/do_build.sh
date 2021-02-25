6ecb3fe48db0146c79b9862dbcef9806b0f3a4da
Vlad Patrascu <vladp@opensips.org>
2021-02-26 00:43:55 +0200

    CI build: exclude event_kafka

--- scripts/build/do_build.sh
+++ scripts/build/do_build.sh
@@ -8,4 +8,4 @@ PKGS=`grep -A 35 packages: .travis.yml  | grep -e '^ *[-]' | awk '{print $2}'`
 
 CC_EXTRA_OPTS=${CC_EXTRA_OPTS:-"-Werror"} FASTER=1 NICER=0 make \
   exclude_modules="db_oracle osp sngtc cachedb_cassandra cachedb_couchbase \
-  cachedb_mongodb auth_jwt" ${MAKE_TGT:-"all"}
+  cachedb_mongodb auth_jwt event_kafka" ${MAKE_TGT:-"all"}
