OpenSIPS/opensips

commit 0d7a71b2f922a3334862a63b3ae9c9bb11115037

Add TCP and TCPv6 communication modes for control sockets. Those modes are supported on RTPProxy end since 2.0 and are getting popular recently due to Kubrenetes and Docker's networking shenanigans and peculiarities.
As discussed with @razvancrainea in the scope of the PR#2309,
shuffling elements of the iovec command structure might have some
unintended consequences with regards to the multiple nodes /
failover.

As such, it ended up in rabbit hole of refactoring messaging
mechanism to automatically extend user-supplied buffer with
some pre/post elements.

Provide sub-set of allocated vectors and expose it as "vu"
member. Only send_rtpp_command() is allowed to access "vs"
part. Code conversion is helped by some script and basic
functionality tested here:

https://travis-ci.com/github/sippy/voiptests/builds/202257620

modules/rtpproxy/rtpproxy_vcmd.h
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2020 Sippy Software, Inc., http://www.sippysoft.com
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
+#ifndef _rtpproxy_vcmd_h
+#define _rtpproxy_vcmd_h
+
+struct rtpproxy_vcmd {
+	struct iovec *vs;
+	struct iovec *vu;
+	int useritems;
+};
+
+#define RTPP_CMD_IOVEC(nitems, ...) \
+	(struct iovec[nitems + 2]){{/* cookie */}, __VA_ARGS__, \
+	    {/* terminator */}}
+#define RTPP_CMD_IOVEC_STATIC(var, nitems, ...) \
+	static struct iovec var[nitems + 2] = \
+	    {{/* cookie */}, __VA_ARGS__, {/* terminator */}};
+
+#define RTPP_VCMD_INIT(vcmd, nitems, ...) { \
+	(vcmd).vs = RTPP_CMD_IOVEC(nitems, __VA_ARGS__); \
+	(vcmd).vu = (vcmd).vs + 1; \
+	(vcmd).useritems = nitems; \
+}
+
+#define RTPP_VCMD_INIT_STATIC(vcmd, nitems, ...) { \
+	RTPP_CMD_IOVEC_STATIC(_var, nitems, __VA_ARGS__); \
+	(vcmd).vs = _var; \
+	(vcmd).vu = (vcmd).vs + 1; \
+	(vcmd).useritems = nitems; \
+}
+
+#endif

modules/rtpproxy/doc/rtpproxy_admin.xml
@@ -197,7 +197,7 @@
 		<title><varname>rtpproxy_sock</varname> (string)</title>
 		<para>
 		Definition of socket(s) used to connect to (a set) RTPProxy. It may 
-		specify a UNIX socket or an IPv4/IPv6 UDP socket.
+		specify a UNIX socket, an IPv4/IPv6 UDP socket or an IPv4/IPv6 TCP socket.
 		</para>
 		<para>
 			The definition also supports to specify a different IP that will
@@ -219,7 +219,7 @@
 modparam("rtpproxy", "rtpproxy_sock", "udp:localhost:12221=2")
 # multiple rtproxies for LB
 modparam("rtpproxy", "rtpproxy_sock",
-	"udp:localhost:12221 udp:localhost:12222")
+	"udp:localhost:12221 udp:localhost:12222 tcp:remote1:33422 tcp6:remote2:32322")
 # multiple sets of multiple rtproxies
 modparam("rtpproxy", "rtpproxy_sock",
 	"1 == udp:localhost:12221 udp:localhost:12222")

modules/rtpproxy/rtppn_connect.c
@@ -1,0 +1,122 @@
+/*
+ * Copyright (C) 2020 Sippy Software, Inc., http://www.sippysoft.com
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
+
+#include "../../mem/mem.h"
+
+#include "rtpproxy.h"
+#include "rtppn_connect.h"
+
+static int try_connect(int s, const struct sockaddr *name, socklen_t namelen, int timeout)
+{
+	int oflags, nflags, cres, pres;
+	struct pollfd pfd;
+
+	oflags = fcntl(s, F_GETFL);
+	if (oflags < 0)
+		return -1;
+	nflags = fcntl(s, F_SETFL, oflags | O_NONBLOCK);
+	if (nflags < 0)
+		return -1;
+
+	cres = connect(s, name, namelen);
+	if (cres < 0 && errno != EINPROGRESS)
+		goto out;
+	if (cres == 0)
+		goto out;
+	pfd.fd = s;
+	pfd.events = POLLOUT;
+	pres = poll(&pfd, 1, timeout);
+	if (pres <= 0) {
+		cres = -1;
+	} else {
+		cres = 0;
+	}
+out:
+	fcntl(s, F_SETFL, oflags);
+	return cres;
+}
+
+int connect_rtpp_node(const struct rtpp_node *pnode)
+{
+	int n, s;
+	char *cp, *hostname;
+	struct addrinfo hints, *res;
+
+	/*
+	 * This is UDP, TCP, UDP6 or TCP6. Detect host and port; lookup host;
+	 * do connect() in order to specify peer address
+	 */
+	hostname = (char*)pkg_malloc(sizeof(char) * (strlen(pnode->rn_address) + 1));
+	if (hostname == NULL) {
+		LM_ERR("no more pkg memory\n");
+		goto e0;
+	}
+	strcpy(hostname, pnode->rn_address);
+
+	cp = strrchr(hostname, ':');
+	if (cp != NULL) {
+		*cp = '\0';
+		cp++;
+	}
+	if (cp == NULL || *cp == '\0')
+		cp = DEFAULT_CPORT;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_flags = 0;
+	hints.ai_family = (pnode->rn_umode == CM_UDP6 || pnode->rn_umode == CM_TCP6) ? AF_INET6 : AF_INET;
+	hints.ai_socktype = CM_STREAM(pnode) ? SOCK_STREAM : SOCK_DGRAM;
+	if ((n = getaddrinfo(hostname, cp, &hints, &res)) != 0) {
+		LM_ERR("%s\n", gai_strerror(n));
+		goto e1;
+	}
+
+	s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (s == -1) {
+		LM_ERR("can't create socket\n");
+		goto e2;
+	}
+	if (CM_STREAM(pnode)) {
+		n = try_connect(s, res->ai_addr, res->ai_addrlen, rtpproxy_tout);
+	} else {
+		n = connect(s, res->ai_addr, res->ai_addrlen);
+	}
+	if (n == -1) {
+		LM_ERR("can't connect to a RTP proxy\n");
+		goto e3;
+	}
+	pkg_free(hostname);
+	freeaddrinfo(res);
+	LM_DBG("connected %s\n", pnode->rn_address);
+	return s;
+e3:
+	close(s);
+e2:
+	freeaddrinfo(res);
+e1:
+	pkg_free(hostname);
+e0:
+	return -1;
+}

modules/rtpproxy/rtppn_connect.h
@@ -1,0 +1,28 @@
+/*
+ * Copyright (C) 2020 Sippy Software, Inc., http://www.sippysoft.com
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
+#ifndef _rtppn_try_connect_h
+#define _rtppn_try_connect_h
+
+struct rtpp_node;
+
+int connect_rtpp_node(const struct rtpp_node *pnode);
+
+#endif

modules/rtpproxy/rtpproxy.h
@@ -31,22 +31,28 @@
 #include "../dialog/dlg_load.h"
 #include "../../rw_locking.h"
 
+struct rtpproxy_vcmd;
+
 /* Handy macros */
 #define STR2IOVEC(sx, ix)       do {(ix).iov_base = (sx).s; (ix).iov_len = (sx).len;} while(0)
 #define SZ2IOVEC(sx, ix)        do {(ix).iov_base = (sx); (ix).iov_len = strlen(sx);} while(0)
 
+enum comm_modes {CM_UNIX = 0, CM_UDP, CM_TCP, CM_UDP6, CM_TCP6};
+
 struct rtpp_node {
 	unsigned int		idx;			/* overall index */
-	str					rn_url;			/* unparsed, deletable */
-	int					rn_umode;
-	char				*rn_address;	/* substring of rn_url */
-	char				*adv_address;	/* advertised address of rtpproxy */
-	int					rn_disabled;	/* found unaccessible? */
-	unsigned			rn_weight;		/* for load balancing */
+	str			rn_url;			/* unparsed, deletable */
+	enum comm_modes		rn_umode;		/* communication mode */
+	char			*rn_address;		/* substring of rn_url */
+	char			*adv_address;		/* advertised address of rtpproxy */
+	int			rn_disabled;		/* found unaccessible? */
+	unsigned		rn_weight;		/* for load balancing */
 	unsigned int		rn_recheck_ticks;
 	unsigned int		capabilities;
 	struct rtpp_node	*rn_next;
 };
+
+#define CM_STREAM(ndp) ((ndp)->rn_umode == CM_TCP || (ndp)->rn_umode == CM_TCP6)
 
 /* Supported version of the RTP proxy command protocol */
 #define	SUP_CPROTOVER	20040107
@@ -73,6 +79,8 @@ struct rtpp_node {
 #define	RECORD_CPROTOVER		"20071218"
 #define	SUBCOMMAND_CAP			(1<<9)
 #define	SUBCOMMAND_CPROTOVER	"20191015"
+
+#define DEFAULT_CPORT 	"22222"
 
 #define RTP_CAP(_c) _c ## _CPROTOVER, sizeof(_c ## _CPROTOVER) - 1
 #define SET_CAP(_n, _c) (_n)->capabilities |= (_c ## _CAP)
@@ -151,6 +159,7 @@ extern str rtpp_notify_socket;
 extern int rtpp_notify_socket_un;
 extern struct dlg_binds dlg_api;
 extern int detect_rtp_idle;
+extern int rtpproxy_tout;
 extern struct rtpp_set_head ** rtpp_set_list;
 extern struct rtpp_notify_head * rtpp_notify_h;
 int init_rtpp_notify_list();
@@ -159,7 +168,7 @@ void notification_listener_process(int rank);
 /* Functions from nathelper */
 struct rtpp_set *get_rtpp_set(nh_set_param_t *);
 struct rtpp_node *select_rtpp_node(struct sip_msg *, str, struct rtpp_set *, pv_spec_p, int);
-char *send_rtpp_command(struct rtpp_node *, struct iovec *, int);
+char *send_rtpp_command(struct rtpp_node *, struct rtpproxy_vcmd *, int);
 int force_rtp_proxy_body(struct sip_msg* msg, struct force_rtpp_args *args,
                pv_spec_p var, pv_spec_p ipvar);
 

modules/rtpproxy/rtpproxy_stream.c
@@ -35,7 +35,7 @@
 #include "../../ut.h"
 #include "rtpproxy.h"
 #include "nhelpr_funcs.h"
-
+#include "rtpproxy_vcmd.h"
 
 static int
 rtpproxy_stream(struct sip_msg* msg, str *pname, int count,
@@ -46,19 +46,20 @@ rtpproxy_stream(struct sip_msg* msg, str *pname, int count,
     struct rtpp_node *node;
     struct rtpp_set *set;
     char cbuf[16];
-    struct iovec v[] = {
-        {NULL,        0},
-        {cbuf,        0}, /* 1 P<count> */
+    struct rtpproxy_vcmd vstream;
+
+    RTPP_VCMD_INIT(vstream, 10,
+        {cbuf,        0}, /*.vu[0] P<count> */
         {" ",         1},
-        {NULL,        0}, /* 3 callid */
+        {NULL,        0}, /*.vu[2] callid */
         {" ",         1},
-        {NULL,        0}, /* 5 pname */
+        {NULL,        0}, /*.vu[4] pname */
         {" session ", 9},
-        {NULL,        0}, /* 7 from tag */
+        {NULL,        0}, /*.vu[6] from tag */
         {";1 ",       3},
-        {NULL,        0}, /* 9 to tag */
+        {NULL,        0}, /*.vu[8] to tag */
         {";1",        2}
-    };
+    );
 
     if (get_callid(msg, &callid) == -1 || callid.len == 0) {
         LM_ERR("can't get Call-Id field\n");
@@ -72,19 +73,19 @@ rtpproxy_stream(struct sip_msg* msg, str *pname, int count,
         LM_ERR("can't get From tag\n");
         return -1;
     }
-    v[1].iov_len = sprintf(cbuf, "P%d", count);
-    STR2IOVEC(callid, v[3]);
-    STR2IOVEC(*pname, v[5]);
+    vstream.vu[0].iov_len = sprintf(cbuf, "P%d", count);
+    STR2IOVEC(callid, vstream.vu[2]);
+    STR2IOVEC(*pname, vstream.vu[4]);
 
-    nitems = 11;
+    nitems = vstream.useritems;
     if (stream2uac == 0) {
         if (to_tag.len == 0)
             return -1;
-        STR2IOVEC(to_tag, v[7]);
-        STR2IOVEC(from_tag, v[9]);
+        STR2IOVEC(to_tag, vstream.vu[6]);
+        STR2IOVEC(from_tag, vstream.vu[8]);
     } else {
-        STR2IOVEC(from_tag, v[7]);
-        STR2IOVEC(to_tag, v[9]);
+        STR2IOVEC(from_tag, vstream.vu[6]);
+        STR2IOVEC(to_tag, vstream.vu[8]);
         if (to_tag.len <= 0)
             nitems -= 2;
     }
@@ -110,7 +111,7 @@ rtpproxy_stream(struct sip_msg* msg, str *pname, int count,
           "node.  Please upgrade the RTPproxy and try again.\n");
         goto end;
     }
-    send_rtpp_command(node, v, nitems);
+    send_rtpp_command(node, &vstream, nitems);
 
 	ret = 1;
 end:
@@ -152,17 +153,18 @@ rtpproxy_stop_stream(struct sip_msg* msg, nh_set_param_t *setid, pv_spec_t *var,
     str callid, from_tag, to_tag;
     struct rtpp_node *node;
     struct rtpp_set *set;
-    struct iovec v[] = {
-        {NULL,        0},
-        {"S",         1}, /* 1 */
+    struct rtpproxy_vcmd vststrm;
+
+    RTPP_VCMD_INIT(vststrm, 8,
+        {"S",         1}, /*.vu[0] */
         {" ",         1},
-        {NULL,        0}, /* 3 callid */
+        {NULL,        0}, /*.vu[2] callid */
         {" ",         1},
-        {NULL,        0}, /* 5 from tag */
+        {NULL,        0}, /*.vu[4] from tag */
         {";1 ",       3},
-        {NULL,        0}, /* 7 to tag */
+        {NULL,        0}, /*.vu[6] to tag */
         {";1",        2}
-    };
+    );
 
     if (get_callid(msg, &callid) == -1 || callid.len == 0) {
         LM_ERR("can't get Call-Id field\n");
@@ -176,16 +178,16 @@ rtpproxy_stop_stream(struct sip_msg* msg, nh_set_param_t *setid, pv_spec_t *var,
         LM_ERR("can't get From tag\n");
         return -1;
     }
-    STR2IOVEC(callid, v[3]);
-    nitems = 9;
+    STR2IOVEC(callid, vststrm.vu[2]);
+    nitems = vststrm.useritems;
     if (stream2uac == 0) {
         if (to_tag.len == 0)
             return -1;
-        STR2IOVEC(to_tag, v[5]);
-        STR2IOVEC(from_tag, v[7]);
+        STR2IOVEC(to_tag, vststrm.vu[4]);
+        STR2IOVEC(from_tag, vststrm.vu[6]);
     } else {
-        STR2IOVEC(from_tag, v[5]);
-        STR2IOVEC(to_tag, v[7]);
+        STR2IOVEC(from_tag, vststrm.vu[4]);
+        STR2IOVEC(to_tag, vststrm.vu[6]);
         if (to_tag.len <= 0)
             nitems -= 2;
     }
@@ -212,7 +214,7 @@ rtpproxy_stop_stream(struct sip_msg* msg, nh_set_param_t *setid, pv_spec_t *var,
           "node.  Please upgrade the RTPproxy and try again.\n");
         goto end;
     }
-    send_rtpp_command(node, v, nitems);
+    send_rtpp_command(node, &vststrm, nitems);
 
 	ret = 1;
 end:

commit efb9e16de160e976ae5a1ba05388b24d798438c5

rtpproxy: alocate anonymous structures in local scope
RTPP_CMD_IOVEC and RTPP_VCMD_INIT_STATIC are initializing a variable
with the content of another (anonymous one). If we however create the
anonymous variable in a block's scope, it will be unavailable at the
end of the block, resulting in invalid memory access - allocating the
structures in the function's context (rather than a block's context)
fixes this issue.
Bug introduced in 0d7a71b2.

modules/rtpproxy/rtpproxy_vcmd.h
@@ -34,17 +34,15 @@ struct rtpproxy_vcmd {
 	static struct iovec var[nitems + 2] = \
 	    {{.iov_base = NULL /* cookie */}, __VA_ARGS__, {.iov_base = NULL /* terminator */}};
 
-#define RTPP_VCMD_INIT(vcmd, nitems, ...) { \
+#define RTPP_VCMD_INIT(vcmd, nitems, ...) \
 	(vcmd).vs = RTPP_CMD_IOVEC(nitems, __VA_ARGS__); \
 	(vcmd).vu = (vcmd).vs + 1; \
 	(vcmd).useritems = nitems; \
-}
 
-#define RTPP_VCMD_INIT_STATIC(vcmd, nitems, ...) { \
+#define RTPP_VCMD_INIT_STATIC(vcmd, nitems, ...) \
 	RTPP_CMD_IOVEC_STATIC(_var, nitems, __VA_ARGS__); \
 	(vcmd).vs = _var; \
 	(vcmd).vu = (vcmd).vs + 1; \
 	(vcmd).useritems = nitems; \
-}
 
 #endif