26bc07a6e8615e61769f8a4c85a80a9cacfe0b18
Bogdan-Andrei Iancu <bogdan@opensips.org>
2021-04-05 10:11:27 +0300

    Fix tracing the outbound ACK in transactional tracing
    (cherry picked from commit 8684370eb7af2137324cc81fc3cd28e45ed0eb3e)

--- modules/siptrace/siptrace.c
+++ modules/siptrace/siptrace.c
@@ -1472,8 +1472,7 @@ static int sip_trace_w(struct sip_msg *msg, char *param1,
 	}
 
 	if (trace_flags == TRACE_TRANSACTION &&
-		msg->first_line.type == SIP_REQUEST &&
-		(msg->REQ_METHOD != METHOD_ACK)) {
+	msg->first_line.type == SIP_REQUEST) {
 		LM_DBG("tracing transaction!\n");
 	} else if (trace_flags == TRACE_TRANSACTION) {
 		LM_DBG("can't trace transaction! Will trace only this message!\n");
