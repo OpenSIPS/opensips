OpenSIPS/opensips

commit 5e5fc98ad07889c6d285a913aee7caf769c2878b

b2b_entities: parse from & to structs if they aren't
This fix aims to solve a crash in case the from/to headers are not
parsed (yet). We are parsing them in a different structure, to make sure
it doesn't overlap with any ongoing processing of the request

modules/b2b_entities/dlg.c
@@ -2594,7 +2594,30 @@ void b2b_tm_cback(struct cell *t, b2b_table htable, struct tmcb_params *ps)
 		callid = msg->callid->body;
 		from_tag = ((struct to_body*)msg->from->parsed)->tag_value;
 	} else if (ps->req) {
-		from_tag = ((struct to_body*)ps->req->from->parsed)->tag_value;
+		if (!(struct to_body*)ps->req->from->parsed) {
+			if (!parse_to(ps->req->from->body.s,
+					ps->req->from->body.s, &from_hdr_parsed)) {
+				from_tag.s = NULL;
+				from_tag.len = 0;
+			} else {
+				from_tag = from_hdr_parsed.tag_value;
+				free_to_params(&from_hdr_parsed);
+			}
+		} else {
+			from_tag = ((struct to_body*)ps->req->from->parsed)->tag_value;
+		}
+		if (!(struct to_body*)ps->req->to->parsed) {
+			if (!parse_to(ps->req->to->body.s,
+					ps->req->to->body.s, &to_hdr_parsed)) {
+				to_tag.s = NULL;
+				to_tag.len = 0;
+			} else {
+				to_tag = to_hdr_parsed.tag_value;
+				free_to_params(&to_hdr_parsed);
+			}
+		} else {
+			to_tag = ((struct to_body*)ps->req->to->parsed)->tag_value;
+		}
 		to_tag = ((struct to_body*)ps->req->to->parsed)->tag_value;
 		callid = ps->req->callid->body;
 	} else {

commit 9e29b549190e133b1fdbbb136acebd9b4229ba85

b2b_entities: fix tags parsing from request
Fix bug introduced in 5e5fc98a

modules/b2b_entities/dlg.c
@@ -2639,8 +2639,10 @@ void b2b_tm_cback(struct cell *t, b2b_table htable, struct tmcb_params *ps)
 		from_tag = ((struct to_body*)msg->from->parsed)->tag_value;
 	} else if (ps->req) {
 		if (!(struct to_body*)ps->req->from->parsed) {
-			if (!parse_to(ps->req->from->body.s,
-					ps->req->from->body.s, &from_hdr_parsed)) {
+			parse_to(ps->req->from->body.s,
+					ps->req->from->body.s + ps->req->from->body.len + 1,
+					&from_hdr_parsed);
+			if (from_hdr_parsed.error == PARSE_ERROR) {
 				from_tag.s = NULL;
 				from_tag.len = 0;
 			} else {
@@ -2651,8 +2653,10 @@ void b2b_tm_cback(struct cell *t, b2b_table htable, struct tmcb_params *ps)
 			from_tag = ((struct to_body*)ps->req->from->parsed)->tag_value;
 		}
 		if (!(struct to_body*)ps->req->to->parsed) {
-			if (!parse_to(ps->req->to->body.s,
-					ps->req->to->body.s, &to_hdr_parsed)) {
+			parse_to(ps->req->to->body.s,
+					ps->req->to->body.s + ps->req->to->body.len + 1,
+					&to_hdr_parsed);
+			if (to_hdr_parsed.error == PARSE_ERROR) {
 				to_tag.s = NULL;
 				to_tag.len = 0;
 			} else {
@@ -2662,7 +2666,6 @@ void b2b_tm_cback(struct cell *t, b2b_table htable, struct tmcb_params *ps)
 		} else {
 			to_tag = ((struct to_body*)ps->req->to->parsed)->tag_value;
 		}
-		to_tag = ((struct to_body*)ps->req->to->parsed)->tag_value;
 		callid = ps->req->callid->body;
 	} else {
 		to_tag.s = NULL;