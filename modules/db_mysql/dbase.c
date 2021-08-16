OpenSIPS/opensips

commit 87f2416517e128fe139c1888fa74beeaa96287dd

Change CON_SET_CURR_PS pattern to always call it _directly_ before db call
Because CON_SET_CURR_PS is always followed by an immediate call to
CON_RESET_CURR_PS inside the DB function, it makes sense to call
CON_SET_CURR_PS just before the DB call.

Otherwise you could get this:

   CON_SET_CURR_PS(...);

   for (i = 0; i < n; ++i) {
       // first insert uses prepared statement
       db.insert(...);
       // but second time in the loop, we use "plain" text queries
   }

The updated pattern would look like this:

   for (i = 0; i < n; ++i) {
       // the ps handle is always reset, so we reinit it every time
       CON_SET_CURR_PS(...);
       db.insert(...);
   }

Note that we don't need any explicit CON_RESET_CURR_PS() at odd points
anymore (like was added in 57caa6c03). A SET is immediately followed by
an implicit RESET.

modules/db_mysql/dbase.c
@@ -976,15 +976,14 @@ int db_mysql_query(const db_con_t* _h, const db_key_t* _k, const db_op_t* _op,
 		}
 
 		ret = db_mysql_do_prepared_query(_h, &query_holder, _v, _n, NULL, 0);
+		CON_RESET_CURR_PS(_h);
 		if (ret != 0) {
-			CON_RESET_CURR_PS(_h);
 			if (_r)
 				*_r = NULL;
 			return ret;
 		}
 
 		ret = db_mysql_store_result(_h, _r);
-		CON_RESET_CURR_PS(_h);
 		return ret;
 	}
 	return db_do_query(_h, _k, _op, _v, _c, _n, _nc, _o, _r,
@@ -1333,7 +1332,10 @@ int db_mysql_delete(const db_con_t* _h, const db_key_t* _k, const db_op_t* _o,
 		if (CON_HAS_UNINIT_PS(_h)||!has_stmt_ctx(_h,&(CON_MYSQL_PS(_h)->ctx))){
 			ret = db_do_delete(_h, _k, _o, _v, _n, db_mysql_val2str,
 				db_mysql_submit_dummy_query);
-			if (ret!=0) {CON_RESET_CURR_PS(_h);return ret;}
+			if (ret != 0) {
+				CON_RESET_CURR_PS(_h);
+				return ret;
+			}
 		}
 		ret = db_mysql_do_prepared_query(_h, &query_holder, _v, _n, NULL, 0);
 		CON_RESET_CURR_PS(_h);
@@ -1366,7 +1368,10 @@ int db_mysql_update(const db_con_t* _h, const db_key_t* _k, const db_op_t* _o,
 		if (CON_HAS_UNINIT_PS(_h)||!has_stmt_ctx(_h,&(CON_MYSQL_PS(_h)->ctx))){
 			ret = db_do_update(_h, _k, _o, _v, _uk, _uv, _n, _un,
 				db_mysql_val2str, db_mysql_submit_dummy_query);
-			if (ret!=0) {CON_RESET_CURR_PS(_h);return ret;}
+			if (ret != 0) {
+				CON_RESET_CURR_PS(_h);
+				return ret;
+			}
 		}
 		ret = db_mysql_do_prepared_query(_h, &query_holder, _uv, _un, _v, _n);
 		CON_RESET_CURR_PS(_h);
@@ -1393,7 +1398,10 @@ int db_mysql_replace(const db_con_t* _h, const db_key_t* _k, const db_val_t* _v,
 		if (CON_HAS_UNINIT_PS(_h)||!has_stmt_ctx(_h,&(CON_MYSQL_PS(_h)->ctx))){
 			ret = db_do_replace(_h, _k, _v, _n, db_mysql_val2str,
 				db_mysql_submit_dummy_query);
-			if (ret!=0) {CON_RESET_CURR_PS(_h);return ret;}
+			if (ret != 0) {
+				CON_RESET_CURR_PS(_h);
+				return ret;
+			}
 		}
 		ret = db_mysql_do_prepared_query(_h, &query_holder, _v, _n, NULL, 0);
 		CON_RESET_CURR_PS(_h);

db/db_insertq.c
@@ -131,11 +131,10 @@ void flush_query_list(void)
 			//Reset prepared statement between query lists/connections
 			my_ps = NULL;
 
-			CON_SET_CURR_PS(it->conn[process_no], &my_ps);
-
 			/* and let's insert the rows */
 			for (i=0;i<it->no_rows;i++)
 			{
+				CON_SET_CURR_PS(it->conn[process_no], &my_ps);
 				if (it->dbf.insert(it->conn[process_no],it->cols,it->rows[i],
 							it->col_no) < 0)
 					LM_ERR("failed to insert into DB\n");

modules/acc/acc.c
@@ -595,9 +595,6 @@ int acc_db_request( struct sip_msg *rq, struct sip_msg *rpl,
 			ps = &my_ps3;
 	}
 
-	CON_SET_CURR_PS(db_handle, ps);
-
-
 	/* multi-leg columns */
 	if (ctx) {
 		/* prevent acces for setting variable */
@@ -612,8 +609,10 @@ int acc_db_request( struct sip_msg *rq, struct sip_msg *rpl,
 
 		if ( !ctx->leg_values ) {
 			accX_unlock(&ctx->lock);
-			if (con_set_inslist(&acc_dbf,db_handle,ins_list,db_keys,n) < 0 )
+			if (con_set_inslist(&acc_dbf, db_handle, ins_list, db_keys, n) < 0) {
 				CON_RESET_INSLIST(db_handle);
+			}
+			CON_SET_CURR_PS(db_handle, ps);
 			if (acc_dbf.insert(db_handle, db_keys, db_vals, n) < 0) {
 				LM_ERR("failed to insert into %.*s table\n", acc_env.text.len, acc_env.text.s);
 				return -1;
@@ -623,8 +622,10 @@ int acc_db_request( struct sip_msg *rq, struct sip_msg *rpl,
 				for (extra=db_leg_tags, i=m; extra; extra=extra->next, i++) {
 					VAL_STR(db_vals+i)=LEG_VALUE( j, extra, ctx);
 				}
-				if (con_set_inslist(&acc_dbf,db_handle,ins_list,db_keys,n) < 0 )
+				if (con_set_inslist(&acc_dbf, db_handle, ins_list, db_keys, n) < 0) {
 					CON_RESET_INSLIST(db_handle);
+				}
+				CON_SET_CURR_PS(db_handle, ps);
 				if (acc_dbf.insert(db_handle, db_keys, db_vals, n) < 0) {
 					LM_ERR("failed to insert into %.*s table\n", acc_env.text.len, acc_env.text.s);
 					accX_unlock(&ctx->lock);
@@ -634,8 +635,10 @@ int acc_db_request( struct sip_msg *rq, struct sip_msg *rpl,
 			accX_unlock(&ctx->lock);
 		}
 	} else {
-		if (con_set_inslist(&acc_dbf,db_handle,ins_list,db_keys,m) < 0 )
-				CON_RESET_INSLIST(db_handle);
+		if (con_set_inslist(&acc_dbf, db_handle, ins_list, db_keys, m) < 0) {
+			CON_RESET_INSLIST(db_handle);
+		}
+		CON_SET_CURR_PS(db_handle, ps);
 		if (acc_dbf.insert(db_handle, db_keys, db_vals, m) < 0) {
 			LM_ERR("failed to insert into %.*s table\n", acc_env.text.len, acc_env.text.s);
 			return -1;
@@ -691,8 +694,6 @@ int acc_db_cdrs(struct dlg_cell *dlg, struct sip_msg *msg, acc_ctx_t* ctx)
 
 	total = ret + 5;
 	acc_dbf.use_table(db_handle, &table);
-	CON_SET_CURR_PS(db_handle, &my_ps);
-
 
 	/* prevent acces for setting variable */
 	accX_lock(&ctx->lock);
@@ -701,8 +702,10 @@ int acc_db_cdrs(struct dlg_cell *dlg, struct sip_msg *msg, acc_ctx_t* ctx)
 		VAL_STR(db_vals+i) = ctx->extra_values[extra->tag_idx].value;
 
 	if (!ctx->leg_values) {
-		if (con_set_inslist(&acc_dbf,db_handle,&ins_list,db_keys,total) < 0 )
+		if (con_set_inslist(&acc_dbf, db_handle, &ins_list, db_keys, total) < 0) {
 			CON_RESET_INSLIST(db_handle);
+		}
+		CON_SET_CURR_PS(db_handle, &my_ps);
 		if (acc_dbf.insert(db_handle, db_keys, db_vals, total) < 0) {
 			LM_ERR("failed to insert into database\n");
 			accX_unlock(&ctx->lock);
@@ -717,8 +720,10 @@ int acc_db_cdrs(struct dlg_cell *dlg, struct sip_msg *msg, acc_ctx_t* ctx)
 				VAL_STR(db_vals+ret+j+1) = LEG_VALUE( i, extra, ctx);
 			}
 
-			if (con_set_inslist(&acc_dbf,db_handle,&ins_list,db_keys,total) < 0 )
+			if (con_set_inslist(&acc_dbf, db_handle, &ins_list, db_keys, total) < 0) {
 				CON_RESET_INSLIST(db_handle);
+			}
+			CON_SET_CURR_PS(db_handle, &my_ps);
 			if (acc_dbf.insert(db_handle,db_keys,db_vals,total) < 0) {
 				LM_ERR("failed inserting into database\n");
 				accX_unlock(&ctx->lock);

modules/alias_db/alookup.c
@@ -103,9 +103,9 @@ static int alias_db_query(struct sip_msg* _msg, str* table_s,
 	}
 
 	adbf.use_table(db_handle, table_s);
+
 	if (!ald_append_branches)
 		CON_SET_CURR_PS(db_handle, my_ps[ps_idx]);
-
 	if(adbf.query( db_handle, db_keys, NULL, db_vals, db_cols,
 		(flags&ALIAS_NO_DOMAIN_FLAG)?1:2 /*no keys*/, 2 /*no cols*/,
 		NULL, &db_res)!=0)

modules/auth_db/checks.c
@@ -117,8 +117,8 @@ static inline int check_username(struct sip_msg* _m, str* _table,
 	VAL_STR(vals + 2) = _uri->user;
 
 	auth_dbf.use_table(auth_db_handle, _table);
-	CON_SET_CURR_PS(auth_db_handle, &my_ps);
 
+	CON_SET_CURR_PS(auth_db_handle, &my_ps);
 	if (auth_dbf.query(auth_db_handle, keys, 0, vals, cols, 3, 1, 0, &res) < 0)
 	{
 		LM_ERR("Error while querying database\n");
@@ -213,7 +213,6 @@ int does_uri_exist(struct sip_msg* _msg, str* uri, str* _table)
 	VAL_STR(vals + 1) = p_uri.host;
 
 	CON_SET_CURR_PS(auth_db_handle, &my_ps);
-
 	if (auth_dbf.query(auth_db_handle, keys, 0, vals, cols, (use_domain ? 2 : 1),
 				1, 0, &res) < 0) {
 		LM_ERR("Error while querying database\n");
@@ -315,10 +314,9 @@ int get_auth_id(struct sip_msg* _msg, str *_table, str* uri,
 	VAL_NULL(vals + 1) = 0;
 	VAL_STR(vals + 1) = sip_uri.host;
 
-	CON_SET_CURR_PS(auth_db_handle, &my_ps);
-
 	/* if use_domain is set also the domain column of the database table will
 	   be honoured in the following query (see sixth parameter) */
+	CON_SET_CURR_PS(auth_db_handle, &my_ps);
 	if (auth_dbf.query(auth_db_handle, keys, 0, vals, cols, (use_domain ? 2 : 1),
 	2, 0, &dbres) < 0) {
 		LM_ERR("Error while querying database\n");

modules/call_center/cc_db.c
@@ -915,7 +915,6 @@ int cc_write_cdr( str *un, str *fid, str *aid, int type, int rt, int wt, int tt,
 	vals[10].val.int_val = cid;
 
 	CON_SET_CURR_PS(cc_acc_db_handle, &my_ps);
-
 	if (cc_acc_dbf.insert( cc_acc_db_handle, columns, vals, 11) < 0) {
 		LM_ERR("CDR insert failed\n");
 		return -1;

modules/dialog/dlg_db_handler.c
@@ -519,7 +519,6 @@ int remove_ended_dlgs_from_db(void)
 	VAL_INT(values) = DLG_STATE_DELETED ;
 
 	CON_SET_CURR_PS(dialog_db_handle, &my_ps);
-
 	if(dialog_dbf.delete(dialog_db_handle, match_keys, 0, values, 1) < 0) {
 		LM_ERR("failed to delete database information\n");
 		return -1;
@@ -884,8 +883,8 @@ int dlg_timer_remove_from_db(struct dlg_cell *cell)
 	if (dlg_del_curr_no == dlg_bulk_del_no) {
 		LM_DBG("triggering delete for %d dialogs\n",dlg_del_curr_no);
 
-		CON_SET_CURR_PS(dialog_db_handle, &my_ps);
 		CON_USE_OR_OP(dialog_db_handle);
+		CON_SET_CURR_PS(dialog_db_handle, &my_ps);
 		if(dialog_dbf.delete(dialog_db_handle, dlg_del_keys,
 					0, dlg_del_values, dlg_bulk_del_no) < 0)
 			LM_ERR("failed to delete bulk database information !!!\n");
@@ -950,7 +949,6 @@ int remove_dialog_from_db(struct dlg_cell * cell)
 	VAL_BIGINT(values) = dlg_get_db_id(cell);
 
 	CON_SET_CURR_PS(dialog_db_handle, &my_ps);
-
 	if(dialog_dbf.delete(dialog_db_handle, match_keys, 0, values, 1) < 0) {
 		LM_ERR("failed to delete database information\n");
 		return -1;
@@ -992,7 +990,6 @@ int update_dialog_timeout_info(struct dlg_cell * cell)
 			 cell->tl.timeout - get_ticks()) );
 
 	CON_SET_CURR_PS(dialog_db_handle, &my_ps_update);
-
 	if((dialog_dbf.update(dialog_db_handle, (insert_keys), 0,
 					(values), (insert_keys+1), (values+1), 1, 1)) !=0){
 		LM_ERR("could not update database timeout info\n");
@@ -1104,7 +1101,6 @@ int update_dialog_dbinfo(struct dlg_cell * cell)
 		SET_ROUTE_VALUE(values+28, cell->rt_on_hangup);
 
 		CON_SET_CURR_PS(dialog_db_handle, &my_ps_insert);
-
 		if((dialog_dbf.insert(dialog_db_handle, insert_keys, values,
 								DIALOG_TABLE_TOTAL_COL_NO)) !=0){
 			LM_ERR("could not add another dialog to db - state=%d callid=%.*s\n",
@@ -1148,7 +1144,6 @@ int update_dialog_dbinfo(struct dlg_cell * cell)
 		SET_STR_VALUE(values+23, cell->legs[callee_leg].contact);
 
 		CON_SET_CURR_PS(dialog_db_handle, &my_ps_update);
-
 		if((dialog_dbf.update(dialog_db_handle, (insert_keys), 0,
 						(values), (insert_keys+11), (values+11), 1, 13)) !=0){
 			LM_ERR("could not update database info\n");
@@ -1175,7 +1170,6 @@ int update_dialog_dbinfo(struct dlg_cell * cell)
 		set_final_update_cols(values+18, cell, 0);
 
 		CON_SET_CURR_PS(dialog_db_handle, &my_ps_update_vp);
-
 		if((dialog_dbf.update(dialog_db_handle, (insert_keys), 0,
 						(values), (insert_keys+18), (values+18), 1, 4)) !=0){
 			LM_ERR("could not update database info\n");
@@ -1643,11 +1637,11 @@ void dialog_update_db(unsigned int ticks, void *do_lock)
 				SET_ROUTE_VALUE(values+27, cell->rt_on_timeout);
 				SET_ROUTE_VALUE(values+28, cell->rt_on_hangup);
 
-				CON_SET_CURR_PS(dialog_db_handle, &my_ps_insert);
-				if (con_set_inslist(&dialog_dbf,dialog_db_handle,
-				&ins_list,insert_keys,DIALOG_TABLE_TOTAL_COL_NO) < 0 )
+				if (con_set_inslist(&dialog_dbf, dialog_db_handle,
+						&ins_list, insert_keys, DIALOG_TABLE_TOTAL_COL_NO) < 0) {
 					CON_RESET_INSLIST(dialog_db_handle);
-
+				}
+				CON_SET_CURR_PS(dialog_db_handle, &my_ps_insert);
 				if((dialog_dbf.insert(dialog_db_handle, insert_keys,
 				values, DIALOG_TABLE_TOTAL_COL_NO)) !=0){
 					LM_ERR("could not add another dialog to db - state=%d callid=%.*s\n",
@@ -2334,11 +2328,11 @@ static int restore_dlg_db(void)
 			SET_ROUTE_VALUE(values+27, cell->rt_on_timeout);
 			SET_ROUTE_VALUE(values+28, cell->rt_on_hangup);
 
-			CON_SET_CURR_PS(dialog_db_handle, &my_ps_insert);
-			if (con_set_inslist(&dialog_dbf,dialog_db_handle,
-			&ins_list,insert_keys,DIALOG_TABLE_TOTAL_COL_NO) < 0 )
+			if (con_set_inslist(&dialog_dbf, dialog_db_handle,
+					&ins_list, insert_keys, DIALOG_TABLE_TOTAL_COL_NO) < 0) {
 				CON_RESET_INSLIST(dialog_db_handle);
-
+			}
+			CON_SET_CURR_PS(dialog_db_handle, &my_ps_insert);
 			if((dialog_dbf.insert(dialog_db_handle, insert_keys,
 			values, DIALOG_TABLE_TOTAL_COL_NO)) !=0){
 				LM_ERR("could not add another dialog to db - state=%d callid=%.*s\n",

modules/emergency/report_emergency.c
@@ -173,13 +173,13 @@ int report(struct emergency_report *report, str db_url, str table_report) {
 
 	LM_DBG("storing info...\n");
 
-	if (con_set_inslist(&db_funcs, db_con, &ins_list, db_keys, NR_KEYS) < 0)
+	if (con_set_inslist(&db_funcs, db_con, &ins_list, db_keys, NR_KEYS) < 0) {
 		CON_RESET_INSLIST(db_con);
+	}
 	CON_SET_CURR_PS(db_con, &emergency_ps);
-
 	if (db_funcs.insert(db_con, db_keys, db_vals, NR_KEYS) < 0) {
 		LM_ERR("failed to insert into database\n");
-		return -1;;
+		return -1;
 	}
 
 	return 1;

commit 4ef6bab4d95d9c23127cad776c126afb0ed77106

db_mysql: Fix regression in PS handling
Commit 87f241651 introduced a regression in db_mysql where modules using
prepared statements on top of it would run into errors.

Example behavior of auth_db on a basic user auth query:

    ERROR:db_mysql:db_mysql_store_result: driver error:
        Commands out of sync; you can't run this command now
    ERROR:auth_db:get_ha1: failed to query database

Credits to @masudmuborakshohi for the accurate report and logs!
Fixes #2593

modules/db_mysql/dbase.c
@@ -976,14 +976,15 @@ int db_mysql_query(const db_con_t* _h, const db_key_t* _k, const db_op_t* _op,
 		}
 
 		ret = db_mysql_do_prepared_query(_h, &query_holder, _v, _n, NULL, 0);
-		CON_RESET_CURR_PS(_h);
 		if (ret != 0) {
+			CON_RESET_CURR_PS(_h);
 			if (_r)
 				*_r = NULL;
 			return ret;
 		}
 
 		ret = db_mysql_store_result(_h, _r);
+		CON_RESET_CURR_PS(_h);
 		return ret;
 	}
 	return db_do_query(_h, _k, _op, _v, _c, _n, _nc, _o, _r,