OpenSIPS/opensips

commit 40355a3c13493ebbaf7ceed78f2a820c4c600d8a

Fix race between data sync and data load
Be sure we trigger the startup cluster sync AFTER loading the data from DB.
Also, when receiving replicated data, be sure the data is actually loaded.
Closes #2581

(cherry picked from commit 3b8bdb794da3828a4fe7639f460c36fffe03031a)

modules/drouting/dr_clustering.c
@@ -160,7 +160,7 @@ static int gw_status_update(bin_packet_t *packet, int raise_event)
 	bin_pop_int(packet, &flags);
 
 	part = get_partition( &part_name );
-	if (part==NULL)
+	if (part==NULL || part->rdata==NULL)
 		return -1;
 
 	lock_start_read(part->ref_lock);
@@ -197,7 +197,7 @@ static int cr_status_update(bin_packet_t *packet)
 	bin_pop_int(packet, &flags);
 
 	part = get_partition( &part_name );
-	if (part==NULL)
+	if (part==NULL || part->rdata==NULL)
 		return -1;
 
 	lock_start_read(part->ref_lock);
@@ -376,9 +376,6 @@ int dr_init_cluster(void)
 	} else {
 		dr_cluster_shtag.len = 0;
 	}
-
-	if (dr_cluster_sync() < 0)
-		return -1;
 
 	return 0;
 }

modules/drouting/drouting.c
@@ -2028,6 +2028,9 @@ static int db_connect_head(struct head_db *x) {
 static void rpc_dr_reload_data(int sender_id, void *unused)
 {
 	dr_reload_data(1);
+
+	dr_cluster_sync();
+
 }
 
 

commit 4206f768923dd95deb1b51ebcfda4905c334f2ce

drouting: Fix startup crash with no clustering
The crash was a regression, introduced in 40355a3c1.

Many thanks to @masudmuborakshohi and Sasmita Panda for the reports!

modules/drouting/dr_clustering.c
@@ -339,6 +339,9 @@ void receive_dr_cluster_event(enum clusterer_event ev, int node_id)
 
 int dr_cluster_sync(void)
 {
+	if (!dr_cluster_id)
+		return 0;
+
 	if (c_api.request_sync(&status_repl_cap, dr_cluster_id) < 0) {
 		LM_ERR("Sync request failed\n");
 		return -1;

modules/drouting/drouting.c
@@ -2030,7 +2030,6 @@ static void rpc_dr_reload_data(int sender_id, void *unused)
 	dr_reload_data(1);
 
 	dr_cluster_sync();
-
 }
 
 