cb0680ab829a76ec23c41d7f63b5dbb1b6afbd63
Liviu Chircu <liviu@opensips.org>
2021-09-27 21:14:07 +0300

    Fix crash with REGISTER + incomplete Authorization header
    Avoid re-using anonymous structures outside of the block scope they were
    declared in.  The compiler allows such broken code, yet it is also quick
    to re-use/re-claim that memory quickly after exiting the block, leading
    to stack corruption later down the road, when the "now re-used struct"
    is read.
    
    Issue discovered during OpenSIPS Security Audit 2021,
        by Alfred Farrugia & Sandro Gauci (Enable Security)
    
    (cherry picked from commit 0fadc0a6cb130d40fba6cf36bb1399d45d0496aa)

--- modules/auth/api.c
+++ modules/auth/api.c
@@ -155,7 +155,7 @@ auth_result_t pre_auth(struct sip_msg* _m, str* _realm, hdr_types_t _hftype,
 	int ret, ecode;
 	auth_body_t* c;
 	struct sip_uri *uri;
-	const str *emsg;
+	str emsg;
 
 	/* ACK and CANCEL must be always authorized, there is
 	 * no way how to challenge ACK and CANCEL cannot be
@@ -169,7 +169,7 @@ auth_result_t pre_auth(struct sip_msg* _m, str* _realm, hdr_types_t _hftype,
 	if (_realm->len == 0) {
 		if (get_realm(_m, _hftype, &uri) < 0) {
 			LM_ERR("failed to extract realm\n");
-			emsg = &str_init(MESSAGE_400);
+			init_str(&emsg, MESSAGE_400);
 			ecode = 400;
 			goto ereply;
 		}
@@ -186,10 +186,10 @@ auth_result_t pre_auth(struct sip_msg* _m, str* _realm, hdr_types_t _hftype,
 	if (ret < 0) {
 		LM_ERR("failed to find credentials\n");
 		if (ret == -2) {
-			emsg = &str_init(MESSAGE_500);
+			init_str(&emsg, MESSAGE_500);
 			ecode = 500;
 		} else {
-			emsg = &str_init(MESSAGE_400);
+			init_str(&emsg, MESSAGE_400);
 			ecode = 400;
 		}
 		goto ereply;
@@ -205,14 +205,14 @@ auth_result_t pre_auth(struct sip_msg* _m, str* _realm, hdr_types_t _hftype,
 	/* Check credentials correctness here */
 	if (check_dig_cred(dcp) != E_DIG_OK) {
 		LM_DBG("received credentials are not filled properly\n");
-		emsg = &str_init(MESSAGE_400);
+		init_str(&emsg, MESSAGE_400);
 		ecode = 400;
 		goto ereply;
 	}
 
 	if (mark_authorized_cred(_m, *_h) < 0) {
 		LM_ERR("failed to mark parsed credentials\n");
-		emsg = &str_init(MESSAGE_400);
+		init_str(&emsg, MESSAGE_400);
 		ecode = 500;
 		goto ereply;
 	}
@@ -253,7 +253,7 @@ auth_result_t pre_auth(struct sip_msg* _m, str* _realm, hdr_types_t _hftype,
 
 	return DO_AUTHORIZATION;
 ereply:
-	if (send_resp(_m, ecode, emsg, 0, 0) == -1) {
+	if (send_resp(_m, ecode, &emsg, 0, 0) == -1) {
 		LM_ERR("failed to send %d reply\n", ecode);
 	}
 	return ERROR;
