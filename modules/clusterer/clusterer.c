OpenSIPS/opensips

commit 14d1a72f433b941c41b9c28951738f11986846b6

clusterer: improve performance of sync packets processing
Dispatch through IPC the processing of received sync packets.

modules/clusterer/clusterer.c
@@ -1148,11 +1148,21 @@ exit:
 
 void run_mod_packet_cb(int sender, void *param)
 {
+	extern char *next_data_chunk;
 	struct packet_rpc_params *p = (struct packet_rpc_params *)param;
 	bin_packet_t packet;
+	str cap_name;
+	int data_version;
 
 	bin_init_buffer(&packet, p->pkt_buf.s, p->pkt_buf.len);
 	packet.src_id = p->pkt_src_id;
+
+	if (packet.type == SYNC_PACKET_TYPE) {
+		/* this packet is cloned and both below fields have been used */
+		bin_pop_str(&packet, &cap_name);
+		bin_pop_int(&packet, &data_version);
+		next_data_chunk = NULL;
+	}
 
 	p->cap->packet_cb(&packet);
 

modules/clusterer/sync.c
@@ -501,7 +501,8 @@ void handle_sync_packet(bin_packet_t *packet, int packet_type,
 		packet->src_id = source_id;
 		set_bin_pkg_version(packet, (short)data_version);
 
-		cap->reg.packet_cb(packet);
+		if (ipc_dispatch_mod_packet(packet, &cap->reg) < 0)
+			LM_ERR("Failed to dispatch handling of module packet\n");
 	} else { /* CLUSTERER_SYNC_END */
 		LM_INFO("Received all sync packets for capability '%.*s' in "
 		        "cluster %d\n", cap_name.len, cap_name.s, cluster->cluster_id);

commit 6d48698cfd92cdbd3f198a9f6cbc2afc77feb271

clusterer: fix type for sync packets passed to modules
This is a regression bug introduced in commit
14d1a72f433b941c41b9c28951738f11986846b6.

modules/clusterer/clusterer.c
@@ -1156,6 +1156,7 @@ void run_mod_packet_cb(int sender, void *param)
 
 	bin_init_buffer(&packet, p->pkt_buf.s, p->pkt_buf.len);
 	packet.src_id = p->pkt_src_id;
+	packet.type = p->pkt_type;
 
 	if (packet.type == SYNC_PACKET_TYPE) {
 		/* this packet is cloned and both below fields have been used */
@@ -1185,6 +1186,7 @@ int ipc_dispatch_mod_packet(bin_packet_t *packet, struct capability_reg *cap)
 	params->pkt_buf.len = packet->buffer.len;
 	params->cap = cap;
 	params->pkt_src_id = packet->src_id;
+	params->pkt_type = packet->type;
 
 	if (ipc_dispatch_rpc(run_mod_packet_cb, params) < 0) {
 		LM_ERR("Failed to dispatch rpc\n");

modules/clusterer/clusterer.h
@@ -114,6 +114,7 @@ struct remote_cap {
 struct packet_rpc_params {
 	struct capability_reg *cap;
 	int pkt_src_id;
+	int pkt_type;
 	str pkt_buf;
 };
 