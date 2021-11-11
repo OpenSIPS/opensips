c6ab3bb406c447e30c7d33a1a8970048b4612100
Bogdan-Andrei Iancu <bogdan@opensips.org>
2021-11-11 17:42:42 +0200

    [sipmsgops] fix codec_delete_XX() parsing (2)
    Issue discovered during OpenSIPS Security Audit 2021,
    	by Alfred Farrugia & Sandro Gauci (Enable Security)
    
    https://github.com/OpenSIPS/opensips/security/advisories/GHSA-67w7-g4j8-3wcx

--- modules/sipmsgops/codecs.c
+++ modules/sipmsgops/codecs.c
@@ -366,7 +366,8 @@ int delete_sdp_line( struct sip_msg * msg, char * s, struct sdp_stream_cell *str
 
 	while(*end != '\n' && end < (stream->body.s+stream->body.len) )
 		end++;
-	end++;
+	if ( *end == '\n')
+		end++;
 
 	/* delete the entry */
 	if( del_lump(msg, start - msg->buf, end - start,0) == NULL )
