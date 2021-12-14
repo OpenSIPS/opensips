OpenSIPS/opensips

commit ecbd9856efb7a420e2fceff6769951d307be8c03

nathelper: Fix rare message corruption due to bitwise opts on strings
Credits to Damien Sandras for the report and initial PR!

Closes #2695

socket_info.h
@@ -432,11 +432,52 @@ static inline char* proto2str(int proto, char *p)
 	return p;
 }
 
+static inline char* proto2upper(int proto, char *p)
+{
+	switch (proto) {
+	case PROTO_UDP:
+		memcpy(p, STR_L("UDP"));
+		break;
+	case PROTO_TCP:
+		memcpy(p, STR_L("TCP"));
+		break;
+	case PROTO_TLS:
+		memcpy(p, STR_L("TLS"));
+		break;
+	case PROTO_SCTP:
+		memcpy(p, STR_L("SCTP"));
+		break;
+	case PROTO_WS:
+		memcpy(p, STR_L("WS"));
+		break;
+	case PROTO_WSS:
+		memcpy(p, STR_L("WSS"));
+		break;
+	case PROTO_BIN:
+		memcpy(p, STR_L("BIN"));
+		break;
+	case PROTO_BINS:
+		memcpy(p, STR_L("BINS"));
+		break;
+	case PROTO_HEP_UDP:
+		memcpy(p, STR_L("HEP_UDP"));
+		break;
+	case PROTO_HEP_TCP:
+		memcpy(p, STR_L("HEP_TCP"));
+		break;
+	case PROTO_SMPP:
+		memcpy(p, STR_L("SMPP"));
+		break;
+	default:
+		LM_CRIT("unsupported proto %d\n", proto);
+	}
+
+	return p;
+}
 
 static inline char *proto2a(int proto)
 {
-	static char b[8]; /* IMPORTANT - keep this max aligned with the proto2str
-	                   * with an extra +1 for NULL terminator */
+	static char b[PROTO_NAME_MAX_SIZE+1];
 	char *p;
 
 	/* print the proto name */

modules/nathelper/sip_pinger.h
@@ -351,7 +351,7 @@ build_sipping(udomain_t *d, str *curi, struct socket_info* s,str *path,
 {
 #define s_len(_s) (sizeof(_s)-1)
 	static char buf[MAX_SIPPING_SIZE];
-	char *p, proto_str[PROTO_NAME_MAX_SIZE];
+	char *p, proto_str[PROTO_NAME_MAX_SIZE+1];
 	str *address, *port;
 	str st;
 	int len;
@@ -370,7 +370,7 @@ build_sipping(udomain_t *d, str *curi, struct socket_info* s,str *path,
 	sbranch.s = branch;
 	sbranch.len = strlen(branch);
 
-	p = proto2str(s->proto, proto_str);
+	p = proto2upper(s->proto, proto_str);
 	*(p++) = ' ';
 	st.s = proto_str;
 	st.len = p - proto_str;
@@ -387,9 +387,6 @@ build_sipping(udomain_t *d, str *curi, struct socket_info* s,str *path,
 		port = default_global_port;
 	else
 		port = &s->port_no_str;
-
-	/* quick proto uppercase */
-	*((int *)st.s) &= ~((1 << 21) | (1 << 13) | (1 << 5));
 
 	if ( sipping_method.len + 1 + curi->len + s_len(" SIP/2.0"CRLF) +
 		s_len("Via: SIP/2.0/") + st.len + address->len +

commit 54bb91a373a2457ad5c5ccc22ab70bd4c83c01d8

nathelper: Fix bug introduced in ecbd9856efb
Credits to Alexey Vasilyev (@vasilevalex) for reviewing the commit and
catching the regression!

socket_info.h
@@ -432,41 +432,43 @@ static inline char* proto2str(int proto, char *p)
 	return p;
 }
 
+/* Similar to proto2str(), but proto is written in uppercase. The
+   new resulting pointer will be returned (where writing ended) */
 static inline char* proto2upper(int proto, char *p)
 {
 	switch (proto) {
 	case PROTO_UDP:
-		memcpy(p, STR_L("UDP"));
+		p = memcpy(p, STR_L("UDP")) + sizeof("UDP")-1;
 		break;
 	case PROTO_TCP:
-		memcpy(p, STR_L("TCP"));
+		p = memcpy(p, STR_L("TCP")) + sizeof("TCP")-1;
 		break;
 	case PROTO_TLS:
-		memcpy(p, STR_L("TLS"));
+		p = memcpy(p, STR_L("TLS")) + sizeof("TLS")-1;
 		break;
 	case PROTO_SCTP:
-		memcpy(p, STR_L("SCTP"));
+		p = memcpy(p, STR_L("SCTP")) + sizeof("SCTP")-1;
 		break;
 	case PROTO_WS:
-		memcpy(p, STR_L("WS"));
+		p = memcpy(p, STR_L("WS")) + sizeof("WS")-1;
 		break;
 	case PROTO_WSS:
-		memcpy(p, STR_L("WSS"));
+		p = memcpy(p, STR_L("WSS")) + sizeof("WSS")-1;
 		break;
 	case PROTO_BIN:
-		memcpy(p, STR_L("BIN"));
+		p = memcpy(p, STR_L("BIN")) + sizeof("BIN")-1;
 		break;
 	case PROTO_BINS:
-		memcpy(p, STR_L("BINS"));
+		p = memcpy(p, STR_L("BINS")) + sizeof("BINS")-1;
 		break;
 	case PROTO_HEP_UDP:
-		memcpy(p, STR_L("HEP_UDP"));
+		p = memcpy(p, STR_L("HEP_UDP")) + sizeof("HEP_UDP")-1;
 		break;
 	case PROTO_HEP_TCP:
-		memcpy(p, STR_L("HEP_TCP"));
+		p = memcpy(p, STR_L("HEP_TCP")) + sizeof("HEP_TCP")-1;
 		break;
 	case PROTO_SMPP:
-		memcpy(p, STR_L("SMPP"));
+		p = memcpy(p, STR_L("SMPP")) + sizeof("SMPP")-1;
 		break;
 	default:
 		LM_CRIT("unsupported proto %d\n", proto);