122c1e31c3b5d4c8a7d3a9c58cd121b8804669fe
Vlad Patrascu <vladp@opensips.org>
2022-01-28 17:58:49 +0200

    dialog: fix crash when dropping unsynced dialogs
    (cherry picked from commit da47168d02a9359de5f95ab83f6f040bbb7c9db9)

--- modules/dialog/dlg_replication.c
+++ modules/dialog/dlg_replication.c
@@ -1114,13 +1114,15 @@ void rcv_cluster_event(enum clusterer_event ev, int node_id)
 					/* successfully removed from timer list */
 					unref++;
 
-				if (dlg_db_mode != DB_MODE_SHUTDOWN) {
+				if (dlg_db_mode != DB_MODE_SHUTDOWN &&
+					!(dlg->flags&DLG_FLAG_DB_DELETED)) {
 					dlg->flags &= ~DLG_FLAG_NEW;
 					remove_dialog_from_db(dlg);
 					dlg->flags |= DLG_FLAG_DB_DELETED;
 				}
 
-				if (dlg_db_mode == DB_MODE_DELAYED)
+				if (dlg_db_mode == DB_MODE_DELAYED &&
+					!(dlg->flags&DLG_FLAG_DB_DELETED))
 					unref++;
 
 				if (old_state != DLG_STATE_DELETED)
