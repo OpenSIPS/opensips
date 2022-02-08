5df84eb90475f6273d2be4ff4cbbf8f26de152e5
Bogdan-Andrei Iancu <bogdan@opensips.org>
2022-02-08 18:45:08 +0200

    [drouting] fix type in header guard
    Thank you CI :)

--- modules/drouting/status_report.h
+++ modules/drouting/status_report.h
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,USA
  */
 
-#ifndef _DROUTONG_STATUS_REPORT_H_
+#ifndef _DROUTING_STATUS_REPORT_H_
 #define _DROUTING_STATUS_REPORT_H_
 
 #include "../../status_report.h"
