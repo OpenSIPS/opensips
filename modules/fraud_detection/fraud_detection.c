977b921a7f66c6c56fb9c438b6bc29d9034391be
Liviu Chircu <liviu@opensips.org>
2022-02-10 12:44:33 +0200

    fraud_detection: Fix the 'call duration' events
    The 'call duration' events were not being raised anymore since commit
    a0fcf857ddb, due to the mismatching callback type check in
    dialog_terminate_CB().
    
    Also rework commit a0fcf857ddb, where instead of switching the
    decrement operation on the DLGCB_DESTROY callback (to guarantee it only
    gets called one time), we just store and use a boolean marker, thus
    achieving the same effect but while using the optimal callbacks!
    
    (cherry picked from commit 7aa272aec749702627404c0d518b4b4fada5ec93)

--- modules/fraud_detection/fraud_detection.c
+++ modules/fraud_detection/fraud_detection.c
@@ -457,7 +457,7 @@ static int check_fraud(struct sip_msg *msg, char *_user, char *_number, char *_p
 		param->calldur_crit = thr->call_duration_thr.critical;
 		param->interval_id = se->interval_id;
 
-		if (dlgb.register_dlgcb(dlgc, DLGCB_DESTROY,
+		if (dlgb.register_dlgcb(dlgc, DLGCB_FAILED|DLGCB_TERMINATED|DLGCB_EXPIRED,
 					dialog_terminate_CB, param, free_dialog_CB_param) != 0) {
 			LM_ERR("failed to register dialog terminated callback\n");
 			shm_free(param->number.s);
