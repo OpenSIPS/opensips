OpenSIPS/opensips

commit b0591d285ad966c2cb13672de8071c03fcba7414

Fix bad body re-assembling when handling a multi part with empty parts
Credits for the finding and for the fix go to John Burke ( @john08burke )
Closes #2794

msg_translator.c
@@ -1262,7 +1262,7 @@ unsigned int prep_reassemble_body_parts( struct sip_msg* msg,
 	char *hdr, *it;
 
 	/* set the offset (in the original buffer) at the beginning of the body */
-	orig_offs = msg->body->part_count ? msg->body->body.s-msg->buf : msg->len ;
+	orig_offs = msg->body ? msg->body->body.s-msg->buf : msg->len ;
 
 	if (msg->body->updated_part_count==0) {
 
@@ -1914,9 +1914,7 @@ static inline void apply_msg_changes(struct sip_msg *msg,
 		*new_offs += max_offset-*orig_offs;
 	} else {
 		/* copy whatever is left in the original buffer (up to the body) */
-		size = (msg->body->part_count) ?
-			  ((msg->body->body.s - msg->buf) - *orig_offs) /* msg had body */
-			: (msg->len - *orig_offs);                      /* no body */
+		size = (msg->body->body.s - msg->buf) - *orig_offs;
 		memcpy(new_buf+*new_offs, msg->buf+*orig_offs, size );
 		*new_offs += size;
 		*orig_offs += size;
@@ -2423,7 +2421,7 @@ char * build_res_buf_from_sip_res( struct sip_msg* msg,
 		new_buf[(int)(msg->first_line.u.reply.status.s-msg->buf)+2] = '0';
 	/* send it! */
 	LM_DBG("copied size: orig:%d, new: %d, rest: %d"
-			" msg=\n%s\n", s_offset, offset, len-s_offset, new_buf);
+			" msg=|\n%s|\n", s_offset, offset, len-s_offset, new_buf);
 
 	*returned_len=new_len;
 	return new_buf;

commit a05893a631193a37548cfae292ed7f00f63044e3

Fix body re-assembling when adding a new body part to an emptry received body
This fixes a regression introduced in b0591d285ad966c2cb13672de8071c03fcba7414
Fixes #2823
Credits go to @pergolafabio for the detailed reporting

msg_translator.c
@@ -1262,7 +1262,8 @@ static unsigned int prep_reassemble_body_parts( struct sip_msg* msg,
 	char *hdr, *it;
 
 	/* set the offset (in the original buffer) at the beginning of the body */
-	orig_offs = msg->body ? msg->body->body.s-msg->buf : msg->len ;
+	orig_offs = (msg->body && msg->body->body.s) ?
+		msg->body->body.s-msg->buf : msg->len ;
 
 	if (msg->body->updated_part_count==0) {
 
@@ -1914,7 +1915,9 @@ static inline void apply_msg_changes(struct sip_msg *msg,
 		*new_offs += max_offset-*orig_offs;
 	} else {
 		/* copy whatever is left in the original buffer (up to the body) */
-		size = (msg->body->body.s - msg->buf) - *orig_offs;
+		size = msg->body->body.s ?
+			((msg->body->body.s - msg->buf) - *orig_offs)  /* msg had body */
+			: (msg->len - *orig_offs);                     /* no body at all */
 		memcpy(new_buf+*new_offs, msg->buf+*orig_offs, size );
 		*new_offs += size;
 		*orig_offs += size;