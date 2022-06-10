OpenSIPS/opensips

commit 27b0b21b2f2aa7b29fdaf0b7bc35274979fc55b0

Fixed ACKs CSEQ number when doing in-dialog pinging
The ACK CSEQ number does not depend on the number of pings or other in-dialog requests since the INVITE transaction, it must be the same as the INVITE
Closes issue #680

(cherry picked from commit 26a0a62621f6d1e4ea9fee90684a143fbfdc8494)

modules/dialog/dlg_handlers.c
@@ -1440,15 +1440,28 @@ after_unlock5:
 				if (dlg->legs[dst_leg].last_gen_cseq) {
 
 					update_val = ++(dlg->legs[dst_leg].last_gen_cseq);
+					if (req->first_line.u.request.method_value == METHOD_INVITE) {
+						/* save INVITE cseq, in case any requests follow after this
+						( pings or other in-dialog requests until the ACK comes in */
+						dlg->legs[dst_leg].last_inv_gen_cseq = dlg->legs[dst_leg].last_gen_cseq;
+					}
+
 					dlg_unlock( d_table, d_entry );
 
 					if (update_msg_cseq(req,0,update_val) != 0) {
 						LM_ERR("failed to update sequential request msg cseq\n");
 						ok = 0;
 					}
-				}
-				else
+				} else {
+					if (req->first_line.u.request.method_value == METHOD_INVITE) {
+						/* we did not generate any pings yet - still we need to store the INV cseq,
+						in case there's a race between the ACK for the INVITE and sending of new pings */
+						str2int(&((struct cseq_body *)req->cseq->parsed)->number,
+						&dlg->legs[dst_leg].last_inv_gen_cseq);
+					}
+
 					dlg_unlock( d_table, d_entry );
+				}
 			}
 
 			if (ok) {
@@ -1459,9 +1472,7 @@ after_unlock5:
 				if (dialog_replicate_cluster)
 					replicate_dialog_updated(dlg);
 			}
-		}
-		else
-		{
+		} else {
 			if (dlg->flags & DLG_FLAG_PING_CALLER ||
 			dlg->flags & DLG_FLAG_PING_CALLEE ||
 			dlg->flags & DLG_FLAG_REINVITE_PING_CALLER ||
@@ -1470,15 +1481,18 @@ after_unlock5:
 
 				dlg_lock (d_table, d_entry);
 
-				if (dlg->legs[dst_leg].last_gen_cseq) {
-					update_val = dlg->legs[dst_leg].last_gen_cseq;
+				if (dlg->legs[dst_leg].last_gen_cseq || 
+				dlg->legs[dst_leg].last_inv_gen_cseq ) {
+					if (dlg->legs[dst_leg].last_inv_gen_cseq)
+						update_val = dlg->legs[dst_leg].last_inv_gen_cseq;
+					else
+						update_val = dlg->legs[dst_leg].last_gen_cseq;
 					dlg_unlock( d_table, d_entry );
 
 					if (update_msg_cseq(req,0,update_val) != 0) {
 						LM_ERR("failed to update ACK msg cseq\n");
 					}
-				}
-				else
+				} else
 					dlg_unlock( d_table, d_entry );
 			}
 		}

modules/dialog/dlg_hash.h
@@ -98,6 +98,7 @@ struct dlg_leg {
 	str sdp;	/* latest SDP provided by this leg ( full body ) */
 	int nr_uris;
 	unsigned int last_gen_cseq; /* FIXME - think this can be atomic_t to avoid locking */
+	unsigned int last_inv_gen_cseq; /* used when translating ACKs */
 	char reply_received;
 	char reinvite_confirmed;
 	struct socket_info *bind_addr;

commit 1d08f91d4c4dab23cbb7d8c5d98d2d88166b5874

Fix in-dialog ACK being relayed with a wrong CSEQ
There are cases where opensips can handle a reINVITE before the ACK that
acknowledges the previous INVITE/200 OK exchange.

In that case, the ACK would be relayed with the reINVITE CSEQ, leading
to interoperability issues.

We now never use the generated invite cseq for ACK requests. This change
also works with reINVITE pings.

It seems the problem was introduced in
27b0b21b2f2aa7b29fdaf0b7bc35274979fc55b0

Closes https://github.com/OpenSIPS/opensips/issues/1071

modules/dialog/dlg_handlers.c
@@ -2129,12 +2129,8 @@ after_unlock5:
 
 			dlg_lock (d_table, d_entry);
 
-			if (dlg->legs[dst_leg].last_gen_cseq ||
-			dlg->legs[dst_leg].last_inv_gen_cseq ) {
-				if (dlg->legs[dst_leg].last_inv_gen_cseq)
-					update_val = dlg->legs[dst_leg].last_inv_gen_cseq;
-				else
-					update_val = dlg->legs[dst_leg].last_gen_cseq;
+			if (dlg->legs[dst_leg].last_gen_cseq) {
+				update_val = dlg->legs[dst_leg].last_gen_cseq;
 				dlg_unlock( d_table, d_entry );
 
 				if (update_msg_cseq(req,0,update_val) != 0) {