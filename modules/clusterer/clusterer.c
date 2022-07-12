0d346f0b6927ceb8895ffd7a4f52f28e685c12f7
Vlad Patrascu <vladp@opensips.org>
2022-07-12 22:45:31 +0300

    clusterer: prevent permanent sync in progress state
    If the sync end marker packet is not received, nodes get stuck in a
    permanent "sync in progress" state. This commit introduces a timeout,
    configurable via the "sync_timeout" modparam, after which a node reverts
    to the not synced state, if no sync data packet is received.
    
    (cherry picked from commit 2e5bf28aa91d9cda8304bc19bbba770e83d7cb22)

--- modules/clusterer/clusterer.c
+++ modules/clusterer/clusterer.c
@@ -54,8 +54,9 @@ extern int ping_interval;
 extern int node_timeout;
 extern int ping_timeout;
 extern int seed_fb_interval;
+extern int sync_timeout;
 
-void seed_fb_check_timer(utime_t ticks, void *param)
+void sync_check_timer(utime_t ticks, void *param)
 {
 	cluster_info_t *cl;
 	struct local_cap *cap;
@@ -75,16 +76,27 @@ void seed_fb_check_timer(utime_t ticks, void *param)
 
 		for (cap = cl->capabilities; cap; cap = cap->next) {
 			lock_get(cl->lock);
+
 			if ((cap->flags & CAP_STATE_ENABLED) &&
-				!(cap->flags & CAP_STATE_OK) &&
-				!(cap->flags & CAP_SYNC_IN_PROGRESS) &&
-				(cl->current_node->flags & NODE_IS_SEED) &&
-				(TIME_DIFF(cap->sync_req_time, now) >= seed_fb_interval*1000000)) {
-				cap->flags |= CAP_STATE_OK;
-				cap->flags &= ~CAP_SYNC_PENDING;
-				LM_INFO("No donor found, falling back to synced state\n");
-				/* send update about the state of this capability */
-				send_single_cap_update(cl, cap, 1);
+				!(cap->flags & CAP_STATE_OK)) {
+				if ((cap->flags & CAP_SYNC_PENDING) &&
+					(cl->current_node->flags & NODE_IS_SEED) &&
+					(TIME_DIFF(cap->sync_req_time, now) >=
+					seed_fb_interval*1000000)) {
+
+					cap->flags |= CAP_STATE_OK;
+					cap->flags &= ~CAP_SYNC_PENDING;
+					LM_INFO("No donor found, falling back to synced state\n");
+					/* send update about the state of this capability */
+					send_single_cap_update(cl, cap, 1);
+
+				} else if ((cap->flags & CAP_SYNC_IN_PROGRESS) &&
+					(get_ticks() - cap->last_sync_pkt >= sync_timeout)) {
+
+					handle_sync_end(cl, cap, 0, 1);
+					LM_INFO("Sync timeout for capability [%.*s], reverting to "
+						"not synced state\n", cap->reg.name.len, cap->reg.name.s);
+				}
 			}
 
 			lock_release(cl->lock);
--- modules/clusterer/sync.h
+++ modules/clusterer/sync.h
@@ -46,6 +46,8 @@ void handle_sync_packet(bin_packet_t *packet, int packet_type,
 int buffer_bin_pkt(bin_packet_t *packet, struct local_cap *cap, int src_id);
 int send_sync_req(str *capability, int cluster_id, int source_id);
 int ipc_dispatch_sync_reply(cluster_info_t *cluster, int node_id, str *cap_name);
+void handle_sync_end(cluster_info_t *cluster, struct local_cap *cap,
+	int source_id, int is_timeout);
 
 #endif  /* CLUSTERER_SYNC_H */
 
