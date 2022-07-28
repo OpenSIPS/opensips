db99f9a6460684c2e401fc13ab49b7f4d6965f4b
Vlad Patrascu <vladp@opensips.org>
2022-07-28 20:25:51 +0300

    clusterer: fix deadlock when broadcasting shtag state

--- modules/clusterer/sharing_tags.c
+++ modules/clusterer/sharing_tags.c
@@ -359,7 +359,8 @@ static int shtag_send_active_info(int c_id, str *tag_name, int node_id)
 	if (bin_push_str(&packet, tag_name) < 0)
 		return CLUSTERER_SEND_ERR;
 
-	if (cl_send_to(&packet, c_id, node_id) != CLUSTERER_SEND_SUCCESS) {
+	if (clusterer_send_msg(&packet, c_id, node_id, 1) !=
+		CLUSTERER_SEND_SUCCESS) {
 		bin_free_packet(&packet);
 		return CLUSTERER_SEND_ERR;
 	}
