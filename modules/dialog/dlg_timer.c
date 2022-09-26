7eb2c1c21069942d06e7e7674548ef2372563fa4
Peter Lemenkov <lemenkov@gmail.com>
2022-09-26 21:07:41 +0200

    dialog: try to get a options/reinvite last direction
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- modules/dialog/dlg_timer.c
+++ modules/dialog/dlg_timer.c
@@ -840,7 +840,17 @@ void dlg_options_routine(unsigned int ticks , void * attr)
 		shm_free(it);
 		it = curr;
 
-		init_dlg_term_reason(dlg,"Ping Timeout",sizeof("Ping Timeout")-1);
+		if (dlg->legs[DLG_CALLER_LEG].reply_received == DLG_PING_FAIL)
+			init_dlg_term_reason(dlg,"Downstream Ping Timeout",sizeof("Downstream Ping Timeout")-1);
+		else if (dlg->legs[callee_idx(dlg)].reply_received == DLG_PING_FAIL)
+			init_dlg_term_reason(dlg,"Upstream Ping Timeout",sizeof("Upstream Ping Timeout")-1);
+		else{
+			LM_WARN("Ping Timeout: flags[%u] caller rr[%u] callee rr[%u]\n",
+					dlg->flags,
+					dlg->legs[DLG_CALLER_LEG].reply_received,
+					dlg->legs[callee_idx(dlg)].reply_received);
+			init_dlg_term_reason(dlg,"Ping Timeout",sizeof("Ping Timeout")-1);
+		}
 		/* FIXME - maybe better not to send BYE both ways as we know for
 		 * sure one end in down . */
 		dlg_end_dlg(dlg,0,1);
@@ -937,7 +947,17 @@ void dlg_reinvite_routine(unsigned int ticks , void * attr)
 		shm_free(it);
 		it = curr;
 
-		init_dlg_term_reason(dlg,"ReINVITE Ping Timeout",sizeof("ReINVITE Ping Timeout")-1);
+		if (dlg->legs[DLG_CALLER_LEG].reinvite_confirmed == DLG_PING_FAIL)
+			init_dlg_term_reason(dlg,"Downstream ReINVITE Ping Timeout",sizeof("Downstream ReINVITE Ping Timeout")-1);
+		else if (dlg->legs[callee_idx(dlg)].reinvite_confirmed == DLG_PING_FAIL)
+			init_dlg_term_reason(dlg,"Upstream ReINVITE Ping Timeout",sizeof("Upstream ReINVITE Ping Timeout")-1);
+		else{
+			LM_WARN("Ping Timeout: flags[%u] caller rc[%u] callee rc[%u]\n",
+					dlg->flags,
+					dlg->legs[DLG_CALLER_LEG].reinvite_confirmed,
+					dlg->legs[callee_idx(dlg)].reinvite_confirmed);
+			init_dlg_term_reason(dlg,"ReINVITE Ping Timeout",sizeof("ReINVITE Ping Timeout")-1);
+		}
 		/* FIXME - maybe better not to send BYE both ways as we know for
 		 * sure one end in down . */
 		dlg_end_dlg(dlg,0,1);
