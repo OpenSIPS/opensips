OpenSIPS/opensips

commit e94ca5fe070ff919552c48869f543ce4714e4ff2

tls_mgm: proper selection of TLS domain per branch
Use $bavp variable to set the TLS domain that should be used per each
branch. Fixes #2816

modules/tls_mgm/tls_domain.c
@@ -433,50 +433,63 @@ struct tls_domain *tls_find_client_domain_name(str *name)
 	return d;
 }
 
+static str *tls_find_domain_avp(int domain_avp)
+{
+	struct usr_avp **backup_list, **bavp_list;
+	struct usr_avp *avp = NULL;
+	int_str val;
+	static str ret;
+
+	/* we first check if there is an existing bavp */
+	bavp_list = get_bavp_list();
+	if (bavp_list) {
+		backup_list = set_avp_list(bavp_list);
+		avp = search_first_avp(0, domain_avp, &val, 0);
+		set_avp_list(backup_list);
+	}
+	if (!avp)
+		avp = search_first_avp(0, domain_avp, &val, 0);
+	if (avp) {
+		ret = val.s;
+		return &ret;
+	}
+	return NULL;
+}
+
 /*
  * find TLS client domain
  * return NULL if virtual domain not found
  */
 struct tls_domain *tls_find_client_domain(struct ip_addr *ip, unsigned short port)
 {
+	str *domain = NULL;
 	struct tls_domain *dom = NULL;
-	struct usr_avp *tls_dom_avp = NULL, *sip_dom_avp = NULL;
-	int_str val;
 	str match_any_dom = str_init("*");
 	str *sip_domain = &match_any_dom;
 
-	if (tls_client_domain_avp > 0) {
-		tls_dom_avp = search_first_avp(0, tls_client_domain_avp, &val, 0);
-		if (!tls_dom_avp) {
-			if (sip_client_domain_avp > 0) {
-				sip_dom_avp = search_first_avp(0, sip_client_domain_avp, &val, 0);
-				if (sip_dom_avp) {
-					sip_domain = &val.s;
-					LM_DBG("Match TLS domain by sip domain AVP: '%.*s'\n",
-						val.s.len, ZSW(val.s.s));
-				}
-			}
-		} else
-			sip_domain = NULL;  /* search by tls domain name */
-	} else {
+	if (tls_client_domain_avp > 0)
+		domain = tls_find_domain_avp(tls_client_domain_avp);
+	if (!domain) {
 		if (sip_client_domain_avp > 0) {
-			sip_dom_avp = search_first_avp(0, sip_client_domain_avp, &val, 0);
-			if (sip_dom_avp) {
-				sip_domain = &val.s;
+			sip_domain = tls_find_domain_avp(sip_client_domain_avp);
+			if (sip_domain) {
 				LM_DBG("Match TLS domain by sip domain AVP: '%.*s'\n",
-					val.s.len, ZSW(val.s.s));
+					sip_domain->len, ZSW(sip_domain->s));
 			}
 		}
+	} else {
+		LM_DBG("Match TLS domain by tls domain AVP: '%.*s'\n",
+				domain->len, domain->s);
 	}
 
-	if (!sip_domain)
-		dom = tls_find_client_domain_name(&val.s);
+	if (domain)
+		dom = tls_find_client_domain_name(domain);
 	else
 		dom = tls_find_domain_by_filters(ip, port, sip_domain, DOM_FLAG_CLI);
 
 	if (dom)
-			LM_DBG("found TLS client domain: %.*s\n",
-				dom->name.len, dom->name.s);
+		LM_DBG("found TLS client domain: %.*s\n",
+			dom->name.len, dom->name.s);
 
 	return dom;
 }

modules/tls_mgm/doc/tls_mgm_admin.xml
@@ -607,6 +607,12 @@ modparam("tls_mgm", "require_cert", "[dom]0")
 			Note: If there is already an existing TLS connection to the remote target,
 			it will be reused and setting this AVP has no effect.
 			</para>
+			<para>
+			Note: You can force a particular domain to be used just for a particular
+			branch by setting the <emphasis>$bavp</emphasis> variable with the same
+			name. When both <emphasis>$bavp</emphasis> and <emphasis>$avp</emphasis>
+			variables are set, the first one takes precedence.
+			</para>
 			<para><emphasis>
 				No default value.
 			</emphasis></para>
@@ -629,6 +635,12 @@ modparam("tls_mgm", "client_tls_domain_avp", "tls_match_dom")
 			<para>
 			Note: If there is already an existing TLS connection to the remote target,
 			it will be reused and setting this AVP has no effect.
+			</para>
+			<para>
+			Note: You can force a particular SIP domain to be used just for a particular
+			branch by setting the <emphasis>$bavp</emphasis> variable with the same
+			name. When both <emphasis>$bavp</emphasis> and <emphasis>$avp</emphasis>
+			variables are set, the first one takes precedence.
 			</para>
 			<para>
 			For the AVP usage example, refer to  <xref linkend="domains-param"/>.

modules/tm/t_cancel.c
@@ -125,6 +125,7 @@ void cancel_branch( struct cell *t, int branch )
 	if (t->uac[branch].br_flags & tcp_no_new_conn_bflag)
 		tcp_no_new_conn = 1;
 	backup_list = set_avp_list( &t->user_avps );
+	set_bavp_list(&t->uac[branch].user_avps);
 	if (SEND_BUFFER( crb )==0) {
 		if ( has_tran_tmcbs( t, TMCB_MSG_SENT_OUT) ) {
 			set_extra_tmcb_params( &crb->buffer, &crb->dst);
@@ -134,6 +135,7 @@ void cancel_branch( struct cell *t, int branch )
 	}
 	set_avp_list(backup_list);
 	tcp_no_new_conn = 0;
+	reset_bavp_list();
 
 	/*sets and starts the FINAL RESPONSE timer */
 	start_retr( crb );

modules/tm/t_fwd.c
@@ -831,14 +831,17 @@ int t_forward_nonack( struct cell *t, struct sip_msg* p_msg ,
 					LM_DBG("blocked by blacklists\n");
 					ser_error=E_IP_BLOCKED;
 				} else {
+					set_bavp_list(&t->uac[i].user_avps);
 					set_extra_tmcb_params( &t->uac[i].request.buffer,
 							&t->uac[i].request.dst);
 					run_trans_callbacks(TMCB_PRE_SEND_BUFFER, t, p_msg, 0, i);
 
 					if (SEND_BUFFER( &t->uac[i].request)==0) {
+						reset_bavp_list();
 						ser_error = 0;
 						break;
 					}
+					reset_bavp_list();
 
 					LM_ERR("sending request failed\n");
 					ser_error=E_SEND;

modules/tm/t_reply.c
@@ -311,6 +311,7 @@ static int send_ack(struct sip_msg* rpl, struct cell *trans, int branch)
 	if (trans->uac[branch].br_flags & tcp_no_new_conn_bflag)
 		tcp_no_new_conn = 1;
 
+	set_bavp_list(&trans->uac[branch].user_avps);
 	backup_list = set_avp_list( &trans->user_avps );
 	if(SEND_PR_BUFFER(&trans->uac[branch].request, ack_buf.s, ack_buf.len)==0){
 		/* successfully sent out */
@@ -321,6 +322,7 @@ static int send_ack(struct sip_msg* rpl, struct cell *trans, int branch)
 		}
 	}
 	set_avp_list(backup_list);
+	reset_bavp_list();
 
 	tcp_no_new_conn = 0;
 

modules/tm/tm.c
@@ -155,7 +155,7 @@ int pv_get_tm_fr_timeout(struct sip_msg*, pv_param_t *, pv_value_t*);
 int pv_set_tm_fr_timeout(struct sip_msg*, pv_param_t *, int, pv_value_t*);
 int pv_get_tm_fr_inv_timeout(struct sip_msg*, pv_param_t *, pv_value_t*);
 int pv_set_tm_fr_inv_timeout(struct sip_msg*, pv_param_t *, int, pv_value_t*);
-struct usr_avp** get_bavp_list(void);
+static struct usr_avp** pv_get_bavp_list(void);
 
 
 /* module parameteres */
@@ -1757,7 +1757,7 @@ int pv_get_tm_branch_avp(struct sip_msg *msg, pv_param_t *param,
 	if (!msg || !val)
 		goto error;
 
-	avp_list = get_bavp_list();
+	avp_list = pv_get_bavp_list();
 	if (!avp_list) {
 		pv_get_null(msg, param, val);
 		goto success;
@@ -1902,7 +1902,7 @@ int pv_set_tm_branch_avp(struct sip_msg *msg, pv_param_t *param, int op,
 		goto error;
 	}
 
-	avp_list = get_bavp_list();
+	avp_list = pv_get_bavp_list();
 	if (!avp_list) {
 		LM_DBG("cannot find the branch avp list!\n");
 		return -2;
@@ -1973,7 +1973,7 @@ success:
 }
 
 
-struct usr_avp** get_bavp_list(void)
+static struct usr_avp** pv_get_bavp_list(void)
 {
 	struct cell* t;
 
@@ -2120,7 +2120,4 @@ static int pv_get_t_id(struct sip_msg *msg, pv_param_t *param,
 	res->rs.len = p-buf;
 
 	return 0;
-
 }
-
-

modules/tm/uac.c
@@ -540,11 +540,13 @@ int t_uac(str* method, str* headers, str* body, dlg_t* dialog,
 	if (new_cell->uac[0].br_flags & tcp_no_new_conn_bflag)
 		tcp_no_new_conn = 1;
 
+	set_bavp_list(&new_cell->uac[0].user_avps);
 	if (SEND_BUFFER(request) == -1) {
 		LM_ERR("attempt to send to '%.*s' failed\n",
 			dialog->hooks.next_hop->len,
 			dialog->hooks.next_hop->s);
 	}
+	reset_bavp_list();
 
 	tcp_no_new_conn = 0;
 

usr_avp.c
@@ -46,6 +46,7 @@
 static gen_lock_t *extra_lock;
 static struct usr_avp *global_avps = 0;
 static struct usr_avp **crt_avps  = &global_avps;
+static struct usr_avp **crt_bavps;
 
 static map_t avp_map = 0;
 static map_t avp_map_shm = 0;
@@ -614,3 +615,22 @@ struct usr_avp *clone_avp_list(struct usr_avp *old)
 	return a;
 }
 
+
+struct usr_avp** set_bavp_list(struct usr_avp **list)
+{
+	struct usr_avp **foo;
+
+	foo = crt_bavps;
+	crt_bavps = list;
+	return foo;
+}
+
+struct usr_avp** get_bavp_list(void)
+{
+	return crt_bavps;
+}
+
+struct usr_avp** reset_bavp_list(void)
+{
+	return set_bavp_list(NULL);
+}

commit 3c05ae78e4b2da392f8ab6b5233f54f760284b3b

tls_mgm: fix crash when the client_sip_domain_avp AVP is not set
This is a regression introduced by commit e94ca5f that would lead to
a crash when the client_sip_domain_avp modparam is set but the actual
AVP is not set before a TLS connect.

Credits to @ovidiusas for reporting and suggesting the fix.

Closes #2909

modules/tls_mgm/tls_domain.c
@@ -475,6 +475,8 @@ struct tls_domain *tls_find_client_domain(struct ip_addr *ip, unsigned short por
 			if (sip_domain) {
 				LM_DBG("Match TLS domain by sip domain AVP: '%.*s'\n",
 					sip_domain->len, ZSW(sip_domain->s));
+			} else {
+				sip_domain = &match_any_dom;
 			}
 		}
 	} else {