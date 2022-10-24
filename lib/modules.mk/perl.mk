87087fba4a9da962dd1aa326a1418d416ccb9524
Maksym Sobolyev <sobomax@sippysoft.com>
2022-10-24 10:30:05 -0700

    o Set -Wno-compound-token-split-by-macro needed to get things   building up with clang > 11.
    o Unify common code in the perl modules to fix db_perlvdb
      as well with clang > 11.

---
+++ lib/modules.mk/perl.mk
@@ -0,0 +1,33 @@
+# define it (or export this) to the dir where xsubpp is on your system (if
+# not on the default path.
+#PERLBINDIR=
+
+LIBS=
+
+ifeq ($(CC_NAME), gcc)
+	DEFS+=-Wno-unused -Wno-redundant-decls
+endif
+ifeq ($(CC_NAME), clang)
+	DEFS+=-Wno-unused -Wno-redundant-decls $(shell test `${CC} -dumpversion | sed 's|[.].*||'` -gt 11 && echo -Wno-compound-token-split-by-macro)
+endif
+
+ifeq ($(PERLLDOPTS),)
+	LIBS+=$(shell perl -MExtUtils::Embed -e ldopts)
+else
+	LIBS+=$(PERLLDOPTS)
+endif
+
+ifeq ($(PERLCCOPTS),)
+	DEFS+=$(shell perl -MExtUtils::Embed -e ccopts)
+else
+	DEFS+=$(PERLCCOPTS)
+endif
+
+# if perl requires _FORTIFY_SOURCE, we need to enable optimizations
+ifneq (,$(findstring -D_FORTIFY_SOURCE,$(DEFS)))
+	DEFS+=-O2
+endif
+
+auto_gen=
+
+include ../../Makefile.modules
