df362fb231fc42735f6aedcbdb8f036a72a98f2e
Liviu Chircu <liviu@opensips.org>
2022-10-26 15:53:01 +0300

    Allow writing to AVPs with negative index
    This includes both SET and DELETE operations, for example:
        $(avp(foo)[-1]) = NULL;
        $(avp(foo)[-1]) = "42";

--- pvar.c
+++ pvar.c
@@ -2835,11 +2835,6 @@ static int pv_set_avp(struct sip_msg* msg, pv_param_t *param,
 			destroy_avps(name_type, avp_name, 1);
 		else
 		{
-			if(idx < 0)
-			{
-				LM_ERR("Index with negative value\n");
-				return -1;
-			}
 			destroy_index_avp(name_type, avp_name, idx);
 		}
 		return 0;
