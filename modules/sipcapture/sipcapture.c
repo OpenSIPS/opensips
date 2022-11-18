5fe2f1095db077ed9b0a2ee11e45109db12d59e0
Liviu Chircu <liviu@opensips.org>
2022-11-18 12:47:12 +0200

    sipcapture: Fix missing lock release on "buffer too small" errors
    Many thanks to Ryan Caicse (@ryancaicse) for reporting this bug!
    
    (cherry picked from commit 9f0fe53744ebf16467b61e9b59da5396bb356896)

--- modules/sipcapture/sipcapture.c
+++ modules/sipcapture/sipcapture.c
@@ -2933,7 +2933,10 @@ db_async_store(db_val_t* vals, db_key_t* keys, int num_keys,
 	async_status = ASYNC_NO_IO;
 
 	return 1;
+
 no_buffer:
+	if (HAVE_SHARED_QUERIES)
+		RELEASE_QUERY_LOCK(crt_as_query);
 	LM_ERR("buffer size exceeded\n");
 	return -1;
 }
