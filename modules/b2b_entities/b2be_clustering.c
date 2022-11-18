c5b5e795e915c535a9daf789a3185106ecc7438e
Liviu Chircu <liviu@opensips.org>
2022-11-18 12:41:39 +0200

    b2b_entities: Fix missing lock release on OOM error case
    Many thanks to Ryan Caicse (@ryancaicse) for reporting this bug!

--- modules/b2b_entities/b2be_clustering.c
+++ modules/b2b_entities/b2be_clustering.c
@@ -472,6 +472,7 @@ int receive_entity_create(bin_packet_t *packet, b2b_dlg_t *dlg, int type,
 	new_key = b2b_htable_insert(htable, new_dlg, hash_index, (time_t)timestamp,
 		type, 1, 1);
 	if (new_key == NULL) {
+		B2BE_LOCK_RELEASE(htable, hash_index);
 		LM_ERR("Failed to insert new record\n");
 		goto error;
 	}
