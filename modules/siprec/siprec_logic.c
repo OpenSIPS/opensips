745a7cb7a4746a6c49106e5459eabac72ca72df1
Razvan Crainea <razvan@opensips.org>
2022-11-23 16:24:43 +0200

    siprec: unref failed dialogs
    Close #2674
    
    (cherry picked from commit e3c74a39df0fd5f5b58679183ade8eee2e0749f6)

--- modules/siprec/siprec_logic.c
+++ modules/siprec/siprec_logic.c
@@ -510,14 +510,19 @@ void tm_start_recording(struct cell *t, int type, struct tmcb_params *ps)
 	str body;
 	struct src_sess *ss;
 
-	if (!is_invite(t) || ps->code >= 300)
+	if (!is_invite(t))
 		return;
+	ss = (struct src_sess *)*ps->param;
+	if (ps->code >= 300) {
+		/* unref so we can release the dialog */
+		srec_dlg.dlg_unref(ss->dlg, 1);
+		return;
+	}
 
 	/* check if we have a reply with body */
 	if (get_body(ps->rpl, &body) != 0 || body.len==0)
 		return;
 
-	ss = (struct src_sess *)*ps->param;
 	/* engage only on successful calls */
 	SIPREC_LOCK(ss);
 	/* if session has been started, do not start it again */
