08cf07e87d17cbe3c8f30b508b51665d232043a1
Vlad Patrascu <vladp@opensips.org>
2023-01-16 11:08:37 +0200

    msrp_ua: fix bad params for the E_MSRP_REPORT_RECEIVED event
    * Fix the length of the 'status' param.
    * Don't include the MSRP header prefix in case of negative replies
      for the 'message_id' param.

--- modules/msrp_ua/msrp_ua.c
+++ modules/msrp_ua/msrp_ua.c
@@ -2241,8 +2241,8 @@ static int handle_msrp_reply(struct msrp_msg *rpl, struct msrp_cell *tran,
 
 			if (rpl) {
 				status_str.len = rpl->fl.u.reply.status.len +
-					rpl->fl.u.reply.reason.len + 1;
-				status_str.s = pkg_malloc(status_str.len);
+					rpl->fl.u.reply.reason.len;
+				status_str.s = pkg_malloc(status_str.len + 1);
 				if (!status_str.s) {
 					LM_ERR("no more pkg memory\n");
 					pkg_free(sess_id.s);
@@ -2335,6 +2335,7 @@ static int msrpua_send_message(str *sess_id, str *mime, str *body,
 	str from = {0};
 	str hdrs[MSRP_HDRS_MAX_NO] = {{0}};
 	char *p;
+	str msgid;
 	str blen;
 
 	if (!mime || !body) {
@@ -2386,6 +2387,8 @@ static int msrpua_send_message(str *sess_id, str *mime, str *body,
 	p = hdrs[0].s;
 	append_string(p, MESSAGE_ID_PREFIX, MESSAGE_ID_PREFIX_LEN);
 	msrpua_gen_id(p, &sess->session_id, NULL);
+	msgid.s = p;
+	msgid.len = MD5_LEN;
 
 	hdrs_no++;
 
@@ -2430,8 +2433,8 @@ static int msrpua_send_message(str *sess_id, str *mime, str *body,
 	trans_param->sess = sess;
 
 	trans_param->message_id.s = (char *)(trans_param + 1);
-	trans_param->message_id.len = hdrs[0].len;
-	memcpy(trans_param->message_id.s, hdrs[0].s, hdrs[0].len);
+	trans_param->message_id.len = msgid.len;
+	memcpy(trans_param->message_id.s, msgid.s, msgid.len);
 	trans_param->byte_range.s = (char *)(trans_param + 1) +
 		trans_param->message_id.len;
 	trans_param->byte_range.len = hdrs[1].len;
