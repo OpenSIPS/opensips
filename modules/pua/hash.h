ed8bf4d47a5302b74006ce0312797928fc843e98
Bogdan-Andrei Iancu <bogdan@opensips.org>
2023-01-23 10:58:11 +0200

    [pua] fix the computing of presentity ID (pres_id)
    - be sure the hash size does not overflow (on user setting), as it has to fit on half long size
    - when computing pres_id, be sure to cast to long to avoid premature overflow
    - when incrementing local_index, be sure it does no overflow half long size

--- modules/pua/hash.h
+++ modules/pua/hash.h
@@ -62,7 +62,8 @@
 
 #define MAX_FORWARD  70
 
-#define PRES_HASH_ID(presentity)(HASH_SIZE*presentity->local_index + presentity->hash_index)
+#define PRES_HASH_ID(presentity) \
+	(HASH_SIZE*(long)presentity->local_index + presentity->hash_index)
 
 typedef struct publ
 {
