22d0b714dc10e7aa779345bd030e7f1a45c61ee5
Maksym Sobolyev <sobomax@sippysoft.com>
2023-02-16 16:30:17 -0800

    Revert "Use apt-spy2 to sanitize list of apt mirrors."
    This reverts commit 72b74d80c57e8c0ed9095da6723db9da46e9438e.

--- scripts/build/install_depends.sh
+++ scripts/build/install_depends.sh
@@ -5,7 +5,7 @@ set -e
 PKGS=""
 for pkg in `grep -A 35 packages: .travis.yml  | grep -e '^ *[-]' | awk '{print $2}'`
 do
-  if [ "${BUILD_OS}" = "ubuntu-22.04" -a "${pkg}" = "python-dev" ]
+  if [ "${BUILD_OS}" = ubuntu-22.04 -a "${pkg}" = python-dev ]
   then
     pkg="python-dev-is-python3"
   fi
@@ -17,13 +17,6 @@ done
 if [ ! -z "${PRE_INSTALL_CMD}" ]
 then
 	${PRE_INSTALL_CMD}
-fi
-
-if [ "${BUILD_OS}" != "ubuntu-18.04" ]
-then
-  sudo gem install apt-spy2
-  sudo apt-spy2 check --strict
-  sudo apt-spy2 fix --commit --strict
 fi
 
 sudo apt-get update -y
