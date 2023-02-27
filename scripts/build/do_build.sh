05a069f412a8db703a8222f235f45745438df4e1
Maksym Sobolyev <sobomax@sippysoft.com>
2023-02-27 14:45:07 -0800

    Run arm64, arm32, i386 (32-bit) and mips builds in a containers. Move everything to run using ubuntu-22.04. Add qemu-based builds allowing to build the whole set of modules and move all cross-builds into a separate workflow for clarity and faster turn-around time on basic builds.

--- scripts/build/do_build.sh
+++ scripts/build/do_build.sh
@@ -2,10 +2,16 @@
 
 set -e
 
-PKGS=`grep -A 35 packages: .travis.yml  | grep -e '^ *[-]' | awk '{print $2}'`
+. $(dirname $0)/dockerize.sub
 
 . $(dirname $0)/build.conf.sub
 
+EXCLUDE_MODULES="db_oracle osp sngtc cachedb_cassandra cachedb_couchbase \
+  cachedb_mongodb auth_jwt event_kafka aaa_diameter"
+if [ ! -z "${EXCLUDE_MODULES_ADD}" ]
+then
+  EXCLUDE_MODULES="${EXCLUDE_MODULES} ${EXCLUDE_MODULES_ADD}"
+fi
+
 CC_EXTRA_OPTS=${CC_EXTRA_OPTS:-"-Werror"} FASTER=1 NICER=0 make \
-  exclude_modules="db_oracle osp sngtc cachedb_cassandra cachedb_couchbase \
-  cachedb_mongodb auth_jwt event_kafka aaa_diameter" "${@}" ${MAKE_TGT:-"all"}
+  exclude_modules="${EXCLUDE_MODULES}" "${@}" ${MAKE_TGT:-"all"}
--- scripts/build/install_depends.sh
+++ scripts/build/install_depends.sh
@@ -2,25 +2,30 @@
 
 set -e
 
-PKGS=""
-for pkg in `grep -A 35 packages: .travis.yml  | grep -e '^ *[-]' | awk '{print $2}'`
+. $(dirname $0)/dockerize.sub
+
+PKGS="`grep -A 35 packages: .travis.yml  | grep -e '^ *[-]' | awk '{print $2}'`"
+
+. $(dirname $0)/build.conf.sub
+
+_PKGS=""
+for pkg in ${PKGS}
 do
   if [ "${BUILD_OS}" = ubuntu-22.04 -a "${pkg}" = python-dev ]
   then
     pkg="python-dev-is-python3"
   fi
-  PKGS="${PKGS} ${pkg}"
+  _PKGS="${_PKGS} ${pkg}"
 done
-
-. $(dirname $0)/build.conf.sub
+PKGS="${_PKGS}"
 
 if [ ! -z "${PRE_INSTALL_CMD}" ]
 then
 	${PRE_INSTALL_CMD}
 fi
 
-sudo apt-get update -y
-sudo apt-get -y --allow-downgrades install ${PKGS}
+${SUDO} apt-get update -y
+${SUDO} apt-get -y --allow-downgrades install ${PKGS}
 
 if [ ! -z "${POST_INSTALL_CMD}" ]
 then
