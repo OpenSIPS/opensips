5f17c118d52d99700296ec79371ed367e0fb0d00
Maksym Sobolyev <sobomax@sippysoft.com>
2023-03-02 09:12:52 -0800

    Also run unit tests on foreign arches using qemu.
    Limit cache size to 100MB per variant.
    
    Display ccache stats for every build it's been used with.

--- scripts/build/build_libtap.sh
+++ scripts/build/build_libtap.sh
@@ -2,4 +2,6 @@
 
 set -e
 
+. $(dirname $0)/dockerize.sub
+
 make -C "${1}" CFLAGS="-O1 -fPIE -fPIC" libtap.a
--- scripts/build/build_test_harness.sh
+++ scripts/build/build_test_harness.sh
@@ -2,13 +2,22 @@
 
 set -e
 
+. $(dirname $0)/dockerize.sub
+
 make Makefile.conf
 sed -i.bak '/-DCC_O0/d' Makefile.conf
 echo 'DEFS+= -I$(shell pwd)/'"${1}" >> Makefile.conf
 echo 'LIBS+= -L$(TOP_SRCDIR)/'"${1}" >> Makefile.conf
-if [ "${BUILD_OS}" == "ubuntu-22.04" ]
+if [ "${BUILD_OS}" = "ubuntu-22.04" ]
 then
-  echo 'DEFS+= -flto' >> Makefile.conf
-  echo 'LIBS+= -flto' >> Makefile.conf
+  case "${COMPILER}" in
+  *-qemu-cross)
+    continue
+    ;;
+  *)
+    echo 'DEFS+= -flto' >> Makefile.conf
+    echo 'LIBS+= -flto' >> Makefile.conf
+    ;;
+  esac
 fi
 sh -x scripts/build/do_build.sh DEFS_EXTRA_OPTS="-DUNIT_TESTS -fPIE -fPIC"
