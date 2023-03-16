8c1f10c7e2e57c974a6932266b1e5b0b4b31926d
Bogdan Andrei IANCU <bogdan@opensips.org>
2023-03-16 08:50:28 +0200

    Merge pull request #3020 from nexbridge/temp
    Fix bug in weight-based sort
    
    (cherry picked from commit 48c051fc16f4d83bf176d1eefe1df642882b6b88)

--- modules/drouting/drouting.c
+++ modules/drouting/drouting.c
@@ -2724,11 +2724,11 @@ static int weight_based_sort(pgw_list_t *pgwl, int size, unsigned short *idx)
 		}
 		if (weight_sum) {
 			/* randomly select number */
-			rand_no = (unsigned int)(weight_sum*((float)rand()/(float)RAND_MAX));
+			rand_no = (unsigned int)(weight_sum*((double)rand()/((double)RAND_MAX)));
 			LM_DBG("random number is %d\n",rand_no);
 			/* select the element */
 			for( i=first ; i<size ; i++ )
-				if (running_sum[i]>rand_no) break;
+				if (running_sum[i]>=rand_no) break;
 			if (i==size) {
 				LM_CRIT("bug in weight sort, first=%u, size=%u, rand_no=%u, total weight=%u\n",
 					first, size, rand_no, weight_sum);
@@ -2741,7 +2741,7 @@ static int weight_based_sort(pgw_list_t *pgwl, int size, unsigned short *idx)
 			}
 		} else {
 			/* randomly select index */
-			//	i = (unsigned int)((size-first)*((float)rand()/RAND_MAX));
+			//	i = (unsigned int)((size-first)*((double)rand()/((double)RAND_MAX)));
 			i = first;
 		}
 		LM_DBG("selecting element %d with weight %d\n",
