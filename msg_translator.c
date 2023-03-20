66b910c678e1fe7c7c33392f9141c2cd4fe63926
David Trihy <david.trihy@genesys.com>
2023-03-20 10:54:13 +0000

    PR Comments

--- msg_translator.c
+++ msg_translator.c
@@ -2179,7 +2179,6 @@ char * build_req_buf_from_sip_req( struct sip_msg* msg,
 		if (via_params && !extra_params.len) {
 			/* if no other parameters were added yet, consider via_params */
 			extra_params.len = via_params->len;
-			id_len = via_params->len;
 			/* otherwise, the via_params were already copied in the id block */
 		}
 		extra_params.len += RPORT_LEN-1; /* last char in RPORT define is '='
@@ -2195,8 +2194,10 @@ char * build_req_buf_from_sip_req( struct sip_msg* msg,
 		if(id_buf!=0) {
 			memcpy(extra_params.s, id_buf, id_len);
 			pkg_free(id_buf);
-		} else if (via_params)
+		} else if (via_params) {
 			memcpy(extra_params.s, via_params->s, via_params->len);
+			id_len = via_params->len;
+		}
 		memcpy(extra_params.s+id_len, RPORT, RPORT_LEN-1);
 		extra_params.s[extra_params.len]='\0';
 		LM_DBG("extra param added: <%.*s>\n",extra_params.len, extra_params.s);
