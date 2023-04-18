2d6dbf7ecbb9f1ad14ff68b15aa650a497b04c9b
Bogdan-Andrei Iancu <bogdan@opensips.org>
2023-04-18 18:03:33 +0300

    [callops] Fix bad MI array termination
    Closes #3057

--- modules/callops/callops.c
+++ modules/callops/callops.c
@@ -220,7 +220,8 @@ static const mi_export_t mi_cmds[] = {
 	{ "call_unhold", 0, 0, 0, {
 		{mi_call_unhold, {"callid", 0}},
 		{EMPTY_MI_RECIPE}}
-	}
+	},
+	{EMPTY_MI_EXPORT}
 };
 
 struct module_exports exports= {
