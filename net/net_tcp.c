080fce6ca5819d67417affc89642216f6fde599a
James Stanley <james@incoherency.co.uk>
2023-05-05 12:05:25 +0100

    Copy peer hostname from sockaddr_union into tcp_connection
    This is so that hostnames in TLS certificates can be validated.

--- net/net_tcp.c
+++ net/net_tcp.c
@@ -958,6 +958,11 @@ struct tcp_connection* tcp_conn_create(int sock, union sockaddr_union* su,
 		return NULL;
 	}
 
+	/* copy peer hostname into the tcp_connection so that tls_openssl can verify
+	 * the certificate hostname */
+	strncpy(c->hostname, su->h.hostname, sizeof(c->hostname)-1);
+	c->hostname[sizeof(c->hostname)-1] = 0;
+
 	if (protos[c->type].net.conn_init &&
 			protos[c->type].net.conn_init(c) < 0) {
 		LM_ERR("failed to do proto %d specific init for conn %p\n",
