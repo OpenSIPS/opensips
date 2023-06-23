4e9595c16e841049edf726b214092677fc31d81d
Maksym Sobolyev <sobomax@sippysoft.com>
2023-06-22 23:57:54 -0700

    Reset sources.lst to default ubuntu's.
    (cherry picked from commit f4e90f3d6ac9478bb1d22e0d456a54dd91bb8192)

---
+++ scripts/build/reset_sources.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+RELEASE="$(lsb_release -cs)"
+URL="http://archive.ubuntu.com/ubuntu"
+
+echo "deb $URL $RELEASE main universe
+deb $URL $RELEASE-updates main universe
+deb $URL $RELEASE-security main universe" | sudo tee /etc/apt/sources.list > /dev/null
