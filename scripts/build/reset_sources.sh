94e5c867b28273ad60850958b8d286b609da80fb
Razvan Crainea <razvan@opensips.org>
2023-06-30 18:16:52 +0300

    Revert "scripts/build: remove apt forced sources"
    This reverts commit e2c3ab3af4f9799f51298c0ef8b130452b6a899c.

--- scripts/build/reset_sources.sh
+++ scripts/build/reset_sources.sh
@@ -6,5 +6,3 @@ URL="http://archive.ubuntu.com/ubuntu"
 echo "deb $URL $RELEASE main universe
 deb $URL $RELEASE-updates main universe
 deb $URL $RELEASE-security main universe" | sudo tee /etc/apt/sources.list > /dev/null
-
-sudo rm -rf /etc/apt/sources.list.d
