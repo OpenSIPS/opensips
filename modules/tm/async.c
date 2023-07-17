cfcd9a65eb6eb41f6c8c5b69a55d0f930c383efc
Vlad Paiu <vladpaiu@opensips.org>
2023-07-17 19:40:30 +0300

    set async ctx to 0
    prevent crash in timeout_f not being set but reactor still somehow timing out fds

--- modules/tm/async.c
+++ modules/tm/async.c
@@ -267,6 +267,8 @@ int t_handle_async(struct sip_msg *msg, struct action* a,
 		goto failure;
 	}
 
+	memset(ctx,0,sizeof(async_tm_ctx));
+
 	async_status = ASYNC_NO_IO; /*assume default status "no IO done" */
 	return_code = ((const acmd_export_t*)(a->elem[0].u.data_const))->function(msg,
 			(async_ctx*)ctx,
