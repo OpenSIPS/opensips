5339179b011f20b002429649fc30fe95ff26e7d7
Razvan Crainea <razvan@opensips.org>
2023-07-26 18:48:13 +0300

    tm: add set_t in API

--- modules/tm/t_lookup.h
+++ modules/tm/t_lookup.h
@@ -79,6 +79,7 @@ typedef struct cell * (*tlookuporiginalt_f)(struct sip_msg*);
 typedef struct cell * (*tgett_f)(void);
 struct cell *get_t();
 
+typedef void (*tsett_f)(struct cell *);
 /* use carefully or better not at all -- current transaction is
  * primarily set by lookup functions */
 void set_t(struct cell *t);
--- modules/tm/tm.c
+++ modules/tm/tm.c
@@ -702,6 +702,7 @@ int load_tm( struct tm_binds *tmb)
 	tmb->t_get_trans_ident = t_get_trans_ident;
 	tmb->t_lookup_ident = t_lookup_ident;
 	tmb->t_gett = get_t;
+	tmb->t_sett = set_t;
 	tmb->t_get_e2eackt = get_e2eack_t;
 	tmb->t_get_picked = t_get_picked_branch;
 	tmb->t_set_remote_t = t_set_remote_t;
