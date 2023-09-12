393dfb52a25bb55a37a6afe2e53325f570b4dfc9
Razvan Crainea <razvan@opensips.org>
2023-09-12 17:47:41 +0300

    rtp_relay: remove context from the list on repicated deletes
    (cherry picked from commit 1af376b71ee235ce723ccda2716ee507c575b8d8)

--- modules/rtp_relay/rtp_relay_ctx.c
+++ modules/rtp_relay/rtp_relay_ctx.c
@@ -260,6 +260,11 @@ static void rtp_relay_ctx_free(struct rtp_relay_ctx *ctx)
 	list_for_each_safe(it, safe, &ctx->sessions)
 		rtp_relay_ctx_free_sess(list_entry(it, struct rtp_relay_sess, list));
 
+	lock_start_write(rtp_relay_contexts_lock);
+	if (list_is_valid(&ctx->list))
+		list_del(&ctx->list);
+	lock_stop_write(rtp_relay_contexts_lock);
+
 	lock_destroy(&ctx->lock);
 	shm_free(ctx);
 }
