d4aead240ca2c209d890f06028660771337803b6
Razvan Crainea <razvan@opensips.org>
2023-09-26 16:29:22 +0300

    dialog: increment last_gen_cseq only if initialized
    Many thanks to Denys Pozniak (@denyspozniak on GitHub) for reporting and
    providing troubleshooting information.
    
    Close #3206
    
    (cherry picked from commit 7bafee1f91327f91c0372318cf0cd8e6dc4bceaa)

--- modules/dialog/dlg_handlers.c
+++ modules/dialog/dlg_handlers.c
@@ -1064,7 +1064,7 @@ static void dlg_update_caller_rpl_contact(struct cell* t, int type,
 
 	LM_DBG("Status Code received =  [%d]\n", statuscode);
 
-	if (statuscode == 401 || statuscode ==407) {
+	if ((statuscode == 401 || statuscode == 407) && dlg->legs[DLG_CALLER_LEG].last_gen_cseq) {
 		dlg->legs[DLG_CALLER_LEG].last_gen_cseq++;
 		LM_DBG("incrementing last_gen_cseq to [%d] for leg[%d]\n", dlg->legs[DLG_CALLER_LEG].last_gen_cseq, DLG_CALLER_LEG);
 	}
@@ -1099,6 +1099,11 @@ static void dlg_update_callee_rpl_contact(struct cell* t, int type,
 	}
 
 	LM_DBG("Status Code received =  [%d]\n", statuscode);
+
+	if ((statuscode == 401 || statuscode == 407) && dlg->legs[callee_idx(dlg)].last_gen_cseq) {
+		dlg->legs[callee_idx(dlg)].last_gen_cseq++;
+		LM_DBG("incrementing last_gen_cseq to [%d] for leg[%d]\n", dlg->legs[callee_idx(dlg)].last_gen_cseq, callee_idx(dlg));
+	}
 
 	if (statuscode >= 200 && statuscode < 300)
 		dlg_update_contact(dlg, rpl, callee_idx(dlg));
