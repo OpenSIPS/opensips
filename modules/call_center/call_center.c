dd9e07d3dc2ad906041a315ae29030c608b356e1
Razvan Crainea <razvan@opensips.org>
2023-10-25 17:19:04 +0300

    call_center: passthrough re-INVITEs
    (cherry picked from commit f971fa17310c654c54305608855cfc28c1477bd0)

--- modules/call_center/call_center.c
+++ modules/call_center/call_center.c
@@ -900,8 +900,12 @@ int b2bl_callback_customer(b2bl_cb_params_t *params, unsigned int event)
 	 * events, just in the BYEs from media/agent side */
 	if (event!=B2B_BYE_CB) {
 		lock_set_release( data->call_locks, call->lock_idx );
-		cc_call_state = CC_CALL_NONE;
-		return 0;
+		if (event != B2B_RE_INVITE_CB) {
+			cc_call_state = CC_CALL_NONE;
+			return 0;
+		} else {
+			return 1;
+		}
 	}
 
 	/* right-side leg of call sent BYE */
