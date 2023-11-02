5a8f6e406180e4d8f37045f95c1e5a1f937df1ab
Liviu Chircu <liviu@opensips.org>
2023-11-02 13:34:01 +0200

    CacheDB: Allow "@" in URL passwords
    (cherry picked from commit 1df9afd3886a3bddf837927aafca7bff8763acd1)

--- cachedb/cachedb_id.c
+++ cachedb/cachedb_id.c
@@ -83,7 +83,7 @@ static int parse_cachedb_url(struct cachedb_id* id, const str* url)
 
 	enum state st;
 	unsigned int len, i, ipv6_flag=0, multi_hosts=0;
-	char* begin;
+	char* begin, *last_at;
 	char* prev_token,*start_host=NULL,*start_prev=NULL,*ptr;
 
 	prev_token = 0;
@@ -105,6 +105,8 @@ static int parse_cachedb_url(struct cachedb_id* id, const str* url)
 
 	if (dupl_string(&id->initial_url,url->s,url->s+url->len) < 0)
 		goto err;
+
+	last_at = q_memrchr(url->s, '@', url->len);
 
 	for(i = 0; i < len; i++) {
 		switch(st) {
@@ -159,6 +161,9 @@ static int parse_cachedb_url(struct cachedb_id* id, const str* url)
 		case ST_USER_HOST:
 			switch(url->s[i]) {
 			case '@':
+				if (&url->s[i] < last_at)
+					break;
+
 				st = ST_HOST;
 				multi_hosts = 0;
 				if (dupl_string(&id->username, begin, url->s + i) < 0) goto err;
@@ -195,6 +200,9 @@ static int parse_cachedb_url(struct cachedb_id* id, const str* url)
 		case ST_PASS_PORT:
 			switch(url->s[i]) {
 			case '@':
+				if (&url->s[i] < last_at)
+					break;
+
 				st = ST_HOST;
 				id->username = prev_token;
 				if (dupl_string(&id->password, begin, url->s + i) < 0) goto err;
