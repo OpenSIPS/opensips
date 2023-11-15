OpenSIPS/opensips

commit 543a40c8ff0469195fd9479d04c5ad1f1606a36d

dialog: support integer type for dialog values

modules/dialog/dlg_hash.c
@@ -850,7 +850,7 @@ struct dlg_cell* get_dlg( str *callid, str *ftag, str *ttag,
 }
 
 
-struct dlg_cell* get_dlg_by_val(str *attr, str *val)
+struct dlg_cell* get_dlg_by_val(struct sip_msg *msg, str *attr, pv_spec_t *val)
 {
 	struct dlg_entry *d_entry;
 	struct dlg_cell  *dlg;
@@ -867,7 +867,7 @@ struct dlg_cell* get_dlg_by_val(str *attr, str *val)
 			LM_DBG("dlg in state %d to check\n",dlg->state);
 			if ( dlg->state>DLG_STATE_CONFIRMED )
 				continue;
-			if (check_dlg_value_unsafe( dlg, attr, val)==0) {
+			if (check_dlg_value_unsafe(msg, dlg, attr, val)==0) {
 				ref_dlg_unsafe( dlg, 1);
 				dlg_unlock( d_table, d_entry);
 				return dlg;
@@ -1477,30 +1477,39 @@ static inline int internal_mi_print_dlg(mi_item_t *dialog_obj,
 
 			/* print dlg values -> iterate the list */
 			for( dv=dlg->vals ; dv ; dv=dv->next) {
-				/* escape non-printable chars */
-				p = pkg_realloc(dlg_val_buf, 4 * dv->val.len + 1);
-				if (!p) {
-					LM_ERR("not enough mem to allocate: %d\n", dv->val.len);
-					continue;
-				}
-				for (i = 0, j = 0; i < dv->val.len; i++) {
-					if (dv->val.s[i] < 0x20 || dv->val.s[i] >= 0x7F) {
-						p[j++] = '\\';
-						switch ((unsigned char)dv->val.s[i]) {
-						case 0x8: p[j++] = 'b'; break;
-						case 0x9: p[j++] = 't'; break;
-						case 0xA: p[j++] = 'n'; break;
-						case 0xC: p[j++] = 'f'; break;
-						case 0xD: p[j++] = 'r'; break;
-						default:
-							p[j++] = 'x';
-							j += snprintf(&p[j], 3, "%02x",
-									(unsigned char)dv->val.s[i]);
-							break;
-						}
-					} else {
-						p[j++] = dv->val.s[i];
+				if (dv->type == DLG_VAL_TYPE_STR) {
+					/* escape non-printable chars */
+					p = pkg_realloc(dlg_val_buf, 4 * dv->val.s.len + 1);
+					if (!p) {
+						LM_ERR("not enough mem to allocate: %d\n", dv->val.s.len);
+						continue;
 					}
+					for (i = 0, j = 0; i < dv->val.s.len; i++) {
+						if (dv->val.s.s[i] < 0x20 || dv->val.s.s[i] >= 0x7F) {
+							p[j++] = '\\';
+							switch ((unsigned char)dv->val.s.s[i]) {
+							case 0x8: p[j++] = 'b'; break;
+							case 0x9: p[j++] = 't'; break;
+							case 0xA: p[j++] = 'n'; break;
+							case 0xC: p[j++] = 'f'; break;
+							case 0xD: p[j++] = 'r'; break;
+							default:
+								p[j++] = 'x';
+								j += snprintf(&p[j], 3, "%02x",
+										(unsigned char)dv->val.s.s[i]);
+								break;
+							}
+						} else {
+							p[j++] = dv->val.s.s[i];
+						}
+					}
+				} else {
+					values_item = add_mi_object(values_arr, NULL, 0);
+					if (!values_item)
+						goto error;
+					if (add_mi_number(values_item,dv->name.s,dv->name.len,
+						dv->val.n) < 0)
+						goto error;
 				}
 
 				values_item = add_mi_object(values_arr, NULL, 0);
@@ -1508,8 +1517,6 @@ static inline int internal_mi_print_dlg(mi_item_t *dialog_obj,
 					goto error;
 				if (add_mi_string(values_item,dv->name.s,dv->name.len,p,j) < 0)
 					goto error;
-
-				dlg_val_buf = p;
 			}
 		}
 
@@ -1757,6 +1764,7 @@ mi_response_t *mi_push_dlg_var(const mi_params_t *params,
 	int shtag_state = 1, db_update = 0;
 	mi_item_t *did_param_arr;
 	int i, no_dids;
+	int_str isval;
 
 	if ( d_table == NULL)
 		goto not_found;
@@ -1799,7 +1807,8 @@ mi_response_t *mi_push_dlg_var(const mi_params_t *params,
 			}
 		}
 
-		if (store_dlg_value( dlg, &dlg_var_name, &dlg_var_value)!=0) {
+		isval.s = dlg_var_value;
+		if (store_dlg_value( dlg, &dlg_var_name, &isval, DLG_VAL_TYPE_STR)!=0) {
 			LM_ERR("failed to store dialog values <%.*s>:<%.*s>\n",
 			dlg_var_name.len,dlg_var_name.s,
 			dlg_var_value.len,dlg_var_value.s);

modules/acc/acc.c
@@ -1470,16 +1470,16 @@ static void complete_dlg_values(str *stored_values,str *val_arr,short nr_vals)
 /* stores core values and leg values into dlg */
 int store_core_leg_values(struct dlg_cell *dlg, struct sip_msg *req)
 {
-	str bytes;
+	int_str bytes;
 
 	if ( build_core_dlg_values(dlg, req) < 0) {
 		LM_ERR("cannot build core value string\n");
 		return -1;
 	}
 
-	bytes.s = cdr_buf.s;
-	bytes.len = cdr_data_len;
-	if ( dlg_api.store_dlg_value(dlg, &core_str, &bytes) < 0) {
+	bytes.s.s = cdr_buf.s;
+	bytes.s.len = cdr_data_len;
+	if ( dlg_api.store_dlg_value(dlg, &core_str, &bytes, DLG_VAL_TYPE_STR) < 0) {
 		LM_ERR("cannot store core values into dialog\n");
 		return -1;
 	}
@@ -1492,16 +1492,16 @@ int store_core_leg_values(struct dlg_cell *dlg, struct sip_msg *req)
 int store_extra_values(extra_value_t* values, str *values_str,
 		struct dlg_cell *dlg)
 {
-	str bytes;
+	int_str bytes;
 
 	if ( build_extra_dlg_values(values) < 0) {
 		LM_ERR("cannot build core value string\n");
 		return -1;
 	}
 
-	bytes.s = cdr_buf.s;
-	bytes.len = cdr_data_len;
-	if ( dlg_api.store_dlg_value(dlg, values_str, &bytes) < 0) {
+	bytes.s.s = cdr_buf.s;
+	bytes.s.len = cdr_data_len;
+	if ( dlg_api.store_dlg_value(dlg, values_str, &bytes, DLG_VAL_TYPE_STR) < 0) {
 		LM_ERR("cannot store core values into dialog\n");
 		return -1;
 	}
@@ -1511,7 +1511,7 @@ int store_extra_values(extra_value_t* values, str *values_str,
 
 int store_leg_values(acc_ctx_t* ctx, str* values_str, struct dlg_cell *dlg)
 {
-	str bytes;
+	int_str bytes;
 
 	if (ctx == NULL || values_str == NULL) {
 		LM_ERR("bad usage!\n");
@@ -1523,9 +1523,9 @@ int store_leg_values(acc_ctx_t* ctx, str* values_str, struct dlg_cell *dlg)
 		return -1;
 	}
 
-	bytes.s = cdr_buf.s;
-	bytes.len = cdr_data_len;
-	if (dlg_api.store_dlg_value(dlg, values_str, &bytes) < 0) {
+	bytes.s.s = cdr_buf.s;
+	bytes.s.len = cdr_data_len;
+	if (dlg_api.store_dlg_value(dlg, values_str, &bytes, DLG_VAL_TYPE_STR) < 0) {
 		LM_ERR("cannot store dialog string\n");
 		return -1;
 	}
@@ -1636,6 +1636,9 @@ struct dlg_cell *create_acc_dlg(struct sip_msg* req)
 /* gets core values from dlg and stores them into val_arr array */
 static int prebuild_core_arr(struct dlg_cell *dlg, str *buffer, struct timeval *start)
 {
+	int_str isval;
+	int val_type;
+
 	if (!start || !buffer) {
 		LM_ERR("invalid parameters\n");
 		return -1;
@@ -1643,11 +1646,13 @@ static int prebuild_core_arr(struct dlg_cell *dlg, str *buffer, struct timeval *
 	buffer->len = 0;
 	buffer->s = 0;
 
+	isval.s = *buffer;
 	/* fetching core string values */
-	if (dlg_api.fetch_dlg_value(dlg, &core_str, buffer, 1) < 0) {
+	if (dlg_api.fetch_dlg_value(dlg, &core_str, &val_type, &isval, 1) < 0) {
 		LM_ERR("cannot fetch core string value\n");
 		return -1;
 	}
+	*buffer = isval.s;
 
 	complete_dlg_values(buffer, val_arr, ACC_CORE_LEN+1);
 	memcpy(start, val_arr[ACC_CORE_LEN].s, val_arr[ACC_CORE_LEN].len);
@@ -1690,23 +1695,24 @@ static int restore_extra(struct dlg_cell* dlg,
 			str *type_str, acc_ctx_t* ctx)
 {
 	int extra_len;
-	str buffer;
+	int_str buffer;
+	int val_type;
 
 	if (ctx == NULL) {
 		LM_ERR("bad call!\n");
 		return -1;
 	}
 
-	if (dlg_api.fetch_dlg_value(dlg, type_str, &buffer, 0) < 0) {
+	if (dlg_api.fetch_dlg_value(dlg, type_str, &val_type, &buffer, 0) < 0) {
 		LM_ERR("cannot fetch <%.*s> value from dialog!\n",
 				type_str->len, type_str->s);
 		return -1;
 	}
 
 	/* jump over the total length */
-	extra_len = GET_LEN(buffer.s);
-	buffer.s += 2;
-	buffer.len -= 2;
+	extra_len = GET_LEN(buffer.s.s);
+	buffer.s.s += 2;
+	buffer.s.len -= 2;
 
 	if (extra_len != extra_tgs_len) {
 		LM_WARN("extra tags were added/removed since last run!"
@@ -1720,7 +1726,7 @@ static int restore_extra(struct dlg_cell* dlg,
 		return -1;
 	}
 
-	if (restore_extra_from_str(ctx->extra_values, &buffer, extra_len) < 0) {
+	if (restore_extra_from_str(ctx->extra_values, &buffer.s, extra_len) < 0) {
 		LM_ERR("failed to restore extra values!\n");
 		free_extra_array(ctx->extra_values, extra_len);
 		return -1;
@@ -1733,21 +1739,22 @@ static int restore_legs(struct dlg_cell* dlg,
 			str *type_str, acc_ctx_t* ctx)
 {
 	short extra_len, i;
-	str buffer;
+	int_str buffer;
+	int val_type;
 
 	if (ctx == NULL) {
 		LM_ERR("bad call!\n");
 		return -1;
 	}
 
-	if (dlg_api.fetch_dlg_value(dlg, type_str, &buffer, 0) < 0) {
+	if (dlg_api.fetch_dlg_value(dlg, type_str, &val_type, &buffer, 0) < 0) {
 		LM_ERR("cannot fetch <%.*s> value from dialog!\n",
 				type_str->len, type_str->s);
 		return -1;
 	}
 
-	ctx->legs_no = GET_LEN(buffer.s+2);
-	extra_len = GET_LEN(buffer.s);
+	ctx->legs_no = GET_LEN(buffer.s.s+2);
+	extra_len = GET_LEN(buffer.s.s);
 
 	if (extra_len != leg_tgs_len) {
 		LM_WARN("tags were added/removed since last run! won't restore values!\n");
@@ -1768,11 +1775,11 @@ static int restore_legs(struct dlg_cell* dlg,
 		}
 	}
 
-	buffer.s += 4;
-	buffer.len -=4;
+	buffer.s.s += 4;
+	buffer.s.len -=4;
 
 	for (i=0; i<ctx->legs_no; i++) {
-		if (restore_extra_from_str(ctx->leg_values[i], &buffer, extra_len) < 0) {
+		if (restore_extra_from_str(ctx->leg_values[i], &buffer.s, extra_len) < 0) {
 			LM_ERR("failed to restore leg values!\n");
 			goto error;
 		}

modules/acc/acc_logic.c
@@ -596,9 +596,10 @@ static void acc_merge_contexts(struct dlg_cell *dlg, int type,
 void acc_update_ctx_callback(struct dlg_cell *dlg, int type,
 			struct dlg_cb_params *_params)
 {
-	str flags_s;
+	int_str flags_s;
 	acc_ctx_t* ctx = (acc_ctx_t *)(*_params->param);
 	unsigned long long flags;
+	int val_type;
 
 	if (!dlg) {
 		LM_ERR("null dialog - cannot fetch message flags\n");
@@ -611,9 +612,9 @@ void acc_update_ctx_callback(struct dlg_cell *dlg, int type,
 		return;
 	}
 
-	flags_s.s = (char *)&flags;
-	flags_s.len = sizeof(flags);
-	if (dlg_api.fetch_dlg_value(dlg, &flags_str, &flags_s, 1) < 0) {
+	flags_s.s.s = (char *)&flags;
+	flags_s.s.len = sizeof(flags);
+	if (dlg_api.fetch_dlg_value(dlg, &flags_str, &val_type, &flags_s, 1) < 0) {
 		LM_DBG("flags were not saved in dialog\n");
 		return;
 	}
@@ -627,26 +628,27 @@ void acc_update_ctx_callback(struct dlg_cell *dlg, int type,
 
 void acc_loaded_callback(struct dlg_cell *dlg, int type,
 			struct dlg_cb_params *_params) {
-		str flags_s, table_s, created_s;
+		int_str flags_s, table_s, created_s;
 		acc_ctx_t* ctx;
 		time_t created;
 		unsigned long long flags;
+		int val_type;
 
 		if (!dlg) {
 			LM_ERR("null dialog - cannot fetch message flags\n");
 			return;
 		}
 
-		flags_s.s = (char *)&flags;
-		flags_s.len = sizeof(flags);
-		if (dlg_api.fetch_dlg_value(dlg, &flags_str, &flags_s, 1) < 0) {
+		flags_s.s.s = (char *)&flags;
+		flags_s.s.len = sizeof(flags);
+		if (dlg_api.fetch_dlg_value(dlg, &flags_str, &val_type, &flags_s, 1) < 0) {
 			LM_DBG("flags were not saved in dialog\n");
 			return;
 		}
 
-		created_s.s = (char *)&created;
-		created_s.len = sizeof(created);
-		if (dlg_api.fetch_dlg_value(dlg, &created_str, &created_s, 1) < 0) {
+		created_s.s.s = (char *)&created;
+		created_s.s.len = sizeof(created);
+		if (dlg_api.fetch_dlg_value(dlg, &created_str, &val_type, &created_s, 1) < 0) {
 			LM_DBG("created time was not saved in dialog\n");
 			return;
 		}
@@ -667,18 +669,18 @@ void acc_loaded_callback(struct dlg_cell *dlg, int type,
 
 		/* restore accounting table if db accounting is used */
 		if (is_db_acc_on(ctx->flags)) {
-			if (dlg_api.fetch_dlg_value(dlg, &table_str, &table_s, 0) < 0) {
+			if (dlg_api.fetch_dlg_value(dlg, &table_str, &val_type, &table_s, 0) < 0) {
 				LM_DBG("table was not saved in dialog\n");
 				return;
 			}
 
-			if ((ctx->acc_table.s=shm_malloc(table_s.len)) == NULL) {
+			if ((ctx->acc_table.s=shm_malloc(table_s.s.len)) == NULL) {
 				LM_ERR("no more shm!\n");
 				return;
 			}
 
-			memcpy(ctx->acc_table.s, table_s.s, table_s.len);
-			ctx->acc_table.len = table_s.len;
+			memcpy(ctx->acc_table.s, table_s.s.s, table_s.s.len);
+			ctx->acc_table.len = table_s.s.len;
 		}
 
 
@@ -903,10 +905,10 @@ static void acc_dlg_ended(struct dlg_cell *dlg, int type,
 static void acc_dlg_onwrite(struct dlg_cell *dlg, int type,
 		struct dlg_cb_params *_params)
 {
-	str flags_s;
+	int_str flags_s;
 	acc_ctx_t* ctx;
-
-	str created_s;
+	int_str isval;
+	int_str created_s;
 
 	if (!_params) {
 		LM_ERR("not enough info!\n");
@@ -927,25 +929,26 @@ static void acc_dlg_onwrite(struct dlg_cell *dlg, int type,
 		return;
 	}
 
-	flags_s.s = (char*)(&ctx->flags);
-	flags_s.len = sizeof(unsigned long long);
+	flags_s.s.s = (char*)(&ctx->flags);
+	flags_s.s.len = sizeof(unsigned long long);
 
 	/* store flags into dlg */
-	if ( dlg_api.store_dlg_value(dlg, &flags_str, &flags_s) < 0) {
+	if ( dlg_api.store_dlg_value(dlg, &flags_str, &flags_s, DLG_VAL_TYPE_STR) < 0) {
 		LM_ERR("cannot store flag value into dialog\n");
 		return;
 	}
 
-	created_s.s = (char*)(&ctx->created);
-	created_s.len = sizeof(time_t);
+	created_s.s.s = (char*)(&ctx->created);
+	created_s.s.len = sizeof(time_t);
 
-	if ( dlg_api.store_dlg_value(dlg,&created_str,&created_s) < 0) {
+	if ( dlg_api.store_dlg_value(dlg,&created_str,&created_s,DLG_VAL_TYPE_STR) < 0) {
 		LM_ERR("cannot store created value!\n");
 		return;
 	}
 
 	if (is_db_acc_on(ctx->flags) && ctx->acc_table.s && ctx->acc_table.len) {
-		if ( dlg_api.store_dlg_value(dlg, &table_str, &ctx->acc_table) < 0) {
+		isval.s = ctx->acc_table;
+		if ( dlg_api.store_dlg_value(dlg, &table_str, &isval, DLG_VAL_TYPE_STR) < 0) {
 			LM_ERR("cannot store table name into dialog\n");
 			return;
 		}

commit 34dc02bf8e118891ca68e8bf06261980ec7130b0

dialog: Mitigate 'dlg_list' PKG fragmentation; Fix dlg_list_ctx memleak
By using pkg_str_extend() instead of pkg_realloc(), we avoid a constant
creation of PKG fragments (the buffer will eventually stop growing),
which seems to gradually fragment the private memory pool, as the MI
'dlg_list' is continuously polled by various monitoring software.

As a bonus, this patch also fixes a 'dlg_list_ctx' PKG memory leak
introduced in 543a40c8ff and only present on OpenSIPS 3.4+.

Related to #3235

modules/dialog/dlg_hash.c
@@ -1348,7 +1348,7 @@ void next_state_dlg(struct dlg_cell *dlg, int event, int dir, int *old_state,
 
 
 /**************************** MI functions ******************************/
-static char *dlg_val_buf;
+static str dlg_val_buf;
 static inline int internal_mi_print_dlg(mi_item_t *dialog_obj,
 									struct dlg_cell *dlg, int with_context)
 {
@@ -1509,11 +1509,12 @@ static inline int internal_mi_print_dlg(mi_item_t *dialog_obj,
 			for( dv=dlg->vals ; dv ; dv=dv->next) {
 				if (dv->type == DLG_VAL_TYPE_STR) {
 					/* escape non-printable chars */
-					p = pkg_realloc(dlg_val_buf, 4 * dv->val.s.len + 1);
-					if (!p) {
-						LM_ERR("not enough mem to allocate: %d\n", dv->val.s.len);
+					if (!pkg_str_extend(&dlg_val_buf, 4 * dv->val.s.len + 1)) {
+						LM_ERR("not enough mem to allocate: %d\n", 4 * dv->val.s.len + 1);
 						continue;
 					}
+
+					p = dlg_val_buf.s;
 					for (i = 0, j = 0; i < dv->val.s.len; i++) {
 						if (dv->val.s.s[i] < 0x20 || dv->val.s.s[i] >= 0x7F) {
 							p[j++] = '\\';