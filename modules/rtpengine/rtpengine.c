c9cb2ea6920083818cc3d4ceccd565b481bdd42a
Razvan Crainea <razvan@opensips.org>
2023-11-27 17:02:42 +0200

    rtpengine: make oom error logs more verbose
    (cherry picked from commit 6d38787c1ecee65899377cef3167987923889ece)

--- modules/rtpengine/rtpengine.c
+++ modules/rtpengine/rtpengine.c
@@ -1918,14 +1918,14 @@ static int parse_flags(struct ng_flags_parse *ng_flags, struct sip_msg *msg,
 		if (!val.s) {
 			bitem = bencode_str(bencode_item_buffer(ng_flags->flags), &key);
 			if (!bitem) {
-				err = "no more memory";
+				err = "no more memory for list value";
 				goto error;
 			}
 			BCHECK(bencode_list_add(ng_flags->flags, bitem));
 		} else {
 			bitem = bencode_str(bencode_item_buffer(ng_flags->dict), &val);
 			if (!bitem) {
-				err = "no more memory";
+				err = "no more memory for dict value";
 				goto error;
 			}
 			BCHECK(bencode_dictionary_add_len(ng_flags->dict, key.s, key.len, bitem));
@@ -1942,7 +1942,7 @@ static int parse_flags(struct ng_flags_parse *ng_flags, struct sip_msg *msg,
 			BCHECK(bencode_list_add(ng_flags->direction, bitem));
 			bitem = bencode_str(bencode_item_buffer(ng_flags->direction), &outiface);
 			if (!bitem) {
-				err = "no more memory";
+				err = "no more memory for direction";
 				goto error;
 			}
 			BCHECK(bencode_list_add(ng_flags->direction, bitem));
