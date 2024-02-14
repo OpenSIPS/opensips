46355f36755e0d56b79036e233e63772c803d8ec
Razvan Crainea <razvan@opensips.org>
2024-02-14 12:31:13 +0200

    auth: make qop parameter build available

--- modules/auth/challenge.c
+++ modules/auth/challenge.c
@@ -38,6 +38,7 @@
 #include "../../mod_fix.h"
 #include "auth_mod.h"
 #include "common.h"
+#include "qop.h"
 #include "challenge.h"
 #include "../../lib/digest_auth/dauth_nonce.h"
 #include "index.h"
@@ -58,10 +59,6 @@
  */
 #define MESSAGE_401        "Unauthorized"
 
-#define QOP_AUTH	  ", qop=\"" QOP_AUTH_STR "\""
-#define QOP_AUTH_INT	  ", qop=\"" QOP_AUTHINT_STR "\""
-#define QOP_AUTH_BOTH_AAI	  ", qop=\"" QOP_AUTH_STR "," QOP_AUTHINT_STR "\""
-#define QOP_AUTH_BOTH_AIA	  ", qop=\"" QOP_AUTHINT_STR "," QOP_AUTH_STR "\""
 #define STALE_PARAM	  ", stale=true"
 #define DIGEST_REALM	  ": Digest realm=\""
 #define DIGEST_NONCE	  "\", nonce=\""
@@ -78,31 +75,12 @@ char *build_auth_hf(struct nonce_context *ncp, struct nonce_params *calc_np,
 {
 	char *hf, *p;
 	str_const alg_param;
-	str_const qop_param = STR_NULL_const;
+	str_const qop_param = get_qop_param(calc_np->qop);
 	str_const stale_param = STR_NULL_const;
 	const str_const digest_realm = str_const_init(DIGEST_REALM);
 	const str_const nonce_param = str_const_init(DIGEST_NONCE);
 	str_const opaque_param;
 
-	if (calc_np->qop) {
-		switch (calc_np->qop) {
-		case QOP_AUTH_D:
-			qop_param = str_const_init(QOP_AUTH);
-			break;
-		case QOP_AUTHINT_D:
-			qop_param = str_const_init(QOP_AUTH_INT);
-			break;
-		case QOP_AUTHINT_AUTH_D:
-			qop_param = str_const_init(QOP_AUTH_BOTH_AAI);
-			break;
-		case QOP_AUTH_AUTHINT_D:
-			qop_param = str_const_init(QOP_AUTH_BOTH_AIA);
-			break;
-		default:
-			LM_ERR("Wrong _qop value: %d\n", calc_np->qop);
-			abort();
-		}
-	}
 	if (_stale)
 		stale_param = str_const_init(STALE_PARAM);
 
--- modules/auth/qop.h
+++ modules/auth/qop.h
@@ -60,4 +60,40 @@ static inline int fixup_qop(void** param)
 	return 0;
 }
 
+#define QOP_AUTH	  ", qop=\"" QOP_AUTH_STR "\""
+#define QOP_AUTH_INT	  ", qop=\"" QOP_AUTHINT_STR "\""
+#define QOP_AUTH_BOTH_AAI	  ", qop=\"" QOP_AUTH_STR "," QOP_AUTHINT_STR "\""
+#define QOP_AUTH_BOTH_AIA	  ", qop=\"" QOP_AUTHINT_STR "," QOP_AUTH_STR "\""
+
+static inline str_const get_qop_param(qop_type_t qop)
+{
+	static str_const qop_param;
+	switch (qop) {
+	case QOP_UNSPEC_D:
+		qop_param = STR_NULL_const;
+		break;
+	case QOP_AUTH_D:
+		qop_param = str_const_init(QOP_AUTH);
+		break;
+	case QOP_AUTHINT_D:
+		qop_param = str_const_init(QOP_AUTH_INT);
+		break;
+	case QOP_AUTHINT_AUTH_D:
+		qop_param = str_const_init(QOP_AUTH_BOTH_AAI);
+		break;
+	case QOP_AUTH_AUTHINT_D:
+		qop_param = str_const_init(QOP_AUTH_BOTH_AIA);
+		break;
+	default:
+		LM_ERR("Wrong _qop value: %d\n", qop);
+		abort();
+	}
+	return qop_param;
+}
+
+#undef QOP_AUTH
+#undef QOP_AUTH_INT
+#undef QOP_AUTH_BOTH_AAI
+#undef QOP_AUTH_BOTH_AIA
+
 #endif /* AUTH_QOP_H */
