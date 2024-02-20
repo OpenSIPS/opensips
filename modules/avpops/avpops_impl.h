fd19fbaf3eb9c83e28e5c4bfe03504ba5534fab5
Bogdan-Andrei Iancu <bogdan@opensips.org>
2024-02-20 13:00:33 +0200

    [avpops] code cleanup after removing old script functions

--- modules/avpops/avpops_impl.h
+++ modules/avpops/avpops_impl.h
@@ -51,49 +51,11 @@
 #define AVPOPS_VAL_STR       (1<<2)
 #define AVPOPS_VAL_PVAR      (1<<3)
 
-#define AVPOPS_USE_FROM      (1<<5)
-#define AVPOPS_USE_TO        (1<<6)
-#define AVPOPS_USE_RURI      (1<<7)
-#define AVPOPS_USE_USERNAME  (1<<8)
-#define AVPOPS_USE_DOMAIN    (1<<9)
-
-#define AVPOPS_USE_SRC_IP    (1<<12)
-#define AVPOPS_USE_DST_IP    (1<<13)
-#define AVPOPS_USE_DURI      (1<<14)
-#define AVPOPS_USE_BRANCH    (1<<15)
-
 /* flags for operation flags    24..31 */
 #define AVPOPS_FLAG_USER0    (1<<24)
 #define AVPOPS_FLAG_DOMAIN0  (1<<25)
 #define AVPOPS_FLAG_URI0     (1<<26)
 #define AVPOPS_FLAG_UUID0    (1<<27)
-
-/* operation flags  */
-#define AVPOPS_OP_EQ        (1<<0)
-#define AVPOPS_OP_NE        (1<<1)
-#define AVPOPS_OP_LT        (1<<2)
-#define AVPOPS_OP_LE        (1<<3)
-#define AVPOPS_OP_GT        (1<<4)
-#define AVPOPS_OP_GE        (1<<5)
-#define AVPOPS_OP_RE        (1<<6)
-#define AVPOPS_OP_FM        (1<<7)
-#define AVPOPS_OP_BAND      (1<<8)
-#define AVPOPS_OP_BOR       (1<<9)
-#define AVPOPS_OP_BXOR      (1<<10)
-#define AVPOPS_OP_BNOT      (1<<11)
-#define AVPOPS_OP_ADD       (1<<12)
-#define AVPOPS_OP_SUB       (1<<13)
-#define AVPOPS_OP_MUL       (1<<14)
-#define AVPOPS_OP_DIV       (1<<15)
-#define AVPOPS_OP_MOD       (1<<16)
-
-/* flags for operation flags    24..31 */
-#define AVPOPS_FLAG_ALL     (1<<24)
-#define AVPOPS_FLAG_CI      (1<<25)
-#define AVPOPS_FLAG_DELETE  (1<<26)
-#define AVPOPS_FLAG_CASTN   (1<<27)
-#define AVPOPS_FLAG_CASTS   (1<<28)
-#define AVPOPS_FLAG_EMPTY   (1<<29)
 
 /* container structer for Flag+Int_Spec_value parameter */
 struct fis_param
@@ -145,29 +107,5 @@ int ops_async_dbquery(struct sip_msg* msg, async_ctx *ctx,
 int resume_async_dbquery(int fd, struct sip_msg *msg, void *_param);
 int timeout_async_dbquery(int fd, struct sip_msg *msg, void *_param);
 
-int ops_delete_avp(struct sip_msg* msg,
-								struct fis_param *ap);
-
-int ops_copy_avp(struct sip_msg* msg, struct fis_param* name1,
-								struct fis_param* name2);
-
-int ops_shuffle_avp(struct sip_msg* msg,
-								struct fis_param *param);
-
-int ops_pushto_avp(struct sip_msg* msg, struct fis_param* dst,
-								struct fis_param* ap);
-
-int ops_check_avp(struct sip_msg* msg, struct fis_param* param,
-								struct fis_param* check);
-
-int ops_op_avp(struct sip_msg* msg, struct fis_param** param,
-								struct fis_param* op);
-
-int ops_subst(struct sip_msg* msg, struct fis_param** src,
-		struct subst_expr* subst);
-
-int ops_is_avp_set(struct sip_msg* msg, struct fis_param *ap);
-
-int ops_print_avp();
 #endif
 
