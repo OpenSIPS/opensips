OpenSIPS/opensips

commit 1ecb32491ff891c5002f8de47373cd1cc5b70376

rest_client: Improve previous commit (async timeouts)
* fix corner-case causing module timeout to be discarded
* change logging level on transfer timeout from INFO -> ERR

modules/rest_client/rest_methods.c
@@ -1055,7 +1055,7 @@ static enum async_ret_code _resume_async_http_req(int fd, struct sip_msg *msg,
 	if (timed_out) {
 		char *url = NULL;
 		curl_easy_getinfo(param->handle, CURLINFO_EFFECTIVE_URL, &url);
-		LM_INFO("async %s timed out, URL: %s\n",
+		LM_ERR("async %s timed out, URL: %s\n",
 		        rest_client_method_str(param->method), url);
 		goto cleanup;
 	}

modules/tm/async.c
@@ -349,10 +349,11 @@ int t_handle_async(struct sip_msg *msg, struct action* a,
 			timeout = 0;
 			LM_ERR("this async function has no support for timeouts -- "
 			       "still using an infinite timeout!\n");
-		} else if (ctx->async.timeout_f && ctx->async.timeout_s
-		        && ctx->async.timeout_s < timeout) {
-			timeout = ctx->async.timeout_s;
 		}
+
+		if (ctx->async.timeout_f && ctx->async.timeout_s
+		        && (!timeout || ctx->async.timeout_s < timeout))
+			timeout = ctx->async.timeout_s;
 
 		/* place the FD + resume function (as param) into reactor */
 		if (reactor_add_reader_with_timeout( fd, F_SCRIPT_ASYNC,

commit d3cd9371e82004e94cf63271f0354306be134dcc

rest_client: Partial revert of 1ecb32491f
This addresses a regression in 1ecb324, where the "SUCCESS" test case
does not work anymore - the async cURL request simply times out.

Document the behavior of async timeouts with rest_client on 3.3 branch.

Related to #3286

modules/rest_client/doc/rest_client_admin.xml
@@ -581,6 +581,15 @@ if (!rest_get("https://example.com"))
 
 	<section>
 	<title>Exported Asynchronous Functions</title>
+	<para>
+		<emphasis role='bold'>Note regarding async timeouts on 3.3</emphasis>:
+			There is a known issue with timeouts for rest_client async function
+			calls on 3.3 stable, where either of the <emphasis>async()</emphasis>
+			statement timeout or the <xref linkend="param_curl_timeout"/> values
+			may be taken into consideration while disregarding the other setting,
+			depending on the usage scenario.  To mitigate such issues, simply
+			set both timeouts to the <emphasis>same value</emphasis>.
+	</para>
 	<section id="afunc_rest_get" xreflabel="rest_get()">
 		<title>
 		<function moreinfo="none">rest_get(url, body_pv[, [ctype_pv][, [retcode_pv]]])

modules/rest_client/rest_methods.c
@@ -893,15 +893,9 @@ static enum async_ret_code _resume_async_http_req(int fd, struct sip_msg *msg,
 		LM_DBG("perform result: %d, running: %d (break: %d)\n", mrc, running,
 			mrc != CURLM_CALL_MULTI_PERFORM && (mrc != CURLM_OK || !running));
 
-		if (mrc == CURLM_OK && running) {
-			async_status = ASYNC_CONTINUE;
-			return 1;
-
-		/* this rc has been removed since cURL 7.20.0 (Feb 2010), but it's not
-		 * yet marked as deprecated, so let's keep the do/while loop */
-		} else if (mrc != CURLM_CALL_MULTI_PERFORM) {
-			break;
-		}
+		if (mrc != CURLM_CALL_MULTI_PERFORM &&
+		     (mrc != CURLM_OK || !running))
+		         break;
 
 		usleep(_async_resume_retr_itv);
 		retr += _async_resume_retr_itv;