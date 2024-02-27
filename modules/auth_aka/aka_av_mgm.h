9489895719aa0413b6ee8a552a71b320095a21ed
Razvan Crainea <razvan@opensips.org>
2024-02-27 15:28:40 +0200

    auth_aka: add new AKA auth module

---
+++ modules/auth_aka/aka_av_mgm.h
@@ -0,0 +1,46 @@
+/*
+ * AKA Authentication - generic Authentication Manager support
+ *
+ * Copyright (C) 2024 Razvan Crainea
+ *
+ * This file is part of opensips, a free SIP server.
+ *
+ * opensips is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * opensips is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ */
+
+#ifndef AKA_AV_MGM_H
+#define AKA_AV_MGM_H
+
+#include "../../str.h"
+#include "../../lib/list.h"
+
+#define AKA_AV_MGM_PREFIX "load_aka_av_"
+
+struct aka_av_async_ctx {
+};
+
+struct aka_av_binds {
+	int (*fetch)(str *realm, str *impu, str *impi, int alg, str *resync, int no, struct aka_av_async_ctx *ctx);
+};
+
+struct aka_av_mgm {
+	str name;
+	struct aka_av_binds binds;
+	struct list_head list;
+	char buf[0];
+};
+
+#endif /* AKA_AV_MGM_H */
