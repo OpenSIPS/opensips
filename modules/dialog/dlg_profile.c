9a2fc7ffa6a9c2f6d5f57c5c2e30278059ad3c0e
Bogdan-Andrei Iancu <bogdan@opensips.org>
2024-04-30 15:34:55 +0300

    [dialog] relaxed the name of the dialog profiles
    '-' and '_' are allowed (besides alphanum) in dialog profiles

--- modules/dialog/dlg_profile.c
+++ modules/dialog/dlg_profile.c
@@ -200,7 +200,7 @@ int add_profile_definitions( char* profiles, unsigned int has_value)
 
 		/* check the name format */
 		for(i=0;i<name.len;i++) {
-			if ( !isalnum(name.s[i]) ) {
+			if ( !isalnum(name.s[i]) && name.s[i]!='_'  && name.s[i]!='-' ) {
 				LM_ERR("bad profile name <%.*s>, char %c - use only "
 						"alphanumerical characters\n", name.len,name.s,name.s[i]);
 				return -1;
