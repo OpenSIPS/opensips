a09e75e8610925f4737b57dbcb1a38ef79068a2e
Liviu Chircu <liviu@opensips.org>
2024-04-30 18:24:13 +0300

    github/workflows: Fix bash multi-line error

--- scripts/build/apt_requirements_postupdate.sh
+++ scripts/build/apt_requirements_postupdate.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 # handle package naming transitions across Ubuntu 20.04 and specific Ubuntu 22.04 arch packaging (e.g. i386)
-libodbc_pkg=$(apt-cache --names-only search odbc | awk '{print $1}' | grep -oE "^(libodbc2|libodbcinst2|libodbc1|odbcinst1debian2)$")
-[ -z "$libodbc_pkg" ] && libodbc_pkg=$(echo -e "libodbc2\nlibodbcinst2")
+libodbc_pkg=$(apt-cache --names-only search odbc | awk '{print $1}' | grep -oE "^(libodbc2|libodbcinst2|libodbc1|odbcinst1debian2)$" | xargs)
+[ -z "$libodbc_pkg" ] && libodbc_pkg="libodbc2 libodbcinst2"
 
-cat <<EOF
-$libodbc_pkg
-EOF
+# Output: a space-separated list of packages
+echo -n "$libodbc_pkg"
