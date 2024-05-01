fa4aa21434da3eb2e46f135284f101857c934b6a
Liviu Chircu <liviu@opensips.org>
2024-05-01 13:41:32 +0300

    python build: Ensure "setuptools" is available
    (cherry picked from commit 4250ccd78f7c900a7eea844e9b41a0f487d08fa9)

--- scripts/build/apt_requirements_postupdate.sh
+++ scripts/build/apt_requirements_postupdate.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+# handle package naming transitions across Ubuntu 20.04
+# and GitHub-specific Ubuntu 22.04 multi-arch (e.g. i386, using 20.04 packages)
 
-# handle package naming transitions across Ubuntu 20.04 and specific Ubuntu 22.04 arch packaging (e.g. i386)
-libodbc_pkg=$(apt-cache --names-only search odbc | awk '{print $1}' | grep -oE "^(libodbc2|libodbcinst2|libodbc1|odbcinst1debian2)$" | xargs)
-[ -z "$libodbc_pkg" ] && libodbc_pkg="libodbc2 libodbcinst2"
+find_debs() {
+  echo -n $(apt-cache --names-only search "$1" | awk '{print $1}' | grep -oE "^($2)$" | xargs)
+}
+
+libodbc_debs=$(find_debs odbc "libodbc2|libodbcinst2|libodbc1|odbcinst1debian2")
+[ -z "$libodbc_debs" ] && libodbc_debs="libodbc2 libodbcinst2"
+
+python_debs=$(find_debs python "python3-setuptools|python-setuptools")
+[ -z "$python_debs" ] && python_debs="python3-setuptools"
 
 # Output: a space-separated list of packages
-echo -n "$libodbc_pkg"
+echo -n "$libodbc_debs $python_debs"
