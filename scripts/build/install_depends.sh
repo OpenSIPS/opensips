764893f43559f5bf969b0d4d341251160a81618b
Maksym Sobolyev <sobomax@sippysoft.com>
2024-06-16 15:53:43 -0700

    Move main builds inside a container, add Ubuntu 24.04.
    This allows us to provide a clean build environment as well as
    to extend os versions to be tested.

--- scripts/build/install_depends.sh
+++ scripts/build/install_depends.sh
@@ -11,7 +11,7 @@ PKGS=$(cat "$(dirname $0)/apt_requirements.txt")
 _PKGS=""
 for pkg in ${PKGS}
 do
-  if [ "${BUILD_OS}" = ubuntu-22.04 -a "${pkg}" = python-dev ]
+  if [ "${BUILD_OS}" != ubuntu-20.04 -a "${BUILD_OS}" != ubuntu-18.04 -a "${pkg}" = python-dev ]
   then
     pkg="python-dev-is-python3"
   fi
@@ -29,7 +29,7 @@ ${SUDO} apt-get -y remove libmemcached11 libpq5
 ${SUDO} apt-get -y autoremove
 
 PKGS="$PKGS $(. "$(dirname $0)/apt_requirements_postupdate.sh")"
-${SUDO} apt-get -y --allow-downgrades install ${PKGS}
+${SUDO} env DEBIAN_FRONTEND=noninteractive apt-get -y --allow-downgrades install ${PKGS}
 
 if [ ! -z "${POST_INSTALL_CMD}" ]
 then
