2d8ea8ef28b4efbcde658ece5a65ec4e440e35ba
Bogdan-Andrei Iancu <bogdan@opensips.org>
2024-06-18 13:52:24 +0300

    [tcp] check if the history list exists when doing unref
    There are cases where the tcp conn may fail right at the beginning (like a failed connect/accept) and maybe the hist list is not yet attached to the conn.

--- lib/dbg/struct_hist.c
+++ lib/dbg/struct_hist.c
@@ -189,6 +189,9 @@ static void sh_free(struct struct_hist *sh, osips_free_f free_f)
 
 void _sh_unref(struct struct_hist *sh, osips_free_f free_f)
 {
+	if (!sh)
+		return;
+
 	gen_lock_t *shl_lock = &sh->shlist->wlock;
 
 	lock_get(shl_lock);
