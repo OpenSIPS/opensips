e496cfc28aa6e88259dd17054912f88859087534
Liviu Chircu <liviu@opensips.org>
2024-07-22 15:28:09 +0300

    TCP debugging: Fine-tune TCP conn history settings
    * reduce logline buffer from 100 -> 80
    * only keep the last 300 events per connection (down from 2000)
    * avoid auto-logging (flushing) of all TCP conn histories to syslog
    
    These changes should reduce the default TCP conn debugging footprint
    from ~2GB to ~0.3GB of shared memory, as well as clean up the logs.

--- lib/dbg/struct_hist.h
+++ lib/dbg/struct_hist.h
@@ -45,7 +45,7 @@
  *    remain available inside the global history list for a while
  */
 
-#define MAX_SHLOG_SIZE 100 /* longer log lines will get truncated */
+#define MAX_SHLOG_SIZE 80 /* longer log lines will get truncated */
 
 /**
  * To be freely extended by any piece of OpenSIPS code which makes use of
@@ -85,7 +85,7 @@ struct struct_hist_action {
 struct struct_hist;
 struct struct_hist_list;
 
-#define FLUSH_LIMIT 2000
+#define FLUSH_LIMIT 300
 #define flushable(sh) (sh->len == FLUSH_LIMIT)
 
 /**
