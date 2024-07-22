2ddd14a35d4ffac6d09da4b25a5875fd0e9a90dc
Liviu Chircu <liviu@opensips.org>
2024-07-22 15:28:09 +0300

    TCP debugging: Fine-tune TCP conn history settings
    * reduce logline buffer from 100 -> 80
    * only keep the last 300 events per connection (down from 2000)
    * avoid auto-logging (flushing) of all TCP conn histories to syslog
    
    These changes should reduce the default TCP conn debugging footprint
    from ~2GB to ~0.3GB of shared memory, as well as clean up the logs.
    
    (cherry picked from commit e496cfc28aa6e88259dd17054912f88859087534)

--- net/net_tcp.c
+++ net/net_tcp.c
@@ -1763,7 +1763,7 @@ int tcp_init(void)
 		return 0;
 
 #ifdef DBG_TCPCON
-	con_hist = shl_init("TCP con", 10000, 1);
+	con_hist = shl_init("TCP con", 10000, 0);
 	if (!con_hist) {
 		LM_ERR("oom con hist\n");
 		goto error;
