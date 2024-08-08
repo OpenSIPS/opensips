09e8303669fef73987a0f29bfb4608713cef5de4
Razvan Crainea <razvan@opensips.org>
2024-08-08 14:08:47 +0300

    build: exclude cachedb_dynamodb

--- scripts/build/do_build.sh
+++ scripts/build/do_build.sh
@@ -7,7 +7,8 @@ set -e
 . $(dirname $0)/build.conf.sub
 
 EXCLUDE_MODULES="db_oracle osp sngtc cachedb_cassandra cachedb_couchbase \
-  cachedb_mongodb auth_jwt event_kafka aaa_diameter launch_darkly http2d snmpstats"
+  cachedb_mongodb auth_jwt event_kafka aaa_diameter launch_darkly http2d \
+  snmpstats cachedb_dynamodb"
 if [ ! -z "${EXCLUDE_MODULES_ADD}" ]
 then
   EXCLUDE_MODULES="${EXCLUDE_MODULES} ${EXCLUDE_MODULES_ADD}"
