OpenSIPS/opensips

commit fa4bf3c67fe7d8f3fcf88bcca12fa5fa09a4ebe0

usrloc: Make the timer cleanup routine cluster-aware
Reported by Xiao Huang

Fixes #1367

modules/usrloc/udomain.c
@@ -1474,6 +1474,13 @@ int mem_timer_udomain(udomain_t* _d)
 				if (exists_ulcb_type(UL_AOR_EXPIRE))
 					run_ul_callbacks(UL_AOR_EXPIRE, ptr);
 
+				if (location_cluster) {
+					if (cluster_mode == CM_FEDERATION_CACHEDB &&
+					    cdb_update_urecord_metadata(&ptr->aor, 1) != 0)
+						LM_ERR("failed to delete metadata, aor: %.*s\n",
+						       ptr->aor.len, ptr->aor.s);
+				}
+
 				iterator_delete(&prev);
 				mem_delete_urecord(_d, ptr);
 			}

commit 3b65c35a7b8a330e9f01436935d9e20a43fff7b5

usrloc federation HA: Fix race condition in managing CacheDB data
This patch fixes a race condition introduced in the fa4bf3c67 fix
for #1367, where the Backup node may now perform *unintended* DELETE
operations on random UA De-REG/Re-REG sequences, simply because its
internal mem_timer_udomain() happened to run, while the Active node's
timer did not.  The UA is re-registered, but data from CacheDB is now
missing, rendering the UA unreachable from other PoPs.

The fix is to give usrloc insight into the state of the HA pair, and
change the mem_timer_udomain() timer/cleanup procedure to only run
DELETE operations against the CacheDB from the Active node.

modules/usrloc/doc/usrloc_admin.xml
@@ -1188,6 +1188,48 @@ modparam("usrloc", "location_cluster", 1)
 		</example>
 	</section>
 
+	<section id="param_ha_cluster" xreflabel="ha_cluster">
+		<title><varname>ha_cluster</varname> (integer)</title>
+		<para>
+		Only relevant in <emphasis role='bold'>"federation-cachedb"</emphasis>
+		<xref linkend="param_cluster_mode"/>.  Denotes the HA cluster ID to use in
+		order to establish the active node within the HA pair, such that only
+		that node performs WRITE operations to CacheDB.
+		</para>
+		<para>
+		Default value is 0 (disabled).
+		</para>
+		<example>
+		<title>Setting the <varname>ha_cluster</varname> parameter</title>
+		<programlisting format="linespecific">
+...
+modparam("usrloc", "ha_cluster", 4)
+...
+</programlisting>
+		</example>
+	</section>
+
+	<section id="param_ha_shtag" xreflabel="ha_shtag">
+		<title><varname>ha_shtag</varname> (string)</title>
+		<para>
+		Only relevant in <emphasis role='bold'>"federation-cachedb"</emphasis>
+		<xref linkend="param_cluster_mode"/>.  Denotes the HA cluster sharing tag to
+		use in order to establish the active node within the HA pair, such that
+		only that node performs WRITE operations to CacheDB.
+		</para>
+		<para>
+		Default value is NULL (disabled).
+		</para>
+		<example>
+		<title>Setting the <varname>ha_shtag</varname> parameter</title>
+		<programlisting format="linespecific">
+...
+modparam("usrloc", "ha_shtag", "vip2")
+...
+</programlisting>
+		</example>
+	</section>
+
 	<section id="param_skip_replicated_db_ops" xreflabel="skip_replicated_db_ops">
 		<title><varname>skip_replicated_db_ops</varname> (int)</title>
 		<para>

modules/usrloc/udomain.c
@@ -1178,7 +1178,7 @@ int mem_timer_udomain(udomain_t* _d)
 				if (exists_ulcb_type(UL_AOR_EXPIRE))
 					run_ul_callbacks(UL_AOR_EXPIRE, ptr, NULL);
 
-				if (location_cluster) {
+				if (location_cluster && ul_is_active_node()) {
 					if (cluster_mode == CM_FEDERATION_CACHEDB &&
 					    cdb_update_urecord_metadata(&ptr->aor, 1) != 0)
 						LM_ERR("failed to delete metadata, aor: %.*s\n",

modules/usrloc/ul_cluster.h
@@ -47,12 +47,21 @@ extern int location_cluster;
 extern struct clusterer_binds clusterer_api;
 extern str ul_shtag_key;
 
+extern int ul_ha_cluster;
+extern str ul_ha_shtag;
+
 extern str contact_repl_cap;
 
 int ul_init_cluster(void);
+
 #define _is_my_ucontact(__ct) \
 	(!__ct->shtag.s || \
 	 clusterer_api.shtag_get(&__ct->shtag, location_cluster) \
+		== SHTAG_STATE_ACTIVE)
+
+#define ul_is_active_node() \
+	(!ul_ha_cluster || !ul_ha_shtag.s || \
+	 clusterer_api.shtag_get(&ul_ha_shtag, ul_ha_cluster) \
 		== SHTAG_STATE_ACTIVE)
 
 /* duplicate local events to other OpenSIPS instances */

modules/usrloc/ul_mod.c
@@ -177,6 +177,9 @@ static char *nat_bflag_str = 0;
  */
 int location_cluster;
 
+int ul_ha_cluster;
+str ul_ha_shtag;
+
 db_con_t* ul_dbh = 0; /* Database connection handle */
 db_func_t ul_dbf;
 
@@ -263,6 +266,8 @@ static const param_export_t params[] = {
 
 	/* data replication through clusterer using TCP binary packets */
 	{ "location_cluster",	INT_PARAM, &location_cluster   },
+	{ "ha_cluster",	        INT_PARAM, &ul_ha_cluster },
+	{ "ha_shtag",	        STR_PARAM, &ul_ha_shtag.s },
 	{ "skip_replicated_db_ops", INT_PARAM, &skip_replicated_db_ops   },
 	{ "max_contact_delete", INT_PARAM, &max_contact_delete },
 	{ "regen_broken_contactid", INT_PARAM, &cid_regen},
@@ -729,6 +734,14 @@ int ul_check_config(void)
 			return -1;
 		}
 
+		if (!ul_ha_cluster)
+			LM_ERR("'ha_cluster' is not set! "
+			        "Backup node will also write to CacheDB!\n");
+
+		if (!ul_ha_shtag.s)
+			LM_ERR("'ha_shtag' is not set! "
+			        "Backup node will also write to CacheDB!\n");
+
 		if (!cdb_url.s) {
 			LM_ERR("no cache database URL defined! ('cachedb_url')\n");
 			return -1;
@@ -848,6 +861,8 @@ int ul_init_globals(void)
 	kv_store_col.len = strlen(kv_store_col.s);
 	attr_col.len = strlen(attr_col.s);
 	last_mod_col.len = strlen(last_mod_col.s);
+	if (ul_ha_shtag.s)
+		ul_ha_shtag.len = strlen(ul_ha_shtag.s);
 
 	if (ul_hash_size > 16) {
 		LM_WARN("hash too big! max 2 ^ 16\n");