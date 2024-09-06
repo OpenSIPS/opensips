4189831163b631f4d4275ad14cff9da629aea37b
Razvan Crainea <razvan@opensips.org>
2024-09-06 14:22:46 +0300

    event_sqs: exclude from building

--- scripts/build/do_build.sh
+++ scripts/build/do_build.sh
@@ -8,7 +8,7 @@ set -e
 
 EXCLUDE_MODULES="db_oracle osp sngtc cachedb_cassandra cachedb_couchbase \
   cachedb_mongodb auth_jwt event_kafka aaa_diameter launch_darkly http2d \
-  snmpstats cachedb_dynamodb"
+  snmpstats cachedb_dynamodb event_sqs"
 if [ ! -z "${EXCLUDE_MODULES_ADD}" ]
 then
   EXCLUDE_MODULES="${EXCLUDE_MODULES} ${EXCLUDE_MODULES_ADD}"
