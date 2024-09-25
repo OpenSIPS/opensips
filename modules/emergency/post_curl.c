884e5374c38b5f58e0085050ce60ca3c7b7db737
Alexandra Titoc <oanatitoc@gmail.com>
2024-09-25 15:49:34 +0300

    emergency: Fix resource leak
    CID #417438

--- modules/emergency/post_curl.c
+++ modules/emergency/post_curl.c
@@ -83,6 +83,7 @@ int post(char*  url, char* xml, char** response){
 	data.data = malloc(1024); /* reasonable size initial buffer */
 	if(NULL == data.data) {
 		LM_ERR("NO MEMORY\n");
+		curl_easy_cleanup(curl);
 		return -1;
 	}
 	memset(data.data, '\0', 1024);
