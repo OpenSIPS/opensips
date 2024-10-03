42fa1db32a259818495282a4eeaa1ae08087d9d3
Liviu Chircu <liviu@opensips.org>
2024-10-03 11:44:06 +0300

    uac_auth(): Fix corner-case with missing "algorithm=" in 401
    Make sure to convert ALG_UNSPEC to ALG_MD5 before doing the bitmask
    conversion, as otherwise the matching will fail!

--- modules/uac/auth.c
+++ modules/uac/auth.c
@@ -252,7 +252,7 @@ static int uac_auth_dlg_leg(struct dlg_cell *dlg, str *tag)
 		return callee_idx(dlg);
 }
 
-int uac_auth( struct sip_msg *msg, int algmask)
+int uac_auth( struct sip_msg *msg, unsigned algmask)
 {
 	struct authenticate_body *auth = NULL;
 	str msg_body;
@@ -307,7 +307,7 @@ int uac_auth( struct sip_msg *msg, int algmask)
 	}
 
 	if (auth == NULL) {
-		LM_ERR("Unable to extract authentication info\n");
+		LM_ERR("Unable to extract a compatible authentication challenge\n");
 		goto error;
 	}
 
