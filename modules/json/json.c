c1b23178a3a0535ee66bafcd36a0c3df8cdab3c3
Liviu Chircu <liviu@opensips.org>
2024-12-03 11:18:05 +0000

    json: Fix handling for long integers
    This patch adds the "enable_long_quoting" modparam (default: OFF), which
    causes larger integers to be returned as strings, in order to avoid the
    integer overflow and data loss.
    
    (cherry picked from commit b191025b75afb07b96562f730a8e9631c8ea35fa)
    
    Author: Vlad Paiu <vladpaiu@opensips.org>
    Add support for bigints ( store as string when exposing to opensips script )
    
    (cherry picked from commit f324a1a432b02137cab73978fe5df5e11005d938)
    
    Author: Vlad Paiu <vladpaiu@opensips.org>
    Fixed undefined int_value
    
    (cherry picked from commit ae113c7271ef0a5baa4674972d3b713ac6def6c7)

--- modules/json/json.c
+++ modules/json/json.c
@@ -99,6 +99,7 @@ typedef struct _json_name
 
 pv_json_t * all;
 char buff[JSON_FILE_BUF_SIZE];
+int json_long_quoting;
 
 static int mod_init(void);
 static int child_init(int );
@@ -128,6 +129,12 @@ static const cmd_export_t cmds[]={
 	{0,0,{{0,0,0}},0}
 };
 
+static const param_export_t mod_params[]={
+	{ "enable_long_quoting",         INT_PARAM, &json_long_quoting       },
+	{ 0,0,0 }
+};
+
+
 static const pv_export_t mod_items[] = {
 	{ str_const_init("json"),    PVT_JSON, pv_get_json,
 		pv_set_json, pv_parse_json_name, pv_parse_json_index, 0, 0},
@@ -143,15 +150,15 @@ struct module_exports exports= {
 	MOD_TYPE_DEFAULT,/* class of this module */
 	MODULE_VERSION,
 	DEFAULT_DLFLAGS, /* dlopen flags */
-	0,				 /* load function */
+	0,               /* load function */
 	NULL,            /* OpenSIPS module dependencies */
 	cmds,            /* exported functions */
 	0,               /* exported async functions */
-	0,      /* param exports */
-	0,       /* exported statistics */
-	0,         /* exported MI functions */
+	mod_params,      /* param exports */
+	0,               /* exported statistics */
+	0,               /* exported MI functions */
 	mod_items,       /* exported pseudo-variables */
-	0,			 	 /* exported transformations */
+	0,               /* exported transformations */
 	0,               /* extra processes */
 	0,               /* module pre-initialization function */
 	mod_init,        /* module initialization function */
@@ -415,6 +422,7 @@ int pv_get_json_ext(struct sip_msg* msg,  pv_param_t* pvp, pv_value_t* val, int 
 	json_t * obj;
 	json_name * id = (json_name *) pvp->pvn.u.dname;
 	UNUSED(id);
+	int64_t int_value;
 
 	if( expand_tag_list( msg, ((json_name *)pvp->pvn.u.dname)->tags ) < 0)
 	{
@@ -457,10 +465,17 @@ int pv_get_json_ext(struct sip_msg* msg,  pv_param_t* pvp, pv_value_t* val, int 
 
 	if( json_object_is_type(obj, json_type_int) )
 	{
-		val->rs.s = sint2str(json_object_get_int(obj), &val->rs.len);
-		val->ri = json_object_get_int(obj);;
-		val->flags |= PV_VAL_INT|PV_TYPE_INT|PV_VAL_STR;
+		int_value = json_object_get_int64(obj);
+		val->rs.s = sint2str(int_value, &val->rs.len);
 
+		if (!json_long_quoting || (int_value>=INT_MIN && int_value<=INT_MAX)) {
+			/* safe to store it as an INT in the pvar */
+			val->ri = int_value;
+			val->flags |= PV_VAL_INT|PV_TYPE_INT|PV_VAL_STR;
+		} else {
+			/* we would overflow/underflow, store as string only */
+			val->flags |= PV_VAL_STR;
+		}
 	}
 	else if( json_object_is_type(obj, json_type_string))
 	{
