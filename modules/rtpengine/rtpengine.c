cdd47254443b43a8139dffc2c739b05cc77deb69
Razvan Crainea <razvan@opensips.org>
2025-06-17 16:12:09 +0300

    rtp*: guard from empty to_tag

--- modules/rtpengine/rtpengine.c
+++ modules/rtpengine/rtpengine.c
@@ -5109,7 +5109,7 @@ static bencode_item_t *rtpengine_api_copy_op(struct rtp_relay_session *sess,
 		bencode_list_add_string(list, "all");
 	} else if (copy_flags & RTP_COPY_LEG_CALLER && sess->from_tag) {
 		bencode_dictionary_add_str(dict, "from-tag", sess->from_tag);
-	} else if (sess->to_tag) {
+	} else if (sess->to_tag && sess->to_tag->len) {
 		bencode_dictionary_add_str(dict, "from-tag", sess->to_tag);
 	}
 	if (copy_flags & RTP_COPY_MODE_DISABLE) {
