295da158f70282f148afad0f3da1a8a33d817051
Razvan Crainea <razvan@opensips.org>
2025-09-12 16:09:08 +0300

    siprec: do not try to engage siprec if media not established
    (cherry picked from commit c5fd8adfcc3172f74b2aa634f3f434f533dfa787)

--- modules/siprec/siprec_sess.h
+++ modules/siprec/siprec_sess.h
@@ -76,6 +76,7 @@ struct src_part {
 #define SIPREC_DLG_CBS	(1<<1)
 #define SIPREC_PAUSED	(1<<2)
 #define SIPREC_ONGOING	(1<<3)
+#define SIPREC_ANSWERED	(1<<5)
 
 #define SIPREC_SRS(_s) (list_entry((_s)->srs.next, struct srs_node, list)->uri)
 
