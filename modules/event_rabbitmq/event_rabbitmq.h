e46b523a01b6ecaffbdab8f4bcaab005da96732b
Ovidiu Sas <osas@voipembedded.com>
2025-11-06 05:02:33 +0000

    event_rabbitmq: rework rmq_server_reconnect() signature

--- modules/event_rabbitmq/event_rabbitmq.h
+++ modules/event_rabbitmq/event_rabbitmq.h
@@ -79,7 +79,7 @@ extern struct timeval rpc_timeout_tv;
 
 int rmq_error(char const *context, amqp_rpc_reply_t x);
 void rmq_destroy_connection(rmq_connection_t *conn, int temporarely);
-int rmq_server_reconnect(rmq_connection_t *conn, int max_frames, str cid);
+int rmq_server_reconnect(rmq_connection_t *conn, char *address, int port, int max_frames, str cid);
 int amqp_check_status(rmq_connection_t *conn, int r, int* retry, str cid);
 int rmq_basic_server_publish(rmq_connection_t *conn, int max_frames,
 							 str *cid, amqp_bytes_t akey, amqp_bytes_t abody,
