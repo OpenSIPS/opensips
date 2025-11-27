7417e64c83c6310e91df76acd8326cee18ae222c
Jupiter Tang <36952362@qq.com>
2025-11-27 10:18:13 +0800

    fix memory leak issue

--- modules/tls_mgm/tls_domain.c
+++ modules/tls_mgm/tls_domain.c
@@ -133,12 +133,18 @@ void tls_free_domain(struct tls_domain *dom)
 		while (m_it) {
 			m_tmp = m_it;
 			m_it = m_it->next;
+			if (m_tmp->s.s) {
+				shm_free(m_tmp->s.s);
+			}
 			shm_free(m_tmp);
 		}
 		m_it = dom->match_addresses;
 		while (m_it) {
 			m_tmp = m_it;
 			m_it = m_it->next;
+			if (m_tmp->s.s) {
+				shm_free(m_tmp->s.s);
+			}
 			shm_free(m_tmp);
 		}
 
