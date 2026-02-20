OpenSIPS/opensips

commit 1ea069057013578dfc036ce19083f270a4c851aa

packaging: force libpcre3 for stable version
except for Debian 13, which is new package and cannot run with libpcre3

packaging/redhat_fedora/opensips.spec
@@ -79,7 +79,7 @@ BuildRequires:  openldap-devel
 BuildRequires:  curl-devel
 # BuildRequires:  GeoIP-devel
 BuildRequires:  libmaxminddb-devel
-BuildRequires:  pcre2-devel
+BuildRequires:  pcre3-devel
 %if 0%{?_with_python3:1}
 BuildRequires:  python3-devel
 %else
@@ -973,14 +973,16 @@ This package provides the SIP to XMPP IM translator module for OpenSIPS.
 %setup -q -n %{name}-%{version}
 
 %build
-LOCALBASE=/usr NICER=0 CFLAGS="%{optflags}" LDFLAGS="%{?__global_ldflags}" %{?_with_python3:PYTHON=python3} %{?_with_db_oracle:ORAHOME="$ORACLE_HOME"} %{__make} all modules-readme %{?_smp_mflags} TLS=1 \
+LOCALBASE=/usr NICER=0 CFLAGS="%{optflags}" LDFLAGS="%{?__global_ldflags}"
+%{?_with_python3:PYTHON=python3} %{?_with_db_oracle:ORAHOME="$ORACLE_HOME"}
+%{__make} all modules-readme %{?_smp_mflags} PCRE_LIB=pcre \
   exclude_modules="%EXCLUDE_MODULES" \
   cfg_target=%{_sysconfdir}/opensips/ \
   modules_prefix=%{buildroot}%{_prefix} \
   modules_dir=%{_lib}/%{name}/modules
 
 %install
-%{__make} install TLS=1 LIBDIR=%{_lib} \
+%{__make} install PCRE_LIB=pcre LIBDIR=%{_lib} \
   exclude_modules="%EXCLUDE_MODULES" \
   basedir=%{buildroot} prefix=%{_prefix} \
   cfg_prefix=%{buildroot} \

.lgtm.yml
@@ -33,6 +33,7 @@ extraction:
     before_index:
       - export FASTER=1
       - export NICER=0
+      - export PCRE_LIB=pcre
     index:
       build_command: make exclude_modules="db_oracle osp sngtc cachedb_cassandra cachedb_couchbase cachedb_mongodb auth_jwt" all
 

packaging/debian/control
@@ -29,7 +29,9 @@ Build-Depends: bison,
                libbson-dev | base-files,
                libmongoc-dev | base-files,
                libncurses5-dev,
-               libpcre2-dev,
+               opentelemetry-cpp-dev <opentelemetry>,
+               libpcre2-dev <!nopcre2>,
+               libpcre3-dev <nopcre2>,
                libperl-dev,
                libpq-dev,
                librabbitmq-dev,

packaging/debian/rules
@@ -336,6 +336,14 @@ endif
 # Enable verbose compile logs
 VARS += NICER=0
 
+#
+# check pcre2 vs pcre3 profile
+#
+ifneq (,$(filter nopcre2,$(DEB_BUILD_PROFILES)))
+	VARS += PCRE_LIB=pcre
+endif
+
+
 # support parallel compiling
 NJOBS =
 FASTER =

commit 36c6eb883e2641f12afbd44d970abe3bfe146bec

packaging/redhat: fix bug introduced in 1ea06905

packaging/redhat_fedora/opensips.spec
@@ -973,9 +973,9 @@ This package provides the SIP to XMPP IM translator module for OpenSIPS.
 %setup -q -n %{name}-%{version}
 
 %build
-LOCALBASE=/usr NICER=0 CFLAGS="%{optflags}" LDFLAGS="%{?__global_ldflags}"
-%{?_with_python3:PYTHON=python3} %{?_with_db_oracle:ORAHOME="$ORACLE_HOME"}
-%{__make} all modules-readme %{?_smp_mflags} PCRE_LIB=pcre \
+LOCALBASE=/usr NICER=0 CFLAGS="%{optflags}" LDFLAGS="%{?__global_ldflags}" \
+  %{?_with_python3:PYTHON=python3} %{?_with_db_oracle:ORAHOME="$ORACLE_HOME"} \
+  %{__make} all modules-readme %{?_smp_mflags} PCRE_LIB=pcre \
   exclude_modules="%EXCLUDE_MODULES" \
   cfg_target=%{_sysconfdir}/opensips/ \
   modules_prefix=%{buildroot}%{_prefix} \