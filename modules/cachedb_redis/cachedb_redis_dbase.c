5d80387959f8ae4e527bf59bbfa8fb1edca1def2
Bogdan Andrei IANCU <bogdan@opensips.org>
2026-03-10 11:50:32 +0200

    Merge pull request #3842 from NormB/fix/cachedb-redis-null-deref
    cachedb_redis: fix NULL deref when redisConnect returns NULL
    
    (cherry picked from commit f0f53a61504538909070335cd0b37b987f773d0a)

--- modules/cachedb_redis/cachedb_redis_dbase.c
+++ modules/cachedb_redis/cachedb_redis_dbase.c
@@ -48,7 +48,7 @@ struct tls_mgm_binds tls_api;
 redisContext *redis_get_ctx(char *ip, int port)
 {
 	struct timeval tv;
-	static char warned = 0;
+	static int warned = 0;
 	redisContext *ctx;
 
 	if (!port)
@@ -66,6 +66,12 @@ redisContext *redis_get_ctx(char *ip, int port)
 	if (ctx && ctx->err != REDIS_OK) {
 		LM_ERR("failed to open redis connection %s:%hu - %s\n",ip,
 				(unsigned short)port,ctx->errstr);
+		return NULL;
+	}
+
+	if (!ctx) {
+		LM_ERR("failed to connect to redis %s:%hu - out of memory\n",
+				ip, (unsigned short)port);
 		return NULL;
 	}
 
