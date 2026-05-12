feccfa614f1059073eea4363bf6d7170906fe922
Razvan Crainea <razvan@opensips.org>
2026-05-12 17:27:17 +0300

    add inttypes format for i686 architectures

--- modules/emergency/post_curl.c
+++ modules/emergency/post_curl.c
@@ -52,7 +52,7 @@ size_t write_data(char *ptr, size_t size, size_t nmemb, void *stream) {
 	data->size += (size * nmemb);
 
 #ifdef DEBUG
-	fprintf(stderr, "data at %p size=%ld nmemb=%ld\n", ptr, size, nmemb);
+	fprintf(stderr, "data at %p size=%zu nmemb=%zu\n", ptr, size, nmemb);
 #endif
 	tmp = realloc(data->data, data->size + 1); /* +1 for '\0' */
 
