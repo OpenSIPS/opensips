3c405d3e25670f607de5737b1e5c66295b183023
David Trihy <david.trihy@genesys.com>
2026-06-12 10:56:59 +0100

    license updates

--- modules/topology_hiding/th_common_logic.h
+++ modules/topology_hiding/th_common_logic.h
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (C) 2026 Genesys Cloud Services, Inc.
+ * Copyright (C) 2015 OpenSIPS Foundation
  *
  * opensips is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
