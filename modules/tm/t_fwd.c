24dad2a27c60bdedbae793979d0786be53bda3e2
Liviu Chircu <liviu@opensips.org>
2026-06-25 18:33:16 +0300

    tm: Fix context for PN timeout 408
    Commit 5dbbd997f7 added a PN wait timeout path which sends a local 408
    when the EBR subscription expires, but called relay_reply() without the
    normal TM reply context.
    
    Install a temporary global context, set the current transaction and branch
    index while generating the fake 408, then restore them afterwards. This
    prevents dialog reply callbacks from crashing on PN timeout.
    
    (cherry picked from commit 8832a4461dab35ac56e2c4b81c0fcb6edcf93690)
    (cherry picked from commit 6a9d4d306805acbc6f7c6f9102bd89bb53c4a4df)

--- modules/tm/t_fwd.c
+++ modules/tm/t_fwd.c
@@ -51,6 +51,7 @@
 #include "../../mem/mem.h"
 #include "../../parser/parser_f.h"
 #include "../../parser/parse_body.h"
+#include "../../context.h"
 #include "t_funcs.h"
 #include "t_hooks.h"
 #include "t_msgbuilder.h"
@@ -1110,14 +1111,39 @@ int t_wait_no_more_branches( struct cell *t, int extra)
 
 int t_wait_no_more_branches_timeout(struct cell *t, int code)
 {
+	static context_p my_ctx = NULL;
+	context_p old_ctx;
+	struct cell *old_t;
 	branch_bm_t cancel_bitmap = BRANCH_BM_ZERO;
 	int b;
 
 	for (b = t->nr_of_outgoings - 1; b >= t->first_branch; b--) {
 		if (TM_BRANCH(t, b).flags & T_UAC_IS_PHONY) {
 			if (TM_BRANCH(t, b).last_received < 200) {
+				old_ctx = current_processing_ctx;
+				old_t = get_t();
+				if (my_ctx == NULL) {
+					my_ctx = context_alloc(CONTEXT_GLOBAL);
+					if (my_ctx == NULL) {
+						LM_ERR("failed to alloc new ctx in pkg\n");
+						return -1;
+					}
+				}
+				memset(my_ctx, 0, context_size(CONTEXT_GLOBAL));
+				set_global_context(my_ctx);
+				set_t(t);
+
+				_tm_branch_index = b;
 				LOCK_REPLIES(t);
 				relay_reply(t, FAKED_REPLY, b, code, cancel_bitmap);
+				_tm_branch_index = 0;
+
+				if (current_processing_ctx == NULL)
+					my_ctx = NULL;
+				else
+					context_destroy(CONTEXT_GLOBAL, my_ctx);
+				set_global_context(old_ctx);
+				set_t(old_t);
 			}
 			TM_BRANCH(t, b).br_flags = t->nr_of_outgoings;
 			return 0;
